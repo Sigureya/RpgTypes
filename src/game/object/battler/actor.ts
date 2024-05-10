ba4e2795871e5ec43c70edd84e664175448972e9
Sigureya <sigureya1208@gmail.com>
2024-05-10 09:10:48 +0900

    不要なimportを削除

--- src/game/object/battler/actor.ts
+++ src/game/object/battler/actor.ts
@@ -1,7 +1,6 @@
 import { Game_Troop } from "@niokasgami/rpg-maker-mz-typescript";
 import { Game_Party } from "../unit/party";
 import { Game_Battler } from "./battler";
-import { IUnit } from "../unit/interface";
 
 export declare class Game_Actor extends Game_Battler {
   isActor(): true;
