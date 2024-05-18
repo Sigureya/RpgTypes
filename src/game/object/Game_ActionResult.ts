9dcd99196513cf4753add47f6a68bebde8a2e5d8
Sigureya <sigureya1208@gmail.com>
2024-05-18 18:03:36 +0900

    単体で完結するクラスをコミット

---
+++ src/game/object/Game_ActionResult.ts
@@ -0,0 +1,43 @@
+import { ParamId } from "./types/battler/base/param";
+
+//-----------------------------------------------------------------------------
+// Game_ActionResult
+//
+// The game object class for a result of a battle action. For convinience, all
+// member variables in this class are public.
+
+export declare class Game_ActionResult {
+  used: boolean;
+  missed: boolean;
+  evaded: boolean;
+  physical: boolean;
+  drain: boolean;
+  critical: boolean;
+  success: boolean;
+  hpAffected: boolean;
+  hpDamage: number;
+  mpDamage: number;
+  tpDamage: number;
+  addedStates: number[];
+  removedStates: number[];
+  addedBuffs: ParamId[];
+  addedDebuffs: ParamId[];
+  removedBuffs: ParamId[];
+  constructor();
+  initialize(): void;
+  clear(): void;
+  addedStateObjects(): number[];
+  removedStateObjects(): number;
+  isStatusAffected(): boolean;
+  isHit(): boolean;
+  isStateAdded(stateId: number): boolean;
+  pushAddedState(stateId: number): void;
+  isStateRemoved(stateId: number): boolean;
+  pushRemovedState(stateId: number): void;
+  isBuffAdded(paramId: ParamId): boolean;
+  pushAddedBuff(paramId: ParamId): void;
+  isDebuffAdded(paramId: ParamId): boolean;
+  pushAddedDebuff(paramId: ParamId): void;
+  isBuffRemoved(paramId: ParamId): boolean;
+  pushRemovedBuff(paramId: ParamId): void;
+}
