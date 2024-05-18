4265be7e2c113e8029e01ea6beff80aea2beaa73
Sigureya <sigureya1208@gmail.com>
2024-05-18 18:58:47 +0900

    使用効果を追加

--- src/game/data/item/usableItem.ts
+++ src/game/data/item/usableItem.ts
@@ -1,3 +1,11 @@
 import { Data_NamedItem } from "./namedItem";
+export interface ItemEffect {
+  code: number;
+  value1: number;
+  value2: number;
+  dataId: number;
+}
 
-export interface Data_UsableItem extends Data_NamedItem {}
+export interface Data_UsableItem extends Data_NamedItem {
+  effect: ItemEffect[];
+}
