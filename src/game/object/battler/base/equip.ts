df061249de9dde10362afb32bbd75f850a9f7423
Sigureya <sigureya1208@gmail.com>
2024-05-18 09:55:17 +0900

    装備関連の改名

--- src/game/object/battler/base/equip.ts
+++ src/game/object/battler/base/equip.ts
@@ -1,7 +1,7 @@
-import { DataArmor, DataEquipment, DataWeapon } from "src/game/data/item";
+import { Data_Armor, Data_Equipment, Data_Weapon } from "src/game/data/item";
 
-export interface Data_Equip {
-  canEquip(item: DataEquipment): boolean;
-  canEquipWeapon(weapon: DataWeapon): boolean;
-  canEquipArmor(armor: DataArmor): boolean;
+export interface Equip {
+  canEquip(item: Data_Equipment): boolean;
+  canEquipWeapon(weapon: Data_Weapon): boolean;
+  canEquipArmor(armor: Data_Armor): boolean;
 }
