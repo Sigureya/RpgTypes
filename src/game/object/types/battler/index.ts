a05952a0d615367206d621c99f931d86d362729a
Sigureya <sigureya1208@gmail.com>
2024-05-18 17:04:13 +0900

    バトラーのインターフェース

--- src/game/object/types/battler/index.ts
+++ src/game/object/types/battler/index.ts
@@ -1,7 +1,10 @@
+import { IUnit } from "../Iunit";
 import { IBattlerBase } from "./base";
 import { TimeProgressBattle } from "./timeProgressBattle";
 
 export interface IBattler extends IBattlerBase, TimeProgressBattle {
   isEnemy(): boolean;
   isActor(): boolean;
+  friendsUnit(): IUnit<IBattler>;
+  opponentsUnit(): IUnit<IBattler>;
 }
