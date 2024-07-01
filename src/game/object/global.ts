86d4d9b8c0e5767984b31954ac07aedc752ea30c
Sigureya <sigureya1208@gmail.com>
2024-07-01 18:28:22 +0900

    公開ファイルを追加

--- src/game/object/global.ts
+++ src/game/object/global.ts
@@ -1,7 +1,9 @@
 import { Game_Map } from "./Game_Map";
 import { Game_Message } from "./Game_Message";
 import { Game_Switches } from "./Game_Switches";
+import { Game_Temp } from "./Game_Temp";
 
 export declare let $gameSwitches: Game_Switches;
 export declare let $gameMesssage: Game_Message;
 export declare let $gameMap: Game_Map;
+export declare let $gameTemp: Game_Temp;
