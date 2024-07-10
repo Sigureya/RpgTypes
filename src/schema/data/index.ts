387ce3563526f3dc2aaadf20e74a826092016313
Sigureya <sigureya1208@gmail.com>
2024-07-10 14:52:53 +0900

    index.tsの要素を並び替え。 名前順にして、vscodeから抜けを発見しやすくした。

--- src/schema/data/index.ts
+++ src/schema/data/index.ts
@@ -1,12 +1,17 @@
-export * from "./global";
-export * from "./state";
-export * from "./commonEvent";
+export * from "./eventCommand";
 export * from "./members";
+
+export * from "./class";
+export * from "./commonEvent";
+export * from "./enemy";
 export * from "./equipment";
+
+export * from "./global";
+export * from "./goods";
 export * from "./item";
+export * from "./namedItem";
 export * from "./skill";
+
+export * from "./state";
 export * from "./troop";
 export * from "./usableItem";
-export * from "./namedItem";
-export * from "./eventCommand";
-export * from "./goods";
