1a1f71bf8dea71ae33077378198561338cff55a5
Sigureya <sigureya1208@gmail.com>
2024-07-18 15:15:50 +0900

    Data_Stateの更新

--- src/schema/data/state.ts
+++ src/schema/data/state.ts
@@ -1,3 +1,8 @@
-import { Data_NamedItem } from "./bases/namedItem";
+import { Data_NamedItem, Data_TraitContainer } from "./bases";
 
-export interface Data_State extends Data_NamedItem {}
+export interface Data_State extends Data_NamedItem, Data_TraitContainer {
+  message1: string;
+  message2: string;
+  message3: string;
+  message4: string;
+}
