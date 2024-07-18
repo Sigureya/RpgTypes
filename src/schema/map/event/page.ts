44526faac1c5a8cac9448fa5d8c4c246ab963040
Sigureya <sigureya1208@gmail.com>
2024-07-18 19:14:32 +0900

    マップイベント関連

--- src/schema/map/event/page.ts
+++ src/schema/map/event/page.ts
@@ -1,5 +1,21 @@
+import { Direction8 } from "src/game/object/types/interface/character";
+import { EventCommand } from "../../data";
 import { Condition } from "./condition";
+import { Event_MoveRoute } from "./moveRoute";
+
+interface MapEvent_Image {
+  characterIndex: number;
+  characterName: string;
+  direction: Direction8;
+  pattern: number;
+  tileId: number;
+}
 
 export interface MapEventPage {
   conditions: Condition;
+  list: EventCommand[];
+  directionFix: boolean;
+  image: MapEvent_Image;
+  moveFrequency: number;
+  moveRoute: Event_MoveRoute;
 }
