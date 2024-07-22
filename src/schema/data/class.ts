daaf602ecfef7c634227269a850aec68fa18bcfa
Sigureya <sigureya1208@gmail.com>
2024-07-22 18:08:43 +0900

    ParamArrayの間違いを修正

--- src/schema/data/class.ts
+++ src/schema/data/class.ts
@@ -2,7 +2,7 @@ import { Data_NamedItem, Data_TraitContainer } from "./bases";
 import { Learning, ParamArray } from "./members";
 
 export interface Data_Class extends Data_NamedItem, Data_TraitContainer {
-  params: ParamArray[];
+  params: ParamArray;
   learnings: Learning[];
   expParams: number[];
 }
