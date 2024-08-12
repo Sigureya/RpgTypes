919c5aca9befda76b8b079ca070d3e82c9383fbb
Sigureya <sigureya1208@gmail.com>
2024-08-12 21:48:15 +0900

    テーブルを一部追加

---
+++ src/schema/data/moveRoute/table.ts
@@ -0,0 +1,8 @@
+import type * as $ from "./code";
+import type { AudioFileParams } from "../../../types/";
+
+export type MoveRouteParameters = {
+  [$.ROUTE_END]: [];
+  [$.ROUTE_PLAY_SE]: [se: AudioFileParams];
+  [$.ROUTE_SCRIPT]: [script: string];
+};
