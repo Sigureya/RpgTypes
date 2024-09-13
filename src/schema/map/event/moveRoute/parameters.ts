970525169c69d2ee884d883610172e25da6a7e1f
Sigureya <sigureya1208@gmail.com>
2024-09-14 04:48:40 +0900

    行グラフィック変更に対応

--- src/schema/map/event/moveRoute/parameters.ts
+++ src/schema/map/event/moveRoute/parameters.ts
@@ -5,4 +5,5 @@ export type MoveRouteParameters = {
   [$.ROUTE_END]: [];
   [$.ROUTE_PLAY_SE]: [se: AudioFileParams];
   [$.ROUTE_SCRIPT]: [script: string];
+  [$.ROUTE_CHANGE_IMAGE]: [characterName: string, characterIndex: number];
 };
