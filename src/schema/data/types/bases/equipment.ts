999166de63029b9428b10107f402231348890566
Sigureya <sigureya1208@gmail.com>
2024-12-21 16:23:43 +0900

    mock作成用にメンバを追加

--- src/schema/data/types/bases/equipment.ts
+++ src/schema/data/types/bases/equipment.ts
@@ -1,9 +1,13 @@
 import type { ParamArray } from "../members";
 import type { Data_NamedItem } from "./namedItem";
-import type { Data_TraitContainer } from "./trait";
+import type { Data_TraitContainer, Trait } from "./trait";
 
 export interface Data_Equipment extends Data_NamedItem, Data_TraitContainer {
+  name: string;
+  id: number;
   description: string;
   price: number;
   params: ParamArray;
+  traits: Trait[];
+  note: string;
 }
