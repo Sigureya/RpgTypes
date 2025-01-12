e3389b7055aeef804a41c2c4d849af8db4ccfc84
Sigureya <sigureya1208@gmail.com>
2025-01-12 19:26:32 +0900

    merge用に手動で調整

--- src/game/index.ts
+++ src/game/index.ts
@@ -2,5 +2,6 @@ export * from "./object";
 export * from "./window";
 export * from "./sprite";
 export * from "./manager";
+export * from "./savedata";
 export * from "./globalManagers";
 export * from "./globalObjects";
