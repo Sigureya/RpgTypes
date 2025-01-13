26ae324998dca98ad817fed03dd7fa244f76dc33
Sigureya <sigureya1208@gmail.com>
2025-01-14 03:00:33 +0900

    全てのメンバを明示的に書いた

--- src/schema/data/types/armor.ts
+++ src/schema/data/types/armor.ts
@@ -1,8 +1,12 @@
-import type { Data_Equipment } from "./bases";
-import { ParamArray } from "./members";
+import type { Data_Equipment, Trait } from "./bases";
+import type { ParamArray } from "./members";
 
 export interface Data_Armor extends Data_Equipment {
+  name: string;
+  id: number;
   description: string;
   price: number;
   params: ParamArray;
+  traits: Trait[];
+  note: string;
 }
