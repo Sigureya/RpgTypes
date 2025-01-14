f57182acffb19701cf8f5f308e0863ddfbfb2a09
Sigureya <sigureya1208@gmail.com>
2025-01-14 23:39:36 +0900

    indexの修正

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,8 +1,3 @@
-export * from "./object";
-export * from "./window";
-export * from "./sprite";
-export * from "./manager";
 export * from "./savedata";
 export * from "./globalManagers";
 export * from "./globalObjects";
-
