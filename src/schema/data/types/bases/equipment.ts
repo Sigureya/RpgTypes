9eac45c9125e863fda729cc29775c293452cc0af
Sigureya <sigureya1208@gmail.com>
2025-01-15 04:53:48 +0900

    types/base/membersへと配置を修正。これにより../が無くなった

--- src/schema/data/types/bases/equipment.ts
+++ src/schema/data/types/bases/equipment.ts
@@ -1,6 +1,6 @@
-import type { ParamArray } from "../members";
 import type { Data_TraitContainer, Trait } from "./trait";
 import type { Data_Goods } from "./goods";
+import type { ParamArray } from "./members";
 
 export interface Data_Equipment extends Data_Goods, Data_TraitContainer {
   name: string;
