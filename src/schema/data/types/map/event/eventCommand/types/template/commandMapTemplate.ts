eff267fbe36db2a6ac159faa77ce14ae3b87484a
Sigureya <sigureya1208@gmail.com>
2025-01-16 04:49:19 +0900

    型名を修正。またテンプレート内部で使っている型も修正した

--- src/schema/data/types/map/event/eventCommand/types/template/commandMapTemplate.ts
+++ src/schema/data/types/map/event/eventCommand/types/template/commandMapTemplate.ts
@@ -1,36 +1,53 @@
 import type { PickByType } from "./filterByValue";
 
+/**
+ * A template for creating a command table with specific code and parameter mappings.
+ *
+ * @template CodeConstants - A mapping of command names to unique codes.
+ * @template Command - The base command object to extend.
+ * @template CodeKey - The property of `Command` that represents the command code.
+ * @template ParamKey - The property of `Command` that holds the parameters.
+ * @template Table - A partial mapping of command codes to parameter types.
+ */
 export type CommandMapTamplate<
-  CodeConstatns extends Record<PropertyKey, string | number>,
-  T extends object,
-  CodeProp extends keyof PickByType<T, ValueOf<CodeConstatns>>,
-  ParamProp extends keyof PickByType<T, object>,
-  Table extends Partial<CommandTableConcept<keyof CodeConstatns, T[ParamProp]>>
-> = CreateTable<
-  CodeConstatns,
+  CodeConstants extends Record<PropertyKey, string | number>,
+  Command extends object,
+  CodeKey extends keyof PickByType<Command, ValueOf<CodeConstants>>,
+  ParamKey extends keyof PickByType<Command, object>,
+  Table extends Partial<CommandMapping<keyof CodeConstants, Command[ParamKey]>>
+> = ConstructTable<
+  CodeConstants,
   {
-    [TableKey in keyof Table]: {
-      [key in keyof T]: key extends ParamProp
-        ? Table[TableKey]
-        : key extends CodeProp
-        ? CodeConstatns[TableKey]
-        : T[key];
+    [Key in keyof Table]: {
+      [Prop in keyof Command]: Prop extends ParamKey
+        ? Table[Key]
+        : Prop extends CodeKey
+        ? CodeConstants[Key]
+        : Command[Prop];
     };
   }
 >;
 
-type CreateTable<
-  CodeConstatns extends Record<PropertyKey, string | number>,
+/**
+ * Constructs a command table type.
+ *
+ * @template CodeConstants - Command code mappings.
+ * @template Table - A mapping of command names to their parameter types.
+ */
+type ConstructTable<
+  CodeConstants extends Record<PropertyKey, string | number>,
   Table extends Record<PropertyKey, object>
 > = {
-  codeType: ValueOf<Pick<CodeConstatns, keyof Table>>;
+  codeType: ValueOf<Pick<CodeConstants, keyof Table>>;
   commandType: ValueOf<Table>;
   commandTable: Table;
   codeKeys: keyof Table;
 };
 
+/** Extracts the value type of a record. */
 type ValueOf<T extends Record<PropertyKey, unknown>> = T[keyof T];
 
-type CommandTableConcept<Key extends PropertyKey, ParamType = unknown> = {
+/** A mapping of command keys to parameter types. */
+type CommandMapping<Key extends PropertyKey, ParamType = unknown> = {
   [K in Key]: ParamType;
 };
