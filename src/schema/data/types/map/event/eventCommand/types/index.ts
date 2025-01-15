7fc91a85c5017fe60d2bd506decb42eda2f896a7
Sigureya <sigureya1208@gmail.com>
2025-01-16 06:07:35 +0900

    indexを修正

--- src/schema/data/types/map/event/eventCommand/types/index.ts
+++ src/schema/data/types/map/event/eventCommand/types/index.ts
@@ -2,3 +2,5 @@ export * from "./audioFileParams";
 export * from "./colorRGBA";
 export * from "./direction8";
 export * from "./moveRoute";
+export * from "./moveRouteCommand";
+export * from "./moveRouteData";
