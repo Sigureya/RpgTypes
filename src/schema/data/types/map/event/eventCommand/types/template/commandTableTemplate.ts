20e6b286b78d938c0fb50dc7b04d125bf1be0085
Sigureya <sigureya1208@gmail.com>
2025-01-16 02:38:10 +0900

    PickByTypeを使用して設定をわかりやすくした

--- src/schema/data/types/map/event/eventCommand/types/template/commandTableTemplate.ts
+++ src/schema/data/types/map/event/eventCommand/types/template/commandTableTemplate.ts
@@ -1,9 +1,11 @@
+import type { PickByType } from "./filterByValue";
+
 // Tとkeyof T(2個)でcodeとparamを確定させる
 export type CommandTableTamplate<
   Rec extends Record<PropertyKey, string | number>,
   T extends object,
-  Code extends keyof T & string,
-  Param extends keyof T,
+  Code extends keyof PickByType<T, ValueOf<Rec>>,
+  Param extends keyof PickByType<T, object>,
   Table extends Partial<CommandTableConcept<keyof Rec, T[Param]>>
 > = {
   codeKeys: keyof Table;
@@ -14,39 +16,8 @@ type CreateTable<T extends Record<PropertyKey, unknown>> = {
   table: T;
   commandType: T[keyof T];
 };
+
 type ValueOf<T extends Record<PropertyKey, unknown>> = T[keyof T];
-
-type CommandConcept = {
-  code: string | number;
-  parameters: object;
-};
-type Command<T extends CommandConcept> = T;
-
-interface GGG {
-  codeKey: string;
-  paramKey: string;
-}
-type XXX<Key extends string, Value> = {
-  [K in Key]: Value;
-};
-interface LLL<T, T2 extends T> {}
-type Command3<
-  NameGGG extends GGG,
-  CodeType extends PropertyKey,
-  ParamType,
-  T extends object = {}
-> = {
-  [C in NameGGG["codeKey"]]: CodeType;
-} & Exclude<T, NameGGG["codeKey"] | NameGGG["paramKey"]>;
-
-type Command2<
-  NameGGG extends GGG,
-  ParamKey extends PropertyKey,
-  ParamType,
-  T extends object
-> = {
-  [K in ParamKey]: ParamType;
-} & Exclude<T, ParamKey | NameGGG["codeKey"] | NameGGG["paramKey"]>;
 
 type CommandTableConcept<Key extends PropertyKey, ParamType = unknown> = {
   [K in Key]: ParamType;
