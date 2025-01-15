be403e461921736f33f01cd5abf829dc86c5c4e3
Sigureya <sigureya1208@gmail.com>
2025-01-16 02:43:58 +0900

    interfaceにできる部分があったので修正

--- src/schema/data/types/map/event/eventCommand/types/template/commandTableTemplate.ts
+++ src/schema/data/types/map/event/eventCommand/types/template/commandTableTemplate.ts
@@ -12,10 +12,10 @@ export type CommandTableTamplate<
   codeType: ValueOf<Pick<Rec, keyof Table>>;
 } & CreateTable<{ [K in keyof Table]: { code: Rec[K]; parameters: Table[K] } }>;
 
-type CreateTable<T extends Record<PropertyKey, unknown>> = {
+interface CreateTable<T extends Record<PropertyKey, unknown>> {
   table: T;
   commandType: T[keyof T];
-};
+}
 
 type ValueOf<T extends Record<PropertyKey, unknown>> = T[keyof T];
 
