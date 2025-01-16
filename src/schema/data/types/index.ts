072fd9b77d9664825719ef6e1191639a0c3e563c
Sigureya <sigureya1208@gmail.com>
2025-01-16 20:16:04 +0900

    マップ関連を中心にファイルを再配置

--- src/schema/data/types/index.ts
+++ src/schema/data/types/index.ts
@@ -2,10 +2,7 @@ export * from "./main";
 
 export * from "./commonEvent";
 
-export * from "./mapInfo";
 export * from "./troop";
-export * from "./tileset";
-export * from "./unionTypes";
 export * from "./animation";
 
 export * from "./map";
