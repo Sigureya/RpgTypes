6fc9ea33b69bc51592186fa29ea806babd6ab90c
Sigureya <sigureya1208@gmail.com>
2025-01-16 22:03:36 +0900

    定数の定義方法を変更した

--- src/schema/data/types/map/event/eventCommand/constants.ts
+++ src/schema/data/types/map/event/eventCommand/constants.ts
@@ -1,14 +1,13 @@
-export const TOGGLE = {
-  ON: 0 as const,
-  OFF: 1 as const,
-};
-// TODO:OPERATIONが他の場所にもあるので、統一方法を考える
-export const OPERATION = {
-  PLUS: 0 as const,
-  MINUS: 1 as const,
-};
+export interface Toggle {
+  ON: 0;
+  OFF: 1;
+}
+export interface Operation {
+  PLUS: 0;
+  MINUS: 1;
+}
 
-export const DESIGNATION = {
-  DIRECT: 0 as const,
-  VARIABLE: 1 as const,
-};
+export interface Designation {
+  DIRECT: 0;
+  VARIABLE: 1;
+}
