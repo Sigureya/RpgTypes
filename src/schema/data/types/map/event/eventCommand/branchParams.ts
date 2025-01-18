2d4bad1c39bfae542db6dda461c34429a69a4cd1
Sigureya <sigureya1208@gmail.com>
2025-01-18 17:19:56 +0900

    条件分岐に使うパラメータを定義

---
+++ src/schema/data/types/map/event/eventCommand/branchParams.ts
@@ -0,0 +1,34 @@
+import type {
+  BranchCode,
+  Operation_PlusMinus,
+  Toggle,
+  ValueOf,
+} from "./paramaters";
+export type BranchBySwitch = [
+  branchCode: BranchCode["SWITCH"],
+  switchId: number,
+  switchValue: ValueOf<Toggle>
+];
+export type BranchByVariable = [
+  branchCode: BranchCode["VARIABLE"],
+  variableId: number,
+  operand: number,
+  value: number
+];
+
+export type BranchBySelfSwitch = [
+  branchCode: BranchCode["SELF_SWITCH"],
+  selfSwitch: string,
+  selfSwitchValue: ValueOf<Toggle>
+];
+export type BranchByTimer = [
+  branchCode: BranchCode["TIMER"],
+  time: number,
+  operation: ValueOf<Operation_PlusMinus>
+];
+export type BranchByActor = [
+  branchCode: BranchCode["ACTOR"],
+  actorId: number,
+  operand: number,
+  value: number
+];
