36ebcfdca9dc08b8a23529736e9304bb2be251d9
Sigureya <sigureya1208@gmail.com>
2025-01-19 05:51:07 +0900

    typeからinterfaceへと修正。また定義された型を扱うクラスを作りやすくするための基底クラスを準備

--- src/schema/data/types/map/event/eventCommand/types/template/commandTemplate.ts
+++ src/schema/data/types/map/event/eventCommand/types/template/commandTemplate.ts
@@ -7,20 +7,20 @@ import type { PickByType } from "./filterByValue";
  * @template ParameterObject - An object type representing the keys used for command parameters.
  * @template CommandParameterMapping - A record mapping command codes to their parameter types.
  */
-export type CommandTemplateSimple<
+export interface CommandTemplateSimple<
   CommandCodeMapping extends Record<PropertyKey, string | number>,
   ParameterObject extends object,
   CommandParameterMapping extends Record<
     keyof CommandCodeMapping,
     ParameterObject
   >
-> = CommandTemplate<
-  CommandCodeMapping,
-  { code: ValueOf<CommandCodeMapping>; parameters: object },
-  "code",
-  "parameters",
-  CommandParameterMapping
->;
+> extends CommandTemplate<
+    CommandCodeMapping,
+    { code: ValueOf<CommandCodeMapping>; parameters: object },
+    "code",
+    "parameters",
+    CommandParameterMapping
+  > {}
 
 /**
  * Defines a command template for structured type mappings.
@@ -31,7 +31,7 @@ export type CommandTemplateSimple<
  * @template ParameterPropertyKey - A key representing the command's parameter property.
  * @template CommandParameterMapping - A record mapping command codes to their parameter types.
  */
-export type CommandTemplate<
+export interface CommandTemplate<
   CommandCodeMapping extends Record<PropertyKey, string | number>,
   Command extends object,
   CodePropertyKey extends keyof PickByType<
@@ -43,24 +43,39 @@ export type CommandTemplate<
     keyof CommandCodeMapping,
     Command[ParameterPropertyKey]
   >
-> = ConstructTable<
-  CommandCodeMapping,
-  {
-    // Using `keyof Table` enables editor-friendly navigation to parameter definitions.
-    // If `keyof CommandCodeMapping` were used, navigation would lead to constant definitions, which is less practical.
-    // Additionally, the `extends keyof CommandCodeMapping` condition removes irrelevant types.
-    [Key in keyof CommandParameterMapping]: Key extends keyof CommandCodeMapping
-      ? {
-          [Prop in keyof Command]: Prop extends ParameterPropertyKey
-            ? CommandParameterMapping[Key]
-            : Prop extends CodePropertyKey
-            ? CommandCodeMapping[Key]
-            : Command[Prop];
-        }
-      : never;
-  },
-  CodePropertyKey
->;
+> extends ConstructTable<
+    CommandCodeMapping,
+    {
+      // Using `keyof Table` enables editor-friendly navigation to parameter definitions.
+      // If `keyof CommandCodeMapping` were used, navigation would lead to constant definitions, which is less practical.
+      // Additionally, the `extends keyof CommandCodeMapping` condition removes irrelevant types.
+      [Key in keyof CommandParameterMapping]: Key extends keyof CommandCodeMapping
+        ? {
+            [Prop in keyof Command]: Prop extends ParameterPropertyKey
+              ? CommandParameterMapping[Key]
+              : Prop extends CodePropertyKey
+              ? CommandCodeMapping[Key]
+              : Command[Prop];
+          }
+        : never;
+    },
+    CodePropertyKey
+  > {}
+
+// テーブルの基底インターフェース
+// コマンドテンプレートで作成されたインターフェースを利用したいときに使う
+export interface C<
+  Command extends object,
+  MappingKey extends PropertyKey = PropertyKey,
+  Code extends string | number = string | number
+> {
+  commandTypeTable: Record<MappingKey, Command>;
+  codeTable: Record<MappingKey, Code>;
+  commandByCode: Record<Code, Command>;
+  codeType: Code;
+  commandType: Command;
+  codeKeys: MappingKey;
+}
 
 /**
  * Constructs a type representing a command table.
@@ -69,11 +84,13 @@ export type CommandTemplate<
  * @template CommandParameterMapping - A mapping of command codes to their respective parameter types.
  * @template CodeKey - A key used to associate codes with command objects.
  */
-type ConstructTable<
+interface ConstructTable<
   CommandCodeMapping extends Record<PropertyKey, string | number>,
   CommandParameterMapping extends Record<keyof CommandCodeMapping, object>,
-  CodeKey extends PropertyKey
-> = {
+  CodeKey extends keyof CommandCodeMapping
+> extends C<object, keyof CommandCodeMapping, ValueOf<CommandCodeMapping>> {
+  parameters: "";
+
   /**
    * @description A mapping of commands to their parameter types.
    **/
@@ -103,7 +120,7 @@ type ConstructTable<
    * @description Keys representing the commands in the table.
    **/
   codeKeys: keyof CommandParameterMapping;
-};
+}
 
 /** Extracts the value type of a record. */
 type ValueOf<T extends Record<PropertyKey, unknown>> = T[keyof T];
