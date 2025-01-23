9a9cfb96b9716654364ba867c20f2a34e99a2cbd
Sigureya <sigureya1208@gmail.com>
2025-01-24 05:54:57 +0900

    code部分で定数テーブルを参照するようにした

---
+++ src/schema/data/types/map/event/eventCommand/eventComandLike.ts
@@ -0,0 +1,10 @@
+import type * as CodeConstants from "./eventCommandCodes";
+
+export interface EventCommandLike2<
+  Code extends (typeof CodeConstants)[keyof typeof CodeConstants],
+  Parameters extends unknown[]
+> {
+  parameters: Parameters;
+  code: Code;
+  indent: number;
+}
