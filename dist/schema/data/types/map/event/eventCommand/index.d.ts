ce4aa54db9440327395366eb96107275c94f4f83
Sigureya <sigureya1208@gmail.com>
2025-01-29 07:17:17 +0900

    exportを忘れていたので修正

--- dist/schema/data/types/map/event/eventCommand/index.d.ts
+++ dist/schema/data/types/map/event/eventCommand/index.d.ts
@@ -1,6 +1,7 @@
 export * from './paramaters';
 export * from './eventCommandCodes';
 export * from './eventCommand';
+export * from './eventComandLike';
 export * from './types';
 export * from './branchParams';
 export * from './eventCommandTypes';
