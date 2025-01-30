7a3ea02c04c8465e4e34b601cb327726786397b4
Sigureya <sigureya1208@gmail.com>
2025-01-30 13:44:34 +0900

    条件分岐のパラメータを整理した

--- dist/schema/data/types/map/event/eventCommand/branchParams.d.ts
+++ dist/schema/data/types/map/event/eventCommand/branchParams.d.ts
@@ -1,48 +1,66 @@
-import { BranchCode, BranchCodeTable, Operation_PlusMinus, Toggle, ValueOf } from './paramaters';
-import { CommandTemplate } from './types/';
-export type BranchParameters = BranchMapper["parameterTable"];
-export type BranchMapper = CommandTemplate<BranchCodeTable, {
-    branchBy: BranchCode;
-    params: unknown[];
-}, "branchBy", "params", {
-    SWITCH: [
-        branchCode: BranchCodeTable["SWITCH"],
-        switchId: number,
-        switchValue: ValueOf<Toggle>
-    ];
-    VARIABLE: [
-        branchCode: BranchCodeTable["VARIABLE"],
-        variableId: number,
-        operand: number,
-        value: number
-    ];
-    SELF_SWITCH: [
-        branchCode: BranchCodeTable["SELF_SWITCH"],
-        selfSwitchId: string,
-        value: ValueOf<Toggle>
-    ];
-    TIMER: [
-        branchCode: BranchCodeTable["TIMER"],
-        time: number,
-        operation: ValueOf<Operation_PlusMinus>
-    ];
-    ACTOR: [
-        branchCode: BranchCodeTable["ACTOR"],
-        actorId: number,
-        operand: number,
-        value: number
-    ];
-    ENEMY: [branchCode: BranchCodeTable["ENEMY"], enemyId: number];
-    CHARACTER: [branchCode: BranchCodeTable["CHARACTER"], characterId: number];
-    GOLD: [
-        branchCode: BranchCodeTable["GOLD"],
-        value: number,
-        compair: 0 | 1 | 2
-    ];
-    ITEM: [branchCode: BranchCodeTable["ITEM"], itemId: number];
-    WEAPON: [branchCode: BranchCodeTable["WEAPON"], weaponId: number];
-    ARMOR: [branchCode: BranchCodeTable["ARMOR"], armorId: number];
-    BUTTON: [branchCode: BranchCodeTable["BUTTON"], buttonId: number];
-    SCRIPT: [branchCode: BranchCodeTable["SCRIPT"], script: string];
-    VEHICLE: [branchCode: BranchCodeTable["VEHICLE"], vehicleId: number];
-}>;
+import { BranchCodeTable, Operation_PlusMinus, Toggle, ValueOf } from './paramaters';
+export type BranchParam_Switch = [
+    branchCode: BranchCodeTable["SWITCH"],
+    switchId: number,
+    switchValue: ValueOf<Toggle>
+];
+export type BranchParam_Variable = [
+    branchCode: BranchCodeTable["VARIABLE"],
+    variableId: number,
+    operand: number,
+    value: number
+];
+export type BranchParam_SelfSwitch = [
+    branchCode: BranchCodeTable["SELF_SWITCH"],
+    selfSwitchId: string,
+    value: ValueOf<Toggle>
+];
+export type BranchParam_Timer = [
+    branchCode: BranchCodeTable["TIMER"],
+    time: number,
+    operation: ValueOf<Operation_PlusMinus>
+];
+export type BranchParam_Actor = [
+    branchCode: BranchCodeTable["ACTOR"],
+    actorId: number,
+    operand: number,
+    value: number
+];
+export type BranchParam_Enemy = [
+    branchCode: BranchCodeTable["ENEMY"],
+    enemyId: number
+];
+export type BranchParam_Character = [
+    branchCode: BranchCodeTable["CHARACTER"],
+    characterId: number
+];
+export type BranchParam_Gold = [
+    branchCode: BranchCodeTable["GOLD"],
+    value: number,
+    compair: 0 | 1 | 2
+];
+export type BranchParam_Item = [
+    branchCode: BranchCodeTable["ITEM"],
+    itemId: number
+];
+export type BranchParam_Weapon = [
+    branchCode: BranchCodeTable["WEAPON"],
+    weaponId: number
+];
+export type BranchParam_Armor = [
+    branchCode: BranchCodeTable["ARMOR"],
+    armorId: number
+];
+export type BranchParam_Button = [
+    branchCode: BranchCodeTable["BUTTON"],
+    buttonId: number
+];
+export type BranchParam_Script = [
+    branchCode: BranchCodeTable["SCRIPT"],
+    script: string
+];
+export type BranchParam_Vehicle = [
+    branchCode: BranchCodeTable["VEHICLE"],
+    vehicleId: number
+];
+export type BranchParameters = BranchParam_Switch | BranchParam_Variable | BranchParam_SelfSwitch | BranchParam_Timer | BranchParam_Actor | BranchParam_Enemy | BranchParam_Character | BranchParam_Gold | BranchParam_Item | BranchParam_Weapon | BranchParam_Armor | BranchParam_Button | BranchParam_Script | BranchParam_Vehicle;
