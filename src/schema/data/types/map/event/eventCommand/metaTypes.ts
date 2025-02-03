66ecc70662930270bf67b35b34d214e5ad72a648
Sigureya <sigureya1208@gmail.com>
2025-02-03 19:57:33 +0900

    用法を踏まえて名前を変更した

--- src/schema/data/types/map/event/eventCommand/metaTypes.ts
+++ src/schema/data/types/map/event/eventCommand/metaTypes.ts
@@ -1,6 +1,11 @@
 import type { EventCommand } from "./eventCommand";
 
-export type PickCommandByParam<P extends unknown[]> = Extract<
+export type ExtractCommandByParam<P extends unknown[]> = Extract<
   EventCommand,
   { parameters: P }
 >;
+
+/**
+ * @deprecated Use `ExtractCommandByParam` instead.
+ */
+export type PickCommandByParam<P extends unknown[]> = ExtractCommandByParam<P>;
