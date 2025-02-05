2e8523fcc9fe7a79aa22d766fede4eb676e1a2c4
Sigureya <sigureya1208@gmail.com>
2025-02-05 16:30:04 +0900

    ピクチャー関連のイベントコマンドを修正

--- dist/schema/data/types/map/event/eventCommand/paramaters/showPictrure.d.ts
+++ dist/schema/data/types/map/event/eventCommand/paramaters/showPictrure.d.ts
@@ -10,4 +10,15 @@ export type ShowPicture = [
     opacity: number,
     blendMode: PicutureBlendMode
 ];
-export type MovePicture = [...ShowPicture, wait: boolean, easingType: number];
+export type MovePicture = [
+    pictureId: number,
+    origin: 0 | 1,
+    x: number,
+    y: number,
+    scaleX: number,
+    scaleY: number,
+    opacity: number,
+    blendMode: PicutureBlendMode,
+    wait: boolean,
+    easingType: number
+];
