12b62f88cfbe957b03211b1da744e74c16fa36e8
Sigureya <sigureya1208@gmail.com>
2025-04-02 08:15:33 +0900

    sparamに対応。特殊能力値

---
+++ src/schema/data/types/main/bases/trait/options/sparam/constants.ts
@@ -0,0 +1,49 @@
+/**
+ * * Target Rate
+ */
+export const SPECIAL_PARAM_TGR = 0 as const;
+
+/**
+ * * Guard Effect Rate
+ */
+export const SPECIAL_PARAM_GRD = 1 as const;
+
+/**
+ * * Recovery Effect Rate
+ */
+export const SPECIAL_PARAM_REC = 2 as const;
+
+/**
+ * * Pharmacology
+ */
+export const SPECIAL_PARAM_PHA = 3 as const;
+
+/**
+ * * MP Cost Rate
+ */
+export const SPECIAL_PARAM_MCR = 4 as const;
+
+/**
+ * * TP Cost Rate
+ */
+export const SPECIAL_PARAM_TCR = 5 as const;
+
+/**
+ * * Physical Damage Rate
+ */
+export const SPECIAL_PARAM_PDR = 6 as const;
+
+/**
+ * * Magic Damage Rate
+ */
+export const SPECIAL_PARAM_MDR = 7 as const;
+
+/**
+ * * Floor Damage Rate
+ */
+export const SPECIAL_PARAM_FDR = 8 as const;
+
+/**
+ * * Experience Rate
+ */
+export const SPECIAL_PARAM_EXR = 9 as const;
