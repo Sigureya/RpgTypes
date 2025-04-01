07860ae3d17dfa1700b7f3a600598715d9fe7348
Sigureya <sigureya1208@gmail.com>
2025-04-02 06:19:42 +0900

    追加能力値のラベルを追加。

---
+++ src/schema/data/types/main/bases/trait/xparams/xparamLables.ts
@@ -0,0 +1,15 @@
+/**
+ * * 追加能力値のラベル
+ */
+export interface XParamLabels {
+  hitRate: string;
+  evasionRate: string;
+  criticalRate: string;
+  criticalEvasionRate: string;
+  magicEvasionRate: string;
+  magicReflectionRate: string;
+  counterAttackRate: string;
+  hpRegenerationRate: string;
+  mpRegenerationRate: string;
+  tpRegenerationRate: string;
+}
