b735703bbd50397a80fee51c8526f672891b8520
Sigureya <sigureya1208@gmail.com>
2025-04-02 01:03:45 +0900

    特徴関連の追加定数

---
+++ src/schema/data/types/main/bases/trait/constants/specialFlags.ts
@@ -0,0 +1,4 @@
+export const FLAG_ID_AUTO_BATTLE = 0 as const;
+export const FLAG_ID_GUARD = 1 as const;
+export const FLAG_ID_SUBSTITUTE = 2 as const;
+export const FLAG_ID_PRESERVE_TP = 3 as const;
