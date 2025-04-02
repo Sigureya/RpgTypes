f34268936bc9e7857932657e57be8002517adf5e
Sigureya <sigureya1208@gmail.com>
2025-04-02 21:53:07 +0900

    定数の名前を変更。英語版マニュアルに準拠してRegularとした

--- dist/schema/data/types/main/bases/trait/options/paramRegular/constants.d.ts
+++ dist/schema/data/types/main/bases/trait/options/paramRegular/constants.d.ts
@@ -1,32 +1,32 @@
 /**
  * * Maximum HP
  */
-export declare const MAIN_PARAM_MAX_HP: 0;
+export declare const REGULAR_PARAM_MAX_HP: 0;
 /**
  * * Maximum MP
  */
-export declare const MAIN_PARAM_MAX_MP: 1;
+export declare const REGULAR_PARAM_MAX_MP: 1;
 /**
  * * Attack Power
  */
-export declare const MAIN_PARAM_ATK: 2;
+export declare const REGULAR_PARAM_ATK: 2;
 /**
  * * Defense Power
  */
-export declare const MAIN_PARAM_DEF: 3;
+export declare const REGULAR_PARAM_DEF: 3;
 /**
  * * Magic Attack Power
  */
-export declare const MAIN_PARAM_MATK: 4;
+export declare const REGULAR_PARAM_MATK: 4;
 /**
  * * Magic Defense Power
  */
-export declare const MAIN_PARAM_MDEF: 5;
+export declare const REGULAR_PARAM_MDEF: 5;
 /**
  * * Agility
  */
-export declare const MAIN_PARAM_AGI: 6;
+export declare const REGULAR_PARAM_AGI: 6;
 /**
  * * Luck
  */
-export declare const MAIN_PARAM_LUK: 7;
+export declare const REGULAR_PARAM_LUK: 7;
--- src/schema/data/types/main/bases/trait/options/paramRegular/constants.ts
+++ src/schema/data/types/main/bases/trait/options/paramRegular/constants.ts
@@ -1,34 +1,35 @@
 /**
  * * Maximum HP
  */
-export const MAIN_PARAM_MAX_HP = 0 as const;
+export const REGULAR_PARAM_MAX_HP = 0 as const;
+
 /**
  * * Maximum MP
  */
-export const MAIN_PARAM_MAX_MP = 1 as const;
+export const REGULAR_PARAM_MAX_MP = 1 as const;
 /**
  * * Attack Power
  */
-export const MAIN_PARAM_ATK = 2 as const;
+export const REGULAR_PARAM_ATK = 2 as const;
 /**
  * * Defense Power
  */
-export const MAIN_PARAM_DEF = 3 as const;
+export const REGULAR_PARAM_DEF = 3 as const;
 /**
  * * Magic Attack Power
  */
-export const MAIN_PARAM_MATK = 4 as const;
+export const REGULAR_PARAM_MATK = 4 as const;
 
 /**
  * * Magic Defense Power
  */
-export const MAIN_PARAM_MDEF = 5 as const;
+export const REGULAR_PARAM_MDEF = 5 as const;
 
 /**
  * * Agility
  */
-export const MAIN_PARAM_AGI = 6 as const;
+export const REGULAR_PARAM_AGI = 6 as const;
 /**
  * * Luck
  */
-export const MAIN_PARAM_LUK = 7 as const;
+export const REGULAR_PARAM_LUK = 7 as const;
