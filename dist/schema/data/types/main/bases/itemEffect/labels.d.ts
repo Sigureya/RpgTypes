1dfaab1213c21bcdc098e171b44de3bda669731d
Sigureya <sigureya1208@gmail.com>
2025-04-07 13:00:24 +0900

    アイテムの効果に関するラベル。コモンイベントは別の箇所で定義しているので除外

---
+++ dist/schema/data/types/main/bases/itemEffect/labels.d.ts
@@ -0,0 +1,14 @@
+export interface ItemCodeLabels {
+    recoverHp: string;
+    recoverMp: string;
+    gainTp: string;
+    addState: string;
+    removeState: string;
+    addBuff: string;
+    addDebuff: string;
+    removeBuff: string;
+    removeDebuff: string;
+    special: string;
+    grow: string;
+    learnSkill: string;
+}
---
+++ src/schema/data/types/main/bases/itemEffect/labels.ts
@@ -0,0 +1,16 @@
+export interface ItemCodeLabels {
+  recoverHp: string;
+  recoverMp: string;
+  gainTp: string;
+  addState: string;
+  removeState: string;
+  addBuff: string;
+  addDebuff: string;
+  removeBuff: string;
+  removeDebuff: string;
+  special: string;
+  grow: string;
+  learnSkill: string;
+  // 「コモンイベント」という単語は別の場所で定義されているので省略
+  //  commonEvent: string;
+}
