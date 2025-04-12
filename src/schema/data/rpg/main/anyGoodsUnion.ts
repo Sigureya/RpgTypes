991ce1242df146f2f9112038b7d54615a54ddeea
Sigureya <sigureya1208@gmail.com>
2025-04-13 03:32:10 +0900

    ファイル名を変更。また古い名前を非推奨にした

--- src/schema/data/rpg/main/anyGoodsUnion.ts
+++ src/schema/data/rpg/main/anyGoodsUnion.ts
@@ -2,4 +2,8 @@ import type { Data_Armor } from "./traitContainers/armor";
 import type { Data_Weapon } from "./traitContainers/weapon";
 import type { Data_Item } from "./usableItems";
 
-export type Data_AnyGoods = Data_Item | Data_Weapon | Data_Armor;
+/**
+ * @deprecated
+ */
+export type Data_AnyGoods = Data_AnyGoodsUnion;
+export type Data_AnyGoodsUnion = Data_Item | Data_Weapon | Data_Armor;
