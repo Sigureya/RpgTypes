34cd5673610bb3eaa91a7a7d8541516f52ded434
Sigureya <sigureya1208@gmail.com>
2025-04-13 09:50:56 +0900

    labelから特定の部位を取得する関数。別リポジトリだとバージョン不一致が発生しやすいのでこちらに移動

---
+++ src/labelFilter.ts
@@ -0,0 +1,57 @@
+import type * as RPG from "./schema";
+
+export const regularParamLabels = (
+  labels: RPG.LabelRegistry
+): RPG.RegularParamLabels => {
+  return labels.rpg.trait.options.paramRegular;
+};
+
+export const specialParamLabels = (
+  labels: RPG.LabelRegistry
+): RPG.SpecialParamLabels => {
+  return labels.rpg.trait.options.paramSpecial;
+};
+
+export const extraParamLabels = (
+  labels: RPG.LabelRegistry
+): RPG.ExtraParamLabels => {
+  return labels.rpg.trait.options.paramExtra;
+};
+
+export const traitLabels = (labels: RPG.LabelRegistry): RPG.TraitLabels => {
+  return labels.rpg.trait;
+};
+
+export const itemEffectLabels = (
+  labels: RPG.LabelRegistry
+): RPG.ItemCodeLabels => {
+  return labels.rpg.itemEffect;
+};
+
+export const globalLabel = (labels: RPG.LabelRegistry): RPG.GlobalLabel => {
+  return labels.global;
+};
+
+export const itemLabels = (labels: RPG.LabelRegistry): RPG.ItemLabels => {
+  return labels.rpg.item;
+};
+
+export const skillLabels = (labels: RPG.LabelRegistry): RPG.SkillLabels => {
+  return labels.rpg.skill;
+};
+
+export const actorLabels = (labels: RPG.LabelRegistry): RPG.ActorLabels => {
+  return labels.rpg.actor;
+};
+
+export const classLabels = (labels: RPG.LabelRegistry): RPG.ClassLabels => {
+  return labels.rpg.class;
+};
+
+export const stateLabels = (labels: RPG.LabelRegistry): RPG.StateLabels => {
+  return labels.rpg.state;
+};
+
+export const enemyLabels = (labels: RPG.LabelRegistry): RPG.EnemyLables => {
+  return labels.rpg.enemy;
+};
