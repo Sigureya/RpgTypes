4194624f6fe09ccd8805a7e1d5d9e9e893c25f61
Sigureya <sigureya1208@gmail.com>
2025-04-17 02:25:46 +0900

    ラベル管理の型。名前＋要素の構造にする。これが正解かは知らない

--- dist/labelUtils/labelEntry.d.ts
+++ dist/labelUtils/labelEntry.d.ts
@@ -2,3 +2,7 @@ export type LabelEntry = {
     domainName: string;
 } & Record<string, string>;
 export type LabelDomains = Record<string, LabelEntry>;
+export interface DomainLabel<T extends Record<string, string>> {
+    domainName: string;
+    options: T;
+}
