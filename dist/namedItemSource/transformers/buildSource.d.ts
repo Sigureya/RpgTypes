80465f42701e4bdbe16ed278d3cfea8d6b9950e3
Sigureya <sigureya1208@gmail.com>
2025-04-18 15:42:32 +0900

    対応関数を追加して再ビルド

--- dist/namedItemSource/transformers/buildSource.d.ts
+++ dist/namedItemSource/transformers/buildSource.d.ts
@@ -1,8 +1,10 @@
-import { CollapsOptionLabels, GlobalLabel, NamedItemSource, PartyAbilityOptionLabels, RegularParamLabels, DomainLabel } from '../../schema';
-export declare const traitDomain: <T>(labels: DomainLabel<T>, sourceKey: string, fn: (options: T) => {
+import { CollapsOptionLabels, GlobalLabel, NamedItemSource, PartyAbilityOptionLabels, RegularParamLabels, DomainLabel, ExtraParamLabels, SpecialParamLabels } from '../../schema';
+export declare const traitDomain: <T>(sourceKey: string, labels: DomainLabel<T>, fn: (options: T) => {
     name: string;
     id: number;
 }[]) => NamedItemSource;
 export declare const buildCollapsSource: (labels: DomainLabel<CollapsOptionLabels>, global: Pick<GlobalLabel, "normal">) => NamedItemSource;
 export declare const buildPartyAbilitySource: (labels: DomainLabel<PartyAbilityOptionLabels>) => NamedItemSource;
 export declare const buildRegularParamSource: (label: DomainLabel<RegularParamLabels>) => NamedItemSource;
+export declare const buildExtraParamSource: (label: DomainLabel<ExtraParamLabels>) => NamedItemSource;
+export declare const buildSpecialParamSource: (label: DomainLabel<SpecialParamLabels>) => NamedItemSource;
