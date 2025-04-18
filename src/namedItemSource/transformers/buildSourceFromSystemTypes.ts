bcd3b3cc2ee473afc353ecaafc5ab386302a7e5f
Sigureya <sigureya1208@gmail.com>
2025-04-19 01:25:32 +0900

    関数を仮置き。systemLabelsが必要なことに気付いたので戻る

---
+++ src/namedItemSource/transformers/buildSourceFromSystemTypes.ts
@@ -0,0 +1,14 @@
+import type { Data_System } from "@RpgTypes/schema";
+import {
+  getArmorTypes,
+  getElementTypes,
+  getEquipTypes,
+  getParamNames,
+  getSkillTypes,
+  getVariableNames,
+  getWeaponTypes,
+} from "./data/system";
+
+const xx = () => {};
+
+const buildElementTypesSource = (system: Pick<Data_System, "elements">) => {};
