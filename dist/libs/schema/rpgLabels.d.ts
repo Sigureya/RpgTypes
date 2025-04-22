cdc97033fa3cc4796df5236774e76074f5700bf8
Sigureya <sigureya1208@gmail.com>
2025-04-23 02:21:48 +0900

    型定義だけ修正

--- dist/libs/schema/rpgLabels.d.ts
+++ dist/libs/schema/rpgLabels.d.ts
@@ -1,26 +1,6 @@
 import { DomainLabel } from '../templates';
-import { ActorLabels, ArmorLabels, ClassLabels, CollapsOptionLabels, DamageLabels, ItemEffectLabelSet, EnemyLabels, ExtraParamLabels, ItemLabels, PartyAbilityOptionLabels, RegularParamLabels, SkillLabels, SpecialFlagOptions, SpecialParamLabels, StateLabels, TraitLabelSet, UsableItemLabels, WeaponLabels } from './data';
-import { DataLabels } from './data/rpg/main/dataLabels';
+import { DamageLabels, DataLabels, ItemEffectLabelSet, TraitLabelSet } from './data';
 export interface RpgLabels {
-    paramSpecial: DomainLabel<SpecialParamLabels>;
-    paramExtra: DomainLabel<ExtraParamLabels>;
-    paramRegular: DomainLabel<RegularParamLabels>;
-    partyAbility: DomainLabel<PartyAbilityOptionLabels>;
-    collaps: DomainLabel<CollapsOptionLabels>;
-    specialFlag: DomainLabel<SpecialFlagOptions>;
-    actor: DomainLabel<ActorLabels>;
-    weapon: DomainLabel<WeaponLabels>;
-    armor: DomainLabel<ArmorLabels>;
-    class: DomainLabel<ClassLabels>;
-    state: DomainLabel<StateLabels>;
-    enemy: DomainLabel<EnemyLabels>;
-    usableItem: DomainLabel<UsableItemLabels>;
-    item: DomainLabel<ItemLabels>;
-    skill: DomainLabel<SkillLabels>;
-    damage: DomainLabel<DamageLabels>;
-    itemEffect: DomainLabel<ItemEffectLabelSet>;
-}
-export interface RpgLabels2 {
     data: DataLabels;
     traits: DomainLabel<TraitLabelSet>;
     itemEffect: DomainLabel<ItemEffectLabelSet>;
--- src/libs/schema/rpgLabels.ts
+++ src/libs/schema/rpgLabels.ts
@@ -1,49 +1,12 @@
 import type { DomainLabel } from "@RpgTypes/templates";
 import type {
-  ActorLabels,
-  ArmorLabels,
-  ClassLabels,
-  CollapsOptionLabels,
   DamageLabels,
+  DataLabels,
   ItemEffectLabelSet,
-  EnemyLabels,
-  ExtraParamLabels,
-  ItemLabels,
-  PartyAbilityOptionLabels,
-  RegularParamLabels,
-  SkillLabels,
-  SpecialFlagOptions,
-  SpecialParamLabels,
-  StateLabels,
-  Trait,
-  TraitLabel,
   TraitLabelSet,
-  UsableItemLabels,
-  WeaponLabels,
 } from "./data";
-import type { DataLabels } from "./data/rpg/main/dataLabels";
 
 export interface RpgLabels {
-  paramSpecial: DomainLabel<SpecialParamLabels>;
-  paramExtra: DomainLabel<ExtraParamLabels>;
-  paramRegular: DomainLabel<RegularParamLabels>;
-  partyAbility: DomainLabel<PartyAbilityOptionLabels>;
-  collaps: DomainLabel<CollapsOptionLabels>;
-  specialFlag: DomainLabel<SpecialFlagOptions>;
-  actor: DomainLabel<ActorLabels>;
-  weapon: DomainLabel<WeaponLabels>;
-  armor: DomainLabel<ArmorLabels>;
-  class: DomainLabel<ClassLabels>;
-  state: DomainLabel<StateLabels>;
-  enemy: DomainLabel<EnemyLabels>;
-  usableItem: DomainLabel<UsableItemLabels>;
-  item: DomainLabel<ItemLabels>;
-  skill: DomainLabel<SkillLabels>;
-  damage: DomainLabel<DamageLabels>;
-  itemEffect: DomainLabel<ItemEffectLabelSet>;
-}
-
-export interface RpgLabels2 {
   data: DataLabels;
   traits: DomainLabel<TraitLabelSet>;
   itemEffect: DomainLabel<ItemEffectLabelSet>;
