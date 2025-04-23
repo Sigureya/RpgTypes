9321fa6b44a297195a863d0349acbd9d9fe137c8
Sigureya <sigureya1208@gmail.com>
2025-04-23 19:11:56 +0900

    暫定的にテストコードを追加

--- src/libs/schema/data/rpg/main/traitContainers/trait/labelDefine.test.ts
+++ src/libs/schema/data/rpg/main/traitContainers/trait/labelDefine.test.ts
@@ -1,17 +1,44 @@
 import { describe, test, expect } from "vitest";
 
-import { defineTraitElementRate } from "./labelDefine";
+import * as DEF from "./labelDefine";
 import { TRAIT_ELEMENT_RATE } from "./constants";
-import { LABEL_SET_TRAIT } from "./options";
+import {
+  SRC_ELEMENTS,
+  type SourceIdentifier,
+} from "@RpgTypes/schema/namedItemSource";
+import { AUTHOR_RMMZ, MODULE_SYSTEM } from "@RpgTypes/namedItemSource";
 describe("", () => {
-  const result = defineTraitElementRate({});
+  const result = DEF.defineTraitElementRate({});
 
   test("", () => {
-    expect(result.label).toBeTypeOf("string");
+    const expected: SourceIdentifier = {
+      author: AUTHOR_RMMZ,
+      module: MODULE_SYSTEM,
+      sourceKey: SRC_ELEMENTS,
+    };
+    expect(result.dataSource).toEqual(expected);
   });
-  // test("", () => {
-  //   expect(result.label).toBe(LABELS_TRAIT_ELEMENT_RATE.domainName);
-  // });
+  test("", () => {
+    expect(result.code).toBe(TRAIT_ELEMENT_RATE);
+  });
+});
+describe("", () => {
+  const result = DEF.defineTraitElementRate({
+    domainName: "aaa",
+    format: "fmt",
+  });
+  test("", () => {
+    expect(result.format).toBe("fmt");
+    expect(result.label).toBe("aaa");
+  });
+  test("", () => {
+    const expected: SourceIdentifier = {
+      author: AUTHOR_RMMZ,
+      module: MODULE_SYSTEM,
+      sourceKey: SRC_ELEMENTS,
+    };
+    expect(result.dataSource).toEqual(expected);
+  });
   test("", () => {
     expect(result.code).toBe(TRAIT_ELEMENT_RATE);
   });
