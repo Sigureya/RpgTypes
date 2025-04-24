e4f75fcd082ce8d9d2285a535e25b4d1e4a3cf5e
Sigureya <sigureya1208@gmail.com>
2025-04-24 12:18:07 +0900

    定数用テスト。as constの設定方法を間違えるとundefinedが混入して事故るので、予め封じておく

---
+++ src/libs/schema/data/rpg/main/traitContainers/trait/options/default.test.ts
@@ -0,0 +1,16 @@
+import { describe, test, expect } from "vitest";
+import { LABEL_SET_TRAIT } from "./default";
+import type { TraitLabel } from "./types";
+
+describe("", () => {
+  Object.entries<TraitLabel>(LABEL_SET_TRAIT.options).forEach(
+    ([key, value]) => {
+      test(`${key} should have a domainName of type string`, () => {
+        expect(value.domainName).toBeTypeOf("string");
+      });
+      test(`${key} should have a format of type string`, () => {
+        expect(value.format).toBeTypeOf("string");
+      });
+    }
+  );
+});
