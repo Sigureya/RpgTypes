570362c97cd71b92561b2bf6734d55c4dbbef510
Sigureya <sigureya1208@gmail.com>
2025-04-24 16:38:41 +0900

    特殊フラグに対応

--- src/libs/schema/data/rpg/main/traitContainers/trait/options/makeOptions.ts
+++ src/libs/schema/data/rpg/main/traitContainers/trait/options/makeOptions.ts
@@ -1,16 +1,36 @@
 import type { Data_NamedItem, GlobalLabel } from "@RpgTypes/schema/";
+import {
+  FLAG_ID_GUARD,
+  FLAG_ID_AUTO_BATTLE,
+  FLAG_ID_PRESERVE_TP,
+  FLAG_ID_SUBSTITUTE,
+} from "./types/specialFlags";
 import type {
   CollapsOptionLabels,
   ExtraParamLabels,
   PartyAbilityOptionLabels,
   RegularParamLabels,
+  SpecialFlagOptions,
   SpecialParamLabels,
 } from "./types";
 import {
+  COLLAPS_NORMAL,
   COLLAPS_BOSS,
   COLLAPS_INSTANT,
   COLLAPS_NONE,
-  COLLAPS_NORMAL,
+} from "./types/collaps";
+
+import {
+  PARTY_ABILITY_CANCEL_SURPRISE,
+  PARTY_ABILITY_DROP_ITEM_DOUBLE,
+  PARTY_ABILITY_ENCOUNTER_HALF,
+  PARTY_ABILITY_ENCOUNTER_NONE,
+  PARTY_ABILITY_GOLD_DOUBLE,
+  PARTY_ABILITY_RAISE_PREEMPTIVE,
+} from "./types/partyAbility";
+
+import {
+  EXTRA_PARAM_TRG,
   EXTRA_PARAM_CEV,
   EXTRA_PARAM_CNT,
   EXTRA_PARAM_CRI,
@@ -20,21 +40,8 @@ import {
   EXTRA_PARAM_MEV,
   EXTRA_PARAM_MRF,
   EXTRA_PARAM_MRG,
-  EXTRA_PARAM_TRG,
-  PARTY_ABILITY_CANCEL_SURPRISE,
-  PARTY_ABILITY_DROP_ITEM_DOUBLE,
-  PARTY_ABILITY_ENCOUNTER_HALF,
-  PARTY_ABILITY_ENCOUNTER_NONE,
-  PARTY_ABILITY_GOLD_DOUBLE,
-  PARTY_ABILITY_RAISE_PREEMPTIVE,
-  REGULAR_PARAM_AGI,
-  REGULAR_PARAM_ATK,
-  REGULAR_PARAM_DEF,
-  REGULAR_PARAM_LUK,
-  REGULAR_PARAM_MATK,
-  REGULAR_PARAM_MAX_HP,
-  REGULAR_PARAM_MAX_MP,
-  REGULAR_PARAM_MDEF,
+} from "./types/paramExtra";
+import {
   SPECIAL_PARAM_EXR,
   SPECIAL_PARAM_FDR,
   SPECIAL_PARAM_GRD,
@@ -45,7 +52,17 @@ import {
   SPECIAL_PARAM_REC,
   SPECIAL_PARAM_TCR,
   SPECIAL_PARAM_TGR,
-} from "./types";
+} from "./types/paramSpecial";
+import {
+  REGULAR_PARAM_ATK,
+  REGULAR_PARAM_MAX_HP,
+  REGULAR_PARAM_MAX_MP,
+  REGULAR_PARAM_DEF,
+  REGULAR_PARAM_MATK,
+  REGULAR_PARAM_MDEF,
+  REGULAR_PARAM_AGI,
+  REGULAR_PARAM_LUK,
+} from "./types/paramRegular";
 
 export const foldCollapsOptions = (
   options: CollapsOptionLabels,
@@ -205,6 +222,29 @@ export const foldSpecialParams = (
     {
       id: SPECIAL_PARAM_EXR,
       name: specialParams.experienceRate,
+    },
+  ];
+};
+
+export const foldSpecialFlag = (
+  label: SpecialFlagOptions
+): Data_NamedItem[] => {
+  return [
+    {
+      id: FLAG_ID_AUTO_BATTLE,
+      name: label.autoBattle,
+    },
+    {
+      id: FLAG_ID_GUARD,
+      name: label.guard,
+    },
+    {
+      id: FLAG_ID_SUBSTITUTE,
+      name: label.substitute,
+    },
+    {
+      id: FLAG_ID_PRESERVE_TP,
+      name: label.preventEscape,
     },
   ];
 };
