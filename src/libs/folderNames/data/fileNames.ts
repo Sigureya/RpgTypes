b36dbc6cfa12bb73c0f3d5414bb479043c7e7c84
Sigureya <sigureya1208@gmail.com>
2025-04-27 20:56:36 +0900

    データ関連のファイル名

--- src/libs/folderNames/data/fileNames.ts
+++ src/libs/folderNames/data/fileNames.ts
@@ -1,1 +1,14 @@
 export const FILENAME_SYSTEM = "System.json";
+export const FILENAME_ACTORS = "Actors.json";
+export const FILANAME_CLASSES = "Classes.json";
+export const FILENAME_SKILLS = "Skills.json";
+export const FILENAME_ITEMS = "Items.json";
+export const FILENAME_WEAPONS = "Weapons.json";
+export const FILENAME_ARMORS = "Armors.json";
+export const FILENAME_ENEMIES = "Enemies.json";
+export const FILENAME_TROOPS = "Troops.json";
+export const FILENAME_STATES = "States.json";
+export const FILENAME_ANIMATIONS = "Animations.json";
+export const FILENAME_TILESET = "Tilesets.json";
+export const FILENAME_COMMON_EVENTS = "CommonEvents.json";
+export const FILENAME_MAP_INFOS = "MapInfos.json";
