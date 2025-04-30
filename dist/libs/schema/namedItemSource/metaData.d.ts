ce9b01043b070f4a83a7ea454a6c174e802578b7
Sigureya <sigureya1208@gmail.com>
2025-04-30 19:35:34 +0900

    makeEnemyの関数名修正

---
+++ dist/libs/schema/namedItemSource/metaData.d.ts
@@ -0,0 +1,8 @@
+import { SourceIdentifier } from './sourceIdentifier';
+export interface MetaDataA {
+    src: SourceIdentifier;
+    fileName: string;
+}
+export interface MetaDataB {
+    src: SourceIdentifier;
+}
