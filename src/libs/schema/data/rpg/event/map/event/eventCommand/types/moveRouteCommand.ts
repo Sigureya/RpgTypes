07a3f9d2e24e8524f1a9395608efab0e740d2fa8
Sigureya <sigureya1208@gmail.com>
2025-04-30 20:08:10 +0900

    移動ルートのあれこれ。AudiFileParamsを移動したので修正

--- src/libs/schema/data/rpg/event/map/event/eventCommand/types/moveRouteCommand.ts
+++ src/libs/schema/data/rpg/event/map/event/eventCommand/types/moveRouteCommand.ts
@@ -1,4 +1,4 @@
-import type { AudioFileParams } from "../../../../../../../utils/types/audioFileParams";
+import type { AudioFileParams } from "@RpgTypes/utils";
 import type * as MoveRouteConstants from "./domains/moveRoute/constants";
 import type { CommandTemplateSimple } from "./template/";
 
