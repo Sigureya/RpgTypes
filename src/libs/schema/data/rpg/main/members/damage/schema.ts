70884b72209a520b4f619e2e24b15f6fd1e54726
Sigureya <sigureya1208@gmail.com>
2025-05-03 18:24:57 +0900

    ダメージ用のschema定義

---
+++ src/libs/schema/data/rpg/main/members/damage/schema.ts
@@ -0,0 +1,13 @@
+import type { Damage } from "./damage";
+import type { JSONSchemaType } from "ajv";
+export const SCHEMA_DAMAGE = {
+  type: "object",
+  required: ["type", "elementId", "formula", "variance", "critical"],
+  properties: {
+    type: { type: "integer", minimum: 0 },
+    elementId: { type: "integer", minimum: 0 },
+    formula: { type: "string" },
+    variance: { type: "integer" },
+    critical: { type: "boolean" },
+  },
+} as const satisfies JSONSchemaType<Damage>;
