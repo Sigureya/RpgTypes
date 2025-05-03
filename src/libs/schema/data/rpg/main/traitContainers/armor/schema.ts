d080898143a7a433bbdc563c6423596f57daae4d
Sigureya <sigureya1208@gmail.com>
2025-05-03 19:21:07 +0900

    防具用のスキーマ

---
+++ src/libs/schema/data/rpg/main/traitContainers/armor/schema.ts
@@ -0,0 +1,56 @@
+import type { Data_Armor } from "./armor";
+import type { JSONSchemaType } from "ajv";
+
+export const SCHEMA_DATA_ARMMOR = {
+  type: "object",
+  required: [
+    "name",
+    "id",
+    "description",
+    "iconIndex",
+    "price",
+    "params",
+    "traits",
+    "note",
+    "etypeId",
+  ],
+
+  properties: {
+    name: { type: "string" },
+    id: { type: "integer", minimum: 0 },
+    description: { type: "string" },
+    iconIndex: { type: "integer", minimum: 0 },
+    price: { type: "integer", minimum: 0 },
+    note: { type: "string" },
+    etypeId: { type: "integer", minimum: 0 },
+
+    params: {
+      type: "array",
+      items: [
+        { type: "integer" }, // mhp
+        { type: "integer" }, // mmp
+        { type: "integer" }, // atk
+        { type: "integer" }, // def
+        { type: "integer" }, // mat
+        { type: "integer" }, // mdf
+        { type: "integer" }, // agi
+        { type: "integer" }, // luk
+      ],
+      minItems: 8,
+      maxItems: 8,
+    },
+
+    traits: {
+      type: "array",
+      items: {
+        type: "object",
+        properties: {
+          code: { type: "integer" },
+          dataId: { type: "integer" },
+          value: { type: "integer" },
+        },
+        required: ["code", "dataId", "value"],
+      },
+    },
+  },
+} as const satisfies JSONSchemaType<Data_Armor>;
