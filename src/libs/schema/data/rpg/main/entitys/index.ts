fad711bbbc63d42bea352b78341cc830769504b2
Sigureya <sigureya1208@gmail.com>
2025-05-04 12:00:30 +0900

    index追加。主にschema

--- src/libs/schema/data/rpg/main/entitys/index.ts
+++ src/libs/schema/data/rpg/main/entitys/index.ts
@@ -1,2 +1,3 @@
 export * from "./namedItem";
 export * from "./namedItemLabels";
+export * from "./goods";
--- src/libs/schema/data/rpg/main/traitContainers/actor/index.ts
+++ src/libs/schema/data/rpg/main/traitContainers/actor/index.ts
@@ -1,3 +1,4 @@
 export * from "./actor";
 export * from "./actorLabels";
 export * from "./make";
+export * from "./schema";
