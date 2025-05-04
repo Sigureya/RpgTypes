528041d3ec36f99c34ac14b637100bf618542b21
Sigureya <sigureya1208@gmail.com>
2025-05-04 12:36:58 +0900

    mapInfo用のschema。関連ファイルが増えたのでフォルダを移動している

---
+++ src/libs/schema/data/rpg/event/map/mapInfo/schema.ts
@@ -0,0 +1,24 @@
+import type { Data_MapInfo } from "./mapInfo";
+import type { JSONSchemaType } from "ajv";
+
+export const SCHEMA_DATA_MAP_INFO = {
+  type: "object",
+  required: [
+    "name",
+    "id",
+    "expanded",
+    "order",
+    "parentId",
+    "scrollX",
+    "scrollY",
+  ],
+  properties: {
+    name: { type: "string" },
+    id: { type: "integer", minimum: 0 },
+    expanded: { type: "boolean" },
+    order: { type: "integer" },
+    parentId: { type: "integer" },
+    scrollX: { type: "integer" },
+    scrollY: { type: "integer" },
+  },
+} as const satisfies JSONSchemaType<Data_MapInfo>;
