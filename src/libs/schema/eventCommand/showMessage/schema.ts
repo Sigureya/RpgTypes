a77046a824d5d1045ea0345471841797dc1c268a
Sigureya <sigureya1208@gmail.com>
2025-05-08 17:41:01 +0900

    イベントコマンドに専用フォルダを与えて再構築する。各種関数も用意していく

---
+++ src/libs/schema/eventCommand/showMessage/schema.ts
@@ -0,0 +1,26 @@
+import type { JSONSchemaType } from "ajv";
+import type { Command2_ShowMessage } from "./types";
+
+export const SHOW_MESSAGE = 101 as const;
+
+export const SCHEMA_COMMAND_SHOW_MESSAGE = {
+  type: "object",
+  required: ["code", "parameters", "indent"],
+  properties: {
+    code: { type: "integer", enum: [101] },
+    parameters: {
+      type: "array",
+      items: [
+        { type: "string" }, // facename
+        { type: "integer", minimum: 0 }, // faceIndex
+        { type: "integer", minimum: 0, maximum: 2 }, // background
+        { type: "integer", minimum: 0, maximum: 2 }, // positionType
+        { type: "string" }, // speakerName
+      ] as const,
+
+      minItems: 5,
+      maxItems: 5,
+    },
+    indent: { type: "integer", minimum: 0, maximum: 2 },
+  },
+} as const satisfies JSONSchemaType<Command2_ShowMessage>;
