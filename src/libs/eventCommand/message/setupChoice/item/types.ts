ddb6575de761bf95387841397965609587be3979
Sigureya <sigureya1208@gmail.com>
2025-05-12 00:16:06 +0900

    選択肢の表示関連の設定

---
+++ src/libs/eventCommand/message/setupChoice/item/types.ts
@@ -0,0 +1,10 @@
+import type { EventCommandLike2 } from "@RpgTypes/eventCommand/frame";
+
+export interface ParamObject_ShowChoiceItem {
+  index: number;
+  name: string;
+}
+
+export type ParamArray_ShowChoiceItem = [index: number, name: string];
+
+export interface Command2_ShowChoiceItem extends EventCommandLike2<402> {}
