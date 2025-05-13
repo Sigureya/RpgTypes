179cac6a1bf5936cf80093d9852b342d986eb879
Sigureya <sigureya1208@gmail.com>
2025-05-14 08:37:40 +0900

    イベントコマンド生成の関数群。audioが別インスタンス＆値が一致を判定することで、コピーを必須化している

---
+++ src/libs/eventCommand/commands/audio/make.test.ts
@@ -0,0 +1,79 @@
+import { describe, test, expect } from "vitest";
+import {
+  makeCommandAudioAny,
+  makeCommandChangeBattleBGM,
+  makeCommandChangeDefeatME,
+  makeCommandChangeVictoryME,
+  makeCommandPlayBGM,
+  makeCommandPlayBGS,
+  makeCommandPlayME,
+  makeCommandPlaySE,
+} from "./make";
+import {
+  CHANGE_BATTLE_BGM,
+  PLAY_BGM,
+  PLAY_BGS,
+  PLAY_ME,
+  PLAY_SE,
+} from "@RpgTypes/schema";
+
+describe("makeCommandAudioAny", () => {
+  test("should create a command with the correct code and parameters", () => {
+    const audio = { name: "test", volume: 90, pitch: 100, pan: 0 };
+    const command = makeCommandAudioAny(132, audio, 2);
+    expect(command.code).toBe(132);
+    expect(command.parameters).toEqual([audio]);
+    expect(command.parameters[0]).not.toBe(audio);
+    expect(command.indent).toBe(2);
+  });
+});
+
+describe("makeCommandPlayBGM", () => {
+  test("should create a Play BGM command", () => {
+    const audio = { name: "bgm_test", volume: 80, pitch: 100, pan: 0 };
+    const command = makeCommandPlayBGM(audio, 1);
+    expect(command.code).toBe(PLAY_BGM);
+    expect(command.parameters).toEqual([audio]);
+    expect(command.parameters[0]).not.toBe(audio);
+  });
+});
+
+describe("makeCommandPlayBGS", () => {
+  test("should create a Play BGS command", () => {
+    const audio = { name: "bgs_test", volume: 80, pitch: 100, pan: 0 };
+    const command = makeCommandPlayBGS(audio, 1);
+    expect(command.code).toBe(PLAY_BGS);
+    expect(command.parameters).toEqual([audio]);
+    expect(command.parameters[0]).not.toBe(audio);
+  });
+});
+
+describe("makeCommandPlayME", () => {
+  test("should create a Play ME command", () => {
+    const audio = { name: "me_test", volume: 80, pitch: 100, pan: 0 };
+    const command = makeCommandPlayME(audio, 1);
+    expect(command.code).toBe(PLAY_ME);
+    expect(command.parameters).toEqual([audio]);
+    expect(command.parameters[0]).not.toBe(audio);
+  });
+});
+
+describe("makeCommandPlaySE", () => {
+  test("should create a Play SE command", () => {
+    const audio = { name: "se_test", volume: 80, pitch: 100, pan: 0 };
+    const command = makeCommandPlaySE(audio, 1);
+    expect(command.code).toBe(PLAY_SE);
+    expect(command.parameters).toEqual([audio]);
+    expect(command.parameters[0]).not.toBe(audio);
+  });
+});
+
+describe("makeCommandChangeBattleBGM", () => {
+  test("should create a Change Battle BGM command", () => {
+    const audio = { name: "battle_bgm_test", volume: 80, pitch: 100, pan: 0 };
+    const command = makeCommandChangeBattleBGM(audio, 1);
+    expect(command.code).toBe(CHANGE_BATTLE_BGM);
+    expect(command.parameters).toEqual([audio]);
+    expect(command.parameters[0]).not.toBe(audio);
+  });
+});
