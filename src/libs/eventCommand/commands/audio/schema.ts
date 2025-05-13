302a23c15ddf06b804501743662fb82c8833b868
Sigureya <sigureya1208@gmail.com>
2025-05-14 07:49:46 +0900

    音声系イベントコマンドに対応したschema。引数が同じものがいくつかあるので、codeをenumにしてまとめてある

---
+++ src/libs/eventCommand/commands/audio/schema.ts
@@ -0,0 +1,43 @@
+import type { JSONSchemaType } from "ajv";
+import type { CommandUnion_AnyAudio } from "./types";
+import type { AudioFileParams } from "@RpgTypes/utils";
+
+export const SCHEMA_COMMAND_ANY_AUDIO = {
+  type: "object",
+  required: ["code", "parameters", "indent"],
+
+  properties: {
+    code: {
+      type: "integer",
+      enum: [
+        132, // ChangeBattleBGM
+        133, // ChangeVictoryME
+        139, // ChangeDefeatME
+        241, // PlayBGM
+        245, // PlayBGS
+        249, // PlayME
+        250, // PlaySE
+      ],
+    },
+    indent: { type: "integer" },
+    parameters: {
+      type: "array",
+      minItems: 1,
+      maxItems: 1,
+      items: [
+        {
+          type: "object",
+          properties: {
+            name: { type: "string" },
+            volume: { type: "integer" },
+            pitch: { type: "integer" },
+            pan: { type: "integer" },
+          },
+          required: ["name", "volume", "pitch", "pan"],
+          additionalProperties: false,
+        } satisfies JSONSchemaType<AudioFileParams>,
+      ],
+    },
+  },
+  additionalProperties: false,
+} as const satisfies JSONSchemaType<CommandUnion_AnyAudio>;
