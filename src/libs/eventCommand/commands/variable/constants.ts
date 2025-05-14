5b93f9f96ea2b89cbc44f2f84a73692b5915e329
Sigureya <sigureya1208@gmail.com>
2025-05-14 12:08:58 +0900

    オペランド種別定数。まあどこかに必要なので

---
+++ src/libs/eventCommand/commands/variable/constants.ts
@@ -0,0 +1,5 @@
+export const OPERAND_CONSTANT = 0 as const;
+export const OPERAND_VARIABLE = 1 as const;
+export const OPERAND_RANDOM = 2 as const;
+export const OPERAND_GAMEDATA = 3 as const;
+export const OPERAND_SCRIPT = 4 as const;
