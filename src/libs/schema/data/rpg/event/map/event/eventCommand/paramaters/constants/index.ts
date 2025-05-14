61fe26ffbed7afe445be9cbbf8aa8950d3312615
Sigureya <sigureya1208@gmail.com>
2025-05-14 14:01:33 +0900

    indexの更新。過渡期に付きカオスなpath指定の箇所あり

--- src/libs/schema/data/rpg/event/map/event/eventCommand/paramaters/constants/index.ts
+++ src/libs/schema/data/rpg/event/map/event/eventCommand/paramaters/constants/index.ts
@@ -1,5 +1,5 @@
 export * from "./valueOf";
 export * from "./constants";
-export * from "./controlVariables";
-export * from "./operand";
+export * from "../../../../../../../../../eventCommand/commands/variable/types/paramArray";
+export * from "../../../../../../../../../eventCommand/commands/variable/types/operand";
 export * from "./branch";
