feb10e1ddfa4c4092f9e2b31a56384e1d7ac99b2
Sigureya <sigureya1208@gmail.com>
2025-05-14 12:14:38 +0900

    パラメータ構築用のオブジェクト

---
+++ src/libs/eventCommand/commands/variable/types/paramObject.ts
@@ -0,0 +1,58 @@
+import type {
+  CHARACTER,
+  GAMEDATA,
+  LAST,
+  OPERAND,
+  OTHER,
+  STATUS,
+} from "./operand";
+import type { ValueOf } from "@RpgTypes/schema";
+
+export interface ParamObject_WritingTarget {
+  startId: number;
+  endId?: number;
+}
+
+export interface ParamObject_Operand_Constant {
+  value: number;
+}
+
+export interface ParamObject_Operand_Variable {
+  operand: 1;
+  variableId: number;
+}
+
+export interface ParamObject_Operand_Random {
+  min: number;
+  max: number;
+}
+export interface ParamObject_Operand_Script {
+  code: string;
+}
+export interface ParamObject_Operand_ItemData {
+  type: 0 | 1 | 2; // ITEM | WEAPON | ARMOR
+  itemId: number;
+}
+export interface ParamObject_Operand_StatusData {
+  type: 3 | 4; // ACTOR | ENEMY
+  index: number;
+  param: ValueOf<STATUS>;
+}
+export interface ParamObject_Operand_CharacterData {
+  type: 5; // CHARACTER
+  id: number;
+  param: number; // ValueOf<CHARACTER>
+}
+
+export interface ParamObject_Operand_PartyData {
+  type: 6; // PARTY
+  index: number;
+}
+export interface ParamObject_Operand_OtherData {
+  type: 7; // OTHER
+  param: number; // ValueOf<OTHER>
+}
+export interface ParamObject_Operand_LastData {
+  type: 8; // LAST
+  param: keyof LAST;
+}
