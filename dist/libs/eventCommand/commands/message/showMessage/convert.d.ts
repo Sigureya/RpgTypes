008fe8d59d5d90a59baf943fbd8e66c3e6c28fe1
Sigureya <sigureya1208@gmail.com>
2025-05-15 13:40:36 +0900

    面倒なオブジェクトを消した。import関連のトラブル原因になるので

--- dist/libs/eventCommand/commands/message/showMessage/convert.d.ts
+++ dist/libs/eventCommand/commands/message/showMessage/convert.d.ts
@@ -1,15 +1,5 @@
 import { Command_ShowMessageHeader, Command2_ShowMessageBody, ParamArray_ShowMessage, ParamObject_ShowMessage } from './types';
 export declare const makeCommandShowMessage: (param: Partial<ParamObject_ShowMessage> | undefined, indent?: number) => Command_ShowMessageHeader;
 export declare const fromArrayShowMessageHeader: (array: ParamArray_ShowMessage) => ParamObject_ShowMessage;
+export declare const toArrayShowMessageHeader: (object: ParamObject_ShowMessage) => ParamArray_ShowMessage;
 export declare const makeCommandShowMessageBody: (text: string, indent?: number) => Command2_ShowMessageBody;
-export declare const CommandShowMessage: {
-    readonly fromArray: (array: ParamArray_ShowMessage) => {
-        facename: string;
-        faceIndex: number;
-        background: number;
-        positionType: number;
-        speakerName: string;
-    };
-    readonly toArray: (object: ParamObject_ShowMessage) => [string, number, number, number, string];
-    readonly makeCommand: (param: Partial<ParamObject_ShowMessage> | undefined, indent?: number | undefined) => Command_ShowMessageHeader;
-};
