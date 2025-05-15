9817f30ecb35c5255efaad6fb01b4db67ecf7f34
Sigureya <sigureya1208@gmail.com>
2025-05-15 14:22:23 +0900

    型指定が逆になっていたので修正

--- src/libs/eventCommand/commands/message/scrollText/types/body.ts
+++ src/libs/eventCommand/commands/message/scrollText/types/body.ts
@@ -1,9 +1,9 @@
 import type { EventCommandLike2 } from "@RpgTypes/eventCommand/frame";
 
-export type ParamObject_ScrollTextBody = [content: string];
-export interface ParamArray_ScrollTextBody extends ParamObject_ScrollTextBody {
+export type ParamArray_ScrollTextBody = [content: string];
+export interface ParamObject_ScrollTextBody {
   content: string;
 }
 export interface Command2_ScrollTextBody extends EventCommandLike2<405> {
-  parameters: ParamObject_ScrollTextBody;
+  parameters: ParamArray_ScrollTextBody;
 }
