3b5b207e30651ffc52cf15acdef7082861a817e9
Sigureya <sigureya1208@gmail.com>
2025-05-16 03:03:19 +0900

    現在のビルド結果を反映

--- dist/libs/eventCommand/commands/message/comment/make.d.ts
+++ dist/libs/eventCommand/commands/message/comment/make.d.ts
@@ -1,6 +1,6 @@
-import { ParamArray_Comment, ParamObject_Comment, Command2_CommentHeader, Command2_CommentBody } from './types';
+import { ParamArray_Comment, ParamObject_Comment, Command_CommentHeader, Command2_CommentBody } from './types';
 export declare const makeCommentArray: (comment: string) => ParamArray_Comment;
 export declare const convertCommentArrayToObject: (paramArray: ParamArray_Comment) => ParamObject_Comment;
-export declare const makeCommandCommentHeader: (comment: string, indent?: number) => Command2_CommentHeader;
+export declare const makeCommandCommentHeader: (comment: string, indent?: number) => Command_CommentHeader;
 export declare const makeCommandCommentBody: (comment: string, indent?: number) => Command2_CommentBody;
-export declare const makeCommentCommandArray: (comments: ReadonlyArray<string>, indent?: number) => [Command2_CommentHeader, ...Command2_CommentBody[]];
+export declare const makeCommentCommandArray: (comments: ReadonlyArray<string>, indent?: number) => [Command_CommentHeader, ...Command2_CommentBody[]];
--- dist/libs/eventCommand/validateTextBody.d.ts
+++ dist/libs/eventCommand/validateTextBody.d.ts
@@ -1,7 +1,7 @@
-import { Command2_ShowMessageBody, Command2_CommentBody, Command2_CommentHeader, Command2_ScrollTextBody, Command2_ScriptHeader, Command2_ScriptBody } from './commands';
+import { Command2_ShowMessageBody, Command2_CommentBody, Command_CommentHeader, Command2_ScrollTextBody, Command2_ScriptHeader, Command2_ScriptBody } from './commands';
 export declare const isCommandShowMessageBody: (command: unknown) => command is Command2_ShowMessageBody;
 export declare const isCommandShowScrollingTextBody: (command: unknown) => command is Command2_ScrollTextBody;
+export declare const isCommandCommentHeader: (command: unknown) => command is Command_CommentHeader;
 export declare const isCommandCommentBody: (command: unknown) => command is Command2_CommentBody;
-export declare const isCommandCommentHeader: (command: unknown) => command is Command2_CommentHeader;
-export declare const isCommandScriptBody: (command: unknown) => command is Command2_ScriptBody;
 export declare const isCommandScriptHeader: (command: unknown) => command is Command2_ScriptHeader;
+export declare const isCommandScriptBody: (command: unknown) => command is Command2_ScriptBody;
