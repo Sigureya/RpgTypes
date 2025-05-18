1daaaaf97a92a7623082b77bbab62e3e9ecb61e2
Sigureya <sigureya1208@gmail.com>
2025-05-18 13:29:59 +0900

    新型の移動ルート定義。複雑すぎて大変なので、型解釈を簡略化している

---
+++ src/libs/schema/data/rpg/event/map/event/eventCommand/moveRoute/schema.ts
@@ -0,0 +1,42 @@
+import type { JSONSchemaType } from "ajv";
+import type { MoveRouteDataUnkwnown } from "./moveRoute";
+import type { AudioFileParams } from "@RpgTypes/utils";
+
+export const SCHEMA_MOVEROUTE_DATA = {
+  type: "object",
+  required: ["wait", "repeat", "skippable", "list"],
+  properties: {
+    wait: { type: "boolean" },
+    repeat: { type: "boolean" },
+    skippable: { type: "boolean" },
+    list: {
+      type: "array",
+      items: {
+        type: "object",
+        required: ["code", "parameters"],
+        properties: {
+          code: { type: "integer" },
+          parameters: {
+            type: "array",
+            items: {
+              oneOf: [
+                { type: "string" },
+                { type: "number" },
+                {
+                  type: "object",
+                  required: ["name", "pan", "pitch", "volume"],
+                  properties: {
+                    name: { type: "string" },
+                    pan: { type: "integer" },
+                    pitch: { type: "integer" },
+                    volume: { type: "integer" },
+                  },
+                } satisfies JSONSchemaType<AudioFileParams>,
+              ],
+            },
+          },
+        },
+      },
+    },
+  },
+} as const satisfies JSONSchemaType<MoveRouteDataUnkwnown>;
