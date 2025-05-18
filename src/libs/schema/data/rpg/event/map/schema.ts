a796e178d6b84b626da0c75cf90e6542179edacc
Sigureya <sigureya1208@gmail.com>
2025-05-18 15:25:34 +0900

    スキーマ定義の修正。イベントコマンドと無関係な場所だけでも完成させる

--- src/libs/schema/data/rpg/event/map/schema.ts
+++ src/libs/schema/data/rpg/event/map/schema.ts
@@ -2,6 +2,69 @@ import type { JSONSchemaType } from "ajv";
 import type { Data_Map } from "./map";
 import type { Encounter } from "./members";
 import type { AudioFileParams } from "@RpgTypes/utils";
+import type {
+  MapEvent,
+  MapEvent_Image,
+  MapEvent_PageCondition,
+  MapEventPage,
+} from "./event";
+import { SCHEMA_MOVEROUTE_DATA } from "./event/eventCommand/moveRoute/schema";
+import type { MoveRouteDataUnkwnown } from "./event/eventCommand/moveRoute";
+import { SCHEMA_MAP_EVENT_PAGE_CONDITION } from "./event/schema";
+
+export const SCHEMA_MAP_EVENT_IMAGE = {
+  type: "object",
+  required: [
+    "characterIndex",
+    "characterName",
+    "direction",
+    "pattern",
+    "tileId",
+  ],
+  properties: {
+    characterIndex: { type: "integer", minimum: 0 },
+    characterName: { type: "string" },
+    direction: { type: "integer", enum: [2, 4, 6, 8] },
+    pattern: { type: "integer" },
+    tileId: { type: "integer" },
+  },
+} as const satisfies JSONSchemaType<MapEvent_Image>;
+
+export const SCHEMA_MAP_EVENT_PAGE = {
+  type: "object",
+  required: [
+    "image",
+    "moveRoute",
+    "conditions",
+    "directionFix",
+    "moveFrequency",
+  ] satisfies (keyof MapEventPage)[],
+  properties: {
+    moveFrequency: { type: "integer" },
+    directionFix: { type: "boolean" },
+    conditions:
+      SCHEMA_MAP_EVENT_PAGE_CONDITION satisfies JSONSchemaType<MapEvent_PageCondition>,
+
+    image: SCHEMA_MAP_EVENT_IMAGE satisfies JSONSchemaType<MapEvent_Image>,
+    moveRoute:
+      SCHEMA_MOVEROUTE_DATA satisfies JSONSchemaType<MoveRouteDataUnkwnown>,
+    // list: {
+    //   type: "array",
+    //   items: {
+    //     type: "object",
+    //     required: ["code", "indent", "parameters"],
+    //     properties: {
+    //       code: { type: "integer" },
+    //       indent: { type: "integer" },
+    //       parameters: {
+    //         type: "array",
+    //         items: {},
+    //       },
+    //     },
+    //   },
+    // },
+  },
+} as const satisfies JSONSchemaType<Omit<MapEventPage, "list">>;
 
 export const SCHEMA_DATA_MAP = {
   type: "object",
@@ -33,8 +96,8 @@ export const SCHEMA_DATA_MAP = {
     battleback1Name: { type: "string" },
     battleback2Name: { type: "string" },
 
-    parallaxLoopY: { type: "integer" },
-    parallaxLoopX: { type: "integer" },
+    parallaxLoopY: { type: "boolean" },
+    parallaxLoopX: { type: "boolean" },
     parallaxName: { type: "string" },
     parallaxShow: { type: "boolean" },
     parallaxSy: { type: "integer" },
@@ -85,5 +148,30 @@ export const SCHEMA_DATA_MAP = {
         },
       } satisfies JSONSchemaType<Encounter>,
     } satisfies JSONSchemaType<Encounter[]>,
-  },
-} as const;
+    events: {
+      type: "array",
+      items: {
+        type: "object",
+        required: [
+          "id",
+          "name",
+          "x",
+          "y",
+          "pages",
+        ] satisfies (keyof MapEvent)[],
+        properties: {
+          id: { type: "integer" },
+          name: { type: "string" },
+          x: { type: "integer" },
+          y: { type: "integer" },
+          note: { type: "string" },
+          pages: {
+            type: "array",
+            items: SCHEMA_MAP_EVENT_PAGE,
+          },
+        },
+      } as const,
+    },
+  }, // satisfies Record<keyof Data_Map, unknown>,
+} as const satisfies JSONSchemaType<Omit<Data_Map, "events">>;
+// evetntsはコモンイベントが入ってて複雑。簡略化して対応する
