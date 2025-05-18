0646994ad1809cc99046644b2bf018abcbdb7c25
MashiroCruz <mashiro.cruz@gmail.com>
2025-05-18 21:50:08 +0900

    微修正

--- src/libs/eventCommand/commands/controlSwtches/types.ts
+++ src/libs/eventCommand/commands/controlSwtches/types.ts
@@ -1,9 +1,17 @@
-import { EventCommandLike, ValueOf, Toggle } from "@RpgTypes/schema";
+import type { EventCommandLike, ValueOf, Toggle } from "@RpgTypes/schema";
 
 export interface Command_ControlSwitches extends EventCommandLike<121> {
   parameters: ParamArray_ControlSwitches;
 }
 
-export type ParamArray_ControlSwitches = [min: number, max: number, value: ValueOf<Toggle>]
+export type ParamArray_ControlSwitches = [
+  min: number,
+  max: number,
+  value: ValueOf<Toggle>
+];
 
-export interface Paramobject_ControlSwitches {min: number, max: number, value: ValueOf<Toggle>}
+export interface Paramobject_ControlSwitches {
+  min: number;
+  max: number;
+  value: ValueOf<Toggle>;
+}
