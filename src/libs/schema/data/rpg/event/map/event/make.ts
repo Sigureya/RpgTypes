92bd36a9511d050fab79bd626c5e3a0c51b54c18
Sigureya <sigureya1208@gmail.com>
2025-05-18 15:39:58 +0900

    イベント出現条件。ラベルなども必要なので、個別フォルダへ分割していく

--- src/libs/schema/data/rpg/event/map/event/make.ts
+++ src/libs/schema/data/rpg/event/map/event/make.ts
@@ -1,4 +1,4 @@
-import type { MapEvent_PageCondition, PageConditionArg } from "./condition";
+import { makeEventPageCondition } from "./condition";
 import type { MapEvent } from "./mapEvent";
 import type { MapEvent_Image, MapEventPage } from "./page";
 
@@ -20,26 +20,6 @@ export const makeMapEventIamge = (): MapEvent_Image => {
     direction: 2,
     pattern: 0,
     tileId: 0,
-  };
-};
-
-export const makeEventPageCondition = (
-  arg: Partial<PageConditionArg> = {}
-): MapEvent_PageCondition => {
-  return {
-    switch1Id: arg.switch1Id ?? 0,
-    switch1Valid: arg.switch1Id !== undefined,
-    switch2Id: arg.switch2Id ?? 0,
-    switch2Valid: arg.switch2Id !== undefined,
-    variableId: arg.variableId ?? 0,
-    variableValid: arg.variableId !== undefined,
-    variableValue: arg.variableValue ?? 0,
-    selfSwitchCh: arg.selfSwitchCh ?? "A",
-    selfSwitchValid: arg.selfSwitchCh !== undefined,
-    itemId: arg.itemId ?? 0,
-    itemValid: arg.itemId !== undefined,
-    actorId: arg.actorId ?? 0,
-    actorValid: arg.actorId !== undefined,
   };
 };
 
