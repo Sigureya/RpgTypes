116e9f1b62592fdb850e21a4a745c6cd834ce217
Sigureya <sigureya1208@gmail.com>
2025-05-20 05:02:31 +0900

    バリデーション関数と定義などを並び替え。対応をわかりやすくした

--- src/libs/eventCommand/validate.ts
+++ src/libs/eventCommand/validate.ts
@@ -23,29 +23,22 @@ import {
   SCHEMA_COMMAND_SHOW_CHOICES,
 } from "./commands/message/setupChoice/schema";
 import { SCHEMA_COMMAND_SHOW_MESSAGE } from "./commands/message/showMessage/schema";
+import type {
+  CommandUnion_EmptyParam,
+  CommandUnion_TextBody,
+} from "./unionTypes";
 
 const ajv = new Ajv();
 // schemaはindex.ts無しで直接importすること！
 // 過去に循環参照エラーで苦しんでます
+
+// 各Schemaはtype以外はimportしないこと
+// 定数の解決に失敗してエラーになることがある
 const changeActorText = ajv.compile(SCHEMA_COMMAND_CHANGE_ACTOR_TEXT);
 export const isCommandChangeActorText = (
   data: unknown
 ): data is CommandUnion_ChangeActorText => {
   return changeActorText(data);
-};
-const inputNumber = ajv.compile(SCHEMA_COMMAND_INPUT_NUMBER);
-const commonVent = ajv.compile(SCHEMA_COMMAND_CALL_COMMON_EVENT);
-
-const showChoiceItem = ajv.compile(SCHEMA_COMMAND_SHOW_CHOICE_WHEN);
-const showMessage = ajv.compile(SCHEMA_COMMAND_SHOW_MESSAGE);
-
-const showChoices = ajv.compile(SCHEMA_COMMAND_SHOW_CHOICES);
-
-const audioCommand = ajv.compile(SCHEMA_COMMAND_ANY_AUDIO);
-export const isCommandAudio = (
-  data: unknown
-): data is CommandUnion_AnyAudio => {
-  return audioCommand(data);
 };
 
 const scrollTextHead = ajv.compile(SCHEMA_COMMAND_SCROLL_TEXT_HEAD);
@@ -55,43 +48,56 @@ export const isCommandScrollTextHead = (
   return scrollTextHead(data);
 };
 
-// const emptyParam = ajv.compile(SCHEMA_COMMAND_EMPTY_PARAM);
+const audioCommand = ajv.compile(SCHEMA_COMMAND_ANY_AUDIO);
+export const isCommandAudio = (
+  data: unknown
+): data is CommandUnion_AnyAudio => {
+  return audioCommand(data);
+};
 
-// export const isCommandNonParam = (data: unknown) =>
-//   // : data is CommandUnion_EmptyParam
-//   {
-//     return emptyParam(data);
-//   };
+const emptyParam = ajv.compile(SCHEMA_COMMAND_EMPTY_PARAM);
+export const isCommandNonParam = (
+  data: unknown
+): data is CommandUnion_EmptyParam => {
+  return emptyParam(data);
+};
+
 const textBody = ajv.compile(SCHEMA_COMMAND_TEXT_BODY);
-export const isCommandTextBody = (command: unknown) =>
-  // : command is CommandUnion_TextBody
-  {
-    return textBody(command);
-  };
+export const isCommandTextBody = (
+  command: unknown
+): command is CommandUnion_TextBody => {
+  return textBody(command);
+};
 
+const commonVent = ajv.compile(SCHEMA_COMMAND_CALL_COMMON_EVENT);
 export const isCommandCommonEvent = (
   data: unknown
 ): data is Command_CommonEvent => {
   return commonVent(data);
 };
 
+const showChoices = ajv.compile(SCHEMA_COMMAND_SHOW_CHOICES);
 export const isCommandShowChoices = (
   data: unknown
 ): data is Command_ShowChoices => {
   return showChoices(data);
 };
+
+const showChoiceItem = ajv.compile(SCHEMA_COMMAND_SHOW_CHOICE_WHEN);
 export const isCommandShowChoiceItem = (
   data: unknown
 ): data is Command_ShowChoiceWhen => {
   return showChoiceItem(data);
 };
 
+const inputNumber = ajv.compile(SCHEMA_COMMAND_INPUT_NUMBER);
 export const isCommandInputNumber = (
   data: unknown
 ): data is Command_InputNumber => {
   return inputNumber(data);
 };
 
+const showMessage = ajv.compile(SCHEMA_COMMAND_SHOW_MESSAGE);
 export const isCommandShowMessage = (
   data: unknown
 ): data is Command_ShowMessageHeader => {
