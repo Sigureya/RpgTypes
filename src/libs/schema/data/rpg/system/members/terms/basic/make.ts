bdbcf3dc54842a387fd8f6440b838bf9fe1eaaf5
Sigureya <sigureya1208@gmail.com>
2025-05-20 08:46:16 +0900

    初期化用の設定を変更

--- src/libs/schema/data/rpg/system/members/terms/basic/make.ts
+++ src/libs/schema/data/rpg/system/members/terms/basic/make.ts
@@ -4,15 +4,15 @@ export const makeTermsBasic = (
   terms: Partial<Terms_Basic>
 ): Terms_BasicArray => {
   return [
-    terms.level || "Level",
-    terms.levelA || "Level",
-    terms.hp || "HP",
-    terms.hpA || "HP",
-    terms.mp || "MP",
-    terms.mpA || "MP",
-    terms.tp || "TP",
-    terms.tpA || "TP",
-    terms.experience || "EXP",
-    terms.exp || "EXP",
+    terms.level ?? "Level",
+    terms.levelA ?? "Level",
+    terms.hp ?? "HP",
+    terms.hpA ?? "HP",
+    terms.mp ?? "MP",
+    terms.mpA ?? "MP",
+    terms.tp ?? "TP",
+    terms.tpA ?? "TP",
+    terms.experience ?? "EXP",
+    terms.exp ?? "EXP",
   ];
 };
