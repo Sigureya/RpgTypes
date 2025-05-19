507abc0a31bfa7ddfff0f52d98310791a58e2ecf
Sigureya <sigureya1208@gmail.com>
2025-05-19 18:45:32 +0900

    オプションデータの作成関数

--- src/libs/schema/data/rpg/system/make.ts
+++ src/libs/schema/data/rpg/system/make.ts
@@ -1,25 +1,58 @@
 import { makeAudioFileParams } from "@RpgTypes/utils";
+import type { System_Advanced, System_SoundsObject } from "./members";
 import { makeSoundsArray, makeSystemAdvanced } from "./members";
 import { makeVehicleData } from "./members/vehicle/make";
-import type { Data_System, System_BoolanOptions } from "./system";
+import type {
+  Data_System,
+  System_Bgm,
+  System_BooleanOptions,
+  System_DataNames,
+  System_Debug,
+  System_Text,
+  System_Vehicle,
+} from "./system";
+import type { EditorSettings } from "./setting";
 
-export const makeSystemData = (
-  options: Partial<System_BoolanOptions>
-): Partial<Data_System> => {
+interface XXX {
+  options: Partial<System_BooleanOptions>;
+  advanced: Partial<System_Advanced>;
+  vehicles: Partial<System_Vehicle>;
+  editing: Partial<EditorSettings>;
+  dataNames: Partial<System_DataNames>;
+  bgm: Partial<System_Bgm>;
+  texts: Partial<System_Text>;
+  sounds: Partial<System_SoundsObject>;
+  debug: Partial<System_Debug>;
+}
+
+const cloneArray = <T>(array?: T[]) => {
+  return array ? [...array] : [];
+};
+
+export const makeSystemData = (p: Partial<XXX>): Partial<Data_System> => {
+  const options = p.options ?? {};
+  const vehicles = p.vehicles ?? {};
+
+  const dataNames = p.dataNames ?? {};
+  const debug = p.debug ?? {};
   return {
+    armorTypes: cloneArray(dataNames.armorTypes),
+    elements: cloneArray(dataNames.elements),
+    equipTypes: cloneArray(dataNames.equipTypes),
+    weaponTypes: cloneArray(dataNames.weaponTypes),
+    skillTypes: cloneArray(dataNames.skillTypes),
+    switches: cloneArray(dataNames.switches),
+    variables: cloneArray(dataNames.variables),
     advanced: makeSystemAdvanced(),
-    airship: makeVehicleData(),
-    armorTypes: [],
     battleback1Name: "",
     battleback2Name: "",
-    battlerName: "",
-    boat: makeVehicleData(),
-    ship: makeVehicleData(),
+    battlerName: debug.battlerName ?? "",
+    airship: makeVehicleData(vehicles.airship),
+    boat: makeVehicleData(vehicles.boat),
+    ship: makeVehicleData(vehicles.ship),
     defeatMe: makeAudioFileParams(),
-    currencyUnit: "",
-    elements: [],
-    equipTypes: [],
-    gameTitle: "",
+    currencyUnit: p.texts?.currencyUnit ?? "",
+    gameTitle: p.texts?.gameTitle ?? "",
     gameoverMe: makeAudioFileParams(),
     optAutosave: options.optAutosave ?? true,
     optDisplayTp: options.optDisplayTp ?? true,
@@ -32,27 +65,23 @@ export const makeSystemData = (
     optSlipDeath: options.optSlipDeath ?? true,
     optSplashScreen: options.optSplashScreen ?? true,
     optTransparent: options.optTransparent ?? true,
-    optMessageSkip: true,
-    skillTypes: [],
-    sounds: makeSoundsArray({}),
-    switches: [],
+    optMessageSkip: options.optMessageSkip ?? true,
+    sounds: makeSoundsArray(p.sounds ?? {}),
     title1Name: "",
     title2Name: "",
     titleBgm: makeAudioFileParams(),
     tileSize: 48,
-    variables: [],
     versionId: 1,
     attackMotions: [],
-    testBattlers: [],
+    testBattlers: debug.testBattlers ?? [],
     battleBgm: makeAudioFileParams(),
     victoryMe: makeAudioFileParams(),
     editMapId: 0,
-    local: "",
+    locale: "",
     startX: 0,
     startY: 0,
     testTroopId: 0,
     windowTone: [0, 0, 0, 0],
-    weaponTypes: [],
     startMapId: 1,
   };
 };
