523448b5119d7dbba4df3e2feb2eeb467e501d7f
Sigureya <sigureya1208@gmail.com>
2025-05-20 00:38:50 +0900

    パラメータ清々巻子を追加

--- src/libs/schema/data/rpg/system/members/terms/systemTerms.ts
+++ src/libs/schema/data/rpg/system/members/terms/systemTerms.ts
@@ -21,3 +21,16 @@ export type Terms_ParamNamesArray = [
   hit: string,
   eva: string
 ];
+
+export interface Terms_ParamNames {
+  mhp: string;
+  mmp: string;
+  atk: string;
+  def: string;
+  mat: string;
+  mdf: string;
+  agi: string;
+  luk: string;
+  hit: string;
+  eva: string;
+}
