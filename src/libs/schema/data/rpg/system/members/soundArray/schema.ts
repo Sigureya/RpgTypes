a3c436bb010fc8fe18f21961bc51dd514145dc2c
Sigureya <sigureya1208@gmail.com>
2025-05-20 08:20:14 +0900

    schemaのミスを修正。要素数が間違っていた

--- src/libs/schema/data/rpg/system/members/soundArray/schema.ts
+++ src/libs/schema/data/rpg/system/members/soundArray/schema.ts
@@ -4,15 +4,15 @@ import type { JSONSchemaType } from "ajv";
 export const SCHEMA_SYSTEM_SOUND_ARRAY = {
   additionalProperties: false,
   type: "array",
-  maxItems: 22,
-  minItems: 22,
+  maxItems: 24,
+  minItems: 24,
   items: {
     type: "object",
     properties: {
       name: { type: "string" },
-      volume: { type: "integer", minimum: 0 },
-      pitch: { type: "integer", minimum: 50, maximum: 150 },
-      pan: { type: "integer", default: 0 },
+      volume: { type: "integer" },
+      pitch: { type: "integer" },
+      pan: { type: "integer" },
     },
     required: ["name"],
     additionalProperties: false,
