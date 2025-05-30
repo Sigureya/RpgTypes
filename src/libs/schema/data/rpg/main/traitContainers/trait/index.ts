9843410a531161dfbf49e95aa8d3c480bac5692c
Sigureya <sigureya1208@gmail.com>
2025-05-30 14:23:44 +0900

    exportを再編。一部の古い関数は今後廃止するので、exportを消した

--- src/libs/schema/data/rpg/main/traitContainers/trait/index.ts
+++ src/libs/schema/data/rpg/main/traitContainers/trait/index.ts
@@ -1,6 +1,4 @@
 export * from "./constants";
 export * from "./options";
-export { resolveTraitLabels } from "./labelDefine";
-export * from "./traitLabelDescriptor";
 export * from "./types";
 export { formatTraitText } from "./format";
