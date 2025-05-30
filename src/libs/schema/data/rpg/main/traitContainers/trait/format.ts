09d323c2a2e668c93e56706b3d7d9c3fad7acc11
Sigureya <sigureya1208@gmail.com>
2025-05-30 14:18:20 +0900

    型修正。引数を間違えないように、traitDescriptorで固める

--- src/libs/schema/data/rpg/main/traitContainers/trait/format.ts
+++ src/libs/schema/data/rpg/main/traitContainers/trait/format.ts
@@ -1,16 +1,16 @@
 import type { Data_NamedItem } from "@RpgTypes/schema";
-import type { Trait } from "./types";
+import type { Trait, TraitDescriptor } from "./types";
 
 import { FORMAT_PARAM_VALUE, FORMAT_NAME } from "./options/default";
 
 export const formatTraitText = (
-  format: string,
+  descriptor: TraitDescriptor,
   trait: Trait,
   items: Data_NamedItem[]
-) => {
+): string => {
   const item = items[trait.dataId];
   const itemName: string = item ? item.name : `?data[${trait.dataId}]`;
-  return format
+  return descriptor.format
     .replaceAll(FORMAT_PARAM_VALUE, trait.value.toString())
     .replaceAll(FORMAT_NAME, itemName);
 };
