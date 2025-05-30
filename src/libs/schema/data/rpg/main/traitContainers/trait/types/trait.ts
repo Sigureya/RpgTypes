3fba7c4211dd36fbd2bca7efc8b358101dc4c2f2
Sigureya <sigureya1208@gmail.com>
2025-05-30 14:15:27 +0900

    trait関連の型を1ファイルに統合。新たに特徴を定義するためのdescriptor型を作った

--- src/libs/schema/data/rpg/main/traitContainers/trait/types/trait.ts
+++ src/libs/schema/data/rpg/main/traitContainers/trait/types/trait.ts
@@ -1,5 +1,19 @@
+import type { SourceIdentifier } from "@RpgTypes/schema/namedItemSource";
+
 export interface Trait {
   code: number;
   dataId: number;
   value: number;
+}
+
+export interface Data_TraitContainer {
+  traits: Trait[];
+  note: string;
+}
+
+export interface TraitDescriptor {
+  format: string;
+  code: number;
+  label: string;
+  dataSource: SourceIdentifier;
 }
