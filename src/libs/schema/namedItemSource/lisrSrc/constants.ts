cade9c6c8eeb5185c419f35325745fcdbd4deb5d
Sigureya <sigureya1208@gmail.com>
2025-06-02 01:06:29 +0900

    system関連への移行を完了したので削除

--- src/libs/schema/namedItemSource/lisrSrc/constants.ts
+++ src/libs/schema/namedItemSource/lisrSrc/constants.ts
@@ -1,10 +1,1 @@
 export const SRC_COLOR = "colors" as const;
-
-export const SRC_WEAPON_TYPES = "weaponTypes" as const;
-export const SRC_ARMOR_TYPES = "armorTypes" as const;
-export const SRC_EQUIP_TYPES = "equipTypes" as const;
-export const SRC_SKILL_TYPES = "skillTypes" as const;
-export const SRC_ELEMENTS = "elements" as const;
-
-export const SRC_VARIABLE = "variables" as const;
-export const SRC_SWITCHES = "switches" as const;
