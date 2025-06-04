e8b75527cc621253d8a64fb5e78530f80286d121
Sigureya <sigureya1208@gmail.com>
2025-06-04 17:23:31 +0900

    余計な要素を消す処理。少なくとも、この関数の現在の用途では不要なデータなので

--- src/libs/namedItemSource/format/namedItemMap.ts
+++ src/libs/namedItemSource/format/namedItemMap.ts
@@ -1,11 +1,20 @@
 import { joinSoruceId } from "./sourceId";
-import type { NamedItemSource } from "./types";
+import type { Data_NamedItem, NamedItemSource } from "./types";
 
 export const mappingNamedItems = (
   list: ReadonlyArray<NamedItemSource>
 ): Map<string, NamedItemSource> => {
   return list.reduce<Map<string, NamedItemSource>>((map, item) => {
-    map.set(joinSoruceId(item.source), item);
+    const newItem: NamedItemSource = {
+      items: cloneItems(item.items), // 余計な情報の削除を兼ねてコピー
+      source: item.source,
+      label: item.label,
+    };
+    map.set(joinSoruceId(item.source), newItem);
     return map;
   }, new Map());
+};
+
+const cloneItems = (list: ReadonlyArray<Data_NamedItem>): Data_NamedItem[] => {
+  return list.map<Data_NamedItem>((item) => ({ id: item.id, name: item.name }));
 };
