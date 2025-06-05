f45e2e0351f5a53d2b871dc634f0d454c074a767
Sigureya <sigureya1208@gmail.com>
2025-06-05 15:10:35 +0900

    numberやstringのシンプルなパラメータに限定。それ以外は個別対応すること

--- src/libs/namedItemSource/format/types/rule/types.ts
+++ src/libs/namedItemSource/format/types/rule/types.ts
@@ -1,6 +1,8 @@
+import type { PickByType } from "src/rpg";
+
 export interface FormatRule<T> {
   itemNamePlaceHolder?: string;
-  placeHolders: Extract<keyof T, string>[];
+  placeHolders: Extract<keyof PickByType<T, number | string>, string>[];
 }
 
 export interface FormatRuleCompiled<T> {
