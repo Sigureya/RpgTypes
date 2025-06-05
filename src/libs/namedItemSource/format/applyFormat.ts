dbc8d571b06bf1f8d1b5ca58878cebdf8a50abd0
Sigureya <sigureya1208@gmail.com>
2025-06-05 18:16:53 +0900

    コンパイル済みルールを使うことを基本にした。以前の実装は次のコミットで消す

--- src/libs/namedItemSource/format/applyFormat.ts
+++ src/libs/namedItemSource/format/applyFormat.ts
@@ -3,6 +3,7 @@ import type {
   FinalFormatEntry,
   FormatResult,
   FormatRule,
+  FormatRuleCompiled,
   FormatWithSource,
 } from "./types";
 import type { FormatLookupKeys } from "./types/accessor";
@@ -11,7 +12,7 @@ import { compileFormatRule, execFormatRule } from "./types/rule/rule";
 
 export const formatUsingItemSourceMap = <Key, T>(
   data: T,
-  rule: FormatRule<T>,
+  rule: FormatRuleCompiled<T>,
   sourceMap: Map<Key, FinalFormatEntry>,
   fallback: FinalFormatEntry,
   lookup: FormatLookupKeys<T, Key>
@@ -21,23 +22,35 @@ export const formatUsingItemSourceMap = <Key, T>(
 
   return {
     label: entry.label,
-    text: applyFormatRule(data, entry.data ?? [], rule, entry, (d) =>
+    text: applyFormatRule2(data, entry.data, rule, entry, (d) =>
       lookup.extractDataId(d)
     ),
   };
 };
 
+export const applyFormatRule2 = <T>(
+  data: T,
+  list: ReadonlyArray<Data_NamedItem> | undefined,
+  rule: FormatRuleCompiled<T>,
+  format: FormatWithSource,
+  getDataId: (data: T) => number
+) => {
+  const text: string = execFormatRule(format.format, data, rule);
+  return list
+    ? text.replaceAll(
+        rule.itemName.placeHolder,
+        makeItemName(list, getDataId(data))
+      )
+    : text;
+};
+
 export const applyFormatRule = <T>(
   data: T,
-  list: ReadonlyArray<Data_NamedItem>,
+  list: ReadonlyArray<Data_NamedItem> | undefined,
   rule: FormatRule<T>,
   format: FormatWithSource,
   getDataId: (data: T) => number
 ): string => {
   const compiledRule = compileFormatRule(rule);
-  const itemName: string = makeItemName(list, getDataId(data));
-  return execFormatRule(format.format, data, compiledRule).replaceAll(
-    compiledRule.itemName.placeHolder,
-    itemName
-  );
+  return applyFormatRule2(data, list, compiledRule, format, getDataId);
 };
