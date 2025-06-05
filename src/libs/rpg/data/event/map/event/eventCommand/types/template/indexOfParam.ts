e7dd407c79848799ca0bbb0253b40e05c3294993
Sigureya <sigureya1208@gmail.com>
2025-06-05 16:01:04 +0900

    import修正

--- src/libs/rpg/data/event/map/event/eventCommand/types/template/indexOfParam.ts
+++ src/libs/rpg/data/event/map/event/eventCommand/types/template/indexOfParam.ts
@@ -1,7 +1,4 @@
-import type {
-  AsRecord,
-  PickByType,
-} from "../../../../../../../../templates/pickByType";
+import type { AsRecord, PickByType } from "@RpgTypes/templates";
 
 export type IndexOfParameter<T extends unknown[], V> = AsNumber<
   keyof PickByType<AsRecord<T>, V>
