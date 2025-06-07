a7c0b119995dda3939a48372831833131c82ec10
Sigureya <sigureya1208@gmail.com>
2025-06-08 02:03:10 +0900

    古いパラメータを削除。扱える型が狭くて不便だったので、新型へ切り替える

--- src/libs/namedItemSource/format/core/rule/types/rule.ts
+++ src/libs/namedItemSource/format/core/rule/types/rule.ts
@@ -1,24 +1,17 @@
-import type { PickByTypeKeys } from "@RpgTypes/templates";
 import type {
   FormatItemMapper,
   FormatItemMapperCompiled,
 } from "./arrayWithKind";
-import type {
-  FormatPlaceholder,
-  FormatProperties,
-  FormatPropertiesCompiled,
-} from "./direct";
+import type { FormatProperties, FormatPropertiesCompiled } from "./direct";
 
 export interface FormatRule<T> {
   itemMapper?: FormatItemMapper<T>;
-  placeHolders: PickByTypeKeys<T, number>[];
   itemMappers?: FormatItemMapper<T>[];
   fallbackFormat?: string;
   placeHolder2?: FormatProperties<T>;
 }
 
 export interface FormatRuleCompiled<T> {
-  properties: FormatPlaceholder<T, number>[];
   itemMappers: FormatItemMapperCompiled<T>[];
   fallbackFormat: string;
   properties2: FormatPropertiesCompiled<T>;
