690ceeb0420f90c9a47c3cacd37297fd840e3319
Sigureya <sigureya1208@gmail.com>
2025-06-09 03:01:32 +0900

    trait関連のSourceId。新しい指定方法を試している

---
+++ src/libs/rpg/data/main/traitContainers/trait/sourceIdTypes.ts
@@ -0,0 +1,16 @@
+import type { SourceIdentifierBase } from "src/namedItemSource";
+
+export interface SourceID_Trait<Kind extends string>
+  extends SourceIdentifierBase<{
+    author: "rmmz";
+    module: "trait";
+  }> {
+  kind: Kind;
+}
+
+export type SourceId_TraitRegularParam = SourceID_Trait<"params">;
+export type SourceId_TraitExtraParam = SourceID_Trait<"xparams">;
+export type SourceId_TraitSpecialParam = SourceID_Trait<"sparams">;
+export type SourceId_TraitCollaps = SourceID_Trait<"collaps">;
+export type SourceId_TraitPartyAbility = SourceID_Trait<"partyAbility">;
+export type SourceId_TraitSpecialFlag = SourceID_Trait<"sflag">;
