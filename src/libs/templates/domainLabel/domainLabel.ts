0a67cb4ad2f197e3019e3aacc5fe983328f7c008
Sigureya <sigureya1208@gmail.com>
2025-06-09 12:23:09 +0900

    塊の中からdomainNameを取り出す関数。用途は狭いが役に立つはず

---
+++ src/libs/templates/domainLabel/domainLabel.ts
@@ -0,0 +1,9 @@
+import type { DomainName } from "./types";
+
+export const domainNames = <T extends Record<string, DomainName>>(
+  record: T
+): string[] => {
+  return Object.entries<DomainName>(record).map(
+    ([, value]) => value.domainName
+  );
+};
