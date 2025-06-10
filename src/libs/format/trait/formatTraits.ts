76bedabc262626b76a14ba1bd1ce454a6b1d688f
Sigureya <sigureya1208@gmail.com>
2025-06-10 13:18:53 +0900

    Traitで使うアイテム一覧と、別途定義されたラベル群を突合するための関数。一番上の方なのでどうしても長い

--- src/libs/format/trait/formatTraits.ts
+++ src/libs/format/trait/formatTraits.ts
@@ -1,48 +1,51 @@
-import { mergeItemsSource } from "@RpgTypes/namedItemSource/format/mergeItemsSource";
-import { defineGameDataSources } from "@RpgTypes/rpg/data/gameData";
-import type { GameData } from "@RpgTypes/rpg/data/gameDataTypes";
 import type { Data_System } from "@RpgTypes/system";
-import type { System_DataNames } from "@RpgTypes/system/core/dataTypes";
-import type {
-  NamedItemSource,
-  FormatCompiled,
-  FormatLabelResolved,
+import {
+  type System_DataNames,
+  defineSystemItems,
+} from "@RpgTypes/system/core/dataTypes";
+import {
+  type NamedItemSource,
+  type FormatCompiled,
+  mergeItemsSource,
 } from "src/namedItemSource";
 import type {
   DataLabels,
-  TraitLabelSet,
+  GameData,
   NormalLabel,
-  TraitFormat,
+  RawTraitLabel,
+  TraitLabelSet,
 } from "src/rpg";
-import { resolveTraitLabels, defineTraitItems } from "src/rpg";
+import {
+  resolveTraitLabels,
+  defineTraitItems,
+  defineGameDataSources,
+} from "src/rpg";
 
 export const defineTraitSources = (
   gameData: GameData,
   gameDataLabels: DataLabels,
   traitLabels: TraitLabelSet,
   global: NormalLabel,
-  system: Data_System,
-  label: Record<keyof System_DataNames, string>
+  system: System_DataNames,
+  label: Record<keyof System_DataNames, RawTraitLabel>
 ): NamedItemSource[] => {
   return [
     ...defineTraitItems(traitLabels, global),
     ...defineGameDataSources(gameData, gameDataLabels),
-    //   ...defineSystemItems(system, label),
+    ...defineSystemItems(system, label),
   ];
 };
 
 export const mergeTraitSource = (
   labels: TraitLabelSet,
   dataLabel: DataLabels,
-  items: GameData
+  items: GameData,
+  normalLabel: NormalLabel,
+  system: Data_System,
+  label: Record<keyof System_DataNames, RawTraitLabel>
 ): Map<number, FormatCompiled> => {
-  const list: FormatLabelResolved<number>[] = resolveTraitLabels(
-    labels
-  ) satisfies TraitFormat[];
-
   return mergeItemsSource(
-    list,
-    []
-    // defineTraitSources(items, dataLabel, labels, { normal: "" })
+    resolveTraitLabels(labels),
+    defineTraitSources(items, dataLabel, labels, normalLabel, system, label)
   );
 };
