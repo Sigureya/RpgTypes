38160e09872bc53ce66d48476e51659777603865
Sigureya <sigureya1208@gmail.com>
2025-06-12 22:18:28 +0900

    冗長な記述を修正

--- src/libs/system/core/gameInitial/gameInitial.ts
+++ src/libs/system/core/gameInitial/gameInitial.ts
@@ -2,13 +2,11 @@ import type { System_GameInitial } from "./types";
 
 export const makeGameInitial = (
   gameInitial: Partial<System_GameInitial> = {}
-): System_GameInitial => {
-  return {
-    startMapId: gameInitial.startMapId ?? 0,
-    startX: gameInitial.startX ?? 0,
-    startY: gameInitial.startY ?? 0,
-    partyMembersArray: gameInitial.partyMembersArray
-      ? Array.from(gameInitial.partyMembersArray)
-      : [1],
-  } satisfies System_GameInitial;
-};
+): System_GameInitial => ({
+  startMapId: gameInitial.startMapId ?? 0,
+  startX: gameInitial.startX ?? 0,
+  startY: gameInitial.startY ?? 0,
+  partyMembersArray: gameInitial.partyMembersArray
+    ? Array.from(gameInitial.partyMembersArray)
+    : [1],
+});
