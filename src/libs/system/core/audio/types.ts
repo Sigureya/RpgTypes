ee56901ab9f5a90f49dc08c2d3e4adff761da56f
Sigureya <sigureya1208@gmail.com>
2025-06-13 02:54:44 +0900

    音声系を統合して扱う型を作成。固定長配列はJSONSchemaで問題が出るので対応を放棄

--- src/libs/system/core/audio/types.ts
+++ src/libs/system/core/audio/types.ts
@@ -1,4 +1,5 @@
 import type { AudioFileParams } from "@RpgTypes/utils";
+import type { System_SoundsArray } from "./soundArray";
 
 export interface System_Me {
   gameoverMe: AudioFileParams;
@@ -10,4 +11,10 @@ export interface System_Bgm {
   titleBgm: AudioFileParams;
   battleBgm: AudioFileParams;
 }
-export type System_AudioFiles = System_Bgm & System_Me;
+
+export interface System_Sounds {
+  sounds: System_SoundsArray;
+}
+
+export type System_AudioFiles = System_Bgm &
+  System_Me & { [K in keyof System_Sounds]: AudioFileParams[] };
