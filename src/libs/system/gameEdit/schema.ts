ca23726259aa317d0de45b7b2791fa2ab95e6758
Sigureya <sigureya1208@gmail.com>
2025-06-13 01:08:54 +0900

    エディター関連のデータをまとめたSchema

---
+++ src/libs/system/gameEdit/schema.ts
@@ -0,0 +1,19 @@
+import type { JSONSchemaType } from "ajv";
+import { SCHEMA_SYSTEM_EDITOR_SETTINGS } from "./editor/schema";
+import type { System_GameEditorRMMZ } from "./gameEditorType";
+import type { TestBattler } from "./testPlay";
+import { SCHEMA_SYSTEM_TEST_BATTLER } from "./testPlay/schema";
+
+export const SCHEMA_SYSTEM_GAME_EDITOR_RMMZ = {
+  additionalProperties: false,
+  required: ["testBattlers", "testTroopId", "editor"],
+  type: "object",
+  properties: {
+    testBattlers: {
+      type: "array",
+      items: SCHEMA_SYSTEM_TEST_BATTLER,
+    } satisfies JSONSchemaType<TestBattler[]>,
+    testTroopId: { type: "number" },
+    editor: SCHEMA_SYSTEM_EDITOR_SETTINGS,
+  },
+} as const satisfies JSONSchemaType<System_GameEditorRMMZ>;
