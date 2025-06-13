7b51a8a2804c6d833706aa3ab334abb1a5fc7a51
Sigureya <sigureya1208@gmail.com>
2025-06-14 05:37:46 +0900

    要素を再編。型・関数・Schemaの組み合わせで作る

---
+++ src/rmmz/plugin/schema/types/primitive/boolean/types.ts
@@ -0,0 +1,19 @@
+import type { AnnotationBase } from "../annotationBase";
+export interface BooleanArg extends AnnotationBase {
+  default: boolean;
+  type: "boolean";
+  on?: string;
+  off?: string;
+}
+
+export interface MetaParam_Boolean {
+  on: string;
+  off: string;
+}
+
+export interface X_MetaParam_Boolean {
+  on?: string;
+  off?: string;
+  kind: "boolean";
+  parent?: string;
+}
