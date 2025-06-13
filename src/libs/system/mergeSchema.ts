9bae6b629e44a03b7497dbca03032c1659f9403d
Sigureya <sigureya1208@gmail.com>
2025-06-13 16:15:42 +0900

    mergeSystemSchema()の型チェックを厳密にした。構造上、内部岳だけで完結している

--- src/libs/system/mergeSchema.ts
+++ src/libs/system/mergeSchema.ts
@@ -1,11 +1,13 @@
 import type { Data_System } from "./system";
 
-export type PartialSystemSchema = {
+export interface PartialSystemSchema {
   required: ReadonlyArray<keyof Data_System>;
-  properties: object;
-};
+  properties: Record<string, object>;
+  additionalProperties: false;
+  type: "object";
+}
 
-export const mergeSchema = (list: ReadonlyArray<PartialSystemSchema>) => {
+export const mergeSystemSchema = (list: ReadonlyArray<PartialSystemSchema>) => {
   const reducedList = reduce(list);
   return {
     additionalProperties: false,
@@ -15,14 +17,13 @@ export const mergeSchema = (list: ReadonlyArray<PartialSystemSchema>) => {
   };
 };
 
-const reduce = (
-  list: ReadonlyArray<PartialSystemSchema>
-): PartialSystemSchema => {
-  return list.reduce<PartialSystemSchema>(
+const reduce = (list: ReadonlyArray<PartialSystemSchema>) => {
+  type Type = Pick<PartialSystemSchema, "required" | "properties">;
+  return list.reduce<Type>(
     (acc, item) => ({
       required: [...acc.required, ...item.required],
       properties: { ...acc.properties, ...item.properties },
     }),
-    { required: [], properties: {} } satisfies PartialSystemSchema
+    { required: [], properties: {} }
   );
 };
