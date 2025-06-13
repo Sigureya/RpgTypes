17d248a5cb74b71e6a31bf33b1f6c0bf4b348389
Sigureya <sigureya1208@gmail.com>
2025-06-13 19:00:48 +0900

    データ取得関数を整理。武器種別とパラメータを分離

--- src/libs/system/core/terms/index.ts
+++ src/libs/system/core/terms/index.ts
@@ -1,6 +1,6 @@
 export * from "./basic";
 export * from "./commands";
 export * from "./messages";
-export * from "./terms";
 export * from "./types";
 export { SCHEMA_SYSTEM_TERMS_BUNDLE } from "./schema";
+export { getParamNames, makeParamNamesArray } from "./terms";
