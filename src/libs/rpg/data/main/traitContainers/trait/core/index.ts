92a7fcd33f9fe74c5ea0192b54fdd5328854a78d
Sigureya <sigureya1208@gmail.com>
2025-06-14 01:16:37 +0900

    ファイルを移動。パス解決をシンプルにするために上へ移動した

--- src/libs/rpg/data/main/traitContainers/trait/core/index.ts
+++ src/libs/rpg/data/main/traitContainers/trait/core/index.ts
@@ -4,4 +4,3 @@ export * from "./labelSet";
 export * from "./types";
 export { LABEL_SET_TRAIT } from "./default";
 export { defineTraitItems } from "./namedItems";
-export { resolveTraitLabels } from "./resolveLabels";
