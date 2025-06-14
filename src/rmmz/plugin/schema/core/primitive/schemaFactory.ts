3b7cc947fb8ab7836b8ef2b8d222bd730bf38677
Sigureya <sigureya1208@gmail.com>
2025-06-15 02:18:21 +0900

    詳細未定だがSchemaのFactoryを構築する関数。動けば良し

--- src/rmmz/plugin/schema/core/primitive/schemaFactory.ts
+++ src/rmmz/plugin/schema/core/primitive/schemaFactory.ts
@@ -1,7 +1,21 @@
-import type { JSONSchemaType } from "ajv";
+import { Ajv, type JSONSchemaType } from "ajv";
 import type { Schema } from "jsonschema";
 import type { BooleanArg } from "./boolean";
 import { booleanMetaParam } from "./boolean";
+import { X_RMMZ_PARAM_NUMBER } from "./keyConstants";
+import { metaSchemaNumberRmmzParam } from "./numbers/numbers";
+
+const makeAjv = () => {
+  return new Ajv({
+    strict: true,
+    keywords: [
+      {
+        keyword: X_RMMZ_PARAM_NUMBER,
+        metaSchema: metaSchemaNumberRmmzParam(),
+      },
+    ],
+  });
+};
 
 export const rmmzParamToSchemaBoolean = (boolArg: BooleanArg) => {
   return {
