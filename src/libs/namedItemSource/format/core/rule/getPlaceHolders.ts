09811497c416e5f5a38b4affa953f72f73e8fdd1
Sigureya <sigureya1208@gmail.com>
2025-06-15 17:29:02 +0900

    事前に計画していたワンライナーに切り替え

--- src/libs/namedItemSource/format/core/rule/getPlaceHolders.ts
+++ src/libs/namedItemSource/format/core/rule/getPlaceHolders.ts
@@ -1,6 +1,8 @@
 import type { FormatItemMapper, FormatRule } from "./core";
 
-const getDataKeys2 = <T>(rule: FormatRule<T>): Set<string & keyof T> => {
+export const getDataKeysFromFormatRule = <T>(
+  rule: FormatRule<T>
+): Set<string & keyof T> => {
   return new Set<string & keyof T>([
     ...(rule.placeHolder?.numbers ?? []),
     ...(rule.placeHolder?.strings ?? []),
@@ -9,28 +11,12 @@ const getDataKeys2 = <T>(rule: FormatRule<T>): Set<string & keyof T> => {
     ...(rule.itemMapper ? [rule.itemMapper.kindKey] : []),
   ]);
 };
-
 export const getPlaceHolderKeys = <T>(rule: FormatRule<T>): Set<string> => {
   return new Set<string>([
     ...(rule.placeHolder?.numbers ?? []),
     ...(rule.itemMapper ? [rule.itemMapper.placeHolder] : []),
     ...(rule.itemMappers?.map((mapper) => mapper.placeHolder) ?? []),
   ]);
-};
-
-export const getDataKeysFromFormatRule = <T>(
-  rule: FormatRule<T>
-): Set<string & keyof T> => {
-  const set = new Set<string & keyof T>(rule.placeHolder?.numbers ?? []);
-  if (rule.itemMapper) {
-    set.add(rule.itemMapper.kindKey);
-  }
-  if (rule.itemMappers) {
-    rule.itemMappers.forEach((mapper) => {
-      set.add(mapper.kindKey);
-    });
-  }
-  return set;
 };
 
 export const getItemMappersFromRule = <T>(
