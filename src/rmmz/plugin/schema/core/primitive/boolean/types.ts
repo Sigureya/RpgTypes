e77eaff3b88ece92d871c81b0a3d2089fa946f6a
Sigureya <sigureya1208@gmail.com>
2025-06-16 00:48:15 +0900

    古いbooelanArgを追放

--- src/rmmz/plugin/schema/core/primitive/boolean/types.ts
+++ src/rmmz/plugin/schema/core/primitive/boolean/types.ts
@@ -1,11 +1,3 @@
-import type { AnnotationBase } from "../annotationBase";
-export interface BooleanArg extends AnnotationBase {
-  default: boolean;
-  type: "boolean";
-  on?: string;
-  off?: string;
-}
-
 export interface MetaParam_Boolean {
   on: string;
   off: string;
