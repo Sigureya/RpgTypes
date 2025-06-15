4dbc93465f2557caf270e6da823138b30396411f
Sigureya <sigureya1208@gmail.com>
2025-06-15 17:20:56 +0900

    対応データ型を追加

--- src/rmmz/plugin/schema/core/primitive/createAjv.ts
+++ src/rmmz/plugin/schema/core/primitive/createAjv.ts
@@ -1,29 +1,39 @@
+import type { Options, Vocabulary } from "ajv";
 import Ajv from "ajv";
 import { metaSchemaBooleanRmmzParam } from "./boolean";
 import {
   X_RMMZ_PARAM_SHARED,
   X_RMMZ_PARAM_NUMBER,
   X_RMMZ_PARAM_BOOLEAN,
+  X_RMMZ_PARAM_DATA_INDEX,
 } from "./keyConstants";
 import { metaSchemaSharedParam } from "./metaTextField";
 import { metaSchemaNumberRmmzParam } from "./numbers";
+import { metaSchemaDataIdParam } from "./rpgDataId";
 
-export const createAjv = () => {
+export const createAjv = (options?: Options) => {
   return new Ajv({
+    ...options,
     strict: true,
-    keywords: [
-      {
-        keyword: X_RMMZ_PARAM_SHARED,
-        metaSchema: metaSchemaSharedParam(),
-      },
-      {
-        keyword: X_RMMZ_PARAM_NUMBER,
-        metaSchema: metaSchemaNumberRmmzParam(),
-      },
-      {
-        keyword: X_RMMZ_PARAM_BOOLEAN,
-        metaSchema: metaSchemaBooleanRmmzParam(),
-      },
-    ],
+    keywords: keywords(),
   });
 };
+
+const keywords = (): Vocabulary => [
+  {
+    keyword: X_RMMZ_PARAM_SHARED,
+    metaSchema: metaSchemaSharedParam(),
+  },
+  {
+    keyword: X_RMMZ_PARAM_NUMBER,
+    metaSchema: metaSchemaNumberRmmzParam(),
+  },
+  {
+    keyword: X_RMMZ_PARAM_BOOLEAN,
+    metaSchema: metaSchemaBooleanRmmzParam(),
+  },
+  {
+    keyword: X_RMMZ_PARAM_DATA_INDEX,
+    metaSchema: metaSchemaDataIdParam(),
+  },
+];
