fd17793bb4c226f23d6aac97b04d6f42c4656c03
Sigureya <sigureya1208@gmail.com>
2025-06-17 17:37:52 +0900

    関数の並び順が変わっただけ

--- dist/libs/namedItemSource/format/core/rule/getPlaceHolders.d.ts
+++ dist/libs/namedItemSource/format/core/rule/getPlaceHolders.d.ts
@@ -1,4 +1,4 @@
 import { FormatItemMapper, FormatRule } from './core';
-export declare const getPlaceHolderKeys: <T>(rule: FormatRule<T>) => Set<string>;
 export declare const getDataKeysFromFormatRule: <T>(rule: FormatRule<T>) => Set<string & keyof T>;
+export declare const getPlaceHolderKeys: <T>(rule: FormatRule<T>) => Set<string>;
 export declare const getItemMappersFromRule: <T>(rule: FormatRule<T>) => ReadonlyArray<FormatItemMapper<T>>;
