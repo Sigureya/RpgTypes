23c0da3c5f60d0c4231b44fb58c41fc760222067
Sigureya <sigureya1208@gmail.com>
2025-06-18 04:28:50 +0900

    体裁を整えるための最小限のテストコード

--- src/rmmz/plugin/schema/core/strcut.test.ts
+++ src/rmmz/plugin/schema/core/strcut.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "vitest";
-import type { JSONSchemaType } from "ajv";
+import { Ajv, type JSONSchemaType } from "ajv";
 import type { StructAnnotation } from "./struct";
 interface Person {
   name: string;
@@ -135,4 +135,13 @@ const personSchemaJson: JSONSchemaType<MockType> = {
   ],
   additionalProperties: false,
 };
-test.skip("", () => {});
+describe("", () => {
+  test("", () => {
+    const ajv = new Ajv();
+    const validate = ajv.compile(personSchemaJson);
+    test("StructAnnotation JSON Schema Validation", () => {
+      const valid = validate(paramAnt.default);
+      expect(valid).toBe(true);
+    });
+  });
+});
