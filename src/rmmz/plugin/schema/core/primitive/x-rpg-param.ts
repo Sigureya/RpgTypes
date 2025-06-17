3faf7bde78f730a62d24aa70c871b0af849c1742
Sigureya <sigureya1208@gmail.com>
2025-06-17 17:28:19 +0900

    差し替え用に新しい型を作成

--- src/rmmz/plugin/schema/core/primitive/x-rpg-param.ts
+++ src/rmmz/plugin/schema/core/primitive/x-rpg-param.ts
@@ -7,6 +7,11 @@ import type {
   ParamSchemaCompiled,
 } from "./x-rpg-param2/types";
 
+export type X_ParamInput_Union =
+  | X_Param_BooleanInput
+  | X_Param_NumberInput
+  | X_Param_DataIdInput;
+
 export type X_Param_DataId = X_RmmzParam<SourceIdentifier, "dataId">;
 export type X_Param_DataIdInput = X_RmmzParamInput<SourceIdentifier, "dataId">;
 
@@ -17,6 +22,10 @@ export type X_Param_NumberInput = X_RmmzParamInput<
 >;
 
 export type X_Param_Boolean = X_RmmzParam<X_MetaParamCore_Boolean, "boolean">;
+export type X_Param_BooleanInput = X_RmmzParamInput<
+  X_MetaParamCore_Boolean,
+  "boolean"
+>;
 export type X_Param_String = X_RmmzParam<{}, "string" | "multiline_string">;
 
 export type NumberParamSchema = ParamSchemaCompiled<
