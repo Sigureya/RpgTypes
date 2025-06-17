11fd6f7fc829e85afc58f8e47248d8426a4881d8
Sigureya <sigureya1208@gmail.com>
2025-06-18 04:45:19 +0900

    この後の用途は未定だけど残敵的に配置

--- src/rmmz/plugin/schema/core/primitive/kindAll.ts
+++ src/rmmz/plugin/schema/core/primitive/kindAll.ts
@@ -1,7 +1,7 @@
 import type { DataKindUnion } from "./rpgDataId";
 
 export type KindAll = DataKindUnion;
-interface KindInfo {
-  type: string;
-}
-type TypeOfKind<T> = "";
+// interface KindInfo {
+//   type: string;
+// }
+// type TypeOfKind<T> = "";
