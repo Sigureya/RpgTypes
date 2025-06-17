b51236ff9d81b1e40d959cae2890e7eb91c4661e
Sigureya <sigureya1208@gmail.com>
2025-06-17 15:59:45 +0900

    入力用の不完全な型・実行可能なコンパイル済みの型を区別。これはFormatRuleと同様の構築

--- src/rmmz/plugin/schema/core/primitive/x-rpg-param.ts
+++ src/rmmz/plugin/schema/core/primitive/x-rpg-param.ts
@@ -1,12 +1,20 @@
 import type { JSONSchemaType } from "ajv";
 import type { SourceIdentifier } from "src/namedItemSource";
+import type { X_MetaParamCore_Boolean } from "./boolean";
+import type { X_MetaParamCore_Number } from "./numbers";
 
 export const X_RPG_PARM = "x-rpg-param" as const;
-export type ParamSchema<Value, X> = JSONSchemaType<Value> & {
+export type ParamSchemaCompiled<Value, X> = JSONSchemaType<Value> & {
   "x-rpg-param": X_RmmzParam<Required<X>>;
   description: string;
   title: string;
 };
+
+export interface X_RmmzParamInput<T, Kind extends string = string> {
+  parent?: string | null;
+  kind: Kind;
+  data: T;
+}
 
 export interface X_RmmzParam<T, Kind extends string = string> {
   parent: string;
@@ -15,8 +23,14 @@ export interface X_RmmzParam<T, Kind extends string = string> {
 }
 
 export type X_Param_DataId = X_RmmzParam<SourceIdentifier, "dataId">;
+export type X_Param_Number = X_RmmzParam<X_MetaParamCore_Number, "number">;
+export type X_Param_Boolean = X_RmmzParam<X_MetaParamCore_Boolean, "boolean">;
+export type X_Param_String = X_RmmzParam<{}, "string" | "multiline_string">;
 
-export type NumberParamSchema = ParamSchema<number, { digit: number }> & {
+export type NumberParamSchema = ParamSchemaCompiled<
+  number,
+  X_MetaParamCore_Number
+> & {
   maximum: number;
   minimum: number;
 };
