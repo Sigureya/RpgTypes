d35f7e9194f68cb395757ebb921b3831d072ba70
Sigureya <sigureya1208@gmail.com>
2025-06-17 15:09:35 +0900

    boolean型のテストを分割

--- src/rmmz/plugin/schema/core/primitive/metaSchema.test.ts
+++ src/rmmz/plugin/schema/core/primitive/metaSchema.test.ts
@@ -4,11 +4,10 @@ import type {
   X_MetaParam_Boolean,
   X_MetaParam_Number,
   X_MetaParamUnion,
-  X_MetaParam_DataId,
 } from "./metaSchema";
-import { makeUnionSchema } from "./metaSchema";
 import { makeSchema3 } from "./metaSchema3";
 import type { X_MetaParam_Shared } from "./metaTextField";
+import type { X_Param_DataId } from "./x-rpg-param";
 
 const makeValidator = () => {
   const schema = makeSchema3();
@@ -43,58 +42,6 @@ describe("", () => {
   });
 });
 
-describe("Union Schema Tests", () => {
-  test("", () => {
-    const validate = makeValidator();
-    const bool: X_MetaParam_Boolean = {
-      kind: "boolean",
-      data: {
-        on: "on",
-        off: "off",
-      },
-    };
-    expect(bool).toSatisfy(validate);
-  });
-  test("", () => {
-    const validate = makeValidator();
-    const bool: X_MetaParamUnion = {
-      kind: "boolean",
-      data: {
-        on: "on",
-        off: "off",
-      },
-      parent: "parentId",
-    };
-    expect(bool).toSatisfy(validate);
-  });
-  describe("Invalid case", () => {
-    const validate = makeValidator();
-    test("", () => {
-      const invalidBool: Record<keyof X_MetaParam_Boolean, unknown> = {
-        kind: "boolean",
-        data: {
-          on: "on",
-          off: 0,
-        },
-        parent: "parentId",
-      };
-      expect(invalidBool).not.toSatisfy(validate);
-    });
-    test("", () => {
-      const validate = makeValidator();
-
-      const invalidBool: Partial<Record<keyof X_MetaParam_Boolean, unknown>> = {
-        kind: "boolean",
-        data: {
-          on: "on",
-          off: 0,
-        },
-      };
-      expect(invalidBool).not.toSatisfy(validate);
-    });
-  });
-});
-
 describe("Number Schema Tests", () => {
   test("", () => {
     const validate = makeValidator();
@@ -122,7 +69,7 @@ describe("Number Schema Tests", () => {
 describe("DataId Schema Tests", () => {
   test("", () => {
     const validate = makeValidator();
-    const dataId: X_MetaParam_DataId = {
+    const dataId: Partial<X_Param_DataId> = {
       kind: "dataId",
       data: {
         author: "author",
