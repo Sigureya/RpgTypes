f1c3b7b08fc932a22a66f362ab7ac29152e6bafc
Sigureya <sigureya1208@gmail.com>
2025-06-18 22:08:28 +0900

    boolean型のテストを分離

---
+++ src/rmmz/plugin/schema/regacyCore/compile.boolean.test.ts
@@ -0,0 +1,48 @@
+import { describe, test, expect } from "vitest";
+import type { JSONSchemaType } from "ajv";
+import type { StructAnnotation } from "./kinds/struct2";
+import { compile } from "./pokemon";
+interface BB {
+  bool: boolean;
+}
+
+describe("bool", () => {
+  const boolStruct = {
+    kind: "struct",
+    struct: {
+      structName: "Bool",
+      params: {
+        bool: {
+          kind: "boolean",
+          default: false,
+          desc: "bool desc", // descは全てのkindにある。省略されているだけ
+          text: "bool text", // textも全てのkindにある。
+          off: "off",
+          on: "on",
+        },
+      },
+    },
+  } as const satisfies StructAnnotation<BB>;
+  const expectedBoolSchema: JSONSchemaType<BB> = {
+    title: "Bool",
+    type: "object",
+    properties: {
+      bool: {
+        type: "boolean",
+        default: false,
+        title: "bool text",
+        description: "bool desc",
+      },
+    },
+    required: ["bool"],
+    additionalProperties: false,
+  };
+  test("schema", () => {
+    const resultBool = compile("moduleName", boolStruct, {});
+    expect(resultBool.schema).toEqual(expectedBoolSchema);
+  });
+  test("schema", () => {
+    const resultBool = compile("moduleName", boolStruct, {});
+    expect(resultBool.logs[0].data).toBe(boolStruct.struct.params.bool);
+  });
+});
--- src/rmmz/plugin/schema/regacyCore/pokemon.test.ts
+++ src/rmmz/plugin/schema/regacyCore/pokemon.test.ts
@@ -10,50 +10,6 @@ import type {
 } from "./mockType";
 import { compile } from "./pokemon";
 import type { StructAnnotation } from "./types";
-interface BB {
-  bool: boolean;
-}
-
-describe("bool", () => {
-  const boolStruct = {
-    kind: "struct",
-    struct: {
-      structName: "Bool",
-      params: {
-        bool: {
-          kind: "boolean",
-          default: false,
-          desc: "bool desc", // descは全てのkindにある。省略されているだけ
-          text: "bool text", // textも全てのkindにある。
-          off: "off",
-          on: "on",
-        },
-      },
-    },
-  } as const satisfies StructAnnotation<BB>;
-  const expectedBoolSchema: JSONSchemaType<BB> = {
-    title: "Bool",
-    type: "object",
-    properties: {
-      bool: {
-        type: "boolean",
-        default: false,
-        title: "bool text",
-        description: "bool desc",
-      },
-    },
-    required: ["bool"],
-    additionalProperties: false,
-  };
-  test("schema", () => {
-    const resultBool = compile("moduleName", boolStruct, {});
-    expect(resultBool.schema).toEqual(expectedBoolSchema);
-  });
-  test("schema", () => {
-    const resultBool = compile("moduleName", boolStruct, {});
-    expect(resultBool.logs[0].data).toBe(boolStruct.struct.params.bool);
-  });
-});
 
 describe("person", () => {
   const personStruct = {
