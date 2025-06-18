b1f9fbbeb697906a5cf5e330e00ab5879a29611a
Sigureya <sigureya1208@gmail.com>
2025-06-19 06:06:12 +0900

    新しい関数に切り替え

--- src/rmmz/plugin/schema/regacyCore/compile.number.test.ts
+++ src/rmmz/plugin/schema/regacyCore/compile.number.test.ts
@@ -1,16 +1,18 @@
 import { describe, test, expect } from "vitest";
 import type { JSONSchemaType } from "ajv";
-import { compile } from "./compile";
-import type { KindOfStruct } from "./kinds";
+import { compilePluginStruct } from "./compile";
+import type { PluginStruct } from "./kinds";
 import type { PluginTitles } from "./kinds/compileOption";
 
 interface Numbers {
   floating: number;
   integer1: number;
   integer2: number;
+  integer3: number;
   numberArray: number[];
   floatArray: number[];
 }
+
 const titles: PluginTitles = {
   moduleName: "moduleName",
   author: "author",
@@ -18,18 +20,16 @@ const titles: PluginTitles = {
 
 describe("numbers", () => {
   const numbersStruct = {
-    kind: "struct",
-    struct: {
-      structName: "Numbers",
-      params: {
-        floating: { kind: "number", default: 0.5, digit: 2 },
-        integer1: { kind: "number", default: 42 },
-        integer2: { kind: "number", default: 42, digit: 0 },
-        numberArray: { kind: "number[]", default: [1, 2, 3], digit: 0 },
-        floatArray: { kind: "number[]", default: [1.1, 2.2, 3.3], digit: 2 },
-      },
+    structName: "Numbers",
+    params: {
+      floating: { kind: "number", default: 0.5, digit: 2 },
+      integer1: { kind: "number", default: 42 },
+      integer2: { kind: "number", default: 42, digit: 0 },
+      integer3: { kind: "number", default: 42 },
+      numberArray: { kind: "number[]", default: [1, 2, 3], digit: 0 },
+      floatArray: { kind: "number[]", default: [1.1, 2.2, 3.3], digit: 2 },
     },
-  } as const satisfies KindOfStruct<Numbers>;
+  } as const satisfies PluginStruct<Numbers>;
   const expectedNumbersSchema: JSONSchemaType<Numbers> = {
     title: "Numbers",
     type: "object",
@@ -37,6 +37,7 @@ describe("numbers", () => {
       floating: { type: "number", default: 0.5 },
       integer1: { type: "integer", default: 42 },
       integer2: { type: "integer", default: 42 },
+      integer3: { type: "integer", default: 42 },
       numberArray: {
         type: "array",
         items: { type: "integer" },
@@ -48,11 +49,18 @@ describe("numbers", () => {
         default: [1.1, 2.2, 3.3],
       },
     },
-    required: ["floating", "integer1", "integer2", "numberArray", "floatArray"],
+    required: [
+      "floating",
+      "integer1",
+      "integer2",
+      "integer3",
+      "numberArray",
+      "floatArray",
+    ],
     additionalProperties: false,
   };
   describe("compile", () => {
-    const resultNumbers = compile(titles, numbersStruct, {});
+    const resultNumbers = compilePluginStruct(titles, numbersStruct, {});
     test("logs", () => {
       expect(resultNumbers.logs.length).toBe(5);
     });
