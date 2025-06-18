ca0c467931eb53fc076c1a775d2f16183546f389
Sigureya <sigureya1208@gmail.com>
2025-06-18 23:12:12 +0900

    log関連を切り離した

--- src/rmmz/plugin/schema/regacyCore/mockType.ts
+++ src/rmmz/plugin/schema/regacyCore/mockType.ts
@@ -1,19 +1,8 @@
-import type { JSONSchemaType } from "ajv";
-
 export interface Person {
   name: string;
   age: number;
 }
 
-export interface CompileResult<T extends object> {
-  schema: JSONSchemaType<T>;
-  logs: CompileLogItem[];
-}
-
-export interface CompileLogItem {
-  path: string;
-  data: unknown;
-}
 export interface Family {
   father: Person;
   mother: Person;
