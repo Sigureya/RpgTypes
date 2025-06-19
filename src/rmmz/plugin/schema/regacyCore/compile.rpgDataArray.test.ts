a3beb02720aeb9bb0c7bb96d22f162f608b4b59c
Sigureya <sigureya1208@gmail.com>
2025-06-19 09:00:04 +0900

    テストケースの分割。RPG系のデータ各種

---
+++ src/rmmz/plugin/schema/regacyCore/compile.rpgDataArray.test.ts
@@ -0,0 +1,85 @@
+import { describe, test, expect } from "vitest";
+import type { JSONSchemaType } from "ajv";
+import { compile, compilePluginStruct } from "./compile";
+import type { KindOfStruct, PluginStruct } from "./kinds";
+import type { PluginTitles } from "./kinds/compileOption";
+
+interface AllDataArray {
+  actor: number[];
+  weapons: number[];
+  armor: number[];
+  skill: number[];
+  item: number[];
+  enemy: number[];
+  state: number[];
+}
+const titles: PluginTitles = {
+  moduleName: "moduleName",
+  author: "author",
+};
+
+describe("alldataArray", () => {
+  const allDataArrayStruct = {
+    structName: "AllDataArray",
+    params: {
+      actor: { kind: "actor[]", default: [1, 2, 3] },
+      weapons: { kind: "weapon[]", default: [1, 2, 3] },
+      armor: { kind: "armor[]", default: [1, 2, 3] },
+      skill: { kind: "skill[]", default: [1, 2, 3] },
+      item: { kind: "item[]", default: [1, 2, 3] },
+      enemy: { kind: "enemy[]", default: [1, 2, 3] },
+      state: { kind: "state[]", default: [1, 2, 3] },
+    },
+  } as const satisfies PluginStruct<AllDataArray>;
+  const expectedAllDataArraySchema: JSONSchemaType<AllDataArray> = {
+    title: "AllDataArray",
+    type: "object",
+    properties: {
+      actor: {
+        type: "array",
+        items: { type: "integer" },
+        default: [1, 2, 3],
+      },
+      weapons: {
+        type: "array",
+        items: { type: "integer" },
+        default: [1, 2, 3],
+      },
+      armor: {
+        type: "array",
+        items: { type: "integer" },
+        default: [1, 2, 3],
+      },
+      skill: {
+        type: "array",
+        items: { type: "integer" },
+        default: [1, 2, 3],
+      },
+      item: {
+        type: "array",
+        items: { type: "integer" },
+        default: [1, 2, 3],
+      },
+      enemy: {
+        type: "array",
+        items: { type: "integer" },
+        default: [1, 2, 3],
+      },
+      state: {
+        type: "array",
+        items: { type: "integer" },
+        default: [1, 2, 3],
+      },
+    },
+    required: ["actor", "weapons", "armor", "skill", "item", "enemy", "state"],
+    additionalProperties: false,
+  };
+  test("schema", () => {
+    const resultAllDataArray = compilePluginStruct(
+      titles,
+      allDataArrayStruct,
+      {}
+    );
+    expect(resultAllDataArray.schema).toEqual(expectedAllDataArraySchema);
+  });
+});
