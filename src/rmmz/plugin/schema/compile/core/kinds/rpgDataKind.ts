f9c31b7aea482424564195b81ba24dc86aeaf597
Sigureya <sigureya1208@gmail.com>
2025-06-21 05:32:27 +0900

    xparam取得部分を統合。別関数に分ける意味が無かった。

--- src/rmmz/plugin/schema/compile/core/kinds/rpgDataKind.ts
+++ src/rmmz/plugin/schema/compile/core/kinds/rpgDataKind.ts
@@ -1,15 +1,7 @@
 import type { JSONSchemaType } from "ajv";
 import { withDefault, withTexts } from "./primitive";
-import type {
-  KindBase,
-  KindOfRpgDataId,
-  KindOfSystemDataId,
-} from "./primitiveParams";
+import type { KindOfRpgDataId, KindOfSystemDataId } from "./primitiveParams";
 import { lookupKind } from "./rpgData/lookup";
-import type {
-  DataKind_RpgUnion,
-  DataKind_SystemUnion,
-} from "./rpgData/rpgDataTypesNames";
 import type { X_Param } from "./x-rpg-param";
 import { xparamBaseData } from "./x-rpg-param";
 
@@ -27,20 +19,6 @@ export const makeIdFieldWithXParam = (
     type: "integer",
     ...withDefault(data.default),
     ...withTexts(data),
-    ...(data.kind ? (xparamDataId(data) satisfies X_Param) : {}),
+    ...(xparamBaseData(data, lookupKind(data.kind)) satisfies X_Param),
   } satisfies JSONSchemaType<number>;
 };
-
-export const xparamDataId = <
-  Kind extends DataKind_RpgUnion | DataKind_SystemUnion
->(
-  dataId: KindOfAnyData<Kind>
-) => xparamBaseData(dataId, lookupKind(dataId.kind));
-
-interface KindOfAnyData<Kind extends DataKind_RpgUnion | DataKind_SystemUnion>
-  extends KindBase {
-  kind: Kind;
-  text?: string;
-  desc?: string;
-  parent?: string;
-}
