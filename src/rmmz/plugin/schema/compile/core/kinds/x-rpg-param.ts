fe3412515e31a8b0e4908b62124583c51d404fe4
Sigureya <sigureya1208@gmail.com>
2025-06-21 05:18:35 +0900

    個別の型を各ファイルへ分離

--- src/rmmz/plugin/schema/compile/core/kinds/x-rpg-param.ts
+++ src/rmmz/plugin/schema/compile/core/kinds/x-rpg-param.ts
@@ -4,13 +4,11 @@ import type {
   KindOfNumber,
   KindOfNumberArray,
 } from "./primitiveParams";
-import { lookupKind } from "./rpgData/lookup";
-import type {
-  DataKind_RpgUnion,
-  DataKind_SystemUnion,
-} from "./rpgData/rpgDataTypesNames";
 
 export const X_RPG_PARM = "x-rpg-param" as const;
+export interface X_Param {
+  "x-rpg-param": X_RmmzParamBaee;
+}
 
 export interface X_RmmzParamBaee {
   parent?: string | null;
@@ -24,14 +22,6 @@ export interface X_RmmzParamInput<T, Kind extends string = string> {
   data: T;
 }
 
-interface KindOfAnyData<Kind extends DataKind_RpgUnion | DataKind_SystemUnion>
-  extends KindBase {
-  kind: Kind;
-  text?: string;
-  desc?: string;
-  parent?: string;
-}
-
 export const xparamBaseData = <T extends object, P extends KindBase>(
   param: P,
   data: T
@@ -42,12 +32,6 @@ export const xparamBaseData = <T extends object, P extends KindBase>(
     data: data,
   },
 });
-
-export const xparamDataId = <
-  Kind extends DataKind_RpgUnion | DataKind_SystemUnion
->(
-  dataId: KindOfAnyData<Kind>
-) => xparamBaseData(dataId, lookupKind(dataId.kind));
 
 export const xparamNumber = (data: KindOfNumber | KindOfNumberArray) =>
   xparamBaseData(
