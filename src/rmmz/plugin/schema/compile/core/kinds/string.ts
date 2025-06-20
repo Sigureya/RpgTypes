09d2f8a5313456fd7fb31c175c9f23a787eb20ef
Sigureya <sigureya1208@gmail.com>
2025-06-21 06:15:27 +0900

    追加パラメータ対応。descとかtextとか

--- src/rmmz/plugin/schema/compile/core/kinds/string.ts
+++ src/rmmz/plugin/schema/compile/core/kinds/string.ts
@@ -3,24 +3,26 @@ import { withDefault, withTexts } from "./primitive";
 import type {
   KindOfString,
   KindOfSelect,
-  KindOFCombo,
+  KindOfCombo,
   KindOfFile,
 } from "./primitiveParams";
 
 export const makeStringField = (data: KindOfString) =>
   ({
     type: "string",
-    ...(data.default !== undefined ? { default: data.default } : {}),
+    ...withTexts(data),
+    ...withDefault(data.default),
   } satisfies JSONSchemaType<string>);
 
 export const makeSelectField = (data: KindOfSelect) =>
   ({
     type: "string",
     ...withDefault(data.default),
+    ...withTexts(data),
     ...(data.options ? { enum: data.options.map((o): string => o.value) } : {}),
   } satisfies JSONSchemaType<string>);
 
-export const makeComboField = (data: KindOFCombo) =>
+export const makeComboField = (data: KindOfCombo) =>
   ({
     type: "string",
     ...withDefault(data.default),
