7f233580cd2ca3b39d548e2b7c0695ea52b05a4c
Sigureya <sigureya1208@gmail.com>
2025-06-21 05:33:43 +0900

    number型のParam情報をSchemaに変換する関数

---
+++ src/rmmz/plugin/schema/compile/core/kinds/numbers.ts
@@ -0,0 +1,47 @@
+import type { JSONSchemaType } from "ajv";
+import { withDefault, withTexts } from "./primitive";
+import type { KindOfNumber, KindOfNumberArray } from "./primitiveParams";
+import type { X_Param } from "./x-rpg-param";
+import { xparamBaseData } from "./x-rpg-param";
+
+const isIntegerKind = (digit: number | undefined) => {
+  return digit === undefined || digit === 0;
+};
+export const makeNumberField = (data: KindOfNumber) =>
+  ({
+    type: isIntegerKind(data.digit) ? "integer" : "number",
+    ...withDefault(data.default),
+    ...withTexts(data),
+  } satisfies JSONSchemaType<number>);
+export const makeNumberArrayField = (data: KindOfNumberArray) =>
+  ({
+    type: "array",
+    items: {
+      type: isIntegerKind(data.digit) ? "integer" : "number",
+    },
+    ...withDefault(data.default),
+    ...withTexts(data),
+  } satisfies JSONSchemaType<number[]>);
+
+const xparamNumber = (data: KindOfNumber | KindOfNumberArray) =>
+  xparamBaseData(
+    data,
+    typeof data.digit === "number" ? { digit: data.digit } : {}
+  );
+export const makeNumberFieldWithXparam = (data: KindOfNumber) =>
+  ({
+    type: isIntegerKind(data.digit) ? "integer" : "number",
+    ...withDefault(data.default),
+    ...withTexts(data),
+    ...(xparamNumber(data) satisfies X_Param),
+  } satisfies JSONSchemaType<number>);
+export const makeNumberArrayFieldWithXParam = (data: KindOfNumberArray) =>
+  ({
+    type: "array",
+    items: {
+      type: isIntegerKind(data.digit) ? "integer" : "number",
+    },
+    ...withDefault(data.default),
+    ...withTexts(data),
+    ...xparamNumber(data),
+  } satisfies JSONSchemaType<number[]>);
