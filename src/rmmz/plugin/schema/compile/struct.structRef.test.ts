a6fbd23ddcbedc53fab8afe1c8345d4be89dd5b5
Sigureya <sigureya1208@gmail.com>
2025-06-20 22:29:55 +0900

    テストケース名を修正。以前の内容は不完全だった

--- src/rmmz/plugin/schema/compile/struct.structRef.test.ts
+++ src/rmmz/plugin/schema/compile/struct.structRef.test.ts
@@ -37,19 +37,23 @@ describe("compilePluginStruct - struct_ref", () => {
     additionalProperties: false,
   };
   const resultFamily = compilePluginStruct(titles, familyStruct, {});
-  test("", () => {
+  test("generates correct JSONSchema with $ref for struct_ref fields", () => {
     expect(resultFamily.schema).toEqual(expectedFamilySchema);
   });
-  test("", () => {
-    expect(
-      resultFamily.logs.find((log) => log.path === "moduleName.Family.father")
-        ?.data
-    ).toEqual(familyStruct.params.father);
+  test("logs correct data for father struct_ref field", () => {
+    const found = resultFamily.logs.find(
+      (log) => log.path === "moduleName.Family.father"
+    );
+    expect(found).toMatchObject({
+      data: familyStruct.params.father,
+    } satisfies Pick<StructCompileLog, "data">);
   });
-  test("", () => {
-    expect(
-      resultFamily.logs.find((log) => log.path === "moduleName.Family.mother")
-        ?.data
-    ).toEqual(familyStruct.params.mother);
+  test("logs correct data for mother struct_ref field", () => {
+    const found = resultFamily.logs.find(
+      (log) => log.path === "moduleName.Family.mother"
+    );
+    expect(found).toMatchObject({
+      data: familyStruct.params.mother,
+    } satisfies Pick<StructCompileLog, "data">);
   });
 });
