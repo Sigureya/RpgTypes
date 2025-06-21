f5851dddd1d0fb3408261233584f5261417e6635
Sigureya <sigureya1208@gmail.com>
2025-06-21 11:59:54 +0900

    今後の削除作業を円滑に進めるため、indexを消す

--- src/rmmz/plugin/schema/core/primitive/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./oldTypes/annotationBase";
-export * from "./paramTypesOld";
-export * from "./primitiveArray";
-export * from "./select";
-export * from "./string";
-export { NumberArg } from "./numbers";
