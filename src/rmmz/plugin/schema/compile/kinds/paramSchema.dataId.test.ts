dbdd513e3466432e8af1807db1ea8f4f16e32252
Sigureya <sigureya1208@gmail.com>
2025-06-23 05:19:01 +0900

    テスト関数を作成して簡略化

--- src/rmmz/plugin/schema/compile/kinds/paramSchema.dataId.test.ts
+++ src/rmmz/plugin/schema/compile/kinds/paramSchema.dataId.test.ts
@@ -1,6 +1,7 @@
 import { describe, test, expect } from "vitest";
 import Ajv from "ajv";
 import type { KindOfRpgDataId } from "./core/primitiveParams";
+import type { DataKind_RpgUnion } from "./core/rpgData/rpgDataTypesNames";
 import { makeParamSchema } from "./paramSchema";
 
 const makeDataParam = (param: Partial<KindOfRpgDataId>): KindOfRpgDataId => {
@@ -13,94 +14,67 @@ const makeDataParam = (param: Partial<KindOfRpgDataId>): KindOfRpgDataId => {
   };
 };
 
-describe("RmmzParam_DataId JSON Schema Validation", () => {
-  const ajv = new Ajv({ strict: true, discriminator: true });
-  const schema = makeParamSchema();
-  const validate = ajv.compile(schema);
+interface TestCase {
+  dataKind: DataKind_RpgUnion;
+}
 
-  describe("valid cases", () => {
-    test("actor", () => {
+const testFn = ({ dataKind }: TestCase) => {
+  describe(`${dataKind} JSON Schema Validation`, () => {
+    const ajv = new Ajv({ strict: true, discriminator: true });
+    const schema = makeParamSchema();
+    const validate = ajv.compile(schema);
+    test(`valid case`, () => {
       const mock: KindOfRpgDataId = makeDataParam({
-        kind: "actor",
+        kind: dataKind,
         default: 1,
+        text: `${dataKind} Field`,
+        desc: `A ${dataKind} field`,
+        parent: "parentId",
       });
       expect(mock).toSatisfy(validate);
     });
-    test("item", () => {
-      const mock: KindOfRpgDataId = makeDataParam({
-        kind: "item",
-        default: 2,
-      });
-      expect(mock).toSatisfy(validate);
-    });
-    test("skill", () => {
-      const mock: KindOfRpgDataId = makeDataParam({
-        kind: "skill",
-        default: 3,
-      });
-      expect(mock).toSatisfy(validate);
-    });
-    test("weapon", () => {
-      const mock: KindOfRpgDataId = makeDataParam({
-        kind: "weapon",
-        default: 4,
-      });
-      expect(mock).toSatisfy(validate);
-    });
-    test("armor", () => {
-      const mock: KindOfRpgDataId = makeDataParam({
-        kind: "armor",
-        default: 5,
-      });
-      expect(mock).toSatisfy(validate);
-    });
-    test("enemy", () => {
-      const mock: KindOfRpgDataId = makeDataParam({
-        kind: "enemy",
-        default: 6,
-      });
-      expect(mock).toSatisfy(validate);
-    });
-    test("state", () => {
-      const mock: KindOfRpgDataId = makeDataParam({
-        kind: "state",
-        default: 7,
-      });
-      expect(mock).toSatisfy(validate);
-    });
-    test("class", () => {
-      const mock: KindOfRpgDataId = makeDataParam({
-        kind: "class",
+    test("", () => {
+      const mock: KindOfRpgDataId = {
+        kind: dataKind,
         default: 1,
-      });
+      };
       expect(mock).toSatisfy(validate);
     });
-    test("common_event", () => {
-      const mock: KindOfRpgDataId = makeDataParam({
-        kind: "common_event",
+    test("", () => {
+      const mock: KindOfRpgDataId = {
+        kind: dataKind,
         default: 1,
-      });
+        desc: undefined,
+        text: undefined,
+        parent: undefined,
+      };
+      expect(mock).toSatisfy(validate);
+    });
+    test("", () => {
+      const mock: Record<keyof KindOfRpgDataId, unknown> = {
+        kind: dataKind,
+        default: 1,
+        desc: null,
+        text: null,
+        parent: null,
+      };
       expect(mock).toSatisfy(validate);
     });
   });
-
-  describe("invalid cases", () => {
-    test("rejects dataId parameter without default value", () => {
-      const mock: Omit<KindOfRpgDataId, "default"> = {
-        kind: "actor",
-        text: "Data ID Field",
-        desc: "A data ID field",
-      };
-      expect(mock).not.toSatisfy(validate);
-    });
-
-    test("rejects invalid kind", () => {
-      const mock: Omit<KindOfRpgDataId, "kind"> = {
-        default: 1,
-        text: "Invalid Data ID Field",
-        desc: "An invalid data ID field",
-      };
-      expect(mock).not.toSatisfy(validate);
-    });
+};
+const dataKinds: DataKind_RpgUnion[] = [
+  "actor",
+  "item",
+  "skill",
+  "weapon",
+  "armor",
+  "enemy",
+  "state",
+  "class",
+  "common_event",
+];
+describe("RmmzParam_DataId JSON Schema Validation", () => {
+  dataKinds.forEach((dataKind) => {
+    testFn({ dataKind });
   });
 });
