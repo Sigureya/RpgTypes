9c41cb5b2004ce9e0e3b3c6d5e192bf8eee538a6
Sigureya <sigureya1208@gmail.com>
2025-06-22 23:55:47 +0900

    イベントコマンド関連の修正。フォルダ構成をがっつり変えた影響により機能不足が色々発生してた。

--- src/rmmz/eventCommand/commands/controlTimer/scema.ts
+++ src/rmmz/eventCommand/commands/controlTimer/scema.ts
@@ -2,10 +2,19 @@ import type { JSONSchemaType } from "ajv";
 import type { Command_ControlTimer } from "./types";
 
 export const SCHEMA_COMMAND_CONTROL_TIMER = {
+  required: ["code", "indent", "parameters"],
   type: "object",
   properties: {
-    operation: { type: "string", enum: ["plus", "minus"] },
-    time: { type: "number" },
+    code: { type: "number", const: 124 },
+    indent: { type: "integer", minimum: 0 },
+    parameters: {
+      type: "array",
+      minItems: 2,
+      maxItems: 2,
+      items: [
+        { type: "number", enum: [0, 1] }, // operation
+        { type: "number", minimum: 0 }, // time
+      ],
+    },
   },
-  required: ["operation", "time"],
-};
+} satisfies JSONSchemaType<Command_ControlTimer>;
--- src/rmmz/eventCommand/commands/controlTimer/types.ts
+++ src/rmmz/eventCommand/commands/controlTimer/types.ts
@@ -1,11 +1,10 @@
 import type { EventCommandLike2 } from "@RpgTypes/eventCommand/frame";
 import type { ValueOf } from "src/templates/valueOf";
-
 export interface Command_ControlTimer extends EventCommandLike2<124> {
   parameters: [operation: ValueOf<Operation_PlusMinus>, time: number];
 }
-export type Operation_PlusMinus = "plus" | "minus";
+export type Operation_PlusMinus = { plus: 0; minus: 1 };
 export interface Paramobject_ControlTimer {
-  operation: Operation_PlusMinus;
+  operation: "plus" | "minus";
   time: number;
 }
