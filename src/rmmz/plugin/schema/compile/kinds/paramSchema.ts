760b3843e368886fb7cdd9d72f57e29f572ebb48
Sigureya <sigureya1208@gmail.com>
2025-06-23 03:49:00 +0900

    全てのParamKindに対応するためのSchemaを作る関数。

--- src/rmmz/plugin/schema/compile/kinds/paramSchema.ts
+++ src/rmmz/plugin/schema/compile/kinds/paramSchema.ts
@@ -1,28 +1,98 @@
 import type { JSONSchemaType } from "ajv";
 import type { DiscriminatedUnionSchemaType3 } from "src/templates/discriminator/discriminator3";
-import { makeSchemaBooleanParam } from "./boolean";
-import { rmmzSchemaComboParam } from "./combo";
 import type {
   KindBase,
   KindOfBoolean,
   KindOfCombo,
   KindOfNumber,
+  KindOfFile,
 } from "./core/primitiveParams";
-import { rmmzSchemaNumberParam } from "./numbers";
 
-const s: DiscriminatedUnionSchemaType3<
+const BASIC_TEXT = {
+  type: "string",
+  nullable: true,
+  maxLength: 100,
+} as const satisfies JSONSchemaType<string | null>;
+
+export const makeParamSchema = (): DiscriminatedUnionSchemaType3<
   KindBase,
   string,
   "kind",
-  KindOfNumber | KindOfCombo | KindOfBoolean
-> = {
+  KindOfBoolean | KindOfNumber | KindOfCombo | KindOfFile
+> => ({
   type: "object",
   discriminator: {
     propertyName: "kind",
   },
   oneOf: [
+    makeSchemaBooleanParam() satisfies JSONSchemaType<KindOfBoolean>,
     rmmzSchemaNumberParam() satisfies JSONSchemaType<KindOfNumber>,
     rmmzSchemaComboParam() satisfies JSONSchemaType<KindOfCombo>,
-    makeSchemaBooleanParam() satisfies JSONSchemaType<KindOfBoolean>,
+    rmmzSchemaFilePathParam() satisfies JSONSchemaType<KindOfFile>,
   ],
-};
+});
+
+const rmmzSchemaNumberParam = (): JSONSchemaType<KindOfNumber> => ({
+  type: "object",
+  required: ["kind", "default"],
+  properties: {
+    digit: { type: "integer", minimum: 0, default: 0, nullable: true },
+    min: { type: "number", nullable: true },
+    max: { type: "number", nullable: true },
+    default: { type: "number", default: 0 },
+    kind: { type: "string", const: "number" },
+    desc: BASIC_TEXT,
+    text: BASIC_TEXT,
+    parent: BASIC_TEXT,
+  },
+});
+
+const makeSchemaBooleanParam = () =>
+  ({
+    additionalProperties: false,
+    type: "object",
+    required: ["kind", "default"],
+    properties: {
+      kind: { type: "string", const: "boolean" },
+      default: { type: "boolean", default: true },
+      on: BASIC_TEXT,
+      off: BASIC_TEXT,
+      desc: BASIC_TEXT,
+      text: BASIC_TEXT,
+      parent: BASIC_TEXT,
+    },
+  } satisfies JSONSchemaType<KindOfBoolean>);
+
+const rmmzSchemaComboParam = () =>
+  ({
+    additionalProperties: false,
+    type: "object",
+    required: ["kind", "default", "options"],
+    properties: {
+      kind: { type: "string", const: "combo" },
+      default: { type: "string", default: "" },
+      options: {
+        type: "array",
+        items: { type: "string" },
+        default: [],
+      },
+      desc: BASIC_TEXT,
+      text: BASIC_TEXT,
+      parent: BASIC_TEXT,
+    },
+  } satisfies JSONSchemaType<KindOfCombo>);
+
+const rmmzSchemaFilePathParam = () =>
+  ({
+    additionalProperties: false,
+    type: "object",
+    required: ["kind", "default", "dir"],
+    properties: {
+      kind: { type: "string", const: "file" },
+      default: { type: "string", default: "" },
+      dir: { type: "string", default: "" },
+      desc: BASIC_TEXT,
+      text: BASIC_TEXT,
+      parent: BASIC_TEXT,
+    },
+  } satisfies JSONSchemaType<KindOfFile>);
