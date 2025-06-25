e0e7501aa885adda3cee4f058d5911d57d8b5cb5
Sigureya <sigureya1208@gmail.com>
2025-06-26 08:17:35 +0900

    テンプレート対応を改善。

--- src/rmmz/plugin/schema/compile/kinds/core/parse/keyword.ts
+++ src/rmmz/plugin/schema/compile/kinds/core/parse/keyword.ts
@@ -3,13 +3,12 @@ import type { Token } from "./types/token";
 export type MappingTable<T> = {
   [K in Extract<keyof T, string>]: (value: string) => T[K];
 };
-export const mapKeywords = <T>(
+export const mapKeywords = <T extends object>(
   tokens: ReadonlyArray<Token>,
   fnTable: MappingTable<T>
 ): Partial<T> => {
   return tokens.reduce((acc, token) => {
     if (token.keyword in acc) {
-      // 追加済みなら無視
       return acc;
     }
     const fn = fnTable[token.keyword as keyof MappingTable<T>];
