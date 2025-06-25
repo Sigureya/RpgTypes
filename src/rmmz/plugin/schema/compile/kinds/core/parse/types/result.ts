4aa3e6243ed4f3156909fd96086d6076f3ce2f3a
Sigureya <sigureya1208@gmail.com>
2025-06-26 02:04:28 +0900

    メンバ追加の準備。仮状態で型を用意する

--- src/rmmz/plugin/schema/compile/kinds/core/parse/types/result.ts
+++ src/rmmz/plugin/schema/compile/kinds/core/parse/types/result.ts
@@ -2,4 +2,17 @@ import type { PluginCommandTokens } from "./token";
 
 export interface ParsingResult {
   commands: PluginCommandTokens[];
+  meta: PluginMeta;
+}
+
+export interface PluginMetaXX {
+  target: string;
+  pluginDesc: string;
+  author: string;
+}
+
+export interface PluginMeta {
+  target?: string;
+  pluginDesc?: string;
+  author?: string;
 }
