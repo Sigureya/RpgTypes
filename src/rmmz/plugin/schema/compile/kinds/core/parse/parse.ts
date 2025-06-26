b2aa10be04d3465083a3130db8d809b096466e53
Sigureya <sigureya1208@gmail.com>
2025-06-26 11:36:56 +0900

    複雑な処理を別関数に分割した

--- src/rmmz/plugin/schema/compile/kinds/core/parse/parse.ts
+++ src/rmmz/plugin/schema/compile/kinds/core/parse/parse.ts
@@ -1,5 +1,6 @@
+import { extractArgs } from "./extratArgs";
 import { createMetaInfo } from "./metaInfo";
-import { extractArgs } from "./semantic";
+import { sliceToken3 } from "./semantic2";
 import { tokenize } from "./toknize";
 import type { ParsingResult } from "./types/result";
 import type {
@@ -9,46 +10,18 @@ import type {
   PluginCommandTokens,
 } from "./types/token";
 
-interface CC {
-  params: ParsingContext[];
-  commands: ParsingContext[];
-}
+// ...parsePlugin等はそのまま...
+export const parsePlugin = (pluginAnnotations: string): ParsingResult => {
+  const tokens: Token[] = tokenize(pluginAnnotations);
+  const cc = sliceToken3(tokens);
+  const commands = cc.commands.map(pluginCommandContext);
+  const params = cc.params.map(pluginParamContext);
 
-const keywordIs = <S extends string>(
-  token: Token,
-  keyword: S
-): token is {
-  keyword: S;
-  value: string;
-} => {
-  return token.keyword === keyword;
-};
-
-const isCommandToken = (token: Token) => keywordIs(token, "command");
-const isParamToken = (token: Token) => keywordIs(token, "param");
-
-const sliceToken2 = (tokens: Token[]): CC => {
-  const tt = sliceToken(tokens);
   return {
-    params: tt.filter((t) => isParamToken(t.head)),
-    commands: tt.filter((t) => isCommandToken(t.head)),
+    commands,
+    meta: createMetaInfo(tokens),
+    params,
   };
-};
-
-// Token列をParsingContext[]に分割
-const sliceToken = (tokens: Token[]): ParsingContext[] => {
-  return tokens.reduce<ParsingContext[]>((acc, token) => {
-    if (isParamToken(token) || isCommandToken(token)) {
-      acc.push({ head: token, tokens: [] });
-    } else if (acc.length > 0) {
-      const last = acc[acc.length - 1];
-      acc[acc.length - 1] = {
-        head: last.head,
-        tokens: [...last.tokens, token],
-      };
-    }
-    return acc;
-  }, []);
 };
 
 // パラメータブロックからParamTokenを生成
@@ -73,19 +46,5 @@ const pluginCommandContext = (context: ParsingContext): PluginCommandTokens => {
     ...(text ? { text } : {}),
     ...(desc ? { desc } : {}),
     args,
-  };
-};
-
-// ...parsePlugin等はそのまま...
-export const parsePlugin = (pluginAnnotations: string): ParsingResult => {
-  const tokens: Token[] = tokenize(pluginAnnotations);
-  const cc = sliceToken2(tokens);
-  const commands = cc.commands.map(pluginCommandContext);
-  const params = cc.params.map(pluginParamContext);
-
-  return {
-    commands,
-    meta: createMetaInfo(tokens),
-    params,
   };
 };
