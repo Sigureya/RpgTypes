48a4a693efea90a8e3d665d70a34ca044ff348f6
Sigureya <sigureya1208@gmail.com>
2025-06-27 00:16:04 +0900

    対応が簡単な箇所だけ実装。パラメータの型を解析してオブジェクトにする処理

---
+++ src/rmmz/plugin/schema/compile/kinds/core/attributes.ts
@@ -0,0 +1,65 @@
+import { KEYWORD_TYPE } from "./parse/constants/keyword";
+import type { MappingTable } from "./parse/keyword";
+import { mapKeywords } from "./parse/keyword";
+import type { Token } from "./parse/types/token";
+import type { BooleanParam, StructParamPrimitive } from "./primitiveParams";
+
+const getType = (tokens: ReadonlyArray<Token>): string | undefined => {
+  const typeToken = tokens.find((t) => t.keyword === KEYWORD_TYPE);
+  return typeToken ? typeToken.value : undefined;
+};
+const attrString = (value: string): string => value;
+
+type XX<T> = MappingTable<Omit<T, "kind">>;
+
+type TableType = {
+  [K in StructParamPrimitive["kind"]]: XX<StructParamPrimitive & { kind: K }>;
+};
+const DATA_ID = {
+  default: (value: string) => parseInt(value, 10),
+  text: attrString,
+  desc: attrString,
+  parent: attrString,
+} as const;
+
+const table = {
+  boolean: {
+    default: (value: string) => value === "true",
+    text: attrString,
+    desc: attrString,
+    on: attrString,
+    off: attrString,
+    parent: attrString,
+  } satisfies XX<BooleanParam>,
+  number: {
+    default: (value: string) => parseFloat(value),
+    text: attrString,
+    desc: attrString,
+    digit: (value: string) => parseInt(value, 10),
+    min: (value: string) => parseFloat(value),
+    max: (value: string) => parseFloat(value),
+    parent: attrString,
+  } satisfies XX<Extract<StructParamPrimitive, { kind: "number" }>>,
+  enemy: DATA_ID,
+  item: DATA_ID,
+  skill: DATA_ID,
+  actor: DATA_ID,
+  class: DATA_ID,
+  weapon: DATA_ID,
+  armor: DATA_ID,
+  state: DATA_ID,
+  troop: DATA_ID,
+  variable: DATA_ID,
+  switch: DATA_ID,
+  file: {
+    default: attrString,
+    text: attrString,
+    desc: attrString,
+    parent: attrString,
+    dir: attrString,
+  },
+} satisfies Partial<TableType>;
+
+const bb = (tokens: ReadonlyArray<Token>) => {
+  return mapKeywords<Omit<BooleanParam, "kind">>(tokens, table.boolean);
+};
