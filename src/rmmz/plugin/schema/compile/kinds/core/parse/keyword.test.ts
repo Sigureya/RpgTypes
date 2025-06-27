b6a54125c2a72269f17404f13e2db15ddb2c85d0
Sigureya <sigureya1208@gmail.com>
2025-06-28 05:25:01 +0900

    サンプルコードを兼ねて使い方を追加。

--- src/rmmz/plugin/schema/compile/kinds/core/parse/keyword.test.ts
+++ src/rmmz/plugin/schema/compile/kinds/core/parse/keyword.test.ts
@@ -1,4 +1,5 @@
 import { test, expect, describe } from "vitest";
+import type { MappingTable } from "./keyword";
 import { mapKeywords } from "./keyword";
 import type { Token } from "./types/token";
 
@@ -15,17 +16,24 @@ interface Person {
 }
 
 describe("mapKeywords", () => {
+  const mockFnTable = {
+    name: (value): string => value,
+    age: (value): number => parseInt(value, 10),
+    isActive: (value): boolean => value === "true",
+  } satisfies MappingTable<Person>;
+
   test("should map keywords using the provided function table", () => {
-    const result = mapKeywords<Person>(mockTokens, {
-      name: (value): string => value,
-      age: (value): number => parseInt(value, 10),
-      isActive: (value): boolean => value === "true",
-    });
+    const result = mapKeywords(mockTokens, mockFnTable);
     const expected: Person = {
       name: "bob",
       age: 30,
       isActive: true,
     };
+    expect(result).toEqual(expected);
+  });
+  test("should return an empty object for no tokens", () => {
+    const result = mapKeywords([], mockFnTable);
+    const expected = {};
     expect(result).toEqual(expected);
   });
 });
