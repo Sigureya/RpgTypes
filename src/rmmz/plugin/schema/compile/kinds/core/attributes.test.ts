2403435ab590a61b9d648f98b1291bf2329a8037
Sigureya <sigureya1208@gmail.com>
2025-06-27 11:09:48 +0900

    comboに対応できるようになった

--- src/rmmz/plugin/schema/compile/kinds/core/attributes.test.ts
+++ src/rmmz/plugin/schema/compile/kinds/core/attributes.test.ts
@@ -1,26 +1,39 @@
 import { describe, expect, test } from "vitest";
 import { compileAttributes } from "./attributes";
 import type { Token } from "./parse/types/token";
-import type { StructParamPrimitive } from "./primitiveParams";
+import type {
+  BooleanParam,
+  StringParam,
+  ComboParam,
+  FileArrayParam,
+  FileParam,
+  GenericIdParam,
+  StructParamPrimitive,
+} from "./primitiveParams";
 
-interface TestCase {
+interface TestCase<T extends StructParamPrimitive = StructParamPrimitive> {
   caseName: string;
   input: Token[];
-  expected: StructParamPrimitive;
+  expected: T;
 }
 
-const runTestCases = (testCases: TestCase[]) => {
-  testCases.forEach(({ caseName, input, expected }) => {
-    test(`${caseName} - ${expected.kind}`, () => {
-      const result = compileAttributes(input);
-      expect(result).toEqual(expected);
+const runTestCases = <T extends StructParamPrimitive>(
+  groupName: string,
+  testCases: TestCase<T>[]
+) => {
+  describe(`compileAttributes - ${groupName}`, () => {
+    testCases.forEach(({ caseName, input, expected }) => {
+      test(`${caseName} - ${expected.kind}`, () => {
+        const result = compileAttributes(input);
+        expect(result).toEqual(expected);
+      });
     });
   });
 };
 
-const testCases: TestCase[] = [
+runTestCases<StringParam>("string", [
   {
-    caseName: "string",
+    caseName: "最小セット",
     input: [
       { keyword: "type", value: "string" },
       { keyword: "default", value: "xyz" },
@@ -30,28 +43,91 @@ const testCases: TestCase[] = [
       default: "xyz",
     },
   },
+]);
+runTestCases<BooleanParam>("boolean", [
   {
-    caseName: "boolean",
+    caseName: "最小セット",
     input: [
       { keyword: "type", value: "boolean" },
-      { keyword: "default", value: "false" },
+      { keyword: "default", value: "true" },
     ],
     expected: {
       kind: "boolean",
-      default: false,
+      default: true,
     },
   },
   {
-    caseName: "number",
+    caseName: "フルセット",
+    input: [
+      { keyword: "type", value: "boolean" },
+      { keyword: "default", value: "true" },
+      { keyword: "text", value: "is true" },
+      { keyword: "desc", value: "this is a boolean" },
+      { keyword: "on", value: "enabled" },
+      { keyword: "off", value: "disabled" },
+    ],
+    expected: {
+      kind: "boolean",
+      default: true,
+      text: "is true",
+      desc: "this is a boolean",
+      on: "enabled",
+      off: "disabled",
+    },
+  },
+]);
+runTestCases("number", [
+  {
+    caseName: "最小セット",
     input: [
       { keyword: "type", value: "number" },
-      { keyword: "default", value: "0" },
+      { keyword: "default", value: "123.45" },
     ],
     expected: {
       kind: "number",
-      default: 0,
+      default: 123.45,
     },
   },
-];
+  {
+    caseName: "フルセット",
+    input: [
+      { keyword: "type", value: "number" },
+      { keyword: "default", value: "123.45" },
+      { keyword: "text", value: "a number" },
+      { keyword: "desc", value: "this is a number" },
+      { keyword: "digit", value: "123" },
+      { keyword: "min", value: "-1000.5" },
+      { keyword: "max", value: "1000.5" },
+    ],
+    expected: {
+      kind: "number",
+      default: 123.45,
+      text: "a number",
+      desc: "this is a number",
+      digit: 123,
+      min: -1000.5,
+      max: 1000.5,
+    },
+  },
+]);
 
-runTestCases(testCases);
+runTestCases<ComboParam>("combo", [
+  {
+    caseName: "最小セット",
+    input: [
+      { keyword: "type", value: "combo" },
+      { keyword: "default", value: "option1" },
+      { keyword: "text", value: "a combo" },
+      { keyword: "desc", value: "this is a combo" },
+      { keyword: "parent", value: "parentId" },
+    ],
+    expected: {
+      kind: "combo",
+      default: "option1",
+      options: [],
+      text: "a combo",
+      desc: "this is a combo",
+      parent: "parentId",
+    },
+  },
+]);
