fb1e156cee1956680727f7f7de534417f0851cb1
Sigureya <sigureya1208@gmail.com>
2025-06-28 06:37:10 +0900

    テスト追加。optionが正しく構築できるかテスト

--- src/rmmz/plugin/schema/compile/kinds/core/attributes.option.test.ts
+++ src/rmmz/plugin/schema/compile/kinds/core/attributes.option.test.ts
@@ -3,8 +3,8 @@ import { compileAttributes } from "./attributes";
 import type { Token } from "./parse/types/token";
 import type { ComboParam } from "./primitiveParams";
 
-describe("compileAttributes - combo and select", () => {
-  test("", () => {
+describe("compileAttributes - combo", () => {
+  test("empty options", () => {
     const token: Token[] = [
       { keyword: "type", value: "combo" },
       { keyword: "default", value: "option1" },
@@ -17,6 +17,28 @@ describe("compileAttributes - combo and select", () => {
       kind: "combo",
       default: "option1",
       options: [],
+      text: "a combo",
+      desc: "this is a combo",
+      parent: "parentId",
+    };
+    const result = compileAttributes(token);
+    expect(result).toEqual(expected);
+  });
+  test("with options", () => {
+    const token: Token[] = [
+      { keyword: "type", value: "combo" },
+      { keyword: "default", value: "option1" },
+      { keyword: "text", value: "a combo" },
+      { keyword: "desc", value: "this is a combo" },
+      { keyword: "parent", value: "parentId" },
+      { keyword: "option", value: "a" },
+      { keyword: "option", value: "b" },
+    ];
+
+    const expected: ComboParam = {
+      kind: "combo",
+      default: "option1",
+      options: ["a", "b"],
       text: "a combo",
       desc: "this is a combo",
       parent: "parentId",
