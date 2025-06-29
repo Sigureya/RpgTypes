241e906188c0b70ab6a870749adbc24705cfe9a7
Sigureya <sigureya1208@gmail.com>
2025-06-29 19:44:38 +0900

    古いテストを一度削除。後々で作り直す

--- src/rmmz/plugin/schema/compile/kinds/core/attributes.dataId.test.ts
+++ src/rmmz/plugin/schema/compile/kinds/core/attributes.dataId.test.ts
@@ -1,7 +1,5 @@
 import { describe, expect, test } from "vitest";
-import { compileAttributes } from "./attributes";
-import { compileAttributes2 } from "./paramAttributeBuild2";
-import type { Token } from "./parse/types/token";
+import { compileAttributes2 } from "./attributes";
 import type { StructParamPrimitive } from "./primitiveParams";
 
 describe("", () => {
@@ -17,66 +15,3 @@ describe("", () => {
     expect(compileAttributes2(mock)).toEqual(expected);
   });
 });
-
-interface TestCase {
-  caseName: string;
-  input: Token[];
-  expected: Extract<StructParamPrimitive, { default: number[] }>;
-}
-
-const runTestCases = (groupName: string, testCases: TestCase[]) => {
-  describe(`compileAttributes - ${groupName}`, () => {
-    testCases.forEach(({ caseName, input, expected }) => {
-      test(`${caseName} - ${expected.kind}`, () => {
-        const result = compileAttributes(input);
-        expect(result).toEqual(expected);
-      });
-    });
-  });
-};
-runTestCases("numberArray", [
-  {
-    caseName: "最小セット(actor[])",
-    input: [
-      { keyword: "type", value: "actor[]" },
-      { keyword: "default", value: `["1", "2", "3"]` },
-    ],
-    expected: {
-      kind: "actor[]",
-      default: [1, 2, 3],
-    },
-  },
-  {
-    caseName: "空の配列(actor[])",
-    input: [
-      { keyword: "type", value: "actor[]" },
-      { keyword: "default", value: `[]` },
-    ],
-    expected: {
-      kind: "actor[]",
-      default: [],
-    },
-  },
-  {
-    caseName: "最小セット(number[])",
-    input: [
-      { keyword: "type", value: "number[]" },
-      { keyword: "default", value: `["1", "2", "3"]` },
-    ],
-    expected: {
-      kind: "number[]",
-      default: [1, 2, 3],
-    },
-  },
-  {
-    caseName: "空の配列(number[])",
-    input: [
-      { keyword: "type", value: "number[]" },
-      { keyword: "default", value: `[]` },
-    ],
-    expected: {
-      kind: "number[]",
-      default: [],
-    },
-  },
-]);
