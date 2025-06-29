6a4d9a1ac6d61e5c0b138935eca5649d83b2b2e3
Sigureya <sigureya1208@gmail.com>
2025-06-30 08:40:33 +0900

    先行してテストコードを作成

---
+++ src/rmmz/plugin/schema/compile/kinds/core/parse/parse.option.test.ts
@@ -0,0 +1,107 @@
+import { describe, test, expect } from "vitest";
+import type { ParsedPlugin } from "./parseV2";
+import { parsePlugin } from "./parseV2";
+
+describe("parsePlugin", () => {
+  test("", () => {
+    const tokens: string[] = [
+      "@param list",
+      "@type select",
+      "@default L",
+      "@option tubasa",
+      "@value L",
+      "@option komachi",
+      "@value Z",
+      "@option east-i",
+    ];
+    const expected: ParsedPlugin = {
+      meta: {},
+      helpLines: [],
+      params: [
+        {
+          name: "list",
+          attr: {
+            kind: "select",
+            default: "L",
+          },
+          options: [
+            { option: "tubasa", value: "L" },
+            { option: "komachi", value: "Z" },
+            { option: "east-i", value: "east-i" },
+          ],
+        },
+      ],
+      commands: [],
+    };
+    const result: ParsedPlugin = parsePlugin(tokens.join("\n"));
+    expect(result).toEqual(expected);
+  });
+
+  test("", () => {
+    const tokens: string[] = [
+      "@param list",
+      "@type select",
+      "@default 300",
+      "@option nozomi",
+      "@value 300",
+      "@option hikari",
+      "@text tokaido",
+      "@value 100",
+      "@option kodama",
+      "@value 0",
+    ];
+    const expected: ParsedPlugin = {
+      meta: {},
+      helpLines: [],
+      params: [
+        {
+          name: "list",
+          attr: {
+            text: "tokaido",
+            kind: "select",
+            default: "300",
+          },
+          options: [
+            { option: "nozomi", value: "300" },
+            { option: "hikari", value: "100" },
+            { option: "kodama", value: "0" },
+          ],
+        },
+      ],
+      commands: [],
+    };
+    const result: ParsedPlugin = parsePlugin(tokens.join("\n"));
+    expect(result).toEqual(expected);
+  });
+  test("", () => {
+    const tokens: string[] = [
+      "@param list",
+      "@type combo",
+      "@option hayabusa",
+      "@option komachi",
+      "@option kagayaki",
+      "@default ",
+    ];
+    const expected: ParsedPlugin = {
+      meta: {},
+      helpLines: [],
+      params: [
+        {
+          name: "list",
+          attr: {
+            kind: "combo",
+            default: "",
+          },
+          options: [
+            { option: "hayabusa", value: "hayabusa" },
+            { option: "komachi", value: "komachi" },
+            { option: "kagayaki", value: "kagayaki" },
+          ],
+        },
+      ],
+      commands: [],
+    };
+    const result: ParsedPlugin = parsePlugin(tokens.join("\n"));
+    expect(result).toEqual(expected);
+  });
+});
