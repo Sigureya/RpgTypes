94f801138f355cb32d1422618cc5e52f1277fb09
Sigureya <sigureya1208@gmail.com>
2025-06-29 19:32:09 +0900

    新しい関数のテストに切り替え

--- src/rmmz/plugin/schema/compile/kinds/core/attributes.boolean.test.ts
+++ src/rmmz/plugin/schema/compile/kinds/core/attributes.boolean.test.ts
@@ -1,63 +1,24 @@
 import { describe, expect, test } from "vitest";
-import { compileAttributes } from "./attributes";
+import type { ParamSoruceRecord } from "./paramAttributeBuild2";
 import { compileAttributes2 } from "./paramAttributeBuild2";
-import type { Token } from "./parse/types/token";
-import type { TokenOf } from "./parse/types/tokenOf";
 import type { BooleanParam } from "./primitiveParams";
 
 describe("compileAttributes - boolean", () => {
   test("minimum set", () => {
-    const tokens: Token[] = [
-      { keyword: "type", value: "boolean" },
-      { keyword: "default", value: "true" },
-    ];
-    const result = compileAttributes(tokens);
+    const tokens: ParamSoruceRecord<BooleanParam> = {
+      kind: "boolean",
+      default: "true",
+    };
+    const result = compileAttributes2(tokens);
     const expected: BooleanParam = {
       default: true,
       kind: "boolean",
     };
     expect(result).toEqual(expected);
   });
-  test("minimum set", () => {
-    const tokens: TokenOf<BooleanParam>[] = [
-      { keyword: "default", value: "false" },
-      { keyword: "type", value: "boolean" },
-    ];
-    const result = compileAttributes(tokens);
-    const expected: BooleanParam = {
-      default: false,
-      kind: "boolean",
-    };
-    expect(result).toEqual(expected);
-  });
 
   test("full set", () => {
-    const tokens: Token[] = [
-      { keyword: "type", value: "boolean" },
-      { keyword: "default", value: "false" },
-      { keyword: "text", value: "Enabled?" },
-      { keyword: "desc", value: "Is this feature enabled?" },
-      { keyword: "on", value: "Yes" },
-      { keyword: "off", value: "No" },
-      { keyword: "parent", value: "Parent Feature" },
-    ];
-    const result = compileAttributes(tokens);
-    const expected: BooleanParam = {
-      default: false,
-      text: "Enabled?",
-      desc: "Is this feature enabled?",
-      on: "Yes",
-      off: "No",
-      parent: "Parent Feature",
-      kind: "boolean",
-    };
-    expect(result).toEqual(expected);
-  });
-});
-
-describe("compileAttributes - boolean", () => {
-  test("full set", () => {
-    const tokens: Record<keyof BooleanParam, string> = {
+    const tokens: ParamSoruceRecord<BooleanParam> = {
       kind: "boolean",
       default: "false",
       text: "Enabled?",
