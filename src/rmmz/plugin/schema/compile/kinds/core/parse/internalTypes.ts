9ec48bdffc040f9a7ed678b824fda3c3df36a16a
Sigureya <sigureya1208@gmail.com>
2025-07-01 08:07:08 +0900

    Parse処理の間だけ使う型を独立。外部用の型と区別する

---
+++ src/rmmz/plugin/schema/compile/kinds/core/parse/internalTypes.ts
@@ -0,0 +1,18 @@
+import type { KeywordEnum } from "./keyword/types";
+import type { OptionItem } from "./selectOption";
+import type { PluginParamTokens, PluginCommandTokens } from "./types";
+
+export interface OptionsState {
+  items: OptionItem[];
+  currentOption?: string;
+}
+
+export interface ParseState {
+  helpLines: string[];
+  params: PluginParamTokens[];
+  commands: PluginCommandTokens[];
+  currentParam: PluginParamTokens | null;
+  currentCommand: PluginCommandTokens | null;
+  currentContext: KeywordEnum | null;
+  currentOption: OptionsState | null;
+}
