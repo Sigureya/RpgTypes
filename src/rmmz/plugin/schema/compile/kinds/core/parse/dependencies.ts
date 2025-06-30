6b87b309aa514f812feac0a233f5c59ef52341c9
Sigureya <sigureya1208@gmail.com>
2025-07-01 08:42:23 +0900

    依存関係を追加登録する関数

--- src/rmmz/plugin/schema/compile/kinds/core/parse/dependencies.ts
+++ src/rmmz/plugin/schema/compile/kinds/core/parse/dependencies.ts
@@ -3,3 +3,36 @@ export interface PluginDependencies {
   orderBefore: string[];
   orderAfter: string[];
 }
+
+export const addBasePlugin = (
+  dep: PluginDependencies,
+  base: string
+): PluginDependencies => {
+  return {
+    orderAfter: dep.orderAfter,
+    orderBefore: dep.orderBefore,
+    base: dep.base.concat(base),
+  };
+};
+
+export const addOrderBefore = (
+  dep: PluginDependencies,
+  order: string
+): PluginDependencies => {
+  return {
+    base: dep.base,
+    orderAfter: dep.orderAfter,
+    orderBefore: dep.orderBefore.concat(order),
+  };
+};
+
+export const addOrderAfter = (
+  dep: PluginDependencies,
+  order: string
+): PluginDependencies => {
+  return {
+    base: dep.base,
+    orderBefore: dep.orderBefore,
+    orderAfter: dep.orderAfter.concat(order),
+  };
+};
