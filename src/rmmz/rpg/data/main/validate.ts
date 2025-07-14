074eed8368a4c350b1329430df2f5eaab8391d3f
Sigureya <sigureya1208@gmail.com>
2025-07-14 11:23:34 +0900

    スキル関係のschemaを新型に差し替え。いくつかの定義を再利用してメモリを節約

--- src/rmmz/rpg/data/main/validate.ts
+++ src/rmmz/rpg/data/main/validate.ts
@@ -1,4 +1,5 @@
 import Ajv from "ajv";
+import { SCHEMA_DATA_ITEM, SCHEMA_DATA_SKILL } from "./schema";
 import type {
   Data_Actor,
   Data_Armor,
@@ -16,46 +17,45 @@ import {
   SCHEMA_DATA_WEAPON,
 } from "./traitContainers";
 import type { Data_Item, Data_Skill } from "./usableItems";
-import { SCHEMA_DATA_ITEM, SCHEMA_DATA_SKILL } from "./usableItems";
 
 const ajv = new Ajv();
 const item = ajv.compile(SCHEMA_DATA_ITEM);
-const skill = ajv.compile(SCHEMA_DATA_SKILL);
-const actor = ajv.compile(SCHEMA_DATA_ACTOR);
-const armor = ajv.compile(SCHEMA_DATA_ARMMOR);
-const weapon = ajv.compile(SCHEMA_DATA_WEAPON);
-const enemy = ajv.compile(SCHEMA_DATA_ENEMY);
-const state = ajv.compile(SCHEMA_DATA_STATE);
-const class_ = ajv.compile(SCHEMA_DATA_CLASS);
 
 export const isDataItem = (data: unknown): data is Data_Item => {
   return item(data);
 };
 
+const skill = ajv.compile(SCHEMA_DATA_SKILL);
 export const isDataSkill = (data: unknown): data is Data_Skill => {
   return skill(data);
 };
 
+const actor = ajv.compile(SCHEMA_DATA_ACTOR);
 export const isDataActor = (data: unknown): data is Data_Actor => {
   return actor(data);
 };
 
+const armor = ajv.compile(SCHEMA_DATA_ARMMOR);
 export const isDataArmor = (data: unknown): data is Data_Armor => {
   return armor(data);
 };
 
+const weapon = ajv.compile(SCHEMA_DATA_WEAPON);
 export const isDataWeapon = (data: unknown): data is Data_Weapon => {
   return weapon(data);
 };
 
+const enemy = ajv.compile(SCHEMA_DATA_ENEMY);
 export const isDataEnemy = (data: unknown): data is Data_Enemy => {
   return enemy(data);
 };
 
+const state = ajv.compile(SCHEMA_DATA_STATE);
 export const isDataState = (data: unknown): data is Data_State => {
   return state(data);
 };
 
+const class_ = ajv.compile(SCHEMA_DATA_CLASS);
 export const isDataClass = (data: unknown): data is Data_Class => {
   return class_(data);
 };
