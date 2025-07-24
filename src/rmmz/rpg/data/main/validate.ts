83580a8b9e3020fa9cefcec8a327ca7c9018af8c
Sigureya <sigureya1208@gmail.com>
2025-07-25 00:23:52 +0900

    厳密な実装に切り替え。new Function()に引っかかってた

--- src/rmmz/rpg/data/main/validate.ts
+++ src/rmmz/rpg/data/main/validate.ts
@@ -18,7 +18,10 @@ import {
 } from "./traitContainers";
 import type { Data_Item, Data_Skill } from "./usableItems";
 
-const ajv = new Ajv();
+const ajv = new Ajv({
+  code: { source: true },
+  strict: true,
+});
 const item = ajv.compile(SCHEMA_DATA_ITEM);
 
 export const isDataItem = (data: unknown): data is Data_Item => {
