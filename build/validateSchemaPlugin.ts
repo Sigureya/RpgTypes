b6d0ca5af82d7229b38d00008b19392bb866a627
Sigureya <sigureya1208@gmail.com>
2025-07-26 00:02:16 +0900

    .d.tsも自動生成

--- build/validateSchemaPlugin.ts
+++ build/validateSchemaPlugin.ts
@@ -6,24 +6,41 @@ import Ajv from "ajv";
 import standaloneCode from "ajv/dist/standalone";
 import { PluginOption } from "vite";
 
-const build = async (schemaPath: string) => {
-  const fullPath = path.resolve(schemaPath);
-  const schemaText = await fs.readFile(fullPath, "utf-8");
-  const schemaJson = JSON.parse(schemaText);
+const makeDTS = (filename: string): string => {
+  return `declare const validate(data: unknown): boolean;
+  export = validate;
+  `;
+};
 
-  const ajv = new Ajv({ code: { source: true }, strict: true });
-  const validate = ajv.compile(schemaJson);
-  const standalone = standaloneCode(ajv, validate);
+const build = async (schemaPath: string): Promise<void> => {
+  const fullPath = path.resolve(schemaPath);
 
   // e.g. person/person.schema.json → person/validatePerson.ts
   const parsed = path.parse(fullPath);
   const fileBase = parsed.name.replace(/\.schema$/, ""); // e.g. "person"
 
-  const targetFile = path.join(parsed.dir, `${fileBase}Validate.cjs`);
-  const codeWithExport = standalone;
+  const filename = `${fileBase}Validate`;
+  console.log(`✅ バリデーション関数を生成: ${filename}`);
 
-  await fs.writeFile(targetFile, codeWithExport, "utf-8");
-  console.log(`✅ バリデーション関数を生成: ${targetFile}`);
+  await Promise.all([
+    async () => {
+      const schemaText = await fs.readFile(fullPath, "utf-8");
+      const schemaJson = JSON.parse(schemaText);
+
+      const ajv = new Ajv({ code: { source: true }, strict: true });
+      const validate = ajv.compile(schemaJson);
+      const standalone: string = standaloneCode(ajv, validate);
+
+      const targetFile = path.join(parsed.dir, `${filename}.cjs`);
+
+      await fs.writeFile(targetFile, standalone, "utf-8");
+    },
+    async () => {
+      const dtsFilename = path.join(parsed.dir, `${filename}.d.ts`);
+      const dtsContent = makeDTS(filename);
+      await fs.writeFile(dtsFilename, dtsContent, "utf-8");
+    },
+  ]);
 };
 
 export function validateSchemaPlugin(): PluginOption {
