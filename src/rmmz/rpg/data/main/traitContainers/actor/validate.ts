db2a5671cce71448dc858c94d14690a7e04c2f56
Sigureya <sigureya1208@gmail.com>
2025-07-26 07:04:11 +0900

    古い方を消したので2が不要になった

--- src/rmmz/rpg/data/main/traitContainers/actor/validate.ts
+++ src/rmmz/rpg/data/main/traitContainers/actor/validate.ts
@@ -1,6 +1,6 @@
 import validate from "./actorValidate";
 import type { Data_Actor } from "./types";
 
-export const isDataActor2 = (data: unknown): data is Data_Actor => {
+export const isDataActor = (data: unknown): data is Data_Actor => {
   return validate(data);
 };
