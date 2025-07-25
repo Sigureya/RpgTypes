1cc68f8cfb7ceb16826153fa3bebfb7c3be4fde7
Sigureya <sigureya1208@gmail.com>
2025-07-25 19:01:51 +0900

    単一exportを徹底するため、別ファイルに分割

---
+++ src/rmmz/plugin/schema/compile/kinds/core/x-rpg-paramTypes.ts
@@ -0,0 +1,6 @@
+import type { SourceIdentifier } from "src/namedItemSource";
+import type { X_ParamData, X_RmmzParam } from "./paramBase/x-rpg-param";
+import type { NumberParam } from "./primitiveParams";
+
+export type X_ParamDataId = X_RmmzParam<SourceIdentifier, "dataId">;
+export type X_ParamNumber = X_ParamData<Omit<NumberParam, "min" | "max">>;
