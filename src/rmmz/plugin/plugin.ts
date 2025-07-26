ccd7e149959f0b16f8dabf1fe5530255edbe5f9d
Sigureya <sigureya1208@gmail.com>
2025-07-26 17:23:38 +0900

    戻り値関連で問題が起きているので、明示的にしてみる

--- src/rmmz/plugin/plugin.ts
+++ src/rmmz/plugin/plugin.ts
@@ -2,8 +2,9 @@ import type { PrimitiveParam } from "./schema/compile/kinds";
 import type { PrimitiveStructSchema } from "./schema/compile/kinds/compileFlatStruct";
 import { compileFlatStruct } from "./schema/compile/kinds/compileFlatStruct";
 import { parsePlugin } from "./schema/compile/kinds/core/parsePlugin";
+import type { PluginJSON } from "./schema/compile/kinds/core/pluginJSONTypes";
 
-export const pluginSourceToJSON = (text: string) => {
+export const pluginSourceToJSON = (text: string): PluginJSON => {
   return parsePlugin(text);
 };
 
