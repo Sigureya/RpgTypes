b1780cdd74de2dcd1543bacb288e9e93e06c41de
Sigureya <sigureya1208@gmail.com>
2025-07-27 04:32:36 +0900

    各種補助関数。eslintに引っかかっているが、後で修正する

---
+++ src/features/eventCommand/commandGroup/core/utils/pick.ts
@@ -0,0 +1,26 @@
+import type { EventCommand, ExtractCommandByParam } from "@sigureya/rpgtypes";
+
+export const pickCommands = <
+  Head extends EventCommand,
+  Body extends ExtractCommandByParam<[string]>
+>(
+  array: ReadonlyArray<unknown>,
+  index: number,
+  headFn: (data: unknown) => data is Head,
+  bodyFn: (data: unknown) => data is Body
+) => {
+  const head = array[index];
+  if (!headFn(head)) {
+    throw new Error(`Invalid head at index ${index}: ${JSON.stringify(head)}`);
+  }
+  const bodys: Body[] = [];
+  for (let i = index + 1; i < array.length; i++) {
+    const body = array[i];
+    if (bodyFn(body)) {
+      bodys.push(body);
+    } else {
+      break;
+    }
+  }
+  return { header: head, bodies: bodys };
+};
