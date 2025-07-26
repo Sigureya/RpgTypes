06ed9d47e3a1ff143c2238075d599be471bef34f
Sigureya <sigureya1208@gmail.com>
2025-07-26 21:08:09 +0900

    exportを追加。これでビルド対象に含まれるようになった

--- src/validate/rmmz/rpg/index.ts
+++ src/validate/rmmz/rpg/index.ts
@@ -1,4 +1,8 @@
-export { isDataActor } from "./actor/validate";
-
-export { isDataArmor as isDataArmor2 } from "./armor/isDataArmor";
+export { isDataActor } from "./actor/isDataActor";
+export { isDataArmor } from "./armor/isDataArmor";
 export { isDataClass } from "./class/isDataClass";
+export { isDataEnemy } from "./enemy/isDataEnemy";
+export { isDataItem } from "./item/isDataItem";
+export { isDataSkill } from "./skill/isDataSkill";
+export { isDataState } from "./state/isDataState";
+export { isDataWeapon } from "./weapon/isDataWeapon";
