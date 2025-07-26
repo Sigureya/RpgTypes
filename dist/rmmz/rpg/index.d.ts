380fe6128326e6cf73aa6537b963196eb880a9d9
Sigureya <sigureya1208@gmail.com>
2025-07-26 15:29:39 +0900

    整理しての移動が大変になってきたので一括で移動

--- dist/rmmz/rpg/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './data';
-export * from './labelRegistry';
-export * from './labels';
-export * from './rpgLabels';
-export { labelsRegistry } from './labelRegistryDefault';
--- dist/rmmz/rpg/rpgLabels.d.ts
+++
@@ -1,8 +0,0 @@
-import { DomainLabel } from '../../libs/templates';
-import { DataLabels, TraitLabelSet, ItemEffectLabelSet, DamageLabels } from './data';
-export interface RpgLabels {
-    data: DataLabels;
-    traits: DomainLabel<TraitLabelSet>;
-    itemEffect: DomainLabel<ItemEffectLabelSet>;
-    damage: DomainLabel<DamageLabels>;
-}
