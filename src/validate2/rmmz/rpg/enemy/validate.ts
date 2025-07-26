233e0262a0f5c9093d65158c0f0947fa1f28f584
Sigureya <sigureya1208@gmail.com>
2025-07-26 16:46:32 +0900

    enemy関連を移動

--- src/validate2/rmmz/rpg/enemy/validate.ts
+++ src/validate2/rmmz/rpg/enemy/validate.ts
@@ -1,5 +1,5 @@
+import type { Data_Enemy } from "@RpgTypes/rpg";
 import validate from "./enemyValidate";
-import type { Data_Enemy } from "./types";
 
 export const isDataEnemy = (data: unknown): data is Data_Enemy => {
   return validate(data);
