663602749f73dc12f58061fe34171ea0da16c65b
Sigureya <sigureya1208@gmail.com>
2025-07-27 16:06:59 +0900

    実装途中だけどコミット。他の箇所を修正しないと動かない

---
+++ src/features/core/eventCommand/allMapping.test.ts
@@ -0,0 +1,216 @@
+import { describe, expect, test, vi } from "vitest";
+import { makeAudioFileParams } from "@RpgTypes/libs";
+import { makeCommandChangeBattleBGM } from "@RpgTypes/rmmz";
+import * as CMD from "@sigureya/rpgtypes";
+import { mappingCommand } from "./allMapping";
+import type { BasicMappingObject } from "./types/basicCommandsMapper";
+
+const createMockMapper = <Key extends string & keyof BasicMappingObject<void>>(
+  targetKey: Key
+) => {
+  return {
+    [targetKey]: vi.fn<BasicMappingObject<void>[Key]>(),
+    other: vi.fn(),
+  };
+};
+const testMapping = <Command extends CMD.EventCommand>(
+  key: keyof BasicMappingObject<void>,
+  command: Command
+) => {
+  test(`${key} should be called with correct arguments`, () => {
+    const mapper = createMockMapper(key);
+    mappingCommand([command], 0, mapper);
+    expect(mapper[key]).toHaveBeenCalledWith(command, 0, [command]);
+
+    // 他の関数が呼ばれていないことを確認
+    Object.entries(mapper).forEach(([k, mockFn]) => {
+      if (k !== key) {
+        expect(mockFn).toHaveBeenCalledTimes(0);
+      }
+    });
+  });
+};
+
+describe("mappingCommand", () => {
+  const mockColor: CMD.ColorRGBA = [0, 0, 0, 0];
+
+  testMapping<CMD.Command_Wait>("wait", {
+    code: CMD.WAIT,
+    indent: 0,
+    parameters: [0],
+  });
+  testMapping<CMD.Command_CommonEvent>("commonEvent", {
+    code: CMD.COMMON_EVENT,
+    indent: 0,
+    parameters: [1],
+  });
+  testMapping<CMD.Command_ChangeBattleBGM>(
+    "changeBattleBGM",
+    makeCommandChangeBattleBGM(makeAudioFileParams())
+  );
+  testMapping<CMD.Command_ChangeVictoryME>(
+    "changeVictoryME",
+    CMD.makeCommandChangeVictoryME(makeAudioFileParams())
+  );
+  testMapping<CMD.Command_ChangeActorName>("changeName", {
+    code: CMD.CHANGE_NAME,
+    indent: 0,
+    parameters: [1, "name"],
+  });
+  testMapping<CMD.Command_ChangeActorProfile>("changeProfile", {
+    code: CMD.CHANGE_PROFILE,
+    indent: 0,
+    parameters: [1, "profile"],
+  });
+  testMapping<CMD.Command_ChangeActorNickName>("changeNickname", {
+    code: CMD.CHANGE_NICKNAME,
+    indent: 0,
+    parameters: [1, "nickname"],
+  });
+  testMapping<CMD.Command_Label>("label", {
+    code: CMD.LABEL,
+    indent: 0,
+    parameters: ["label"],
+  });
+  testMapping<CMD.Command_LabelJump>("labelJump", {
+    code: CMD.LABEL_JUMP,
+    indent: 0,
+    parameters: ["label"],
+  });
+  testMapping<CMD.Command_ChangeActorImages>(
+    "changeActorImages",
+    CMD.makeCommandChangeActorImages({
+      actorId: 1,
+      faceIndex: 0,
+      characterIndex: 0,
+    })
+  );
+  testMapping<CMD.Command_ChangeWindowColor>("changeWindowColor", {
+    code: CMD.CHANGE_WINDOW_COLOR,
+    indent: 0,
+    parameters: [mockColor],
+  });
+  testMapping<CMD.Command_ChangeGold>("changeGold", {
+    code: CMD.CHANGE_GOLD,
+    indent: 0,
+    parameters: [0, 0, 0],
+  });
+  testMapping<CMD.Command_ShowAnimation>("showAnimation", {
+    code: CMD.SHOW_ANIMATION,
+    indent: 0,
+    parameters: [0, 0, 0],
+  });
+
+  testMapping<CMD.Command_TransferPlayer>("transferPlayer", {
+    code: CMD.TRANSFER_PLAYER,
+    indent: 0,
+    parameters: [0, 0, 0, 0, 4, 0],
+  });
+  testMapping<CMD.Command_SetVehicleLocation>("setVehicleLocation", {
+    code: CMD.SET_VEHICLE_LOCATION,
+    indent: 0,
+    parameters: [0, 0, 0, 0, 0],
+  });
+  testMapping<CMD.Command_SetEventLocation>("setEventLocation", {
+    code: CMD.SET_EVENT_LOCATION,
+    indent: 0,
+    parameters: [0, 0, 0, 0, 6],
+  });
+  testMapping<CMD.Command_GameOver>("gameover", {
+    code: CMD.GAME_OVER,
+    indent: 0,
+    parameters: [],
+  });
+  testMapping<CMD.Command_TintScreen>("tintScreen", {
+    code: CMD.TINT_SCREEN,
+    indent: 0,
+    parameters: [mockColor, 0, false],
+  });
+  testMapping<CMD.Command_FlashScreen>("flashScreen", {
+    code: CMD.FLASH_SCREEN,
+    indent: 0,
+    parameters: [mockColor, 0, false],
+  });
+  testMapping<CMD.Command_ShakeScreen>("shakeScreen", {
+    code: CMD.SHAKE_SCREEN,
+    indent: 0,
+    parameters: [0, 0, 0, false],
+  });
+  testMapping<CMD.Command_BattleProcessing>("battleProcessing", {
+    code: CMD.BATTLE_PROCESSING,
+    indent: 0,
+    parameters: [0, 0, false, false],
+  });
+  testMapping<CMD.Command_ChangeEncounter>("changeEncounter", {
+    code: CMD.CHANGE_ENCOUNTER,
+    indent: 0,
+    parameters: [0],
+  });
+  testMapping<CMD.Command_ChangeFormationAccess>("changeFormationAccess", {
+    code: CMD.CHANGE_FORMATION_ACCESS,
+    indent: 0,
+    parameters: [1],
+  });
+  testMapping<CMD.Command_ChangeMenuAccess>("changeMenuAccess", {
+    code: CMD.CHANGE_MENU_ACCESS,
+    indent: 0,
+    parameters: [1],
+  });
+  testMapping<CMD.Command_ChangeSaveAccess>("changeSaveAccess", {
+    code: CMD.CHANGE_SAVE_ACCESS,
+    indent: 0,
+    parameters: [1],
+  });
+  testMapping<CMD.Command_ChangeDefeatME>("changeDefeatME", {
+    code: CMD.CHANGE_DEFEAT_ME,
+    indent: 0,
+    parameters: [makeAudioFileParams()],
+  });
+  testMapping<CMD.Command_ChangeVehicleImage>("changeVehicleImage", {
+    code: CMD.CHANGE_VEHICLE_IMAGE,
+    indent: 0,
+    parameters: [0, "image", 2],
+  });
+  testMapping<CMD.Command_ShowPicture>("showPicture", RmmzMock.MockShowPicture);
+  testMapping<CMD.Command_MovePicture>("movePicture", RmmzMock.MockMovePicture);
+  testMapping<CMD.Command_RotatePicture>("rotatePicture", {
+    code: CMD.ROTATE_PICTURE,
+    indent: 0,
+    parameters: [0, 0],
+  });
+  testMapping<CMD.Command_TintPicture>("tintPicture", RmmzMock.MockTintPicture);
+  testMapping<CMD.Command_ErasePicture>("erasePicture", {
+    code: CMD.ERASE_PICTURE,
+    indent: 0,
+    parameters: [0],
+  });
+  testMapping<CMD.Command_SetWeatherEffect>("setWeatherEffects", {
+    code: CMD.SET_WEATHER_EFFECT,
+    indent: 0,
+    parameters: ["", 0, 0, false],
+  });
+  testMapping<CMD.Command_PlayBGM>("playBGM", RmmzMock.MockPlayBGM);
+  testMapping<CMD.Command_FadeOutBGM>("fadeOutBGM", RmmzMock.MockFadeOutBGM);
+  testMapping<CMD.Command_PlaySE>("playSE", RmmzMock.MockPlaySE);
+  testMapping<CMD.Command_StopSE>("stopSE", {
+    code: CMD.STOP_SE,
+    indent: 0,
+    parameters: [],
+  });
+  testMapping<CMD.Command_PlayMovie>("playMovie", {
+    code: CMD.PLAY_MOVIE,
+    indent: 0,
+    parameters: [""],
+  });
+  // testMapping<CMD.Command_ChangePlayerFollowers>("changePlayerFollowers", {
+  //   code: CMD.CHANGE_PLAYER_FOLLOWERS,
+  //   indent: 0,
+  //   parameters: [0, 0],
+  // });
+
+  testMapping<CMD.Command_ChangeTransparency>("changeTransparency", {
+    code: CMD.CHANGE_TRANSPARENCY,
+    indent: 0,
+    parameters: [0],
+  });
+});
---
+++ src/features/core/eventCommand/allMapping.ts
@@ -0,0 +1,375 @@
+import type { EventCommand } from "@RpgTypes/rmmz";
+import * as Code from "@RpgTypes/rmmz";
+import {
+  createMessageGroup,
+  createScrollTextGroup,
+  createCommentGroup,
+  createScriptGroup,
+} from "./commandGroup";
+import type { PartialMappingObject } from "./mapperType";
+import type { CallBackFunc } from "./types/basicCommandsMapper";
+
+export const callHandler = <T, Command extends EventCommand>(
+  command: Command,
+  index: number,
+  array: ReadonlyArray<EventCommand>,
+  handler: CallBackFunc<Command, T> | undefined,
+  fallback: CallBackFunc<EventCommand, T>
+): T => {
+  return handler
+    ? handler(command, index, array)
+    : fallback(command, index, array);
+};
+
+export const mappingCommandList = <T>(
+  array: ReadonlyArray<EventCommand>,
+  table: PartialMappingObject<T>
+): T[] => {
+  return array.map<T>((command, index) => mappingCommand(array, index, table));
+};
+
+export const flatMappingCommandList = <T>(
+  array: ReadonlyArray<EventCommand>,
+  table: PartialMappingObject<T[]>
+): T[] => {
+  return array.flatMap((command, index) => mappingCommand(array, index, table));
+};
+
+export const mappingCommand = <T>(
+  array: ReadonlyArray<EventCommand>,
+  index: number,
+  table: PartialMappingObject<T>
+): T => {
+  const command: EventCommand = array[index];
+  switch (command.code) {
+    // メッセージ関連
+    case Code.SHOW_MESSAGE:
+      return table.showMessage
+        ? table.showMessage(createMessageGroup(array, index))
+        : table.other(command, index, array);
+    case Code.SHOW_MESSAGE_BODY:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.showMessageBody,
+        table.other
+      );
+    case Code.SHOW_SCROLLING_TEXT:
+      return table.showScrollingText
+        ? table.showScrollingText(createScrollTextGroup(array, index))
+        : table.other(command, index, array);
+    case Code.SHOW_SCROLLING_TEXT_BODY:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.showScrollingTextBody,
+        table.other
+      );
+    case Code.SHOW_CHOICES:
+      return callHandler(command, index, array, table.showChoices, table.other);
+    case Code.SHOW_CHOICES_ITEM:
+      return callHandler(command, index, array, table.choiceWhen, table.other);
+
+    // コメント・スクリプト関連
+    case Code.COMMENT_HEAD:
+      return table.comment
+        ? table.comment(createCommentGroup(array, index))
+        : table.other(command, index, array);
+    case Code.COMMENT_BODY:
+      return callHandler(command, index, array, table.commentBody, table.other);
+    case Code.SCRIPT_EVAL:
+      return table.script
+        ? table.script(createScriptGroup(array, index))
+        : table.other(command, index, array);
+    case Code.SCRIPT_EVAL_BODY:
+      return callHandler(command, index, array, table.scriptBody, table.other);
+
+    // 条件・制御系
+    case Code.CONDITIONAL_BRANCH:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.conditionalBranch,
+        table.other
+      );
+    case Code.CONTROL_SWITCHES:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.controlSwitch,
+        table.other
+      );
+    case Code.CONTROL_VARIABLES:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.controlVariable,
+        table.other
+      );
+    case Code.CONTROL_SELF_SWITCH:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.controlSelfSwitch,
+        table.other
+      );
+    case Code.CONTROL_TIMER:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.controlTimer,
+        table.other
+      );
+
+    case Code.COMMON_EVENT:
+      return callHandler(command, index, array, table.commonEvent, table.other);
+    // メニュー
+    case Code.CHANGE_SAVE_ACCESS:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeSaveAccess,
+        table.other
+      );
+    case Code.CHANGE_MENU_ACCESS:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeMenuAccess,
+        table.other
+      );
+    case Code.CHANGE_ENCOUNTER:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeEncounter,
+        table.other
+      );
+    case Code.CHANGE_FORMATION_ACCESS:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeFormationAccess,
+        table.other
+      );
+
+    // キャラクター情報変更
+    case Code.CHANGE_NAME:
+      return callHandler(command, index, array, table.changeName, table.other);
+    case Code.CHANGE_PROFILE:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeProfile,
+        table.other
+      );
+    case Code.CHANGE_NICKNAME:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeNickname,
+        table.other
+      );
+    case Code.CHANGE_ACTOR_IMAGES:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeActorImages,
+        table.other
+      );
+    case Code.CHANGE_VEHICLE_IMAGE:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeVehicleImage,
+        table.other
+      );
+
+    // BGM・音楽関連
+    case Code.CHANGE_BATTLE_BGM:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeBattleBGM,
+        table.other
+      );
+    case Code.CHANGE_VICTORY_ME:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeVictoryME,
+        table.other
+      );
+    case Code.CHANGE_DEFEAT_ME:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeDefeatME,
+        table.other
+      );
+
+    case Code.PLAY_BGM:
+      return callHandler(command, index, array, table.playBGM, table.other);
+    case Code.FADEOUT_BGM:
+      return callHandler(command, index, array, table.fadeOutBGM, table.other);
+    case Code.PLAY_SE:
+      return callHandler(command, index, array, table.playSE, table.other);
+    case Code.STOP_SE:
+      return callHandler(command, index, array, table.stopSE, table.other);
+    case Code.PLAY_ME:
+      return callHandler(command, index, array, table.playME, table.other);
+
+    // ラベル・ジャンプ
+    case Code.LABEL:
+      return callHandler(command, index, array, table.label, table.other);
+    case Code.LABEL_JUMP:
+      return callHandler(command, index, array, table.labelJump, table.other);
+
+    // 画面演出
+    case Code.CHANGE_WINDOW_COLOR:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeWindowColor,
+        table.other
+      );
+    case Code.TINT_SCREEN:
+      return callHandler(command, index, array, table.tintScreen, table.other);
+    case Code.FLASH_SCREEN:
+      return callHandler(command, index, array, table.flashScreen, table.other);
+    case Code.SHAKE_SCREEN:
+      return callHandler(command, index, array, table.shakeScreen, table.other);
+
+    // ピクチャー
+    case Code.SHOW_PICTURE:
+      return callHandler(command, index, array, table.showPicture, table.other);
+    case Code.MOVE_PICTURE:
+      return callHandler(command, index, array, table.movePicture, table.other);
+    case Code.ROTATE_PICTURE:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.rotatePicture,
+        table.other
+      );
+    case Code.TINT_PICTURE:
+      return callHandler(command, index, array, table.tintPicture, table.other);
+    case Code.ERASE_PICTURE:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.erasePicture,
+        table.other
+      );
+
+    // ゲーム進行
+    case Code.BATTLE_PROCESSING:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.battleProcessing,
+        table.other
+      );
+    case Code.WAIT:
+      return callHandler(command, index, array, table.wait, table.other);
+    case Code.GAME_OVER:
+      return callHandler(command, index, array, table.gameover, table.other);
+
+    // 未整理
+    case Code.CHANGE_PARALLAX:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeParallax,
+        table.other
+      );
+    case Code.CHANGE_BATTLE_BACKGROUND:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeBattleBackground,
+        table.other
+      );
+
+    case Code.PLAY_MOVIE:
+      return callHandler(command, index, array, table.playMovie, table.other);
+    case Code.CHANGE_TRANSPARENCY:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.changeTransparency,
+        table.other
+      );
+    case Code.SET_WEATHER_EFFECT:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.setWeatherEffects,
+        table.other
+      );
+
+    case Code.CHANGE_GOLD:
+      return callHandler(command, index, array, table.changeGold, table.other);
+    case Code.SHOW_ANIMATION:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.showAnimation,
+        table.other
+      );
+    case Code.TRANSFER_PLAYER:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.transferPlayer,
+        table.other
+      );
+    case Code.SET_VEHICLE_LOCATION:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.setVehicleLocation,
+        table.other
+      );
+    case Code.SET_EVENT_LOCATION:
+      return callHandler(
+        command,
+        index,
+        array,
+        table.setEventLocation,
+        table.other
+      );
+
+    default:
+      return table.other(command, index, array);
+  }
+};
