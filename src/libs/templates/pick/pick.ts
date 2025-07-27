f890c151ed2f5a53678f31529500d308f167416e
Sigureya <sigureya1208@gmail.com>
2025-07-27 23:23:10 +0900

    データ取得を追加。外部リポジトリに置いてた機能を移植

---
+++ src/libs/templates/pick/pick.ts
@@ -0,0 +1,26 @@
+import type { PickByType } from "./pickByType";
+
+export type PickableRecordType<T, Value> = Record<
+  string & keyof PickByType<T, Value>,
+  Value
+>;
+
+export const pickPropertys = <
+  Result,
+  Value,
+  T extends PickableRecordType<T, Value>
+>(
+  data: T,
+  keyList: ReadonlyArray<keyof PickableRecordType<T, string>>,
+  fn: (key: string, value: Value, data: T) => Result
+): Result[] => {
+  return keyList.map((key) => fn(key, data[key], data));
+};
+
+export const pickString = <Result, T extends PickableRecordType<T, string>>(
+  data: T,
+  keyList: ReadonlyArray<keyof PickableRecordType<T, string>>,
+  fn: (key: string, value: string, data: T) => Result
+): Result[] => {
+  return pickPropertys(data, keyList, fn);
+};
