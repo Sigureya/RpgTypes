0360649f8f3b6e3a0493d6f6ead68e42c72e02d6
Sigureya <sigureya1208@gmail.com>
2025-07-27 22:46:25 +0900

    イベントコマンドのパラメータを取る関数。型推論を効かせるための機能

---
+++ src/rmmz/eventCommand/pickCommandParam.ts
@@ -0,0 +1,52 @@
+import type { AudioFileParams } from "@RpgTypes/libs";
+import type { IndexOfCommandParameter } from "./utils/pickParam";
+
+export interface CommandParameter<T> {
+  code: number;
+  paramIndex: number;
+  value: T;
+}
+
+export const pickCommandParamString = <
+  Params extends unknown[],
+  Index extends IndexOfCommandParameter<
+    { code: number; parameters: Params },
+    string
+  >
+>(
+  command: { code: number; parameters: Params },
+  index: Index
+): CommandParameter<Params[Index]> => ({
+  code: command.code,
+  paramIndex: index,
+  value: command.parameters[index],
+});
+
+export const pickCommandParamNumber = <
+  Params extends unknown[],
+  Index extends IndexOfCommandParameter<
+    { code: number; parameters: Params },
+    number
+  >
+>(
+  command: { code: number; parameters: Params },
+  index: Index
+): CommandParameter<Params[Index]> => ({
+  code: command.code,
+  paramIndex: index,
+  value: command.parameters[index],
+});
+export const pickCommandParamAudio = <
+  Params extends unknown[],
+  Index extends IndexOfCommandParameter<
+    { code: number; parameters: Params },
+    AudioFileParams
+  >
+>(
+  command: { code: number; parameters: Params },
+  index: Index
+): CommandParameter<Params[Index]> => ({
+  code: command.code,
+  paramIndex: index,
+  value: command.parameters[index],
+});
