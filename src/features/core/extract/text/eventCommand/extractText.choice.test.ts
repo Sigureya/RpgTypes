c88041f3bd60a49513754aca1e35cd6a3509cab6
Sigureya <sigureya1208@gmail.com>
2025-07-28 03:20:14 +0900

    選択肢からのテキスト抽出、のテストコード

---
+++ src/features/core/extract/text/eventCommand/extractText.choice.test.ts
@@ -0,0 +1,30 @@
+import { describe, test, expect } from "vitest";
+import type { Command_ShowChoices } from "@RpgTypes/rmmz";
+import { makeCommandSetupChoice, SHOW_CHOICES } from "@RpgTypes/rmmz";
+import type { CommandParameter } from "@RpgTypes/rmmz/eventCommand/pickCommandParam";
+import { extractTextParamsFromChoice } from "./extractText";
+
+describe("extractTextParamsFromChoice", () => {
+  describe("", () => {
+    const command: Command_ShowChoices = makeCommandSetupChoice({
+      choices: [],
+    });
+    test("", () => {
+      const result = extractTextParamsFromChoice(command);
+      expect(result).toEqual([]);
+    });
+  });
+  describe("", () => {
+    const command: Command_ShowChoices = makeCommandSetupChoice({
+      choices: ["choice1", "choice2"],
+    });
+    const expected: CommandParameter<string>[] = [
+      { code: SHOW_CHOICES, value: "choice1", paramIndex: 0 },
+      { code: SHOW_CHOICES, value: "choice2", paramIndex: 1 },
+    ];
+    test("", () => {
+      const result = extractTextParamsFromChoice(command);
+      expect(result).toEqual(expected);
+    });
+  });
+});
