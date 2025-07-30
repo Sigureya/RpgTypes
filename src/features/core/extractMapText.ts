671702d753cea26ca4411c3e0213143315966a6f
Sigureya <sigureya1208@gmail.com>
2025-07-30 16:01:31 +0900

    マップからのテキスト抽出

--- src/features/core/extractMapText.ts
+++ src/features/core/extractMapText.ts
@@ -1,23 +1,40 @@
-import type { Data_Map } from "@RpgTypes/rmmz";
+import type { NoteReadResult, Data_Map } from "@RpgTypes/rmmz";
+import { readNote } from "@RpgTypes/rmmz";
 import type { TextCommandParameter } from "./extract/text/eventCommand";
 import { extractTextFromEventCommands } from "./getTextFromCommand";
 import { collectMapEvents } from "./rpg";
 
-const xx = (map: Data_Map) => {
-  return collectMapEvents(map, (page, pageIndex, event) => {
-    const texts = extractTextFromEventCommands(page.list);
-    return [
-      {
-        eventId: event.id,
-        pageIndex,
-        texts,
-      },
-    ];
-  });
-};
+export interface ExtractedMapTexts {
+  commands: ExtractedMapEventTexts[];
+  note: string;
+  noteItems: NoteReadResult[];
+  displayedName: string;
+}
 
-export interface ExtractedEventText {
+export interface ExtractedMapEventTexts {
   eventId: number;
   pageIndex: number;
   commands: TextCommandParameter[];
+  note: string;
+  noteItems: NoteReadResult[];
 }
+
+export const extractMapText = (map: Data_Map): ExtractedMapTexts => ({
+  note: map.note,
+  noteItems: readNote(map.note),
+  displayedName: map.displayName,
+  commands: extractMapEventTexts(map),
+});
+
+const extractMapEventTexts = (map: Data_Map): ExtractedMapEventTexts[] => {
+  return collectMapEvents(
+    map,
+    (page, pageIndex, event): ExtractedMapEventTexts => ({
+      eventId: event.id,
+      pageIndex,
+      commands: extractTextFromEventCommands(page.list),
+      note: event.note,
+      noteItems: [],
+    })
+  );
+};
