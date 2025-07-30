6e9c177f0e347ebf7a0f6cc2ac77aae7cbd8522d
Sigureya <sigureya1208@gmail.com>
2025-07-30 15:38:09 +0900

    バトルイベント及びコモンイベントの抽出関数

---
+++ src/features/core/extractEventText.ts
@@ -0,0 +1,50 @@
+import type {
+  Data_CommonEvent,
+  Data_Troop,
+  EventCommand,
+} from "@RpgTypes/rmmz";
+import type { TextCommandParameter } from "./extract/text/eventCommand";
+import { extractTextFromEventCommands } from "./getTextFromCommand";
+import { processCommonEvents, processTroopEvents } from "./rpg";
+
+export interface ExtractedCommonEventText {
+  eventId: number;
+  commands: TextCommandParameter[];
+}
+
+export const extractCommonEventTexts = (
+  commons: Data_CommonEvent[]
+): ExtractedCommonEventText[] => {
+  return processCommonEvents(
+    commons,
+    (
+      page: { list: EventCommand[] },
+      pageIndex: number,
+      { id }: { id: number }
+    ) => ({
+      eventId: id,
+      commands: extractTextFromEventCommands(page.list),
+    })
+  );
+};
+
+export interface ExtractedBattleEventText {
+  eventId: number;
+  commands: TextCommandParameter[];
+  pageIndex: number;
+}
+
+export const expectBattleEventTexts = (list: ReadonlyArray<Data_Troop>) => {
+  return processTroopEvents(
+    list,
+    (
+      page: { list: EventCommand[] },
+      pageIndex: number,
+      { id }: { id: number }
+    ): ExtractedBattleEventText => ({
+      eventId: id,
+      pageIndex,
+      commands: extractTextFromEventCommands(page.list),
+    })
+  );
+};
