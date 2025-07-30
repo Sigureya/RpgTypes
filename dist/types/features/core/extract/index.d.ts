23e7300b0c4d2a005588111bcf2edeec7adadfea
Sigureya <sigureya1208@gmail.com>
2025-07-30 19:41:56 +0900

    indexを追加して再ビルド

--- dist/types/features/core/extract/index.d.ts
+++ dist/types/features/core/extract/index.d.ts
@@ -1,2 +1,5 @@
-export { type ExtractedActorImage, type ExtractedEnemyImage, type ImageExtractableActor, extractImageFromActor, extractImageFromEnemy, } from './image/mainData';
-export { type ExtractedSystemImage, extractImageFromSystem, } from './image/system';
+export type { ExtractedActorImage, ExtractedEnemyImage, ImageExtractableActor, } from './image/mainData';
+export { ExtractedSystemImage, extractImageFromSystem } from './image/system';
+export type { ExtractedText, ExtractedTextItem, TextExtractable, } from './text/mainData';
+export { extractImageFromActor, extractImageFromEnemy } from './image/mainData';
+export { extractNoteText, extractTextData, extractTextFromActor, extractTextFromArmor, extractTextFromClass, extractTextFromEnemy, extractTextFromItem, extractTextFromSkill, extractTextFromState, extractTextFromWeapon, } from './text/mainData';
