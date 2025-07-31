25b274990ff48b302c1e0097ca4d7be8c35716c2
Sigureya <sigureya1208@gmail.com>
2025-08-01 01:21:41 +0900

    export忘れなどがあったので修正。

--- dist/types/features/extractText.d.ts
+++ dist/types/features/extractText.d.ts
@@ -1,4 +1,7 @@
-export { type ExtractedBattleEventText, type ExtractedCommonEventText, expectBattleEventTexts, extractCommonEventTexts, } from './core/extractEventText';
-export { type ExtractedMapEventTexts, type ExtractedMapTexts, extractMapText, } from './core/extractMapText';
-export { type ExtractedActorImage, type ExtractedEnemyImage, type ImageExtractableActor, extractImageFromActor, extractImageFromEnemy, } from './core/extract/image/mainData';
+export { expectBattleEventTexts, extractCommonEventTexts, } from './core/extractEventText';
+export type { ExtractedBattleEventText, ExtractedCommonEventText, } from './core/extractEventText';
+export { extractMapText } from './core/extractMapText';
+export type { ExtractedActorImage, ExtractedEnemyImage, ImageExtractableActor, } from './core/extract/image/mainData';
+export { extractImageFromActor, extractImageFromEnemy, } from './core/extract/image/mainData';
+export type { ExtractedText } from './core/extract/text/mainData/types';
 export { extractNoteText, extractTextData, extractTextFromActor, extractTextFromArmor, extractTextFromClass, extractTextFromEnemy, extractTextFromItem, extractTextFromSkill, extractTextFromState, extractTextFromWeapon, } from './core/extract/text/mainData';
