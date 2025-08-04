e705b6d2688d3c5f7e19a51fb678eb5b38b64030
Sigureya <sigureya1208@gmail.com>
2025-08-04 21:21:37 +0900

    export内容を追加。

--- dist/types/features/extractText.d.ts
+++ dist/types/features/extractText.d.ts
@@ -3,6 +3,6 @@ export type { ExtractedBattleEventText, ExtractedCommonEventText, } from './core
 export { type ExtractedMapEventTexts, type ExtractedMapTexts, extractMapText, } from './core/extractMapText';
 export type { ExtractedActorImage, ExtractedEnemyImage, ImageExtractableActor, } from './core/extract/image/mainData';
 export { extractImageFromActor, extractImageFromEnemy, } from './core/extract/image/mainData';
-export type { ExtractedText } from './core/extract/text/mainData/types';
+export type { ExtractedText, ExtractedTextProperty, ExtractedTextItem, } from './core/extract/text/mainData/types';
 export { extractNoteText, extractTextData, extractTextFromActor, extractTextFromArmor, extractTextFromClass, extractTextFromEnemy, extractTextFromItem, extractTextFromSkill, extractTextFromState, extractTextFromWeapon, } from './core/extract/text/mainData';
 export { type ExtractedSystemTexts, extractTextFromSystem, } from './core/extract/text/system';
