60bcf65783de03f8d31cbf8fe4534d34832bba95
Sigureya <sigureya1208@gmail.com>
2025-08-05 11:53:18 +0900

    イベントコードを制限する。出力結果を制限するために必要

--- src/features/core/extract/text/eventCommand/extractText.ts
+++ src/features/core/extract/text/eventCommand/extractText.ts
@@ -3,26 +3,31 @@ import type {
   CHANGE_NICKNAME,
   CHANGE_PROFILE,
   Command_ShowChoices,
+  ExtractCommandByCode,
 } from "@RpgTypes/rmmz";
-import { SHOW_CHOICES } from "@RpgTypes/rmmz";
+import type { SHOW_CHOICES } from "@RpgTypes/rmmz";
+import type { CommandParameter } from "@RpgTypes/rmmz/eventCommand/pickCommandParam";
 import { pickCommandParamString } from "@RpgTypes/rmmz/eventCommand/pickCommandParam";
-import type { ExtractCommandByCode } from "dist/types";
-import type { TextCommandParameter } from "./types";
 
 export const extractTextFromActorCommand = (
   command: ExtractCommandByCode<
     typeof CHANGE_NICKNAME | typeof CHANGE_PROFILE | typeof CHANGE_NAME
   >
-): TextCommandParameter => {
+): CommandParameter<
+  string,
+  typeof CHANGE_NICKNAME | typeof CHANGE_PROFILE | typeof CHANGE_NAME
+> => {
   return pickCommandParamString(command, 1);
 };
 
 export const extractTextParamsFromChoice = (
   command: Readonly<Command_ShowChoices>
-): TextCommandParameter[] => {
-  return command.parameters[0].map<TextCommandParameter>((msg, index) => ({
-    code: SHOW_CHOICES,
-    paramIndex: index,
-    value: msg,
-  }));
+): CommandParameter<string, typeof SHOW_CHOICES>[] => {
+  return command.parameters[0].map(
+    (msg, index): CommandParameter<string, typeof SHOW_CHOICES> => ({
+      code: 102,
+      paramIndex: index,
+      value: msg,
+    })
+  );
 };
