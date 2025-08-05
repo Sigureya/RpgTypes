31dfe32021f873d8220f4d49390d8e42980f047a
Sigureya <sigureya1208@gmail.com>
2025-08-05 11:49:49 +0900

    要件的にはこっち。型を絞り込む

--- src/features/core/extract/text/eventCommand/extractText.ts
+++ src/features/core/extract/text/eventCommand/extractText.ts
@@ -1,13 +1,18 @@
 import type {
+  CHANGE_NAME,
+  CHANGE_NICKNAME,
+  CHANGE_PROFILE,
   Command_ShowChoices,
-  ExtractCommandByParam,
 } from "@RpgTypes/rmmz";
 import { SHOW_CHOICES } from "@RpgTypes/rmmz";
 import { pickCommandParamString } from "@RpgTypes/rmmz/eventCommand/pickCommandParam";
+import type { ExtractCommandByCode } from "dist/types";
 import type { TextCommandParameter } from "./types";
 
 export const extractTextFromActorCommand = (
-  command: ExtractCommandByParam<[number, string]>
+  command: ExtractCommandByCode<
+    typeof CHANGE_NICKNAME | typeof CHANGE_PROFILE | typeof CHANGE_NAME
+  >
 ): TextCommandParameter => {
   return pickCommandParamString(command, 1);
 };
