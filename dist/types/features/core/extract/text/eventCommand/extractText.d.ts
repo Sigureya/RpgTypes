2def7fd6e2d6c61c3b98f2c6c70e81458574ed39
Sigureya <sigureya1208@gmail.com>
2025-08-05 12:17:19 +0900

    文字列抽出の強化。戻り値のcodeを限定することでテーブル対応を容易にした

--- dist/types/features/core/extract/text/eventCommand/extractText.d.ts
+++ dist/types/features/core/extract/text/eventCommand/extractText.d.ts
@@ -1,4 +1,4 @@
-import { Command_ShowChoices, ExtractCommandByParam } from '../../../../../rmmz';
-import { TextCommandParameter } from './types';
-export declare const extractTextFromActorCommand: (command: ExtractCommandByParam<[number, string]>) => TextCommandParameter;
-export declare const extractTextParamsFromChoice: (command: Readonly<Command_ShowChoices>) => TextCommandParameter[];
+import { CHANGE_NAME, CHANGE_NICKNAME, CHANGE_PROFILE, Command_ShowChoices, ExtractCommandByCode, SHOW_CHOICES } from '../../../../../rmmz';
+import { CommandParameter } from '../../../../../rmmz/eventCommand/pickCommandParam';
+export declare const extractTextFromActorCommand: (command: ExtractCommandByCode<typeof CHANGE_NICKNAME | typeof CHANGE_PROFILE | typeof CHANGE_NAME>) => CommandParameter<string, typeof CHANGE_NICKNAME | typeof CHANGE_PROFILE | typeof CHANGE_NAME>;
+export declare const extractTextParamsFromChoice: (command: Readonly<Command_ShowChoices>) => CommandParameter<string, typeof SHOW_CHOICES>[];
