c45c4f6db99b2d4422999b1469b8be0bb88b8c67
Sigureya <sigureya1208@gmail.com>
2025-08-19 07:46:12 +0900

    MV用の処理。発言者を指定するためのコマンドを構築する

---
+++ src/features/core/eventCommand/commandGroup/speakerNameMV.ts
@@ -0,0 +1,31 @@
+import type {
+  Command_ShowMessageBody,
+  Command_ShowMessageHeader,
+  EventCommand,
+} from "@RpgTypes/rmmz";
+import type { SimpleEventCommandGroup } from "./core/simple";
+
+interface ResultXX<Command extends EventCommand> {
+  spekerCommand: Command;
+  messaege: string;
+}
+
+export const insertSpeakerCommand = <Command extends EventCommand>(
+  commandGroup: SimpleEventCommandGroup<
+    Command_ShowMessageHeader,
+    Command_ShowMessageBody
+  >,
+  fn: (text: string) => ResultXX<Command> | undefined
+): EventCommand[] => {
+  const speaker = fn(commandGroup.getBodyText());
+  if (!speaker) {
+    return commandGroup.normalizedCommands();
+  }
+
+  const newBody: Command_ShowMessageBody = {
+    code: 401,
+    indent: commandGroup.header.indent,
+    parameters: [speaker.messaege],
+  };
+  return [speaker.spekerCommand, commandGroup.header, newBody];
+};
