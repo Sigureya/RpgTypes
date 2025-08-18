438ba42b57dda3539167289a831b6f705d73e60d
Sigureya <sigureya1208@gmail.com>
2025-08-18 19:49:34 +0900

    exportを追加

--- src/rmmz/rpg/data/note/index.ts
+++ src/rmmz/rpg/data/note/index.ts
@@ -1,4 +1,5 @@
-export * from "./normarize";
 export * from "./note";
 export * from "./types";
+export { type NormalizedNote, buildNoteFromNormalized } from "./normarize";
+export { buildNoteString } from "./build";
 export { readNote, readNoteEx, readNoteObject } from "./read";
