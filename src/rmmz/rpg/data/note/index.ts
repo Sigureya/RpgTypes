f97b5de36377f5a2ae092ff05a1f6092567f8ac2
Sigureya <sigureya1208@gmail.com>
2025-08-20 08:52:50 +0900

    indexの修正。型名と関数名を十九視させてしまうミスがあり、色々面倒になった

--- src/rmmz/rpg/data/note/index.ts
+++ src/rmmz/rpg/data/note/index.ts
@@ -1,5 +1,9 @@
 export * from "./note";
 export * from "./types";
-export { type NormalizedNote, buildNoteFromNormalized } from "./normarize";
+export {
+  type NormalizedNote,
+  buildNoteFromNormalized,
+  normalizeNote,
+} from "./normarize";
 export { buildNoteString } from "./build";
 export { readNote, readNoteEx, readNoteObject } from "./read";
