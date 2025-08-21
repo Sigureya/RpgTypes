5680341fd44e4f20a9725f8859189fda0da94f6e
Sigureya <sigureya1208@gmail.com>
2025-08-22 05:12:45 +0900

    アクター系のイベントコマンドで使うschema

---
+++ src/validate/rmmz/eventCommand/actor/schema.ts
@@ -0,0 +1,31 @@
+import type {
+  EventCommandLike2,
+  CHANGE_PROFILE,
+  CHANGE_NICKNAME,
+  CHANGE_NAME,
+} from "@RpgTypes/rmmz";
+import type { JSONSchemaType } from "ajv";
+
+export const SCHEMA_COMMAND_ACTOR = {
+  type: "object",
+  required: ["code", "parameters", "indent"],
+  properties: {
+    code: {
+      type: "integer",
+      enum: [320, 324, 325],
+    },
+    indent: { type: "integer" },
+    parameters: {
+      type: "array",
+      minItems: 2,
+      maxItems: 2,
+      items: [{ type: "number" }, { type: "string" }],
+    },
+  },
+  additionalProperties: false,
+} as const satisfies JSONSchemaType<
+  EventCommandLike2<
+    typeof CHANGE_PROFILE | typeof CHANGE_NICKNAME | typeof CHANGE_NAME,
+    [number, string]
+  >
+>;
