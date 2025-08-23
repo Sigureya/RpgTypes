b59dac52df4e7acfa9799621e787c96fff418e45
Sigureya <sigureya1208@gmail.com>
2025-08-23 10:37:35 +0900

    武器で使う為のあれこれ

--- src/rmmz/eventCommand/commands/item/types/weapon.ts
+++ src/rmmz/eventCommand/commands/item/types/weapon.ts
@@ -1,6 +1,7 @@
 import type { ValueOf } from "@RpgTypes/libs/templates/valueOf";
 import type { EventCommandLike2 } from "@RpgTypes/rmmz/eventCommand/frame";
 import type { CHANGE_WEAPONS, Operation_PlusMinus } from "@RpgTypes/rmmz/rpg";
+import type { OPERAND_DIRECT, OPERAND_VARIABLE } from "./constants";
 
 export interface Command_ChangeWeapons2
   extends EventCommandLike2<typeof CHANGE_WEAPONS> {
@@ -11,11 +12,24 @@ export interface ParamObject_ChangeWeapons2 {
   operation: ValueOf<Operation_PlusMinus>;
   weaponId: number;
   value: number;
+  operand: typeof OPERAND_DIRECT | typeof OPERAND_VARIABLE;
+
   includesEquip: boolean;
 }
 
 export type ParamArray_ChangeWeapons2 = [
   operation: ValueOf<Operation_PlusMinus>,
   weaponId: number,
-  value: number
+  value: number,
+  direct: typeof OPERAND_DIRECT | typeof OPERAND_VARIABLE,
+  includesEquip: boolean
 ];
+
+export interface ParamObject_GainWeapon {
+  weaponId: number;
+  value: number;
+}
+export interface ParamObject_GainWeaponV {
+  weaponId: number;
+  variableId: number;
+}
