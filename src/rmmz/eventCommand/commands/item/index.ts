923bb36d1851c1baf4ddaf7e27c6976411b50b1c
Sigureya <sigureya1208@gmail.com>
2025-08-23 12:04:41 +0900

    indexを追加。個別にexportする

---
+++ src/rmmz/eventCommand/commands/item/index.ts
@@ -0,0 +1,50 @@
+export type {
+  Command_ChangeArmors2,
+  ParamArray_ChangeArmors2,
+  ParamObject_ChangeArmors2,
+  ParamObject_GainArmor,
+  ParamObject_GainArmorV,
+} from "./types/armor";
+
+export type {
+  Command_ChangeItems2,
+  ParamArray_ChangeItems2,
+  ParamObject_ChangeItems,
+  ParamObject_ChangeItemsFullset,
+  ParamObject_ChangeItemsV,
+} from "./types/item";
+
+export type {
+  Command_ChangeWeapons2,
+  ParamArray_ChangeWeapons2,
+  ParamObject_ChangeWeapons,
+  ParamObject_ChangeWeaponsFullset,
+  ParamObject_ChangeWeaponsV,
+} from "./types/weapon";
+
+export {
+  fromArrayChangeArmors,
+  makeCommandChangeArmors,
+  makeCommandGainArmor,
+  makeCommandGainArmorV,
+  makeCommandLoseArmor,
+  makeCommandLoseArmorV,
+} from "./armor";
+
+export {
+  fromArrayChangeItems,
+  makeCommandChangeItems,
+  makeCommandGainItem,
+  makeCommandGainItemV,
+  makeCommandLoseItem,
+  makeCommandLoseItemV,
+} from "./item";
+
+export {
+  fromArrayChangeWeapons,
+  makeCommandChangeWeapons,
+  makeCommandGainWeapon,
+  makeCommandGainWeaponV,
+  makeCommandLoseWeapon,
+  makeCommandLoseWeaponV,
+} from "./weapon";
