de09a88c3846fd356b2cd6cb90fdae3ed2758181
Sigureya <sigureya1208@gmail.com>
2025-08-23 12:00:48 +0900

    イベントコマンドからアイテムの増減に関する情報を取得する関数。

--- src/rmmz/eventCommand/operateValue.ts
+++ src/rmmz/eventCommand/operateValue.ts
@@ -1,38 +1,26 @@
-import type {
-  Command_ChangeArmors2,
-  Command_ChangeItems2,
-  Command_ChangeWeapons2,
-} from "./commands/item/types";
+import type { Command_ChangeArmors2 } from "./commands/item/types/armor";
+import type { Command_ChangeItems2 } from "./commands/item/types/item";
+import type { Command_ChangeWeapons2 } from "./commands/item/types/weapon";
 import type { Command_ChangeGold2 } from "./commands/party/gold/changeGold";
-import type { EventCommandLike2 } from "./frame";
 import { operateValue } from "./runtime/operateValue/operateValue";
 import type { OperateValueHandlers } from "./runtime/operateValue/types";
 
 export const operateValueChangeGoods = <T, V>(
-  command:
-    | Command_ChangeArmors2
-    | Command_ChangeItems2
-    | Command_ChangeWeapons2,
+  {
+    parameters,
+  }: Command_ChangeArmors2 | Command_ChangeItems2 | Command_ChangeWeapons2,
   handlers: OperateValueHandlers<T, V>
 ): T => {
-  return basicOperate(command, handlers);
+  const operation = parameters[1];
+  const operandType = parameters[2];
+  const operand = parameters[3];
+  return operateValue(operation, operandType, operand, handlers);
 };
 
 export const operateValueChangeGold = <T, V>(
   command: Command_ChangeGold2,
   handlers: OperateValueHandlers<T, V>
 ): T => {
-  return basicOperate(command, handlers);
-};
-
-const basicOperate = <
-  Command extends EventCommandLike2<number, [number, number, number]>,
-  Result,
-  Value
->(
-  command: Command,
-  handlers: OperateValueHandlers<Result, Value>
-): Result => {
   const operation = command.parameters[0];
   const operandType = command.parameters[1];
   const operand = command.parameters[2];
