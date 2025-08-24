15ff599e84579a0c76e0acd8691d00a493944df9
Sigureya <sigureya1208@gmail.com>
2025-08-25 06:20:19 +0900

    アクターのHPなどを書き換えるイベントコマンド。paramObjectは単純化させてある

---
+++ src/rmmz/eventCommand/commands/actor/changeValues/types.ts
@@ -0,0 +1,33 @@
+import type { EventCommandLike2 } from "@RpgTypes/rmmz/eventCommand/frame";
+import type { CHANGE_HP, CHANGE_MP, CHANGE_TP } from "@RpgTypes/rmmz/rpg";
+import type { TARGET_DIRECT, TARGET_VARIABLE } from "./constants";
+
+export interface Command_ChangeActorHP
+  extends EventCommandLike2<typeof CHANGE_HP> {
+  parameters: ParamArray_ChangeActorValue;
+}
+
+export interface Command_ChangeActorMP
+  extends EventCommandLike2<typeof CHANGE_MP> {
+  parameters: ParamArray_ChangeActorValue;
+}
+
+export interface Command_ChangeActorTP
+  extends EventCommandLike2<typeof CHANGE_TP> {
+  parameters: ParamArray_ChangeActorValue;
+}
+
+export type ParamArray_ChangeActorValue = [
+  targetType: typeof TARGET_DIRECT | typeof TARGET_VARIABLE,
+  index: number,
+  operation: number,
+  valueType: number,
+  value: number
+];
+
+export interface ParamObject_ChangeActorValue {
+  targetType: "direct" | "variable" | "each";
+  target: number;
+  operation: "plus" | "minus";
+  operand: { type: "variable" | "direct"; value: number };
+}
