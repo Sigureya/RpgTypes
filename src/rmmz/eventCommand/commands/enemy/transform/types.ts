56af859702575184a04b6f5a561ebf6754f66f5d
Sigureya <sigureya1208@gmail.com>
2025-08-24 17:35:29 +0900

    enemyの変身

---
+++ src/rmmz/eventCommand/commands/enemy/transform/types.ts
@@ -0,0 +1,14 @@
+import type { EventCommandLike2 } from "@RpgTypes/rmmz/eventCommand/frame";
+import type { ENEMY_TRANSFORM } from "@RpgTypes/rmmz/rpg";
+
+export interface Command_EnemyTransform
+  extends EventCommandLike2<typeof ENEMY_TRANSFORM> {
+  parameters: ParamArray_EnemyTransfrom;
+}
+
+export type ParamArray_EnemyTransfrom = [enemyId: number, newEnemyId: number];
+
+export interface ParamObject_EnemyTransform {
+  enemyId: number;
+  newEnemyId: number;
+}
