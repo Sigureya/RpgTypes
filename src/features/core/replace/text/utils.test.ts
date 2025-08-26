3a31fd290df9a1c35b6e964ce0c7824cb55d10dd
Sigureya <sigureya1208@gmail.com>
2025-08-27 00:10:07 +0900

    note部分の置換処理のテスト。区切り文字は切り替え可能にした

---
+++ src/features/core/replace/text/utils.test.ts
@@ -0,0 +1,36 @@
+import { describe, expect, test } from "vitest";
+import { replaceNoteTextByMap, replaceTextByMap } from "./utils";
+
+describe("replaceTextByMap", () => {
+  test("replaces text using map", () => {
+    const map = new Map([
+      ["Hello", "Hi"],
+      ["World", "Earth"],
+    ]);
+    expect(replaceTextByMap("Hello", map)).toBe("Hi");
+    expect(replaceTextByMap("World", map)).toBe("Earth");
+    expect(replaceTextByMap("NoChange", map)).toBe("NoChange");
+  });
+
+  test("trims end spaces before lookup and after replacement", () => {
+    const map = new Map([
+      ["Hello", "Hi"],
+      ["World", "Earth"],
+    ]);
+    expect(replaceTextByMap("Hello   ", map)).toBe("Hi");
+    expect(replaceTextByMap("World   ", map)).toBe("Earth");
+    expect(replaceTextByMap("NoChange   ", map)).toBe("NoChange");
+  });
+});
+
+describe("replaceNoteTextByMap", () => {
+  test("replaces note text using map", () => {
+    const map = new Map([
+      ["foo", "bar"],
+      ["baz", "qux"],
+    ]);
+    const data = { note: "<test:foo> and <test:baz> and <test:nochange>" };
+    const result = replaceNoteTextByMap(data, map, "\n");
+    expect(result).toBe("<test:bar>\n<test:qux>\n<test:nochange>\n and  and");
+  });
+});
