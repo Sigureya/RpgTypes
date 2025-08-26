ac2fb68b9b20f3295f150fe7f62f851f644d74aa
Sigureya <sigureya1208@gmail.com>
2025-08-26 09:49:14 +0900

    一応objectも用意しておく

--- src/rpgNext/event/commands/core/functions.ts
+++ src/rpgNext/event/commands/core/functions.ts
@@ -1,5 +1,13 @@
+import type { EventState } from "./state";
+
 export interface FunctionsTable {
-  callBoolean(name: string, arg: object): boolean;
-  callNumber(name: string, arg: object): number;
-  callString(name: string, arg: object): string;
+  callBoolean(state: EventState, name: string, arg: object): boolean;
+  callNumber(state: EventState, name: string, arg: object): number;
+  callString(state: EventState, name: string, arg: object): string;
+  callObject(
+    state: EventState,
+    name: string,
+    arg: object,
+    requiredResultType: string
+  ): object;
 }
