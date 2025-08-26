ed7e483a6df627f8b8567f0317e82dfd3a660929
Sigureya <sigureya1208@gmail.com>
2025-08-27 00:47:19 +0900

    マップイベントの生成関数を更新。イベントコマンドの切り替えに対応した

--- src/rmmz/rpg/data/event/map/event/page/make.ts
+++ src/rmmz/rpg/data/event/map/event/page/make.ts
@@ -1,41 +1,67 @@
+import type { EventCommandUnknown } from "@RpgTypes/rmmz/eventCommand";
 import { makeEventPageCondition } from "./condition";
 import type { MapEvent_Image } from "./image/types";
 import type { MapEvent } from "./mapEvent";
 import type { MapEventPage } from "./page";
 
-export const makeMapEvent = (): MapEvent => {
+export const makeMapEvent = <Command extends EventCommandUnknown>({
+  id = 1,
+  name = "",
+  pages = [],
+  note = "",
+  x = 0,
+  y = 0,
+}: Partial<MapEvent<Command>> = {}): MapEvent<Command> => {
   return {
-    id: 1,
-    name: "",
-    pages: [],
-    note: "",
-    x: 0,
-    y: 0,
+    id: id,
+    name: name,
+    pages: pages,
+    note: note,
+    x: x,
+    y: y,
   };
 };
 
-export const makeMapEventIamge = (): MapEvent_Image => {
+export const makeMapEventIamge = ({
+  characterIndex = 0,
+  characterName = "",
+  direction = 2,
+  pattern = 0,
+  tileId = 0,
+}: Partial<MapEvent_Image> = {}): MapEvent_Image => {
   return {
-    characterIndex: 0,
-    characterName: "",
-    direction: 2,
-    pattern: 0,
-    tileId: 0,
+    characterIndex: characterIndex,
+    characterName: characterName,
+    direction: direction,
+    pattern: pattern,
+    tileId: tileId,
   };
 };
 
-export const makeMapEventPage = (): MapEventPage => {
-  return {
-    conditions: makeEventPageCondition(),
+export const makeMapEventPage = <Command extends EventCommandUnknown>({
+  list = [],
+  conditions = makeEventPageCondition(),
+  image = makeMapEventIamge(),
+  moveFrequency = 5,
+  directionFix = false,
+  moveRoute = {
     list: [],
-    directionFix: false,
-    image: makeMapEventIamge(),
-    moveFrequency: 5,
+    repeat: false,
+    skippable: false,
+    wait: false,
+  },
+}: Partial<MapEventPage<Command>> = {}): MapEventPage<Command> => {
+  return {
+    conditions: conditions,
+    list: list,
+    directionFix: directionFix,
+    image: image,
+    moveFrequency: moveFrequency,
     moveRoute: {
-      list: [],
-      repeat: false,
-      skippable: false,
-      wait: false,
+      list: moveRoute.list,
+      repeat: moveRoute.repeat,
+      skippable: moveRoute.skippable,
+      wait: moveRoute.wait,
     },
   };
 };
