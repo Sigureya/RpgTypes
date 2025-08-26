a0e5f6010c269504ade77f4cb7e2986249582e59
Sigureya <sigureya1208@gmail.com>
2025-08-27 01:35:55 +0900

    codeを定数のtypeofに切り替え

--- src/rmmz/eventCommand/commands/audio/other/types.ts
+++ src/rmmz/eventCommand/commands/audio/other/types.ts
@@ -1,5 +1,19 @@
 import type { EventCommandLike2 } from "@RpgTypes/rmmz/eventCommand/frame";
+import type {
+  FADEOUT_BGM,
+  FADEOUT_BGS,
+  RESUME_BGM,
+  SAVE_BGM,
+} from "@RpgTypes/rmmz/rpg";
 
-export interface Command_FadeOutScreen extends EventCommandLike2<221, []> {}
-export interface Command_SaveBGM extends EventCommandLike2<243, []> {}
-export interface Command_ResumeBGM extends EventCommandLike2<244, []> {}
+export interface Command_FadeOutBGM
+  extends EventCommandLike2<typeof FADEOUT_BGM, []> {}
+
+export interface Command_SaveBGM
+  extends EventCommandLike2<typeof SAVE_BGM, []> {}
+
+export interface Command_ResumeBGM
+  extends EventCommandLike2<typeof RESUME_BGM, []> {}
+
+export interface Command_FadeOutBGS
+  extends EventCommandLike2<typeof FADEOUT_BGS> {}
