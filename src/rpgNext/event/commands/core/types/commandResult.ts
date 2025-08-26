3ec47622cf603eeba45cc43950497a9ead64c360
Sigureya <sigureya1208@gmail.com>
2025-08-26 17:09:31 +0900

    戻り値が２通りになったので別バージョンも作成。後で名前を変える

--- src/rpgNext/event/commands/core/types/commandResult.ts
+++ src/rpgNext/event/commands/core/types/commandResult.ts
@@ -1,8 +1,13 @@
 import type { EvaluteResult } from "./evaluteResult";
+import type { EventState } from "./state";
 import type { SideEffect, WaitXXX } from "./types";
 
 export interface CommandExecuteResult {
   sideEffect?: SideEffect;
   wait?: WaitXXX;
   result?: EvaluteResult;
+}
+export interface CommandExecuteResult2 {
+  sideEffect?: SideEffect;
+  newState: EventState;
 }
