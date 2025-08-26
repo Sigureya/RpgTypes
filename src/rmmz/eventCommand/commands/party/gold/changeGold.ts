40bb83825cd4718934f2a0be36a846e383d2841b
Sigureya <sigureya1208@gmail.com>
2025-08-27 02:10:56 +0900

    export範囲を制御することで名前重複を可能にした

--- src/rmmz/eventCommand/commands/party/gold/changeGold.ts
+++ src/rmmz/eventCommand/commands/party/gold/changeGold.ts
@@ -2,7 +2,7 @@ import type { ValueOf } from "@RpgTypes/libs/templates/valueOf";
 import type { EventCommandLike2 } from "@RpgTypes/rmmz/eventCommand/frame";
 import type { CHANGE_GOLD, Operation_PlusMinus } from "@RpgTypes/rmmz/rpg";
 
-export interface Command_ChangeGold2
+export interface Command_ChangeGold
   extends EventCommandLike2<typeof CHANGE_GOLD> {
   parameters: [
     operation: ValueOf<Operation_PlusMinus>,
