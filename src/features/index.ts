64d262b70c131adb4df19ec81eba923de0d350de
Sigureya <sigureya1208@gmail.com>
2025-08-27 18:49:18 +0900

    indexを色々修正

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,4 +1,4 @@
 export * from "./core/extract/item/index";
+export * from "./core/replace/text";
 export * from "./extractText";
 export * from "./format/trait/formatTraits";
-export * from "./replacetText";
