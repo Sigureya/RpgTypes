3ac4250c05e49c6ce49d54f32349e38dfa95e530
Sigureya <sigureya1208@gmail.com>
2025-08-27 16:09:19 +0900

    位置情報の取得コマンド。訂正が必要かもしれないが、後回し

---
+++ src/rmmz/eventCommand/commands/mapEvent/getLocationInfo/types.ts
@@ -0,0 +1,14 @@
+import type { ValueOf } from "@RpgTypes/libs/templates/valueOf";
+import type { EventCommandLike2 } from "@RpgTypes/rmmz/eventCommand/frame";
+import type { Designation, GET_LOCATION_INFO } from "@RpgTypes/rmmz/rpg";
+
+export interface Command_GetLocationInfo
+  extends EventCommandLike2<typeof GET_LOCATION_INFO> {}
+
+export type ParamArray_GetLocationInfo = [
+  variableId: number,
+  mode: number,
+  designation: ValueOf<Designation>,
+  x: number,
+  y: number
+];
