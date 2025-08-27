2cc3f3189928a1aa34b8e2437aa61c8c777cde28
Sigureya <sigureya1208@gmail.com>
2025-08-27 14:03:28 +0900

    生成関数を消した

--- src/rmmz/rpg/data/event/map/event/page/eventCommand/index.ts
+++ src/rmmz/rpg/data/event/map/event/page/eventCommand/index.ts
@@ -3,6 +3,6 @@ export * from "./eventComandLike";
 export * from "./eventCommand";
 export * from "./eventCommandCodes";
 export * from "./eventCommandTypes";
-export * from "./make";
+
 export * from "./paramaters";
 export * from "./types";
