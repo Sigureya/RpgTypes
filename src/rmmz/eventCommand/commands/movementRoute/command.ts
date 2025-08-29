7931b659261a40103de960a5d38f475d963f581d
Sigureya <sigureya1208@gmail.com>
2025-08-29 22:23:20 +0900

    paramArrayを追加

--- src/rmmz/eventCommand/commands/movementRoute/command.ts
+++ src/rmmz/eventCommand/commands/movementRoute/command.ts
@@ -3,3 +3,8 @@ import type { EventCommandLike2 } from "../../frame";
 export interface Command_SetMovementRoute extends EventCommandLike2<205> {
   parameters: [characterId: number, movement: MoveRouteData];
 }
+
+export type ParamArray_MovementRoute = [
+  characterId: number,
+  movement: MoveRouteData
+];
