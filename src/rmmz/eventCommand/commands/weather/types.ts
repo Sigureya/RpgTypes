69f018a250ff5e5602b91ed1d5ff7cbf8d01dc8c
Sigureya <sigureya1208@gmail.com>
2025-08-30 15:47:25 +0900

    typeArrayを追加

--- src/rmmz/eventCommand/commands/weather/types.ts
+++ src/rmmz/eventCommand/commands/weather/types.ts
@@ -1,10 +1,14 @@
+import type { SET_WEATHER_EFFECT } from "@RpgTypes/rmmz/rpg";
 import type { EventCommandLike2 } from "../../frame";
 
-export interface Command_SetWeatherEffect extends EventCommandLike2<236> {
-  parameters: [
-    type: string,
-    power: number,
-    duration: number,
-    needsWait: boolean
-  ];
+export interface Command_SetWeatherEffect
+  extends EventCommandLike2<typeof SET_WEATHER_EFFECT> {
+  parameters: ParamArray_WeatherEffect;
 }
+
+export type ParamArray_WeatherEffect = [
+  type: string,
+  power: number,
+  duration: number,
+  needsWait: boolean
+];
