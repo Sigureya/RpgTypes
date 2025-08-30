6bc9536054cd213ee00b86b0413b46a69f153c80
Sigureya <sigureya1208@gmail.com>
2025-08-30 15:44:19 +0900

    ParamArrayを色々追加

--- src/rmmz/eventCommand/commands/battle/background/types.ts
+++ src/rmmz/eventCommand/commands/battle/background/types.ts
@@ -1,5 +1,10 @@
 import type { EventCommandLike2 } from "@RpgTypes/rmmz/eventCommand/frame";
 
 export interface Command_ChangeBattleBackground extends EventCommandLike2<283> {
-  parameters: [background1: string, background2: string];
+  parameters: ParamArray_ChangeBattleBackground;
 }
+
+export type ParamArray_ChangeBattleBackground = [
+  background1: string,
+  background2: string
+];
