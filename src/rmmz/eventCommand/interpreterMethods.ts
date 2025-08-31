4a803d35804bea9ae4697482ad7a663223cc6005
Sigureya <sigureya1208@gmail.com>
2025-08-31 17:44:10 +0900

    インタプリタで実装が必要な部分。typeでも推論可能だが、コンパイルコストが大きくなるので明示的なバージョンを作成

--- src/rmmz/eventCommand/interpreterMethods.ts
+++ src/rmmz/eventCommand/interpreterMethods.ts
@@ -21,7 +21,6 @@ import type {
   ParamArray_SelfSwitch,
   ParamArray_ChangeEnabled,
   ParamArray_SetVehicleLocation,
-  ExtractCommandByCode,
   ParamArray_SetEventLocation,
   ParamArray_ScrollMap,
   ParamArray_MovementRoute,
@@ -40,19 +39,29 @@ import type {
   ParamArray_GetLocationInfo,
   ParamArray_ChangeBattleBackground,
   ParamArray_ChangeParallax,
+  ParamArray_BattleProcessing,
+  ParamArray_ShopProcessing,
+  ParamArray_ChangeActorState,
+  ParamArray_ChangeExp,
+  ParamArray_ChangeSkill,
+  ParamArray_ChangeClass,
 } from "./commands";
 import type { ParamArray_ChangeActorImages } from "./commands/actor/changeImages/types";
-import type { ParamArray_ChangeActorHP } from "./commands/actor/changeValues/types";
+import type { ParamArray_ChangeActorLevel } from "./commands/actor/changeLevel/types";
+import type { ParamArray_ChangeParam } from "./commands/actor/changeParam/types";
+import type {
+  ParamArray_ChangeActorHP,
+  ParamArray_ChangeActorMP,
+  ParamArray_ChangeActorTP,
+} from "./commands/actor/changeValues/types";
 import type { ParamArray_ControlTimer } from "./commands/controlTimer/types";
 import type { BranchParameters } from "./commands/flow/branch/types/branchParams";
 import type { ParamArray_SelectItem } from "./commands/item/select/types";
 import type { ParamArray_ChangeMapNameDisplay } from "./commands/map/changeMapNameDisplay/types";
-import type {
-  Command_ChangePlayerFollowers,
-  ParamArray_ChangePlayerFollowers,
-} from "./commands/mapFollwer/types";
+import type { ParamArray_ChangePlayerFollowers } from "./commands/mapFollwer/types";
 import type { ParamArray_TransferPlayer } from "./commands/mapPlayer/transferPlayer/types";
 import type { ParamArray_ChangePartyMember } from "./commands/party/changeMember/types";
+import type { ParamArray_RecoverAll } from "./commands/party/types/recoverAll";
 import type { ParamArray_ErasePicture } from "./commands/picture/erase/types";
 import type { ParamArray_ShakeScreen } from "./commands/screen/shake/types";
 import type { ParamArray_ChangeTileset } from "./commands/tileset/types";
@@ -74,34 +83,32 @@ type CommandTypeAssert = {
     { code: K }
   > extends undefined
     ? never
-    : (param: Extract<MainCommands, { code: K }>["parameters"]) => boolean;
+    : (params: Extract<MainCommands, { code: K }>["parameters"]) => boolean;
 };
 
 export interface EventCommandMapper extends CommandTypeAssert {
-  command282: (param: ParamArray_ChangeTileset) => boolean;
-
   command101: (showMessage: ParamArray_ShowMessage) => boolean;
   command102: (setupChoice: ParamArray_SetupChoice) => boolean;
-  command103: (param: ParamArray_InputNumber) => boolean;
-  command104: (param: ParamArray_SelectItem) => boolean;
-  command105: (param: ParamArray_ScrollTextHeader) => boolean;
-  command108: (param: ParamArray_Comment) => boolean;
-  command111: (param: BranchParameters) => boolean;
+  command103: (params: ParamArray_InputNumber) => boolean;
+  command104: (params: ParamArray_SelectItem) => boolean;
+  command105: (params: ParamArray_ScrollTextHeader) => boolean;
+  command108: (params: ParamArray_Comment) => boolean;
+  command111: (params: BranchParameters) => boolean;
   command112: (params: []) => boolean;
   command113: (params: []) => boolean;
   command115: (params: []) => boolean;
-  command117: (param: ParamArray_CommonEvent) => boolean;
-  command118: (param: ParamArray_Label) => boolean;
-  command119: (param: ParamArray_Label) => boolean;
+  command117: (params: ParamArray_CommonEvent) => boolean;
+  command118: (params: ParamArray_Label) => boolean;
+  command119: (params: ParamArray_Label) => boolean;
 
-  command121: (param: ParamArray_ControlSwitches) => boolean;
-  command122: (param: ParamArray_ControlVariables) => boolean;
-  command123: (param: ParamArray_SelfSwitch) => boolean;
-  command124: (param: ParamArray_ControlTimer) => boolean;
-  command126: (param: ParamArray_ChangeItems) => boolean;
-  command127: (param: ParamArray_ChangeWeapons) => boolean;
-  command128: (param: ParamArray_ChangeArmors) => boolean;
-  command129: (param: ParamArray_ChangePartyMember) => boolean;
+  command121: (params: ParamArray_ControlSwitches) => boolean;
+  command122: (params: ParamArray_ControlVariables) => boolean;
+  command123: (params: ParamArray_SelfSwitch) => boolean;
+  command124: (params: ParamArray_ControlTimer) => boolean;
+  command126: (params: ParamArray_ChangeItems) => boolean;
+  command127: (params: ParamArray_ChangeWeapons) => boolean;
+  command128: (params: ParamArray_ChangeArmors) => boolean;
+  command129: (params: ParamArray_ChangePartyMember) => boolean;
 
   command132: (params: [AudioFileParams]) => boolean;
   command133: (params: [AudioFileParams]) => boolean;
@@ -111,25 +118,27 @@ export interface EventCommandMapper extends CommandTypeAssert {
   command137: (params: ParamArray_ChangeEnabled) => boolean;
   command138: (params: [ColorRGBA]) => boolean;
   command139: (params: [AudioFileParams]) => boolean;
-  command201: (param: ParamArray_TransferPlayer) => boolean;
-  command202: (param: ParamArray_SetVehicleLocation) => boolean;
+  command201: (params: ParamArray_TransferPlayer) => boolean;
+  command202: (params: ParamArray_SetVehicleLocation) => boolean;
   command203: (params: ParamArray_SetEventLocation) => boolean;
 
-  command204: (param: ParamArray_ScrollMap) => boolean;
+  command204: (params: ParamArray_ScrollMap) => boolean;
   command205: (params: ParamArray_MovementRoute) => boolean;
   command206: (params: []) => boolean;
+
   command211: (params: ParamArray_ChangeTransparency) => boolean;
   command212: (params: ParamArray_ShowAnimation) => boolean;
   command213: (params: ParamArray_ShowBalloonIcon) => boolean;
   command214: (params: ParamArray_EraseEvent) => boolean;
-
   command216: (params: ParamArray_ChangePlayerFollowers) => boolean;
   command217: (params: []) => boolean;
-  command221: (params: []) => boolean;
-  command222: (params: []) => boolean;
+
+  command221: (fadeoutScreen: []) => boolean;
+  command222: (fadeinScreen: []) => boolean;
   command223: (params: ParamArray_TintScreen) => boolean;
   command224: (params: ParamArray_TintScreen) => boolean;
   command225: (params: ParamArray_ShakeScreen) => boolean;
+
   command230: (params: ParamArray_Wait) => boolean;
   command231: (params: ParamsArray_ShowPicture) => boolean;
   command232: (params: ParamsArray_MovePicture) => boolean;
@@ -150,19 +159,27 @@ export interface EventCommandMapper extends CommandTypeAssert {
   command251: (stopSE: []) => boolean;
   command261: (playMove: ParamArray_PlayMovie) => boolean;
 
-  command281: (param: ParamArray_ChangeMapNameDisplay) => boolean;
+  command281: (params: ParamArray_ChangeMapNameDisplay) => boolean;
+  command282: (params: ParamArray_ChangeTileset) => boolean;
+
   command283: (params: ParamArray_ChangeBattleBackground) => boolean;
-  command284: (param: ParamArray_ChangeParallax) => boolean;
-  command285: (param: ParamArray_GetLocationInfo) => boolean;
+  command284: (params: ParamArray_ChangeParallax) => boolean;
+  command285: (params: ParamArray_GetLocationInfo) => boolean;
 
-  command311: (param: ParamArray_ChangeActorHP) => boolean;
-  command322: (param: ParamArray_ChangeActorImages) => boolean;
+  command301: (params: ParamArray_BattleProcessing) => boolean;
+
+  command302: (params: ParamArray_ShopProcessing) => boolean;
+
+  command311: (params: ParamArray_ChangeActorHP) => boolean;
+  command312: (changeMP: ParamArray_ChangeActorMP) => boolean;
+  command326: (changeTP: ParamArray_ChangeActorTP) => boolean;
+  command313: (params: ParamArray_ChangeActorState) => boolean;
+  command314: (params: ParamArray_RecoverAll) => boolean;
+  command315: (params: ParamArray_ChangeExp) => boolean;
+  command316: (changeLevel: ParamArray_ChangeActorLevel) => boolean;
+  command317: (params: ParamArray_ChangeParam) => boolean;
+  command318: (params: ParamArray_ChangeSkill) => boolean;
+  command322: (params: ParamArray_ChangeActorImages) => boolean;
+  command320: (params: [number, string]) => boolean;
+  command321: (params: ParamArray_ChangeClass) => boolean;
 }
-
-// const ee: ExtractCommandByCode<213> = {};
-
-const xx2: Partial<CommandTypeAssert> = {
-  command204: (param) => true,
-
-  // command111,
-};
