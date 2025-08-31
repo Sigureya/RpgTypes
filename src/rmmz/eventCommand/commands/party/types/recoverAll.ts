0d61d658770bbf2b585f8e34fcaafbf18dbcc433
Sigureya <sigureya1208@gmail.com>
2025-08-31 15:38:12 +0900

    アクターなどの全回復。パラメータ部分は後で作る

--- src/rmmz/eventCommand/commands/party/types/recoverAll.ts
+++ src/rmmz/eventCommand/commands/party/types/recoverAll.ts
@@ -2,4 +2,8 @@ import type { EventCommandLike2 } from "@RpgTypes/rmmz/eventCommand/frame";
 import type { RECOVER_ALL } from "@RpgTypes/rmmz/rpg";
 
 export interface Command_RecoverAll
-  extends EventCommandLike2<typeof RECOVER_ALL> {}
+  extends EventCommandLike2<typeof RECOVER_ALL> {
+  parameters: ParamArray_RecoverAll;
+}
+
+export type ParamArray_RecoverAll = [];
