f5684d83bdd2bf6aa538ad05dd616521a4740044
Sigureya <sigureya1208@gmail.com>
2025-09-03 19:21:51 +0900

    別名でのexportを追加。探しづらくて混乱したので

--- src/features/core/replace/text/index.ts
+++ src/features/core/replace/text/index.ts
@@ -1,10 +1,12 @@
 export {
   replaceActorText,
+  replaceItemText as replaceArmorText,
   replaceClassText,
   replaceEnemyText,
   replaceItemText,
   replaceSkillText,
   replaceStateText,
+  replaceItemText as replaceWeaponText,
 } from "./rpgData";
 export { replaceCommonEventTexts, replaceTroopTexts } from "./event";
 export {
