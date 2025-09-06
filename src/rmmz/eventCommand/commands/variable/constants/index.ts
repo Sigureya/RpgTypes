fce58f5f41aaadb9cc47d0854f999c74baf1ff65
Sigureya <sigureya1208@gmail.com>
2025-09-06 19:56:53 +0900

    フォルダ名を間違えていたので修正

---
+++ src/rmmz/eventCommand/commands/variable/constants/index.ts
@@ -0,0 +1,8 @@
+export {
+  OPERAND_CONSTANT,
+  OPERAND_GAMEDATA,
+  OPERAND_RANDOM,
+  OPERAND_SCRIPT,
+  OPERAND_VARIABLE,
+} from "./operand";
+export { TYPE_ARMOR, TYPE_ITEM, TYPE_WEAPON } from "./itemType";
