5c6783b3b9b4a6ce0f5544df6ac79241662abb82
Sigureya <sigureya1208@gmail.com>
2025-09-06 13:12:15 +0900

    change/index.tsで範囲の制限をしている。こっちは一括exportでOK

--- src/rmmz/eventCommand/commands/item/index.ts
+++ src/rmmz/eventCommand/commands/item/index.ts
@@ -1,51 +1,7 @@
-export type {
-  Command_ChangeArmors,
-  ParamArray_ChangeArmors,
-  ParamObject_ChangeArmors,
-  ParamObject_GainArmor,
-  ParamObject_GainArmorV,
-} from "./change/types/armor";
-export type {
-  Command_ChangeItems,
-  ParamArray_ChangeItems,
-  ParamObject_ChangeItems,
-  ParamObject_ChangeItemsFullset,
-  ParamObject_ChangeItemsV,
-} from "./change/types/item";
-export type {
-  Command_ChangeWeapons,
-  ParamArray_ChangeWeapons,
-  ParamObject_ChangeWeapons,
-  ParamObject_ChangeWeaponsFullset,
-  ParamObject_ChangeWeaponsV,
-} from "./change/types/weapon";
+export * from "./change";
 export type {
   Command_SelectItem,
   ParamArray_SelectItem,
   ParamObject_SelectItem,
 } from "./select/types";
-export {
-  fromArrayChangeArmors,
-  makeCommandChangeArmors,
-  makeCommandGainArmor,
-  makeCommandGainArmorV,
-  makeCommandLoseArmor,
-  makeCommandLoseArmorV,
-} from "./change/armor";
-export {
-  fromArrayChangeItems,
-  makeCommandChangeItems,
-  makeCommandGainItem,
-  makeCommandGainItemV,
-  makeCommandLoseItem,
-  makeCommandLoseItemV,
-} from "./change/item";
-export {
-  fromArrayChangeWeapons,
-  makeCommandChangeWeapons,
-  makeCommandGainWeapon,
-  makeCommandGainWeaponV,
-  makeCommandLoseWeapon,
-  makeCommandLoseWeaponV,
-} from "./change/weapon";
 export { fromArraySelectItem, makeCommandSelectItem } from "./select/select";
