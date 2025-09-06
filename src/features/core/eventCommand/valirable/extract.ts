36c05cc24bd876b54dcd9c75b98997cc18196cb3
Sigureya <sigureya1208@gmail.com>
2025-09-06 22:21:52 +0900

    別ファイルに移動したので削除

--- src/features/core/eventCommand/valirable/extract.ts
+++ src/features/core/eventCommand/valirable/extract.ts
@@ -1,9 +1,5 @@
-import type {
-  Command_ChangeItemsByVariable,
-  Command_InputNumber,
-  Command_SelectItem,
-} from "@RpgTypes/rmmz";
-import { CHANGE_WEAPONS, INPUT_NUMBER, SELECT_ITEM } from "@RpgTypes/rmmz";
+import type { Command_InputNumber, Command_SelectItem } from "@RpgTypes/rmmz";
+import { INPUT_NUMBER, SELECT_ITEM } from "@RpgTypes/rmmz";
 import type { VariableParam } from "./types";
 
 export const extractVariableInputNumber = (
@@ -12,15 +8,6 @@ export const extractVariableInputNumber = (
   return {
     code: INPUT_NUMBER,
     variableId: command.parameters[0],
-  };
-};
-
-export const extractVariableGainItem = (
-  command: Command_ChangeItemsByVariable
-): VariableParam => {
-  return {
-    code: CHANGE_WEAPONS,
-    variableId: command.parameters[2],
   };
 };
 
