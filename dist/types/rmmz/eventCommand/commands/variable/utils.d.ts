899abf1c778705e93075283549ff479cd5864ccb
Sigureya <sigureya1208@gmail.com>
2025-09-06 21:16:58 +0900

    最新ビルド結果。

---
+++ dist/types/rmmz/eventCommand/commands/variable/utils.d.ts
@@ -0,0 +1,2 @@
+import { Command_ControlVariables, Operand_Variable } from './types';
+export declare const isCommandOperandVariables: (command: Command_ControlVariables) => command is Command_ControlVariables<Operand_Variable>;
