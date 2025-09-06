c17b7091fa55fbd5d1fd1ef1d599b4a57b3be0f3
Sigureya <sigureya1208@gmail.com>
2025-09-06 21:03:06 +0900

    変換関数を種類別に分割

---
+++ src/rmmz/eventCommand/commands/variable/random.ts
@@ -0,0 +1,34 @@
+import { CONTROL_VARIABLES } from "@RpgTypes/rmmz/rpg";
+import { OPERAND_RANDOM } from "./constants";
+import type {
+  ParamObject_WritingTarget,
+  ParamObject_Operand_Random,
+  Operand_Random,
+  Command_ControlVariables,
+} from "./types";
+import type { MakeOtherParam } from "./types/other";
+
+export const makeCommandVariableFromRandom = (
+  target: ParamObject_WritingTarget,
+  value: ParamObject_Operand_Random,
+  other: MakeOtherParam = {}
+): Command_ControlVariables<Operand_Random> => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent: other.indent ?? 0,
+    parameters: toArrayOperandRandom(target, value, other.operation ?? 0),
+  };
+};
+
+export const toArrayOperandRandom = (
+  target: ParamObject_WritingTarget,
+  value: ParamObject_Operand_Random,
+  operation: number = 0
+): Operand_Random => [
+  target.startId,
+  target.endId ?? target.startId,
+  operation,
+  OPERAND_RANDOM,
+  value.min,
+  value.max,
+];
