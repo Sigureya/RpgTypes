f08e31f8d71939cd2fc5147c58c6208b3edbae9e
Sigureya <sigureya1208@gmail.com>
2025-09-06 17:18:06 +0900

    抽出部分を変更。読み書きの範囲を指定するようにした。

--- src/features/core/eventCommand/valirable/variableControle.ts
+++ src/features/core/eventCommand/valirable/variableControle.ts
@@ -1,10 +1,12 @@
-import type { Command_ControlVariables } from "@RpgTypes/rmmz";
+import type {
+  Command_ControlVariables,
+  Operand_Variable,
+} from "@RpgTypes/rmmz";
+import { CONTROL_VARIABLES } from "@RpgTypes/rmmz";
+import { isCommandOperandVariables } from "@RpgTypes/rmmz/eventCommand/commands/variable/utils";
+import type { VariableParam, VVV } from "./types";
 
-export const range = (command: Command_ControlVariables): number[] => {
-  return rangeEx(command, (id): number => id);
-};
-
-export const rangeEx = <T>(
+const rangeEx = <T>(
   command: Command_ControlVariables,
   fn: (variableId: number) => T
 ): T[] => {
@@ -13,4 +15,37 @@ export const rangeEx = <T>(
     return [fn(startId)];
   }
   return Array.from({ length: endId - startId + 1 }, (_, i) => fn(startId + i));
+};
+
+export const extractVariableFromControlVariables = (
+  command: Command_ControlVariables
+): VVV => {
+  return isCommandOperandVariables(command)
+    ? vvv(command)
+    : {
+        write: rrr2(command),
+        read: [],
+      };
+};
+
+const rrr2 = (command: Command_ControlVariables): VariableParam[] => {
+  return rangeEx(
+    command,
+    (id): VariableParam => ({
+      code: CONTROL_VARIABLES,
+      variableId: id,
+    })
+  );
+};
+
+const vvv = (command: Command_ControlVariables<Operand_Variable>): VVV => {
+  return {
+    write: rrr2(command),
+    read: [
+      {
+        code: CONTROL_VARIABLES,
+        variableId: command.parameters[4],
+      },
+    ],
+  };
 };
