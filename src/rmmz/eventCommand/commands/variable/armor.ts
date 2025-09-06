ad404ba4e8b07e0438137afe561f0e3ca5ee5e26
Sigureya <sigureya1208@gmail.com>
2025-09-06 21:08:21 +0900

    装備関連を単独ファイルに分割

---
+++ src/rmmz/eventCommand/commands/variable/armor.ts
@@ -0,0 +1,31 @@
+import { CONTROL_VARIABLES } from "@RpgTypes/rmmz/rpg";
+import { OPERAND_GAMEDATA, TYPE_ARMOR } from "./constants";
+import type {
+  ParamObject_WritingTarget,
+  ParamObject_Operand_ArmorData,
+  Command_ControlVariables,
+  Operand_ArmorData,
+} from "./types";
+
+export const makeCommandVariableFromArmor = (
+  target: ParamObject_WritingTarget,
+  value: ParamObject_Operand_ArmorData
+): Command_ControlVariables<Operand_ArmorData> => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent: 0,
+    parameters: toArrayOperandArmorData(target, value, 0),
+  };
+};
+export const toArrayOperandArmorData = (
+  target: ParamObject_WritingTarget,
+  value: ParamObject_Operand_ArmorData,
+  operation: number = 0
+): Operand_ArmorData => [
+  target.startId,
+  target.endId ?? target.startId,
+  operation,
+  OPERAND_GAMEDATA,
+  TYPE_ARMOR,
+  value.armorId,
+];
---
+++ src/rmmz/eventCommand/commands/variable/weapon.ts
@@ -0,0 +1,32 @@
+import { CONTROL_VARIABLES } from "@RpgTypes/rmmz/rpg";
+import { OPERAND_GAMEDATA, TYPE_WEAPON } from "./constants";
+import type {
+  ParamObject_WritingTarget,
+  ParamObject_Operand_WeaponData,
+  Command_ControlVariables,
+  Operand_WeaponData,
+} from "./types";
+
+export const makeCommandVariableFromWeapon = (
+  target: ParamObject_WritingTarget,
+  value: ParamObject_Operand_WeaponData
+): Command_ControlVariables<Operand_WeaponData> => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent: 0,
+    parameters: toArrayOperandWeaponData(target, value, 0),
+  };
+};
+
+export const toArrayOperandWeaponData = (
+  target: ParamObject_WritingTarget,
+  value: ParamObject_Operand_WeaponData,
+  operation: number = 0
+): Operand_WeaponData => [
+  target.startId,
+  target.endId ?? target.startId,
+  operation,
+  OPERAND_GAMEDATA,
+  TYPE_WEAPON,
+  value.weaponId,
+];
