d5db3f100e74843ebc85ba7dc4e347abb32cb68f
Sigureya <sigureya1208@gmail.com>
2025-09-06 21:10:34 +0900

    各種関数を別ファイルに移した

--- src/rmmz/eventCommand/commands/variable/convert.ts
+++ src/rmmz/eventCommand/commands/variable/convert.ts
@@ -1,33 +1,11 @@
-import { TYPE_WEAPON, TYPE_ARMOR, TYPE_ITEM } from "./constants";
-import {
-  OPERAND_CONSTANT,
-  OPERAND_GAMEDATA,
-  OPERAND_RANDOM,
-  OPERAND_SCRIPT,
-  OPERAND_VARIABLE,
-} from "./constants/operand";
+import { OPERAND_CONSTANT, OPERAND_VARIABLE } from "./constants/operand";
 import type {
-  Operand_ActorStatus,
-  Operand_ArmorData,
   Operand_Constatant,
-  Operand_EnemyStatus,
-  Operand_ItemData,
-  Operand_Random,
-  Operand_Script,
   Operand_Variable,
-  Operand_WeaponData,
-  ParamObject_Operand_ActorStatus,
-  ParamObject_Operand_ArmorData,
   ParamObject_Operand_Constant,
-  ParamObject_Operand_Enemy,
-  ParamObject_Operand_ItemData,
-  ParamObject_Operand_Random,
-  ParamObject_Operand_Script,
   ParamObject_Operand_Variable,
-  ParamObject_Operand_WeaponData,
   ParamObject_WritingTarget,
 } from "./types";
-import { ENEMY_PARAM_INDEX } from "./types/enemy/dataSource";
 
 export const toArrayOperandConstant = (
   target: ParamObject_WritingTarget,
@@ -51,96 +29,4 @@ export const toArrayOperandVariable = (
   operation,
   OPERAND_VARIABLE,
   value.variableId,
-];
-
-export const toArrayOperandRandom = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_Random,
-  operation: number = 0
-): Operand_Random => [
-  target.startId,
-  target.endId ?? target.startId,
-  operation,
-  OPERAND_RANDOM,
-  value.min,
-  value.max,
-];
-
-export const toArrayOperandScript = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_Script,
-  operation: number = 0
-): Operand_Script => [
-  target.startId,
-  target.endId ?? target.startId,
-  operation,
-  OPERAND_SCRIPT,
-  value.code,
-];
-
-export const toArrayOperandItemData = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_ItemData,
-  operation: number = 0
-): Operand_ItemData => [
-  target.startId,
-  target.endId ?? target.startId,
-  operation,
-  OPERAND_GAMEDATA,
-  TYPE_ITEM,
-  value.itemId,
-];
-
-export const toArrayOperandWeaponData = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_WeaponData,
-  operation: number = 0
-): Operand_WeaponData => [
-  target.startId,
-  target.endId ?? target.startId,
-  operation,
-  OPERAND_GAMEDATA,
-  TYPE_WEAPON,
-  value.weaponId,
-];
-
-export const toArrayOperandArmorData = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_ArmorData,
-  operation: number = 0
-): Operand_ArmorData => [
-  target.startId,
-  target.endId ?? target.startId,
-  operation,
-  OPERAND_GAMEDATA,
-  TYPE_ARMOR,
-  value.armorId,
-];
-
-export const toArrayOperandActorStatus = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_ActorStatus,
-  operation: number = 0
-): Operand_ActorStatus => [
-  target.startId,
-  target.endId ?? target.startId,
-  operation,
-  OPERAND_GAMEDATA,
-  3,
-  value.index,
-  value.param,
-];
-
-export const toArrayOperandEnemyStatus = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_Enemy,
-  operation: number = 0
-): Operand_EnemyStatus => [
-  target.startId,
-  target.endId ?? target.startId,
-  operation,
-  OPERAND_GAMEDATA,
-  4,
-  value.index,
-  ENEMY_PARAM_INDEX[value.param],
 ];
--- src/rmmz/eventCommand/commands/variable/make.ts
+++ src/rmmz/eventCommand/commands/variable/make.ts
@@ -1,42 +1,14 @@
 import { CONTROL_VARIABLES } from "@RpgTypes/rmmz/rpg";
-import {
-  toArrayOperandConstant,
-  toArrayOperandItemData,
-  toArrayOperandRandom,
-  toArrayOperandScript,
-  toArrayOperandActorStatus,
-  toArrayOperandVariable,
-  toArrayOperandEnemyStatus,
-  toArrayOperandWeaponData,
-  toArrayOperandArmorData,
-} from "./convert";
+import { toArrayOperandConstant, toArrayOperandVariable } from "./convert";
 import type {
   Command_ControlVariables,
   Operand_Constatant,
   Operand_Variable,
-  Operand_Random,
-  Operand_Script,
-  Operand_ItemData,
-  Operand_ActorStatus,
-  Operand_EnemyStatus,
   ParamObject_Operand_Constant,
   ParamObject_Operand_Variable,
-  ParamObject_Operand_Random,
   ParamObject_WritingTarget,
-  ParamObject_Operand_Script,
-  ParamObject_Operand_ItemData,
-  ParamObject_Operand_ActorStatus,
-  ParamObject_Operand_Enemy,
-  ParamObject_Operand_WeaponData,
-  Operand_WeaponData,
-  Operand_ArmorData,
-  ParamObject_Operand_ArmorData,
 } from "./types";
-
-interface MakeOtherParam {
-  indent?: number;
-  operation?: number;
-}
+import type { MakeOtherParam } from "./types/other";
 
 export const makeCommandVariableFromConstant = (
   target: ParamObject_WritingTarget,
@@ -59,87 +31,5 @@ export const makeCommandVariableFromVariable = (
     code: CONTROL_VARIABLES,
     indent: other.indent ?? 0,
     parameters: toArrayOperandVariable(target, value, other.operation ?? 0),
-  };
-};
-
-export const makeCommandVariableFromRandom = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_Random,
-  other: MakeOtherParam = {}
-): Command_ControlVariables<Operand_Random> => {
-  return {
-    code: CONTROL_VARIABLES,
-    indent: other.indent ?? 0,
-    parameters: toArrayOperandRandom(target, value, other.operation ?? 0),
-  };
-};
-
-export const makeCommandVariableFromScript = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_Script,
-  other: MakeOtherParam = {}
-): Command_ControlVariables<Operand_Script> => {
-  return {
-    code: CONTROL_VARIABLES,
-    indent: other.indent ?? 0,
-    parameters: toArrayOperandScript(target, value, other.operation ?? 0),
-  };
-};
-
-export const makeCommandVariableFromItemData = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_ItemData,
-  other: MakeOtherParam = {}
-): Command_ControlVariables<Operand_ItemData> => {
-  return {
-    code: CONTROL_VARIABLES,
-    indent: other.indent ?? 0,
-    parameters: toArrayOperandItemData(target, value, other.operation ?? 0),
-  };
-};
-
-export const makeCommandVariableFromWeapon = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_WeaponData
-): Command_ControlVariables<Operand_WeaponData> => {
-  return {
-    code: CONTROL_VARIABLES,
-    indent: 0,
-    parameters: toArrayOperandWeaponData(target, value, 0),
-  };
-};
-
-export const makeCommandVariableFromArmor = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_ArmorData
-): Command_ControlVariables<Operand_ArmorData> => {
-  return {
-    code: CONTROL_VARIABLES,
-    indent: 0,
-    parameters: toArrayOperandArmorData(target, value, 0),
-  };
-};
-
-export const makeCommandVariableFromStatusData = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_ActorStatus,
-  other: MakeOtherParam = {}
-): Command_ControlVariables<Operand_ActorStatus> => {
-  return {
-    code: CONTROL_VARIABLES,
-    indent: other.indent ?? 0,
-    parameters: toArrayOperandActorStatus(target, value, other.operation ?? 0),
-  };
-};
-
-export const makeCommandVariableFromEnemyData = (
-  target: ParamObject_WritingTarget,
-  value: ParamObject_Operand_Enemy,
-  other: MakeOtherParam = {}
-): Command_ControlVariables<Operand_EnemyStatus> => {
-  return {
-    code: CONTROL_VARIABLES,
-    indent: other.indent ?? 0,
-    parameters: toArrayOperandEnemyStatus(target, value, other.operation ?? 0),
   };
 };
