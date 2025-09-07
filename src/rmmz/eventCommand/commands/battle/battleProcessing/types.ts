9941b57f29da12fe751ac89271fca1a4d0d10e97
Sigureya <sigureya1208@gmail.com>
2025-09-07 13:39:20 +0900

    敵グループの指定方法別にパラメータを３分割した

--- src/rmmz/eventCommand/commands/battle/battleProcessing/types.ts
+++ src/rmmz/eventCommand/commands/battle/battleProcessing/types.ts
@@ -1,13 +1,35 @@
-import type { ValueOf } from "@RpgTypes/libs/templates/valueOf";
 import type { EventCommandLike } from "@RpgTypes/rmmz/eventCommand/frame";
-import type { Designation } from "@RpgTypes/rmmz/rpg";
+import type {
+  BATTLE_DIRECT,
+  BATTLE_ENCOUNT,
+  BATTLE_VARIABLE,
+} from "./constants";
 
 export interface Command_BattleProcessing extends EventCommandLike<301> {
   parameters: ParamArray_BattleProcessing;
 }
 
-export type ParamArray_BattleProcessing = [
-  designation: ValueOf<Designation> | 2,
+export type ParamArray_BattleProcessing =
+  | ParamArray_BattleProcessing_Direct
+  | ParamArray_BattleProcessing_Variable
+  | ParamArray_BattleProcessing_Encount;
+
+export type ParamArray_BattleProcessing_Direct = [
+  designation: typeof BATTLE_DIRECT,
+  troopId: number,
+  canEscape: boolean,
+  canLose: boolean
+];
+
+export type ParamArray_BattleProcessing_Variable = [
+  designation: typeof BATTLE_VARIABLE,
+  troopId: number,
+  canEscape: boolean,
+  canLose: boolean
+];
+
+export type ParamArray_BattleProcessing_Encount = [
+  designation: typeof BATTLE_ENCOUNT,
   troopId: number,
   canEscape: boolean,
   canLose: boolean
