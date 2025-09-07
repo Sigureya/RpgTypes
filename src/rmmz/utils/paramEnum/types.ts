fade991aa103989b62b476503e6722d7500757e3
Sigureya <sigureya1208@gmail.com>
2025-09-07 16:35:03 +0900

    この手のパラメータをutilsに移動。プラグインコマンドの補助用であり、専用フォルダに隔離する価値がある

---
+++ src/rmmz/utils/paramEnum/types.ts
@@ -0,0 +1,18 @@
+export type Toggle = {
+  ON: 0;
+  OFF: 1;
+};
+export type Operation_PlusMinus = {
+  PLUS: 0;
+  MINUS: 1;
+};
+
+// 補足 イベントコマンドによって設定が逆なので注意
+export type Designation = {
+  DIRECT: 0;
+  VARIABLE: 1;
+};
+export type Operation_AddSub = {
+  ADD: 0;
+  SUB: 1;
+};
