c01df8dbabfc627f0599cb2c644371777c4a6bd9
Sigureya <sigureya1208@gmail.com>
2025-09-10 14:47:05 +0900

    troopクラス。機能が複雑なので段階的に作っていく

--- src/rmmzRuntime/objects/troop.ts
+++ src/rmmzRuntime/objects/troop.ts
@@ -1,3 +1,7 @@
+import type { Rmmz_Enemy } from "./core/battler/enemy";
 import type { Rmmz_Troop } from "./core/troop";
 
-export declare class Game_Troop implements Partial<Rmmz_Troop> {}
+export declare class Game_Troop implements Partial<Rmmz_Troop> {
+  members(): Rmmz_Enemy[];
+  setup(troopId: number): void;
+}
