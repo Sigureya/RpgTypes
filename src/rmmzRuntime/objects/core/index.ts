4115c879fb63c7c851a6f949f8e995f21e9035b5
Sigureya <sigureya1208@gmail.com>
2025-09-11 22:57:16 +0900

    適当にindexを追加

--- src/rmmzRuntime/objects/core/index.ts
+++ src/rmmzRuntime/objects/core/index.ts
@@ -1,3 +1,5 @@
-export * from "./interpreter";
+export type * from "./battler";
 
+export * from "./interpreter";
+export type * from "./map";
 export type { Rmmz_ActionBase } from "./action/types";
