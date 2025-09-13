74d020328eef9ae308b9bb3ae88442c90b6038a9
Sigureya <sigureya1208@gmail.com>
2025-09-13 19:40:37 +0900

    unit追加。テストの範囲的にこっちでも足りるので作っておく

--- src/rmmzRuntime/objects/index.ts
+++ src/rmmzRuntime/objects/index.ts
@@ -4,3 +4,4 @@ export { Game_Enemy } from "./enemy";
 export { Game_Interpreter } from "./interpreter";
 export { Game_Party } from "./party";
 export { Game_Troop } from "./troop";
+export { Game_Unit } from "./unit";
