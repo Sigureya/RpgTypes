bcb30e9d865cbef3b2862d8b31adea6c447ed522
Sigureya <sigureya1208@gmail.com>
2025-09-14 01:37:35 +0900

    関数が増えそうなので、実際の型と同様にクラスを継承させる

--- src/rmmzRuntime/objects/party.ts
+++ src/rmmzRuntime/objects/party.ts
@@ -1,6 +1,7 @@
 import type { Rmmz_Actor } from "./core";
 import type { Rmmz_Unit } from "./core/unit/unit";
+import { Game_Unit } from "./unit";
 
-export declare class Game_Party implements Partial<Rmmz_Unit<Rmmz_Actor>> {
-  members(): Rmmz_Actor[];
-}
+export declare class Game_Party
+  extends Game_Unit<Rmmz_Actor>
+  implements Partial<Rmmz_Unit<Rmmz_Actor>> {}
