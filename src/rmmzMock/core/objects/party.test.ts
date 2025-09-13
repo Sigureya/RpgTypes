e6240274f71d66576c51b9894117f8440767ce85
Sigureya <sigureya1208@gmail.com>
2025-09-14 01:37:08 +0900

    パスできるテストケースを作れたので保存。予想以上に複雑で制御が難しい

--- src/rmmzMock/core/objects/party.test.ts
+++ src/rmmzMock/core/objects/party.test.ts
@@ -1,54 +1,26 @@
 import type { MockedObject } from "vitest";
 import { describe, test, expect, vi } from "vitest";
-import type { IdentifiedItems } from "@RpgTypes/libs";
-import type { EventCommand } from "@RpgTypes/rmmz/eventCommand";
+import type { Command_ChangeActorHP } from "@RpgTypes/rmmz/eventCommand";
 import { makeCommandGainActorHP } from "@RpgTypes/rmmz/eventCommand";
-import type { Data_Actor, Data_Enemy } from "@RpgTypes/rmmz/rpg";
-import { makeParamArray } from "@RpgTypes/rmmz/rpg";
 import type { Rmmz_Actors } from "@RpgTypes/rmmzRuntime";
 import type { Rmmz_ActorsTemplate } from "@RpgTypes/rmmzRuntime/objects/core/battler/actors";
-import type { Rmmz_Unit } from "@RpgTypes/rmmzRuntime/objects/core/unit/unit";
 import { createFakeMap } from "./fakes/map";
-import type { FakeUnit, FakeMap, FakeBattler, ActorKeys } from "./fakes/types";
-import { Game_Interpreter } from "./rmmz_objects";
+import type { FakeMap, FakeBattler } from "./fakes/types";
+import { Game_Interpreter, Game_Party } from "./rmmz_objects";
 
-const mockBirdman: Data_Enemy = {
-  name: "birdman",
-  battlerName: "Birdman",
-  battlerHue: 0,
-  actions: [],
-  dropItems: [],
-  exp: 257,
-  gold: 123,
-  id: 1,
-  note: "",
-  params: makeParamArray({
-    mhp: 80,
-    atk: 120,
-    def: 70,
-    mat: 110,
-    mdf: 70,
-    agi: 80,
-    mmp: 99,
-    luk: 77,
-  }),
-  traits: [],
-};
-
-const mockEnemies: IdentifiedItems<Data_Enemy> = [null, mockBirdman];
-
-globalThis.$dataEnemies = mockEnemies;
 globalThis.$gameMap = createFakeMap();
 
 declare global {
-  var $dataActors: IdentifiedItems<Data_Actor>;
-  var $gameParty: FakeUnit;
+  var $gameParty: Game_Party;
   var $gameMap: Partial<FakeMap>;
   var $gameActors: Rmmz_Actors;
 }
 
-const makeMockBattler = () => {
+type FakeActor = FakeBattler & { actorId(): number };
+
+const makeMockBattler = (id: number): MockedObject<FakeActor> => {
   return {
+    actorId: vi.fn(() => id),
     loseHp: vi.fn(),
     loseMp: vi.fn(),
     loseTp: vi.fn(),
@@ -65,88 +37,90 @@ const makeMockMap = (): FakeMap => ({
 });
 
 const makeMockActors = (
-  actor: FakeBattler
-): Rmmz_ActorsTemplate<FakeBattler> => ({
+  actor: ({ actorId(): number } & FakeBattler)[]
+): MockedObject<Rmmz_ActorsTemplate<FakeBattler>> => ({
   _data: [],
-  actor: vi.fn(() => actor),
+  actor: vi.fn((id: number) => actor.find((a) => a.actorId() === id) || null),
   initialize: vi.fn(),
 });
 
-const makeMockParty = (
-  battles: FakeBattler[]
-): MockedObject<Pick<Rmmz_Unit<FakeBattler>, "members">> => ({
-  members: vi.fn(() => battles),
-});
-
-const makeMocks = () => {
-  const mockBattler = makeMockBattler();
-  const mockActors = makeMockActors(mockBattler);
-  const mockMap = makeMockMap();
-  const mockParty = makeMockParty([mockBattler]);
-
-  return { mockBattler, mockActors, mockMap, mockParty };
-};
-
-interface TestCase {
-  command: EventCommand;
-  key: ActorKeys;
-  value: number;
-  targetIds: number[];
-  xx: (globalThis: typeof global) => void;
+interface MakeMocksResult {
+  mockBattler: MockedObject<FakeActor>;
+  mockBattler2: MockedObject<FakeActor>;
+  mockActors: ReturnType<typeof makeMockActors>;
+  mockMap: ReturnType<typeof makeMockMap>;
+  mockParty: Game_Party;
 }
+
+const makeMocks = (): MakeMocksResult => {
+  const mockBattler = makeMockBattler(1);
+  const mockBattler2 = makeMockBattler(2);
+  const mockActors = makeMockActors([mockBattler, mockBattler2]);
+  const mockMap = makeMockMap();
+  const mockParty = new Game_Party();
+  return { mockBattler, mockActors, mockMap, mockParty, mockBattler2 };
+};
 
 const makeInterpreter = () => {
   const interpreter = new Game_Interpreter();
   vi.spyOn(interpreter, "operateValue");
   vi.spyOn(interpreter, "iterateActorEx");
+  vi.spyOn(interpreter, "command311");
   return interpreter;
 };
 
-const runCommandTest = ({ command, key, value }: TestCase) => {
-  const { mockActors, mockBattler, mockMap, mockParty } = makeMocks();
-  globalThis.$gameActors = mockActors as Rmmz_Actors;
-  globalThis.$gameMap = mockMap;
-  globalThis.$gameParty = mockParty as unknown as FakeUnit;
-  const interpreter = makeInterpreter();
-  test(key, () => {
-    interpreter.setup([command], 0);
-    const result = interpreter.executeCommand();
-    expect(interpreter.iterateActorEx).toHaveBeenCalledWith(
-      command.parameters[0],
-      command.parameters[1],
-      expect.any(Function)
-    );
-
-    expect(interpreter.operateValue).toHaveBeenCalledWith(
-      command.parameters[2],
-      command.parameters[3],
-      command.parameters[4]
-    );
-
-    expect(result).toBe(true);
-    expect(mockBattler[key]).toBeDefined();
-    expect(mockBattler[key]).toHaveBeenCalledWith(value);
-  });
+const setupGlobal = (mocks: MakeMocksResult) => {
+  globalThis.$gameActors = mocks.mockActors as unknown as Rmmz_Actors;
+  globalThis.$gameMap = mocks.mockMap;
+  globalThis.$gameParty = new Game_Party();
 };
 
-const testCases: TestCase[] = [
-  {
-    command: makeCommandGainActorHP({
-      allowDeath: false,
-      operand: {
-        mode: "direct",
-        value: 123,
-      },
-      targetType: "direct",
-      target: 1,
-    }),
-    targetIds: [1],
-    key: "gainHp",
-    value: 123,
-    xx: (g) => {},
-  },
-];
+const paaramCalledWith = (
+  command: Command_ChangeActorHP,
+  interpreter: Game_Interpreter
+) => {
+  expect(interpreter.iterateActorEx).toHaveBeenCalledWith(
+    command.parameters[0],
+    command.parameters[1],
+    expect.any(Function)
+  );
+  expect(interpreter.operateValue).toHaveBeenCalledWith(
+    command.parameters[2],
+    command.parameters[3],
+    command.parameters[4]
+  );
+};
 
-describe("", () => {
-  testCases.forEach(runCommandTest);
+describe("gainHp single", () => {
+  const mocks = makeMocks();
+  setupGlobal(mocks);
+  const { mockBattler, mockBattler2, mockActors } = mocks;
+  const interpreter = makeInterpreter();
+
+  const command: Command_ChangeActorHP = {
+    code: 311,
+    indent: 0,
+    parameters: [0, 1, 0, 0, 123, false],
+  };
+  test("make command", () => {
+    const newCommand = makeCommandGainActorHP({
+      allowDeath: false,
+      target: 1,
+      operand: { mode: "direct", value: 123 },
+      targetType: "direct",
+    });
+    expect(newCommand).toEqual(command);
+  });
+
+  test("run command", () => {
+    interpreter.setup([command], 0);
+    const result = interpreter.executeCommand();
+    paaramCalledWith(command, interpreter);
+    expect(interpreter.command311).toHaveBeenCalledWith(command.parameters);
+    expect(mockActors.actor).toHaveBeenCalled();
+
+    expect(result).toBe(true);
+    expect(mockBattler.gainHp).toHaveBeenCalledWith(123);
+    expect(mockBattler2.gainHp).not.toHaveBeenCalled();
+  });
 });
