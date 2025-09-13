52d8d66be73e15f9d51fe1a75bcd8a64a056de7f
Sigureya <sigureya1208@gmail.com>
2025-09-14 03:07:45 +0900

    内側では不要なので消しておく

--- src/rmmzRuntime/objects/core/battler/actors.ts
+++ src/rmmzRuntime/objects/core/battler/actors.ts
@@ -5,7 +5,6 @@ export type Rmmz_Actors = Rmmz_ActorsTemplate<Rmmz_Actor>;
 export interface Rmmz_ActorsTemplate<
   Actor extends Partial<Rmmz_Actor> = Rmmz_Actor
 > {
-  _data: Actor[];
   initialize(): void;
   actor(actorId: number): Actor | null;
 }
