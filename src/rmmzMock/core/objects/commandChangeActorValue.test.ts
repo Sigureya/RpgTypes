ba17f3337abba4fd040cff799e4266535cee2412
Sigureya <sigureya1208@gmail.com>
2025-09-16 11:08:38 +0900

    アクターのHPなどを書き換えるイベントコマンドのテスト

---
+++ src/rmmzMock/core/objects/commandChangeActorValue.test.ts
@@ -0,0 +1,327 @@
+import type { MockedObject } from "vitest";
+import { describe, expect, test, vi } from "vitest";
+import type {
+  Command_ChangeActorHP,
+  Command_ChangeActorMP,
+  Command_ChangeActorTP,
+  EventCommand,
+} from "@RpgTypes/rmmz/eventCommand";
+import {
+  makeCommandGainActorHP,
+  makeCommandGainActorMP,
+  makeCommandGainActorTP,
+} from "@RpgTypes/rmmz/eventCommand";
+import type { Rmmz_Variables } from "@RpgTypes/rmmzRuntime";
+import type { Rmmz_ActorsTemplate } from "@RpgTypes/rmmzRuntime/objects/core/battler/actors";
+import type { FakeBattler, FakeMap } from "./fakes/types";
+import { Game_Party, Game_Interpreter } from "./rmmz_objects";
+
+type FakeActor = FakeBattler & { actorId(): number };
+type MockedActors = MockedObject<Rmmz_ActorsTemplate<FakeBattler>>;
+
+interface MakeMocksResult {
+  mockBattler: MockedObject<FakeActor>;
+  mockBattler2: MockedObject<FakeActor>;
+  mockActors: MockedActors;
+  mockMap: FakeMap;
+  mockParty: Game_Party;
+  mockVariables: MockedObject<Rmmz_Variables>;
+}
+
+const each = (mockParty: Game_Party) => {
+  expect(mockParty.members).toHaveBeenCalledTimes(1);
+};
+
+const single = (mockParty: Game_Party) => {
+  expect(mockParty.members).not.toHaveBeenCalled();
+};
+
+type CommandTypes =
+  | Command_ChangeActorHP
+  | Command_ChangeActorMP
+  | Command_ChangeActorTP;
+
+const paramCalledWith = (
+  command: CommandTypes,
+  interpreter: Game_Interpreter
+) => {
+  expect(interpreter.iterateActorEx).toHaveBeenCalledWith(
+    command.parameters[0],
+    command.parameters[1],
+    expect.any(Function)
+  );
+  expect(interpreter.operateValue).toHaveBeenCalledWith(
+    command.parameters[2],
+    command.parameters[3],
+    command.parameters[4]
+  );
+  const key: keyof Game_Interpreter = `command${command.code}` as const;
+  expect(interpreter[key]).toHaveBeenCalledWith(command.parameters);
+};
+
+interface ActorCCC {
+  called: number[];
+  notCalled: number[];
+}
+
+const actorsCalled = (mockActors: MockedActors, ids: ActorCCC) => {
+  ids.called.forEach((id) => {
+    expect(mockActors.actor).toHaveBeenCalledWith(id);
+  });
+  ids.notCalled.forEach((id) => {
+    expect(mockActors.actor).not.toHaveBeenCalledWith(id);
+  });
+};
+
+const makeInterpreter = (command: EventCommand) => {
+  const interpreter = new Game_Interpreter();
+  vi.spyOn(interpreter, "operateValue");
+  vi.spyOn(interpreter, "iterateActorEx");
+  vi.spyOn(interpreter, "iterateActorId");
+
+  vi.spyOn(interpreter, "command311");
+  vi.spyOn(interpreter, "command312");
+  vi.spyOn(interpreter, "command326");
+  interpreter.setup([command], 0);
+  return interpreter;
+};
+
+const makeMockBattler = (id: number): MockedObject<FakeActor> => {
+  return {
+    actorId: vi.fn(() => id),
+    loseHp: vi.fn(),
+    loseMp: vi.fn(),
+    loseTp: vi.fn(),
+    gainHp: vi.fn(),
+    gainMp: vi.fn(),
+    gainTp: vi.fn(),
+    isAlive: vi.fn(() => true),
+    isDead: vi.fn(() => false),
+  };
+};
+const makeMockActors = (
+  actor: ({ actorId(): number } & FakeBattler)[]
+): MockedActors => ({
+  actor: vi.fn((id: number) => actor.find((a) => a.actorId() === id) || null),
+  initialize: vi.fn(),
+});
+
+const makeMockParty = (actorIds: number[]): Game_Party => {
+  const party = new Game_Party();
+  party._actors = [...actorIds];
+  vi.spyOn(party, "members");
+  return party;
+};
+
+interface MockParam {
+  variables?: Record<number, number>;
+}
+const makeMockMap = (): FakeMap => ({
+  mapId: () => 1,
+});
+
+const makeMockVariables = (
+  values: Record<number, number>
+): MockedObject<Rmmz_Variables> => {
+  return {
+    clear: vi.fn(),
+    value: vi.fn((id: number) => values[id] || 0),
+    setValue: vi.fn(),
+    onChange: vi.fn(),
+  };
+};
+
+const makeMocks = ({ variables = {} }: MockParam = {}): MakeMocksResult => {
+  const mockBattler1 = makeMockBattler(1);
+  const mockBattler2 = makeMockBattler(2);
+  const mockActors = makeMockActors([mockBattler1, mockBattler2]);
+  const mockMap = makeMockMap();
+  const mockParty = makeMockParty([1, 2]);
+  const mockVariables = makeMockVariables(variables);
+  return {
+    mockBattler: mockBattler1,
+    mockActors,
+    mockMap,
+    mockParty,
+    mockBattler2,
+    mockVariables,
+  };
+};
+const setupGlobal = (mocks: MakeMocksResult) => {
+  vi.stubGlobal("$gameActors", mocks.mockActors);
+  vi.stubGlobal("$gameParty", mocks.mockParty);
+  vi.stubGlobal("$gameMap", mocks.mockMap);
+  vi.stubGlobal("$gameVariables", mocks.mockVariables);
+};
+
+interface TestCaseTemplate<Command> {
+  caseName: string;
+  command: Command;
+  expected: Command;
+  variableLiteral?: Record<number, number>;
+  members: (party: Game_Party) => void;
+  actors: ActorCCC;
+  changeValue: (battlers: MockedObject<FakeActor>[]) => void;
+  usingVariables: (v: MockedObject<Rmmz_Variables>) => void;
+}
+
+type TestCase =
+  | TestCaseTemplate<Command_ChangeActorHP>
+  | TestCaseTemplate<Command_ChangeActorMP>
+  | TestCaseTemplate<Command_ChangeActorTP>;
+
+const runTestCase = (testCase: TestCase) => {
+  describe(testCase.caseName, () => {
+    test("make command", () => {
+      expect(testCase.command).toEqual(testCase.expected);
+    });
+    test("exec", () => {
+      const mock = makeMocks(testCase.variableLiteral);
+      setupGlobal(mock);
+      const interpreter = makeInterpreter(testCase.command);
+      const result: boolean = interpreter.executeCommand();
+      expect(result).toBe(true);
+      testCase.members(mock.mockParty);
+      paramCalledWith(testCase.command, interpreter);
+      actorsCalled(mock.mockActors, testCase.actors);
+      testCase.changeValue([mock.mockBattler, mock.mockBattler2]);
+      testCase.usingVariables(mock.mockVariables);
+    });
+  });
+};
+
+const testCases: TestCase[] = [
+  {
+    caseName: "gain HP direct",
+    command: {
+      code: 311,
+      indent: 0,
+      parameters: [0, 1, 0, 0, 123, false],
+    },
+    expected: makeCommandGainActorHP({
+      allowDeath: false,
+      targetType: "direct",
+      target: 1,
+      operand: { mode: "direct", value: 123 },
+    }),
+    members: (party) => single(party),
+    actors: { called: [1], notCalled: [2] },
+    changeValue: ([a1, a2]) => {
+      expect(a1.gainHp).toHaveBeenCalledWith(123);
+      expect(a2.gainHp).not.toHaveBeenCalled();
+    },
+    usingVariables: (v) => {
+      expect(v.value).not.toHaveBeenCalled();
+    },
+  },
+  {
+    caseName: "gain HP each",
+    command: {
+      code: 311,
+      indent: 0,
+      parameters: [0, 0, 0, 0, 123, true],
+    },
+    expected: makeCommandGainActorHP({
+      allowDeath: true,
+      targetType: "each",
+      operand: { mode: "direct", value: 123 },
+    }),
+    members: (party) => each(party),
+    actors: { called: [1, 2], notCalled: [] },
+    changeValue: ([a1, a2]) => {
+      expect(a1.gainHp).toHaveBeenCalledWith(123);
+      expect(a2.gainHp).toHaveBeenCalledWith(123);
+    },
+    usingVariables: (v) => {
+      expect(v.value).not.toHaveBeenCalled();
+    },
+  },
+  {
+    caseName: "gain MP direct",
+    command: {
+      code: 312,
+      indent: 0,
+      parameters: [0, 1, 0, 0, 123],
+    },
+    expected: makeCommandGainActorMP({
+      targetType: "direct",
+      target: 1,
+      operand: { mode: "direct", value: 123 },
+    }),
+    members: (party) => single(party),
+    actors: { called: [1], notCalled: [2] },
+    changeValue: ([a1, a2]) => {
+      expect(a1.gainMp).toHaveBeenCalledWith(123);
+      expect(a2.gainMp).not.toHaveBeenCalled();
+    },
+    usingVariables: (v) => {
+      expect(v.value).not.toHaveBeenCalled();
+    },
+  },
+  {
+    caseName: "gain MP each",
+    command: {
+      code: 312,
+      indent: 0,
+      parameters: [0, 0, 0, 0, 123],
+    },
+    expected: makeCommandGainActorMP({
+      targetType: "each",
+      operand: { mode: "direct", value: 123 },
+    }),
+    members: (party) => each(party),
+    actors: { called: [1, 2], notCalled: [] },
+    changeValue: ([a1, a2]) => {
+      expect(a1.gainMp).toHaveBeenCalledWith(123);
+      expect(a2.gainMp).toHaveBeenCalledWith(123);
+    },
+    usingVariables: (v) => {
+      expect(v.value).not.toHaveBeenCalled();
+    },
+  },
+  {
+    caseName: "gain TP direct",
+    command: {
+      code: 326,
+      indent: 0,
+      parameters: [0, 1, 0, 0, 123],
+    },
+    expected: makeCommandGainActorTP({
+      targetType: "direct",
+      target: 1,
+      operand: { mode: "direct", value: 123 },
+    }),
+    members: (party) => single(party),
+    actors: { called: [1], notCalled: [2] },
+    changeValue: ([a1, a2]) => {
+      expect(a1.gainTp).toHaveBeenCalledWith(123);
+      expect(a2.gainTp).not.toHaveBeenCalled();
+    },
+    usingVariables: (v) => {
+      expect(v.value).not.toHaveBeenCalled();
+    },
+  },
+  {
+    caseName: "gain TP each",
+    command: {
+      code: 326,
+      indent: 0,
+      parameters: [0, 0, 0, 0, 123],
+    },
+    expected: makeCommandGainActorTP({
+      targetType: "each",
+      operand: { mode: "direct", value: 123 },
+    }),
+    members: (party) => each(party),
+    actors: { called: [1, 2], notCalled: [] },
+    changeValue: ([a1, a2]) => {
+      expect(a1.gainTp).toHaveBeenCalledWith(123);
+      expect(a2.gainTp).toHaveBeenCalledWith(123);
+    },
+    usingVariables: (v) => {
+      expect(v.value).not.toHaveBeenCalled();
+    },
+  },
+];
+
+testCases.forEach((tc) => runTestCase(tc));
