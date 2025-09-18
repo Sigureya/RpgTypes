c77db2f066dfe60ebae41a63ff2a8f34b3f2c18b
Sigureya <sigureya1208@gmail.com>
2025-09-18 14:56:58 +0900

    Game_Systemに対応した型。

--- src/rmmzRuntime/objects/core/index.ts
+++ src/rmmzRuntime/objects/core/index.ts
@@ -5,4 +5,5 @@ export type * from "./map";
 export * from "./message";
 export * from "./troop";
 export type { Rmmz_ActionBase } from "./action/types";
+export type { Rmmz_System } from "./system/system";
 export type { Rmmz_Variables } from "./variables";
