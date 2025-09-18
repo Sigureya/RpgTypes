868cb104d77ff57dd03c263ea516e43d4483b6c6
Sigureya <sigureya1208@gmail.com>
2025-09-19 03:47:33 +0900

    対応Sceneを追加

---
+++ src/rmmzRuntime/scene/core/equip.ts
@@ -0,0 +1,26 @@
+import type { Rectangle } from "@RpgTypes/libs";
+
+export interface Rmmz_Scene_Equip {
+  createStatusWindow(): void;
+  statusWindowRect(): Rectangle;
+  createCommandWindow(): void;
+  commandWindowRect(): Rectangle;
+  createSlotWindow(): void;
+  slotWindowRect(): Rectangle;
+  createItemWindow(): void;
+  itemWindowRect(): Rectangle;
+  statusWidth(): number;
+  needsPageButtons(): boolean;
+  arePageButtonsEnabled(): boolean;
+  refreshActor(): void;
+  commandEquip(): void;
+  commandOptimize(): void;
+  commandClear(): void;
+  onSlotOk(): void;
+  onSlotCancel(): void;
+  onItemOk(): void;
+  executeEquipChange(): void;
+  onItemCancel(): void;
+  nActorChange(): void;
+  hideItemWindow(): void;
+}
