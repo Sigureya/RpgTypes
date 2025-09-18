79fd1d5a4dcff0e30c11b9381e1f9b404477997a
Sigureya <sigureya1208@gmail.com>
2025-09-18 15:20:29 +0900

    importやexportの修正

--- src/rmmzRuntime/objects/core/index.ts
+++ src/rmmzRuntime/objects/core/index.ts
@@ -4,6 +4,7 @@ export * from "./interpreter";
 export type * from "./map";
 export * from "./message";
 export * from "./troop";
+export * from "./unit";
 export type { Rmmz_ActionBase } from "./action/types";
 export type { Rmmz_System } from "./system/system";
 export type { Rmmz_Variables } from "./variables";
