d68f53f3d8c88c1c59b19618a52bf85c148755b8
Sigureya <sigureya1208@gmail.com>
2025-09-19 22:51:06 +0900

    構造体の解析に対応

--- src/rmmz/plugin/schema/compile/kinds/core/parsePlugin.ts
+++ src/rmmz/plugin/schema/compile/kinds/core/parsePlugin.ts
@@ -4,8 +4,13 @@ import type {
   ParsedPlugin,
   PluginCommandTokens,
   PluginParamTokens,
+  StructParseState,
 } from "./parse/types";
-import type { PluginCommandBody, PluginJSON } from "./pluginJSONTypes";
+import type {
+  PluginCommandBody,
+  PluginJSON,
+  PluginStructBody,
+} from "./pluginJSONTypes";
 import type { PrimitiveParam } from "./primitiveParams";
 
 export interface PluginParam {
@@ -23,6 +28,7 @@ const compilePlugin = (parsedPlugin: ParsedPlugin): PluginJSON => {
     meta: parsedPlugin.meta,
     commands: reduceCommands(parsedPlugin.commands),
     params: reduceParams(parsedPlugin.params),
+    structs: reduceStructs(parsedPlugin.structs),
   };
 };
 
@@ -40,15 +46,27 @@ const reduceParams = (
 const reduceCommands = (
   tokens: ReadonlyArray<PluginCommandTokens>
 ): Record<string, PluginCommandBody> => {
-  return tokens.reduce((acc, token) => {
-    const body: PluginCommandBody = {
-      desc: token.desc,
-      text: token.text,
-      args: reduceParams(token.args),
-    };
-    return {
-      [token.command]: body,
-      ...acc,
-    };
-  }, {});
+  return Object.fromEntries(
+    tokens.map((token): [string, PluginCommandBody] => [
+      token.command,
+      {
+        desc: token.desc,
+        text: token.text,
+        args: reduceParams(token.args),
+      },
+    ])
+  );
+};
+
+const reduceStructs = (
+  structs: ReadonlyArray<StructParseState>
+): Record<string, PluginStructBody> => {
+  return Object.fromEntries(
+    structs.map((struct): [string, PluginStructBody] => [
+      struct.name,
+      {
+        params: reduceParams(struct.params),
+      },
+    ])
+  );
 };
--- src/rmmz/plugin/schema/compile/kinds/core/pluginJSONTypes.ts
+++ src/rmmz/plugin/schema/compile/kinds/core/pluginJSONTypes.ts
@@ -7,12 +7,19 @@ export interface PluginJSON {
   commands: Record<string, PluginCommandBody>;
   params: Record<string, PrimitiveParam>;
   dependencies?: PluginDependencies;
+  structs: Record<string, PluginStructBody>;
 }
 
 export interface PluginCommandBody {
   desc?: string;
   text?: string;
   args: {
+    [key: string]: PrimitiveParam;
+  };
+}
+
+export interface PluginStructBody {
+  params: {
     [key: string]: PrimitiveParam;
   };
 }
