d0f96d7a441d97c2a3725959ca630ab3a27822be
Sigureya <sigureya1208@gmail.com>
2025-09-23 01:50:17 +0900

    プラグインコマンドからのデータ抽出。string[]やstruct<T>に未対応なので修正が必要

---
+++ src/features/core/extract/text/plugin/pluginCommand.ts
@@ -0,0 +1,42 @@
+import type { Command_PluginCommandMZ } from "@RpgTypes/rmmz";
+import type {
+  FilteredPluginSchema,
+  PrimitiveStringParam,
+} from "@RpgTypes/rmmz/plugin/schema/compile";
+import type { ExtractedPluginCommandArgs } from "./types";
+
+export const extractPluginCommandArgs = (
+  command: Command_PluginCommandMZ,
+  pluginSchema: {
+    get(s: string): undefined | FilteredPluginSchema<PrimitiveStringParam>;
+  }
+): ExtractedPluginCommandArgs[] => {
+  const pluginName = command.parameters[0];
+  const plugin = pluginSchema.get(pluginName);
+  if (!plugin) {
+    return [];
+  }
+  const commandName: string = command.parameters[1];
+  const commandSchema = plugin.commands[commandName];
+  if (!commandSchema) {
+    return [];
+  }
+
+  return xxxCC(command, commandSchema);
+};
+
+const xxxCC = (
+  command: Command_PluginCommandMZ,
+  schema: { args: Record<string, PrimitiveStringParam> }
+): ExtractedPluginCommandArgs[] => {
+  return Object.entries(schema.args).map(
+    ([argName, paramSchema]): ExtractedPluginCommandArgs => ({
+      value: command.parameters[3][argName],
+      type: paramSchema.kind,
+      kind: paramSchema.text ?? `${command.parameters[1]}.${argName}`,
+      desc: paramSchema.desc ?? "",
+      commandName: command.parameters[1],
+      pluginName: command.parameters[0],
+    })
+  );
+};
