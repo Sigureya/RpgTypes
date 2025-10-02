28b0be8140e9141accfcd65e70f20c40301fbeb4
Sigureya <sigureya1208@gmail.com>
2025-10-02 18:56:40 +0900

    泥縄で型を追加。方針が見えないので半端な状態が続く

--- src/rmmz/plugin/schema/compile/filter2Type2.ts
+++ src/rmmz/plugin/schema/compile/filter2Type2.ts
@@ -5,10 +5,11 @@ import type {
   StructArrayRefParam,
   StructRefParam,
 } from "./kinds";
+import type { PluginStructSchemaArray } from "./kinds/core/types";
 
-export interface PluginParamGroups<T = PrimitiveParam> {
+export interface PluginParamGroups<T extends PrimitiveParam = PrimitiveParam> {
   single: NamedAttribute<ScalaParam>[];
-  array: NamedAttribute<Extract<PrimitiveParam, ArrayParam>>[];
+  array: NamedAttribute<Extract<T, ArrayParam>>[];
   struct: NamedAttribute<StructRefParam>[];
   structArray: NamedAttribute<StructArrayRefParam>[];
 }
@@ -16,4 +17,17 @@ export interface PluginParamGroups<T = PrimitiveParam> {
 export interface NamedAttribute<T> {
   name: string;
   attr: T;
+}
+
+export interface FilterdPlugin<T extends PrimitiveParam = PrimitiveParam> {
+  params: PluginParamGroups<T>;
+  structs: PluginStructSchemaArray[];
+  commands: Cmd2[];
+}
+
+export interface Cmd2 {
+  command: string;
+  desc?: string;
+  text?: string;
+  args: PluginParamGroups;
 }
