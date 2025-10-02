ac6ac07bd7805c83fe12cbeb788c22a529520bd3
Sigureya <sigureya1208@gmail.com>
2025-10-03 06:53:41 +0900

    filter修正。コンパイルエラーが出て邪魔なのでコメントアウト

--- src/rmmz/plugin/plugin.ts
+++ src/rmmz/plugin/plugin.ts
@@ -1,6 +1,6 @@
+import type { PrimitiveStructSchema } from "../../features/pluguinJSONSchema/compileFlatStruct";
+import { compileFlatStruct } from "../../features/pluguinJSONSchema/compileFlatStruct";
 import type { PrimitiveParam } from "./schema/compile/kinds";
-import type { PrimitiveStructSchema } from "./schema/compile/kinds/compileFlatStruct";
-import { compileFlatStruct } from "./schema/compile/kinds/compileFlatStruct";
 import { compilePluginToObject } from "./schema/compile/kinds/core/compilePlugin";
 import type { PluginJSON } from "./schema/compile/kinds/core/pluginJSONTypes";
 
--- src/rmmz/plugin/schema/compile/filter3.ts
+++ src/rmmz/plugin/schema/compile/filter3.ts
@@ -11,20 +11,20 @@ export const isEmptyParamGroup = (gropu: PluginParamGroups): boolean => {
   );
 };
 
-export const filterStructsEx = <T extends PluginParam>(
-  structs: ReadonlyArray<PluginStructSchemaArray>,
-  fn: (param: PluginParam) => param is T
-): PluginStructSchemaArray<T>[] => {
-  return structs.reduce((acc, schema): PluginStructSchemaArray<T>[] => {
-    const filtered = pickParams(schema, fn);
-    return filtered.params.length > 0 ? [...acc, filtered] : acc;
-  }, []);
-};
+// export const filterStructsEx = <T extends PluginParam>(
+//   structs: ReadonlyArray<PluginStructSchemaArray>,
+//   fn: (param: PluginParam) => param is T
+// ): PluginStructSchemaArray<T>[] => {
+//   return structs.reduce((acc, schema): PluginStructSchemaArray<T>[] => {
+//     const filtered = pickParams(schema, fn);
+//     return filtered.params.length > 0 ? [...acc, filtered] : acc;
+//   }, []);
+// };
 
-const pickParams = <T extends PluginParam>(
-  strcutSchema: PluginStructSchemaArray,
-  fn: (schema: PluginParam<PrimitiveParam>) => schema is T
-): PluginStructSchemaArray<T> => ({
-  struct: strcutSchema.struct,
-  params: strcutSchema.params.filter((p) => fn(p)),
-});
+// const pickParams = <T extends PluginParam>(
+//   strcutSchema: PluginStructSchemaArray,
+//   fn: (schema: PluginParam<PrimitiveParam>) => schema is T
+// ): PluginStructSchemaArray<T> => ({
+//   struct: strcutSchema.struct,
+//   params: strcutSchema.params.filter((p) => fn(p)),
+// });
