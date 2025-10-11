6fd3c5de71c2b7d9b21ecb31bc5bb2d3ec9b8d9e
Sigureya <sigureya1208@gmail.com>
2025-10-11 20:41:58 +0900

    暫定テストコード。方針が決まるまでは不完全なコードで行く

---
+++ src/rmmz/plugin/paramFilter.test.ts
@@ -0,0 +1,18 @@
+import { test, expect, describe } from "vitest";
+import type { ParamFilter } from "./paramFilter";
+import { ppp, createParamFilter } from "./paramFilter";
+
+describe("createParamFilter", () => {
+  const eeFilter: ParamFilter<"boolean" | "number"> = {
+    kinds: ["boolean", "number"],
+    single: new Set(["boolean", "number"]),
+    array: new Set(["boolean[]", "number[]"]),
+  };
+  test("creates a filter correctly", () => {
+    const xxFilter: ParamFilter<"boolean" | "number"> = createParamFilter([
+      "boolean",
+      "number",
+    ]);
+    expect(xxFilter).toEqual(eeFilter);
+  });
+});
