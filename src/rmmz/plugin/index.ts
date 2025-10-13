97d4ecf84b321a22a98a045be699392d8b6c8d36
Sigureya <sigureya1208@gmail.com>
2025-10-13 23:56:30 +0900

    typeと関数を切り分けた

--- src/rmmz/plugin/index.ts
+++ src/rmmz/plugin/index.ts
@@ -1,5 +1,5 @@
 export * from "./core";
 export * from "./typeTest";
-export { type ClassifiedPluginParams, classifyPluginParams } from "./classify";
+export { classifyPluginParams } from "./classify";
 export { createStructMap, structDependencies } from "./structDependencies";
 export { pluginSourceToJSON } from "./plugin";
