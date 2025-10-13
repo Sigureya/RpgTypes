af4a70c28c647510bcb953a5312f8d07147edd9a
Sigureya <sigureya1208@gmail.com>
2025-10-14 02:00:09 +0900

    配列に部分対応

--- src/features/plugin/JSONPath/jsonPathString.test.ts
+++ src/features/plugin/JSONPath/jsonPathString.test.ts
@@ -46,10 +46,12 @@ interface Family {
   father: Person;
   mother: Person;
   children: Person[];
+  id: number;
 }
 
 describe("JSONPathType nested", () => {
   const mockFamily = {
+    id: 211,
     father: { name: "Bob", age: 40, items: [1, 2, 3] },
     mother: { name: "Alice", age: 38, items: [4, 5, 6] },
     children: [
@@ -57,6 +59,13 @@ describe("JSONPathType nested", () => {
       { name: "Daisy", age: 8, items: [9] },
     ],
   } as const satisfies Family;
+  test("number param id", () => {
+    const mockPath: JSONPathType<Family> = "$.id";
+    const path = new JSONPathJS(mockPath);
+    const value = path.find(mockFamily);
+    expect(value).toEqual([211]);
+  });
+
   test("string param name", () => {
     const mockPath: JSONPathType<Family> = "$.father.name";
     const path = new JSONPathJS(mockPath);
@@ -69,10 +78,30 @@ describe("JSONPathType nested", () => {
     const value = path.find(mockFamily);
     expect(value).toEqual([38]);
   });
+
   test("array param items", () => {
+    const mockPath: JSONPathType<Family> = "$.father.items";
+    const path = new JSONPathJS(mockPath);
+    const value = path.find(mockFamily);
+    expect(value).toEqual([[1, 2, 3]]);
+  });
+  //   test("array param items with wildcard", () => {
+  //     const mockPath: JSONPathType<Family> = "$.father.items[*]";
+  //     const path = new JSONPathJS(mockPath);
+  //     const value = path.find(mockFamily);
+  //     expect(value).toEqual([1, 2, 3]);
+  //   });
+  test("array param children age with wildcard", () => {
     const mockPath: JSONPathType<Family> = "$.children[*].age";
     const path = new JSONPathJS(mockPath);
     const value = path.find(mockFamily);
     expect(value).toEqual([10, 8]);
+  });
+  test("array param children name with wildcard", () => {
+    const mockPath: JSONPathType<Family> = "$.children[*].name";
+    const path = new JSONPathJS(mockPath);
+    const value = path.find(mockFamily);
+    const expected: string[] = ["Charlie", "Daisy"];
+    expect(value).toEqual(expected);
   });
 });
