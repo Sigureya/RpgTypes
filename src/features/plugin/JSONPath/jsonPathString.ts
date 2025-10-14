ef14994b70283c4bc94d586c20e6961e1c050d16
Sigureya <sigureya1208@gmail.com>
2025-10-14 13:12:14 +0900

    対応範囲を強化。ネストされたオブジェクトおよび配列に対応した

--- src/features/plugin/JSONPath/jsonPathString.ts
+++ src/features/plugin/JSONPath/jsonPathString.ts
@@ -1,26 +1,27 @@
 export type JSONPathType<T, Root extends string = "$"> = string &
-  (ValueOf<JSONPathDirect<T, Root>> | ValueOf<JSONPathNested<T, Root>>);
+  ValueOf<T extends object ? JSONPathNested<T, Root> : {}>;
 type ValueOf<T> = T[keyof T];
-
-type JSONPathDirect<T, Parent extends string> =
-  | {
-      [K in keyof T]:
-        | `${Parent}.${K & string}`
-        | (T[K] extends (number | string)[]
-            ? `${Parent}.${K & string}${ArrayAccsess}`
-            : never);
-    };
 
 type ArrayAccsess = `[${number}]` | "[*]";
 
-type JSONPathNested<T, Root extends string> =
+type JSONPathNested<T extends object, Parent extends string> =
   | {
-      [K in Extract<keyof T, string>]: T[K] extends Array<
-        infer U extends object
-      >
-        ?
-            | `${Root}.${K}${ArrayAccsess}`
-            | `${Root}.${K}${ArrayAccsess}.${Extract<keyof U, string>}`
-            | JSONPathNested<U, `${Root}.${K}${ArrayAccsess}`>
-        : "[*]" | `${Root}.${K}.${Extract<keyof T[K], string>}`;
+      [K in Extract<keyof T, string>]:
+        | `${Parent}.${K & string}`
+        | (T[K] extends object
+            ? T[K] extends unknown[]
+              ? `${Parent}.${K & string}${ArrayAccsess}`
+              : JSONPathNested<T[K], `${Parent}.${K & string}`>
+            : never)
+        | (T[K] extends Array<infer U>
+            ?
+                | `${Parent}.${K}${ArrayAccsess}.${Extract<keyof U, string>}`
+                | (U extends unknown[]
+                    ? never
+                    : U extends object
+                    ? ValueOf<
+                        JSONPathNested<U, `${Parent}.${K}${ArrayAccsess}`>
+                      >
+                    : never)
+            : `${Parent}.${K}.${Extract<keyof T[K], string>}`);
     };
