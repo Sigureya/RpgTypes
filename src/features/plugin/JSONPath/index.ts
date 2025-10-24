31212aeaa1a90d4778d0e51bd644a6cf87558fdd
Sigureya <sigureya1208@gmail.com>
2025-10-25 06:20:31 +0900

    旧実装を削除。古い方は処理が複雑。

--- src/features/plugin/JSONPath/index.ts
+++ src/features/plugin/JSONPath/index.ts
@@ -1,4 +1,2 @@
 export type * from "./types";
 export type { JSONPathType } from "./types/jsonPathString";
-export { createPathFromSchema } from "./struct";
-export { extractPluginParamValues } from "./extractValues";
