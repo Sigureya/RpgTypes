753936734950d991b97d0057ede38f09adc892af
Sigureya <sigureya1208@gmail.com>
2025-10-26 10:00:34 +0900

    暫定実装。まずは単独の型だけ

--- src/rmmz/plugin/core/testValueType.ts
+++ src/rmmz/plugin/core/testValueType.ts
@@ -1,7 +1,33 @@
-import type { ParamKinds, PrimitiveParam } from "./paramUnion";
+import type { ParamKinds, PrimitiveParam, ScalaParam } from "./paramUnion";
 
-const TABLE = {} as const satisfies {
-  [key in ParamKinds]?: string;
+const TABLE = {
+  string: { type: "string" },
+  number: { type: "number" },
+  boolean: { type: "boolean" },
+  armor: { type: "number" },
+  actor: { type: "number" },
+  class: { type: "number" },
+  enemy: { type: "number" },
+  skill: { type: "number" },
+  state: { type: "number" },
+  item: { type: "number" },
+  weapon: { type: "number" },
+  common_event: { type: "number" },
+  switch: { type: "number" },
+  variable: { type: "number" },
+  troop: { type: "number" },
+  multiline_string: { type: "string" },
+  file: { type: "string" },
+  combo: { type: "string" },
+  select: { type: "string" },
+  any: { type: "string" },
+} as const satisfies {
+  [key in ParamKinds]?: { type: string };
 };
 
-const isStringValueParam = (param: PrimitiveParam) => {};
+export const isStringValueParam = (
+  param: ScalaParam
+): param is Extract<PrimitiveParam, { default: string }> => {
+  const info = TABLE[param.kind];
+  return info.type === "string";
+};
