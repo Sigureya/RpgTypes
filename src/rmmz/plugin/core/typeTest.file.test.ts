1b57aa7d9cd267dc59305e1b7cef8526f3dc5609
Sigureya <sigureya1208@gmail.com>
2025-10-26 11:17:05 +0900

    ファイル型のテストを行う関数。

---
+++ src/rmmz/plugin/core/typeTest.file.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from "vitest";
+import type { FileParam, FileArrayParam } from "./primitiveParams";
+import { isArrayParam, paramHasText, isStringValueParam } from "./typeTest";
+
+describe("FileParam", () => {
+  const param: FileParam = { kind: "file", default: "icon", dir: "img" };
+  it("is not array param", () => {
+    expect(param).not.toSatisfy(isArrayParam);
+    expect(isArrayParam(param)).toBe(false);
+  });
+
+  it("is string value param", () => {
+    expect(param).toSatisfy(isStringValueParam);
+    expect(isStringValueParam(param)).toBe(true);
+  });
+
+  it("does not have text", () => {
+    expect(param).not.toSatisfy(paramHasText);
+    expect(paramHasText(param)).toBe(false);
+  });
+});
+
+describe("FileArrayParam", () => {
+  const param: FileArrayParam = {
+    kind: "file[]",
+    dir: "img",
+    default: ["icon1", "icon2"],
+  };
+  it("is array param", () => {
+    expect(param).toSatisfy(isArrayParam);
+  });
+  it("does not have text", () => {
+    expect(param).not.toSatisfy(paramHasText);
+  });
+});
