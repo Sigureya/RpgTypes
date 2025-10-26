19bb36ff7363c7c4838c5fd39506ac0cbc06560a
Sigureya <sigureya1208@gmail.com>
2025-10-26 11:19:58 +0900

    テスト範囲を追加。toSatisfyでオブジェクトの値チェック、tobe(true)で関数の引数型チェックをしている

--- src/rmmz/plugin/core/typeTest.file.test.ts
+++ src/rmmz/plugin/core/typeTest.file.test.ts
@@ -1,6 +1,12 @@
 import { describe, expect, it } from "vitest";
 import type { FileParam, FileArrayParam } from "./primitiveParams";
-import { isArrayParam, paramHasText, isStringValueParam } from "./typeTest";
+import {
+  isArrayParam,
+  paramHasText,
+  isStringValueParam,
+  isStructParam,
+  isStructArrayParam,
+} from "./typeTest";
 
 describe("FileParam", () => {
   const param: FileParam = { kind: "file", default: "icon", dir: "img" };
@@ -18,6 +24,14 @@ describe("FileParam", () => {
     expect(param).not.toSatisfy(paramHasText);
     expect(paramHasText(param)).toBe(false);
   });
+  it("is not struct param", () => {
+    expect(param).not.toSatisfy(isStructParam);
+    expect(isStructParam(param)).toBe(false);
+  });
+  it("is not struct array param", () => {
+    expect(param).not.toSatisfy(isStructArrayParam);
+    expect(isStructArrayParam(param)).toBe(false);
+  });
 });
 
 describe("FileArrayParam", () => {
@@ -28,8 +42,18 @@ describe("FileArrayParam", () => {
   };
   it("is array param", () => {
     expect(param).toSatisfy(isArrayParam);
+    expect(isArrayParam(param)).toBe(true);
   });
   it("does not have text", () => {
     expect(param).not.toSatisfy(paramHasText);
+    expect(paramHasText(param)).toBe(false);
+  });
+  it("is not struct param", () => {
+    expect(param).not.toSatisfy(isStructParam);
+    expect(isStructParam(param)).toBe(false);
+  });
+  it("is not struct array param", () => {
+    expect(param).not.toSatisfy(isStructArrayParam);
+    expect(isStructArrayParam(param)).toBe(false);
   });
 });
