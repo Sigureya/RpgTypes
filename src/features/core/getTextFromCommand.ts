26f2c5d8594f57d356063ff6c4f12c79c8258ec6
Sigureya <sigureya1208@gmail.com>
2025-10-29 07:06:26 +0900

    プラグインコマンド対応版。テストコードは後で作る

--- src/features/core/getTextFromCommand.ts
+++ src/features/core/getTextFromCommand.ts
@@ -1,13 +1,17 @@
-import type { EventCommand } from "@RpgTypes/rmmz";
+import type { Command_PluginCommandMZ, EventCommand } from "@RpgTypes/rmmz";
 import {
   CHANGE_NAME,
   CHANGE_NICKNAME,
   CHANGE_PROFILE,
+  PLUGIN_COMMAND_MZ,
   SHOW_CHOICES,
 } from "@RpgTypes/rmmz";
 import type { GroopMapper } from "./eventCommand/commandGroup";
 import { getGroupHandlingFunc } from "./eventCommand/commandGroup/mapping";
-import type { TextCommandParameter } from "./extract/text/eventCommand";
+import type {
+  PluginCommandMzParameter,
+  TextCommandParameter,
+} from "./extract/text/eventCommand";
 import {
   extractTextFromActorCommand,
   extractTextParamsFromChoice,
@@ -22,6 +26,25 @@ import {
 export const extractTextFromEventCommands = (
   list: ReadonlyArray<EventCommand>
 ): TextCommandParameter[] => {
+  return extractTextFromEventCommandsCore(
+    list,
+    (): PluginCommandMzParameter[] => []
+  );
+};
+
+export const extractTextFromEventCommandsEx = <
+  T extends PluginCommandMzParameter
+>(
+  list: ReadonlyArray<EventCommand>,
+  pluginCommandFn: (command: Command_PluginCommandMZ) => T[]
+): TextCommandParameter[] => {
+  return extractTextFromEventCommandsCore(list, pluginCommandFn);
+};
+
+const extractTextFromEventCommandsCore = <T extends PluginCommandMzParameter>(
+  list: ReadonlyArray<EventCommand>,
+  pluginCommandFn: (command: Command_PluginCommandMZ) => T[]
+): (TextCommandParameter | T)[] => {
   return list.reduce<TextCommandParameter[]>((acc, command, index) => {
     if (command.code === SHOW_CHOICES) {
       return [...acc, ...extractTextParamsFromChoice(command)];
@@ -45,6 +68,9 @@ export const extractTextFromEventCommands = (
     }
     if (command.code === CHANGE_PROFILE) {
       return [...acc, extractTextFromActorCommand(command)];
+    }
+    if (command.code === PLUGIN_COMMAND_MZ) {
+      return [...acc, ...pluginCommandFn(command)];
     }
 
     return acc;
