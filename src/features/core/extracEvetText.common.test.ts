0f37c1ae358e10431436b1534c8dfaeaea6e45ec
Sigureya <sigureya1208@gmail.com>
2025-10-29 09:14:52 +0900

    暫定テストコード。空データだけだが一応作成

---
+++ src/features/core/extracEvetText.common.test.ts
@@ -0,0 +1,26 @@
+import { test, expect, describe, vi } from "vitest";
+import type { Data_CommonEvent } from "@RpgTypes/rmmz";
+import type { ExtractedCommonEventText } from "./extract/text/types";
+import { extractCommonEventTexts } from "./extractEventText";
+
+describe("extractCommonEventTexts", () => {
+  test("emptyCommonEvents", () => {
+    const mock: Data_CommonEvent[] = [
+      {
+        id: 1,
+        name: "Common Event 1",
+        trigger: 0,
+        switchId: 0,
+        list: [],
+      },
+    ];
+    const mockFn = vi.fn(() => []);
+    const result: ExtractedCommonEventText[] = extractCommonEventTexts(
+      mock,
+      mockFn
+    );
+    expect(mockFn).not.toHaveBeenCalled();
+    const expected: ExtractedCommonEventText[] = [{ eventId: 1, commands: [] }];
+    expect(result).toEqual(expected);
+  });
+});
---
+++ src/features/core/extractEventText.battle.test.ts
@@ -0,0 +1,22 @@
+import { test, expect, describe, vi } from "vitest";
+import type { Data_Troop } from "@RpgTypes/rmmz";
+import type { ExtractedBattleEventText } from "../extractText";
+import { extractBattleEventTexts } from "./extractEventText";
+
+describe("extractBattleEventTexts", () => {
+  test("emptyTroops", () => {
+    const mockTroops: Data_Troop[] = [
+      {
+        id: 1,
+        name: "Troop 1",
+        members: [],
+        pages: [],
+      },
+    ];
+    const mockFn = vi.fn(() => []);
+    const result = extractBattleEventTexts(mockTroops, mockFn);
+    expect(mockFn).not.toHaveBeenCalled();
+    const expected: ExtractedBattleEventText[][] = [[]];
+    expect(result).toEqual(expected);
+  });
+});
