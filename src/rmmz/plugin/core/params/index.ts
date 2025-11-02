939f2a78823cd2f8ccc13e18f0358587283d7099
Sigureya <sigureya1208@gmail.com>
2025-11-02 16:50:40 +0900

    関数がやたら増えたので一括に切り替え

--- src/rmmz/plugin/core/params/index.ts
+++ src/rmmz/plugin/core/params/index.ts
@@ -1,6 +1,6 @@
+export * from "./classify";
 export * from "./lookupRpgDataKind";
 export * from "./typeTest";
 export type * from "./types";
-export { classifyPluginParams } from "./classify";
 export { collectDependentStructNames } from "./arraySchemaDependent";
 export { structDependencies } from "./structDependencies";
