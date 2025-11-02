08ddc20c40e1f298a0d82f7df53036dd86af9197
Sigureya <sigureya1208@gmail.com>
2025-11-02 17:24:34 +0900

    仮実装。commandへの対応はこの後考える

--- src/features/plugin/JSONPath/extractPluginParam.ts
+++ src/features/plugin/JSONPath/extractPluginParam.ts
@@ -1,14 +1,27 @@
-import type { PluginSchemaArray } from "@RpgTypes/rmmz/plugin";
-import { classifyPluginParams } from "@RpgTypes/rmmz/plugin";
-import { filterPluginSchemaByStringParam } from "@RpgTypes/rmmz/plugin/core/params/arraySchemaFilterByParam";
+import type {
+  ClassifiedTextParams,
+  PluginSchemaArray,
+} from "@RpgTypes/rmmz/plugin";
+import {
+  classifyTextParams,
+  filterPluginSchemaByStringParam,
+} from "@RpgTypes/rmmz/plugin";
+import type { StructPathResult } from "./core";
+import {
+  getPathFromStructParam,
+  getPathFromStructSchema,
+} from "./core/paramStruct";
 
 const p = (plugin: PluginSchemaArray) => {
   const texts = filterPluginSchemaByStringParam(plugin);
-  const c = texts.structs.map(
-    (s): [string, ReturnType<typeof classifyPluginParams>] => [
-      s.struct,
-      classifyPluginParams(s),
-    ]
+  const structs = texts.structs.map((s): [string, ClassifiedTextParams] => [
+    s.struct,
+    classifyTextParams(s),
+  ]);
+
+  const map = new Map<string, ClassifiedTextParams>(structs);
+  const vvvv: StructPathResult[] = texts.structs.map((struct) =>
+    getPathFromStructSchema(struct.struct, "$", map)
   );
   return "";
 };
