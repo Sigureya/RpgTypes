2d42e86e6cdeedfd7bf8c4d2211c97a9194d07ce
Sigureya <sigureya1208@gmail.com>
2025-11-02 13:07:58 +0900

    目立たせるためにトップレベルの場所に置く

--- src/rmmz/plugin/plugin.ts
+++ src/rmmz/plugin/plugin.ts
@@ -1,6 +1,14 @@
+import type { PluginSchemaArray } from "./core";
 import { compilePluginToObject } from "./core/compilePlugin";
+import { compilePluginAsArraySchema } from "./core/compilePluginAsArraySchema";
+import { parsePlugin } from "./core/parse/parse";
 import type { PluginJSON } from "./core/pluginJSONTypes";
 
 export const pluginSourceToJSON = (text: string): PluginJSON => {
   return compilePluginToObject(text);
+};
+
+export const pluginSourceToArraySchema = (text: string): PluginSchemaArray => {
+  const tokens = parsePlugin(text);
+  return compilePluginAsArraySchema(tokens);
 };
