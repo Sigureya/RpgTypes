8dbae968d4fb61097b2d748a059193d7eb5bb502
Sigureya <sigureya1208@gmail.com>
2025-11-02 15:21:58 +0900

    判定部分の修正中。

--- src/rmmz/plugin/core/params/classify.ts
+++ src/rmmz/plugin/core/params/classify.ts
@@ -8,8 +8,38 @@ import type {
   ScalaParam,
   ArrayParamTypes,
   ClassifiedPluginParams,
+  PluginParam,
 } from "./types";
 import { isStructParam, isStructArrayParam, isArrayParam } from "./typeTest";
+
+const xxx = <T extends PluginParam, A extends ArrayParamTypes>(
+  structSchema: PluginStructSchemaArray,
+  predicate: (param: PluginParam) => param is T,
+  arrayPredicate: (param: PluginParam) => param is PluginParamEx<A>
+) => {
+  const structs: PluginParamEx<StructRefParam>[] = [];
+  const structArrays: PluginParamEx<StructArrayRefParam>[] = [];
+  const scalas: T[] = [];
+  const scalaArrays: PluginParamEx<A>[] = [];
+  structSchema.params.forEach((p) => {
+    if (isStructParam(p.attr)) {
+      structs.push({ name: p.name, attr: p.attr });
+      return;
+    }
+    if (isStructArrayParam(p.attr)) {
+      structArrays.push({ name: p.name, attr: p.attr });
+      return;
+    }
+    if (arrayPredicate(p)) {
+      scalaArrays.push(p);
+      return;
+    }
+    if (predicate(p)) {
+      scalas.push(p);
+    }
+  });
+  return { structs, structArrays, scalas, scalaArrays };
+};
 
 export const classifyPluginParams = (
   structSchema: PluginStructSchemaArray
