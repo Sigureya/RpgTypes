4d32c3f7f82a3f8555e0eb86be7d2d680adbb25d
Sigureya <sigureya1208@gmail.com>
2025-11-08 10:52:32 +0900

    別リポジトリに移すので削除。プラグイン関連は全て別リポジトリに移す予定

--- src/features/plugin/JSONSchema/sturctRoot.ts
+++
@@ -1,9 +0,0 @@
-import type { PrimitiveParam } from "@RpgTypes/rmmz/plugin";
-import type { PrimitiveStructSchema } from "./compileFlatStruct";
-import { compileFlatStruct } from "./compileFlatStruct";
-
-export const structToJSONSchema = <T extends object>(
-  params: Record<keyof T, PrimitiveParam>
-): PrimitiveStructSchema<T> => {
-  return compileFlatStruct(params);
-};
