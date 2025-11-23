81b89286eb54821e9e66ef7a7727b0601df4c77d
Sigureya <sigureya1208@gmail.com>
2025-11-24 01:01:11 +0900

    単独で使いそうなのでexport

--- src/features/index.ts
+++ src/features/index.ts
@@ -2,3 +2,4 @@ export * from "./core/extract/item/index";
 export * from "./core/replace/text";
 export * from "./extractText";
 export * from "./format/trait/formatTraits";
+export { extractPluginCommandParams } from "./core/extract/plugin/pluginCommand";
