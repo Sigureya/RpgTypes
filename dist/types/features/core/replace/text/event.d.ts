66258c279a0e774d0725b0620718bbbe3c9dcb6c
Sigureya <sigureya1208@gmail.com>
2026-01-12 16:18:00 +0900

    プラグインコマンドの変換にも対応。本体ロジックは移譲しておく

--- dist/types/features/core/replace/text/event.d.ts
+++ dist/types/features/core/replace/text/event.d.ts
@@ -1,3 +1,4 @@
-import { Data_CommonEvent, Data_Troop } from '../../../../rmmz';
-export declare const replaceTroopTexts: (troop: Data_Troop, fn: (key: string) => string | undefined) => Data_Troop;
-export declare const replaceCommonEventTexts: (commonEvent: Data_CommonEvent, fn: (key: string) => string | undefined) => Data_CommonEvent;
+import { Data_Map, Command_PluginCommandMZ, Data_CommonEvent, Data_Troop, EventCommand } from '../../../../rmmz';
+export declare const replaceTroopTexts: (troop: Data_Troop, fn: (key: string) => string | undefined, pluginCommandFn?: (command: Command_PluginCommandMZ) => Command_PluginCommandMZ) => Data_Troop;
+export declare const replaceCommonEventTexts: (commonEvent: Data_CommonEvent, fn: (key: string) => string | undefined, pluginCommandFn?: (command: Command_PluginCommandMZ) => Command_PluginCommandMZ) => Data_CommonEvent;
+export declare const replaceMapDataTexts: (mapData: Data_Map<EventCommand>, fn: (key: string) => string | undefined, pluginCommandFn?: (command: Command_PluginCommandMZ) => Command_PluginCommandMZ) => Data_Map<EventCommand>;
