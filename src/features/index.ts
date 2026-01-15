3cc305b901ed8fa9e8c86ccb0d88548f6a7ffb86
Sigureya <sigureya1208@gmail.com>
2026-01-15 15:37:10 +0900

    indexに登録しビルド範囲として指定

--- src/features/index.ts
+++ src/features/index.ts
@@ -2,4 +2,5 @@ export * from "./core/extract/item/index";
 export * from "./core/replace/text";
 export * from "./extractText";
 export * from "./format/trait/formatTraits";
+export * from "./replace";
 export { extractPluginCommandMzArgs } from "./core/extract/plugin/pluginCommand";
