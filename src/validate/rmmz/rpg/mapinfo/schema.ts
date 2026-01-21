09bbd085e43558d5cbdf0520de1a21497b9157f3
Sigureya <sigureya1208@gmail.com>
2026-01-22 07:32:51 +0900

    まずschemaを定義

---
+++ src/validate/rmmz/rpg/mapinfo/schema.ts
@@ -0,0 +1,25 @@
+import type { Data_MapInfo } from "@RpgTypes/rmmz";
+import type { JSONSchemaType } from "ajv";
+
+export const SCHEMA_DATA_MAPINFO = {
+  type: "object",
+  required: [
+    "name",
+    "id",
+    "order",
+    "expanded",
+    "scrollX",
+    "scrollY",
+    "parentId",
+  ],
+  properties: {
+    id: { type: "integer", minimum: 0 },
+    name: { type: "string" },
+    displayName: { type: "string" },
+    order: { type: "integer" },
+    expanded: { type: "boolean" },
+    scrollX: { type: "integer" },
+    scrollY: { type: "integer" },
+    parentId: { type: "integer", minimum: 0 },
+  },
+} as const satisfies JSONSchemaType<Data_MapInfo>;
