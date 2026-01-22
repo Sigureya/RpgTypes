7836af6e936bd8503985a7cbc8397823a8ff0744
Sigureya <sigureya1208@gmail.com>
2026-01-22 23:43:38 +0900

    新フォルダに移動したので削除

--- src/rmmz/system/core/battle/battleSystem/schema.ts
+++
@@ -1,8 +0,0 @@
-import type { JSONSchemaType } from "ajv";
-
-const SCHEMA_SYSTEM_BATTLE_RULE = {
-  type: "number",
-  const: [0, 1, 2],
-} as const satisfies JSONSchemaType<number>;
-
-export default SCHEMA_SYSTEM_BATTLE_RULE;
