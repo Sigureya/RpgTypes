a650a27476e90b1c4f1aade0a273a5e8fa858eb3
Sigureya <sigureya1208@gmail.com>
2026-01-22 21:13:45 +0900

    本体でのデータの持ち方が想定と違ったので修正

--- src/validate/rmmz/system/gameEdit/testBattle.ts
+++ src/validate/rmmz/system/gameEdit/testBattle.ts
@@ -1,32 +1,15 @@
-import type { System_TestBattle } from "@RpgTypes/rmmz";
+import type { TestBattler } from "@RpgTypes/rmmz";
 import type { JSONSchemaType } from "ajv";
 
-export const SCHEMA_SYSTEM_TEST_PLAY = {
+export const SCHEMA_SYSTEM_TEST_BATTLE = {
   type: "object",
-  required: [
-    "testBattlers",
-    "testTroopId",
-    "battleback1Name",
-    "battleback2Name",
-  ],
+  required: ["actorId", "level", "equips"],
   properties: {
-    testTroopId: { type: "integer", minimum: 0 },
-    battleback1Name: { type: "string" },
-    battleback2Name: { type: "string" },
-    testBattlers: {
+    level: { type: "integer", minimum: 0 },
+    actorId: { type: "integer", minimum: 0 },
+    equips: {
       type: "array",
-      items: {
-        type: "object",
-        required: ["actorId", "equips", "level"],
-        properties: {
-          actorId: { type: "integer" },
-          level: { type: "integer", minimum: 0 },
-          equips: {
-            type: "array",
-            items: { type: "integer" },
-          },
-        },
-      },
+      items: { type: "integer" },
     },
   },
-} as const satisfies JSONSchemaType<System_TestBattle>;
+} as const satisfies JSONSchemaType<TestBattler>;
