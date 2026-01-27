9bd3d68dc548f13dd234782b5b92feb69ff8ad4c
Sigureya <sigureya1208@gmail.com>
2026-01-28 06:59:29 +0900

    パーティの情報を追加。プレイヤーとしてのパーティの部分を切り出す

--- src/rmmzRuntime/objects/party.ts
+++ src/rmmzRuntime/objects/party.ts
@@ -1,10 +1,15 @@
 import type { Rmmz_Actor } from "./core";
+import type { Rmmz_UnitPlayer } from "./core/unit/player";
 import type { Rmmz_Unit } from "./core/unit/unit";
 import { Game_Unit } from "./unit";
 
 export declare class Game_Party
   extends Game_Unit<Rmmz_Actor>
-  implements Partial<Rmmz_Unit<Rmmz_Actor>>
+  implements Partial<Rmmz_Unit<Rmmz_Actor> & Rmmz_UnitPlayer>
 {
+  gold(): number;
+  steps(): number;
+  gainGold(amount: number): void;
+  loseGold(amount: number): void;
   _actors: number[];
 }
