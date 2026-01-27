a2a393d0ae850f9200dd9b3ed4753160c6ee0c30
Sigureya <sigureya1208@gmail.com>
2026-01-28 05:53:40 +0900

    systemの情報。フォーマットが固まったので実装

--- src/rmmz/eventCommand/commands/variable/system.ts
+++ src/rmmz/eventCommand/commands/variable/system.ts
@@ -6,7 +6,25 @@ import type {
   ParamObject_WritingTarget,
 } from "./types";
 
-export const makeCommandSystemPlaytime = (
+export const makeCommandVariableFromPlaytime = (
+  target: ParamObject_WritingTarget,
+  operation: number = 0,
+): Command_ControlVariables<Operand_OtherData> => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent: 0,
+    parameters: [
+      target.startId,
+      target.endId ?? target.startId,
+      operation,
+      3, // Game Data
+      7, // Other
+      4 satisfies OTHER["PLAY_TIME"], // Play Time
+    ],
+  };
+};
+
+export const makeCommandSystemSteps = (
   target: ParamObject_WritingTarget,
   operation: number = 0,
 ): Command_ControlVariables<Operand_OtherData> => {
@@ -19,7 +37,97 @@ export const makeCommandSystemPlaytime = (
       operation,
       3,
       7,
-      4 satisfies OTHER["PLAY_TIME"],
+      3 satisfies OTHER["STEPS"],
+    ],
+  };
+};
+
+export const makeCommandSystemTimer = (
+  target: ParamObject_WritingTarget,
+  operation: number = 0,
+): Command_ControlVariables<Operand_OtherData> => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent: 0,
+    parameters: [
+      target.startId,
+      target.endId ?? target.startId,
+      operation,
+      3,
+      7,
+      5 satisfies OTHER["TIMER"],
+    ],
+  };
+};
+
+export const makeCommandSystemSaveCount = (
+  target: ParamObject_WritingTarget,
+  operation: number = 0,
+): Command_ControlVariables<Operand_OtherData> => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent: 0,
+    parameters: [
+      target.startId,
+      target.endId ?? target.startId,
+      operation,
+      3,
+      7,
+      6 satisfies OTHER["SAVE_COUNT"],
+    ],
+  };
+};
+
+export const makeCommandSystemBattleCount = (
+  target: ParamObject_WritingTarget,
+  operation: number = 0,
+): Command_ControlVariables<Operand_OtherData> => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent: 0,
+    parameters: [
+      target.startId,
+      target.endId ?? target.startId,
+      operation,
+      3,
+      7,
+      7 satisfies OTHER["BATTLE_COUNT"],
+    ],
+  };
+};
+
+export const makeCommandSystemWinCount = (
+  target: ParamObject_WritingTarget,
+  operation: number = 0,
+): Command_ControlVariables<Operand_OtherData> => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent: 0,
+    parameters: [
+      target.startId,
+      target.endId ?? target.startId,
+      operation,
+      3,
+      7,
+      8 satisfies OTHER["WIN_COUNT"],
+    ],
+  };
+};
+
+export const makeCommandSystemEscapeCount = (
+  target: ParamObject_WritingTarget,
+  operation: number = 0,
+): Command_ControlVariables<Operand_OtherData> => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent: 0,
+    parameters: [
+      target.startId,
+      target.endId ?? target.startId,
+      operation,
+      3,
+      7,
+      9 satisfies OTHER["ESCAPE_COUNT"],
     ],
   };
 };
