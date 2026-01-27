2c7aec8fb9a74d0cbe06847c77142024426794f6
Sigureya <sigureya1208@gmail.com>
2026-01-28 05:53:14 +0900

    partyの情報。とりあえずまとめておく

---
+++ src/rmmz/eventCommand/commands/variable/party.ts
@@ -0,0 +1,61 @@
+import { CONTROL_VARIABLES } from "@RpgTypes/rmmz/rpg";
+import type {
+  ParamObject_WritingTarget,
+  Command_ControlVariables,
+  Operand_OtherData,
+  OTHER,
+} from "./types";
+
+export const makeCommandVariableFromMapId = (
+  target: ParamObject_WritingTarget,
+  operation: number = 0,
+): Command_ControlVariables<Operand_OtherData> => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent: 0,
+    parameters: [
+      target.startId,
+      target.endId ?? target.startId,
+      operation,
+      3,
+      7,
+      0 satisfies OTHER["MAP_ID"],
+    ],
+  };
+};
+
+export const makeCommandVariableFromPartyMembers = (
+  target: ParamObject_WritingTarget,
+  operation: number = 0,
+): Command_ControlVariables<Operand_OtherData> => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent: 0,
+    parameters: [
+      target.startId,
+      target.endId ?? target.startId,
+      operation,
+      3,
+      7,
+      1 satisfies OTHER["PARTY_MEMBERS"],
+    ],
+  };
+};
+
+export const makeCommandVariableFromPartyGold = (
+  target: ParamObject_WritingTarget,
+  operation: number = 0,
+): Command_ControlVariables<Operand_OtherData> => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent: 0,
+    parameters: [
+      target.startId,
+      target.endId ?? target.startId,
+      operation,
+      3,
+      7,
+      2 satisfies OTHER["GOLD"],
+    ],
+  };
+};
