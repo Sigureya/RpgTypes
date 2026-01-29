89d554276cb7f237bc37871ea16b6681c7cb34cc
Sigureya <sigureya1208@gmail.com>
2026-01-29 20:53:32 +0900

    index追加。バトル開始のテストで使う

--- src/rmmzRuntime/objects/core/map/index.ts
+++ src/rmmzRuntime/objects/core/map/index.ts
@@ -1,3 +1,4 @@
 export type { Rmmz_Event } from "./event";
 export type { Rmmz_Map } from "./map";
+export type { Rmmz_PlayerCharactor } from "./player";
 export type { Rmmz_Vehicle } from "./vehicle";
