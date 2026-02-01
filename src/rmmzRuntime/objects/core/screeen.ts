9f87f341820ca44bf260b96938eb8e9b0d36026a
Sigureya <sigureya1208@gmail.com>
2026-02-02 06:10:12 +0900

    Scewwn用のインターフェースを仮置き

---
+++ src/rmmzRuntime/objects/core/screeen.ts
@@ -0,0 +1,102 @@
+import type { ColorRGBA } from "@RpgTypes/libs";
+
+export interface Rmmz_ScreenProperties {
+  // プロパティ
+  _brightness: number;
+  _fadeOutDuration: number;
+  _fadeInDuration: number;
+  _tone: number[];
+  _toneTarget: number[];
+  _toneDuration: number;
+  _flashColor: number[];
+  _flashDuration: number;
+  _shakePower: number;
+  _shakeSpeed: number;
+  _shakeDuration: number;
+  _shakeDirection: number;
+  _shake: number;
+  _zoomX: number;
+  _zoomY: number;
+  _zoomScale: number;
+  _zoomScaleTarget: number;
+  _zoomDuration: number;
+  _weatherType: string;
+  _weatherPower: number;
+  _weatherPowerTarget: number;
+  _weatherDuration: number;
+  _pictures: (Rmmz_Picture | null)[];
+}
+
+export interface Rmmz_Screen {
+  clear(): void;
+  onBattleStart(): void;
+  brightness(): number;
+  tone(): number[];
+  flashColor(): number[];
+  shake(): number;
+  zoomX(): number;
+  zoomY(): number;
+  zoomScale(): number;
+  weatherType(): string;
+  weatherPower(): number;
+  picture(pictureId: number): Rmmz_Picture | null;
+  realPictureId(pictureId: number): number;
+  clearFade(): void;
+  clearTone(): void;
+  clearFlash(): void;
+  clearShake(): void;
+  clearZoom(): void;
+  clearWeather(): void;
+  clearPictures(): void;
+  eraseBattlePictures(): void;
+  maxPictures(): number;
+  startFadeOut(duration: number): void;
+  startFadeIn(duration: number): void;
+  startTint(tone: number[], duration: number): void;
+  startFlash(color: ColorRGBA, duration: number): void;
+  startShake(power: number, speed: number, duration: number): void;
+  startZoom(x: number, y: number, scale: number, duration: number): void;
+  setZoom(x: number, y: number, scale: number): void;
+  changeWeather(type: string, power: number, duration: number): void;
+  update(): void;
+  updateFadeOut(): void;
+  updateFadeIn(): void;
+  updateTone(): void;
+  updateFlash(): void;
+  updateShake(): void;
+  updateZoom(): void;
+  updateWeather(): void;
+  updatePictures(): void;
+  startFlashForDamage(): void;
+  showPicture(
+    pictureId: number,
+    name: string,
+    origin: number,
+    x: number,
+    y: number,
+    scaleX: number,
+    scaleY: number,
+    opacity: number,
+    blendMode: number,
+  ): void;
+  movePicture(
+    pictureId: number,
+    origin: number,
+    x: number,
+    y: number,
+    scaleX: number,
+    scaleY: number,
+    opacity: number,
+    blendMode: number,
+    duration: number,
+    easingType: number,
+  ): void;
+  rotatePicture(pictureId: number, speed: number): void;
+  tintPicture(pictureId: number, tone: ColorRGBA, duration: number): void;
+  erasePicture(pictureId: number): void;
+}
+
+// 依存する型
+interface Rmmz_Picture {
+  // 必要に応じて定義
+}
