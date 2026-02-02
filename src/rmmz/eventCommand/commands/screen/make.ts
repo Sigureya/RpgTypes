518156661c3d4204a0b58c355422b6e3ffc3fb23
Sigureya <sigureya1208@gmail.com>
2026-02-02 16:43:22 +0900

    コマンド生成関数を追加。初期値がこれで良いのかは不明

--- src/rmmz/eventCommand/commands/screen/make.ts
+++ src/rmmz/eventCommand/commands/screen/make.ts
@@ -2,7 +2,13 @@ import type {
   Command_FadeInScreen,
   Command_FadeOutScreen,
   Command_SetWeatherEffect,
+  Command_FlashScreen,
+  Command_ShakeScreen,
+  Command_TintScreen,
   ParamObject_SetWeatherEffect,
+  ParamObject_FlashScreen,
+  ParamObject_ShakeScreen,
+  ParamObject_TintScreen,
 } from "./types";
 
 export const makeCommandSetWeatherEffect = (
@@ -33,4 +39,47 @@ export const makeCommandFadeOutScreen = (
   code: 221,
   indent: indent,
   parameters: [],
+});
+
+export const makeCommandFlashScreen = (
+  param: Partial<ParamObject_FlashScreen>,
+  indent: number = 0,
+): Command_FlashScreen => ({
+  code: 224,
+  indent: indent,
+  parameters: [
+    param.color
+      ? [param.color[0], param.color[1], param.color[2], param.color[3]]
+      : [255, 255, 255, 255],
+    param.duration ?? 0,
+    param.wait ?? false,
+  ],
+});
+
+export const makeCommandShakeScreen = (
+  param: Partial<ParamObject_ShakeScreen>,
+  indent: number = 0,
+): Command_ShakeScreen => ({
+  code: 225,
+  indent: indent,
+  parameters: [
+    param.power ?? 5,
+    param.speed ?? 5,
+    param.duration ?? 60,
+    param.wait ?? false,
+  ],
+});
+export const makeCommandTintScreen = (
+  param: Partial<ParamObject_TintScreen>,
+  indent: number = 0,
+): Command_TintScreen => ({
+  code: 223,
+  indent: indent,
+  parameters: [
+    param.color
+      ? [param.color[0], param.color[1], param.color[2], param.color[3]]
+      : [0, 0, 0, 0],
+    param.duration ?? 60,
+    param.wait ?? false,
+  ],
 });
