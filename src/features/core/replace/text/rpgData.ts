fc9e56ba0dd3b6e636ebca3922c800fb38477090
Sigureya <sigureya1208@gmail.com>
2026-02-09 18:12:11 +0900

    noteの書き換えを部分的に行いたいパターンがあるので修正。テストコードや他の箇所も修正が必要なので、段階的に進めていく

--- src/features/core/replace/text/rpgData.ts
+++ src/features/core/replace/text/rpgData.ts
@@ -8,19 +8,32 @@ import type {
   Data_Skill,
   Data_State,
   Data_Weapon,
+  NoteReplaceHandlers,
 } from "@RpgTypes/rmmz";
-import { replaceNoteTextByFunction } from "./note";
-import { replaceTextByFunction } from "./utils";
+import { replaceNoteWithHandlers } from "@RpgTypes/rmmz";
+import { replaceTextByFunction, replaceTextByHandlers } from "./utils";
 
 export const replaceActorText = <Actor extends PickByType<Data_Actor, string>>(
   actor: Actor,
-  fn: (key: string) => string | undefined
+  fn: (text: string) => string | undefined,
 ) => {
-  const note: string = replaceNoteTextByFunction(actor, fn);
+  return replaceActorText2(actor, {
+    replaceText: (text: string) => fn(text),
+    isReplaceTargetNote: () => true,
+  });
+};
+
+export const replaceActorText2 = <Actor extends PickByType<Data_Actor, string>>(
+  actor: Actor,
+  handlers: NoteReplaceHandlers,
+) => {
+  const fn = (key: string): string | undefined => {
+    return handlers.replaceText(key);
+  };
+  const note: string = replaceNoteWithHandlers(actor.note, handlers);
   const name: string = replaceTextByFunction(actor.name, fn);
   const nickname: string = replaceTextByFunction(actor.nickname, fn);
   const profile: string = replaceTextByFunction(actor.profile, fn);
-
   return {
     ...actor,
     name: name,
@@ -32,10 +45,19 @@ export const replaceActorText = <Actor extends PickByType<Data_Actor, string>>(
 
 export const replaceEnemyText = <Enemy extends PickByType<Data_Enemy, string>>(
   enemy: Enemy,
-  fn: (key: string) => string | undefined
+  fn: (key: string) => string | undefined,
 ) => {
-  const note: string = replaceNoteTextByFunction(enemy, fn);
-  const name: string = replaceTextByFunction(enemy.name, fn);
+  return replaceEnemyText2(enemy, {
+    replaceText: (text: string) => fn(text),
+    isReplaceTargetNote: () => true,
+  });
+};
+export const replaceEnemyText2 = <Enemy extends PickByType<Data_Enemy, string>>(
+  enemy: Enemy,
+  handlers: NoteReplaceHandlers,
+) => {
+  const note: string = replaceNoteWithHandlers(enemy.note, handlers);
+  const name: string = replaceTextByHandlers(enemy.name, handlers);
   return {
     ...enemy,
     name: name,
@@ -45,10 +67,20 @@ export const replaceEnemyText = <Enemy extends PickByType<Data_Enemy, string>>(
 
 export const replaceClassText = <Class extends PickByType<Data_Class, string>>(
   data: Class,
-  fn: (key: string) => string | undefined
+  fn: (key: string) => string | undefined,
 ) => {
-  const note: string = replaceNoteTextByFunction(data, fn);
-  const name: string = replaceTextByFunction(data.name, fn);
+  return replaceClassText2(data, {
+    replaceText: (text: string) => fn(text),
+    isReplaceTargetNote: () => true,
+  });
+};
+
+export const replaceClassText2 = <Class extends PickByType<Data_Class, string>>(
+  data: Class,
+  handlers: NoteReplaceHandlers,
+) => {
+  const note: string = replaceNoteWithHandlers(data.note, handlers);
+  const name: string = replaceTextByHandlers(data.name, handlers);
   return {
     ...data,
     name: name,
@@ -56,16 +88,18 @@ export const replaceClassText = <Class extends PickByType<Data_Class, string>>(
   };
 };
 
-export const replaceSkillText = <Skill extends PickByType<Data_Skill, string>>(
+export const replaceSkillText2 = <Skill extends PickByType<Data_Skill, string>>(
   skill: Skill,
-  fn: (key: string) => string | undefined
+  handlers: NoteReplaceHandlers,
 ) => {
-  const note: string = replaceNoteTextByFunction(skill, fn);
-  const name: string = replaceTextByFunction(skill.name, fn);
-  const description: string = replaceTextByFunction(skill.description, fn);
-  const message1: string = replaceTextByFunction(skill.message1, fn);
-  const message2: string = replaceTextByFunction(skill.message2, fn);
-
+  const note: string = replaceNoteWithHandlers(skill.note, handlers);
+  const name: string = replaceTextByHandlers(skill.name, handlers);
+  const description: string = replaceTextByHandlers(
+    skill.description,
+    handlers,
+  );
+  const message1: string = replaceTextByHandlers(skill.message1, handlers);
+  const message2: string = replaceTextByHandlers(skill.message2, handlers);
   return {
     ...skill,
     name: name,
@@ -76,32 +110,53 @@ export const replaceSkillText = <Skill extends PickByType<Data_Skill, string>>(
   };
 };
 
-export const replaceItemText = <T extends Data_Item | Data_Weapon | Data_Armor>(
-  item: T,
-  fn: (key: string) => string | undefined
+export const replaceSkillText = <Skill extends PickByType<Data_Skill, string>>(
+  skill: Skill,
+  fn: (key: string) => string | undefined,
 ) => {
-  const note: string = replaceNoteTextByFunction(item, fn);
-  const name: string = replaceTextByFunction(item.name, fn);
-  const description: string = replaceTextByFunction(item.description, fn);
+  return replaceSkillText2(skill, {
+    replaceText: (text: string) => fn(text),
+    isReplaceTargetNote: () => true,
+  });
+};
+
+export const replaceItemText2 = <
+  T extends Data_Item | Data_Weapon | Data_Armor,
+>(
+  item: T,
+  handlers: NoteReplaceHandlers,
+) => {
+  const note: string = replaceNoteWithHandlers(item.note, handlers);
+  const name: string = replaceTextByHandlers(item.name, handlers);
+  const description: string = replaceTextByHandlers(item.description, handlers);
   return {
     ...item,
     name: name,
     description: description,
     note: note,
-  } satisfies T;
+  };
 };
 
-export const replaceStateText = <State extends PickByType<Data_State, string>>(
-  state: State,
-  fn: (key: string) => string | undefined
+export const replaceItemText = <T extends Data_Item | Data_Weapon | Data_Armor>(
+  item: T,
+  fn: (key: string) => string | undefined,
 ) => {
-  const note: string = replaceNoteTextByFunction(state, fn);
-  const name: string = replaceTextByFunction(state.name, fn);
-  const message1: string = replaceTextByFunction(state.message1, fn);
-  const message2: string = replaceTextByFunction(state.message2, fn);
-  const message3: string = replaceTextByFunction(state.message3, fn);
-  const message4: string = replaceTextByFunction(state.message4, fn);
+  return replaceItemText2(item, {
+    replaceText: (text: string) => fn(text),
+    isReplaceTargetNote: () => true,
+  });
+};
 
+export const replaceStateText2 = <State extends PickByType<Data_State, string>>(
+  state: State,
+  handlers: NoteReplaceHandlers,
+) => {
+  const note: string = replaceNoteWithHandlers(state.note, handlers);
+  const name: string = replaceTextByHandlers(state.name, handlers);
+  const message1: string = replaceTextByHandlers(state.message1, handlers);
+  const message2: string = replaceTextByHandlers(state.message2, handlers);
+  const message3: string = replaceTextByHandlers(state.message3, handlers);
+  const message4: string = replaceTextByHandlers(state.message4, handlers);
   return {
     ...state,
     name: name,
@@ -111,4 +166,14 @@ export const replaceStateText = <State extends PickByType<Data_State, string>>(
     message4: message4,
     note: note,
   };
+};
+
+export const replaceStateText = <State extends PickByType<Data_State, string>>(
+  state: State,
+  fn: (key: string) => string | undefined,
+) => {
+  return replaceStateText2(state, {
+    replaceText: (text: string) => fn(text),
+    isReplaceTargetNote: () => true,
+  });
 };
