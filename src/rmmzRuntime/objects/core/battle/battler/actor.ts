487838e9897ca5d9a618e94a446f32b083400544
Sigureya <sigureya1208@gmail.com>
2026-02-19 00:25:42 +0900

    actorの不足しているメソッドを定義

--- src/rmmzRuntime/objects/core/battle/battler/actor.ts
+++ src/rmmzRuntime/objects/core/battle/battler/actor.ts
@@ -2,4 +2,12 @@ import type { Rmmz_Battler } from "./battler";
 
 export interface Rmmz_Actor extends Rmmz_Battler {
   actorId(): number;
+  changeEquipById(etypeId: number, itemId: number): void;
+  setName(name: string): void;
+  setCharacterImage(characterName: string, characterIndex: number): void;
+  setFaceImage(faceName: string, faceIndex: number): void;
+  setBattlerImage(battlerName: string): void;
+  changeClass(classId: number, keepExp: boolean): void;
+  chnageNickname(nickname: string): void;
+  changeProfile(profile: string): void;
 }
