2928ea635705b989c2680ed42cdf4deb2e2978be
Sigureya <sigureya1208@gmail.com>
2026-02-19 01:02:00 +0900

    メソッド呼び出しの検証を追加

--- src/rmmzMock/core/objects/commandActors.test.ts
+++ src/rmmzMock/core/objects/commandActors.test.ts
@@ -31,9 +31,23 @@ const createFakeMap = (): FakeMap => ({
   mapId: () => 653,
 });
 
+const createMockedActor = (): MockedObject<FakeActor> => ({
+  changeEquipById: vi.fn(),
+  setBattlerImage: vi.fn(),
+  setCharacterImage: vi.fn(),
+  setFaceImage: vi.fn(),
+  setName: vi.fn(),
+  changeClass: vi.fn(),
+  chnageNickname: vi.fn(),
+  changeProfile: vi.fn(),
+});
+
+const mockDataClasses = [null, { id: 1 }, { id: 2 }, { id: 3 }] as const;
+
 const stubGlobal = (actors: MockedObject<FakeActors>) => {
   vi.stubGlobal("$gameActors", actors);
   vi.stubGlobal("$gameMap", createFakeMap());
+  vi.stubGlobal("$dataClasses", mockDataClasses);
 };
 
 interface MethodCalls {
@@ -53,7 +67,7 @@ const runTestCase = (testCase: TestCase) => {
     test("make", () => {
       expect(testCase.command).toEqual(testCase.commandLiteral);
     });
-    test("execute with relevant actor calls", () => {
+    test("execute with no actor found, should not call any actor method", () => {
       const emptyActors: MockedObject<FakeActors> = {
         actor: vi.fn().mockReturnValue(null),
       };
@@ -63,6 +77,27 @@ const runTestCase = (testCase: TestCase) => {
       interpreter.executeCommand();
       expect(emptyActors.actor).toHaveBeenCalledWith(testCase.calls.actorId);
       expect(emptyActors.actor).toHaveBeenCalledOnce();
+    });
+    test("execute with actor found", () => {
+      const mockActor = createMockedActor();
+      const emptyActors: MockedObject<FakeActors> = {
+        actor: vi.fn().mockReturnValue(mockActor),
+      };
+      stubGlobal(emptyActors);
+      const interpreter = new Game_Interpreter();
+      interpreter.setup([testCase.commandLiteral], 0);
+      interpreter.executeCommand();
+      expect(emptyActors.actor).toHaveBeenCalledWith(testCase.calls.actorId);
+      expect(emptyActors.actor).toHaveBeenCalledOnce();
+      KEYS.forEach((k) => {
+        const call = testCase.calls.actor.find((c) => c.fn === k);
+        if (call) {
+          expect(mockActor[k]).toHaveBeenCalledWith(...call.args);
+          expect(mockActor[k]).toHaveBeenCalledOnce();
+        } else {
+          expect(mockActor[k]).not.toHaveBeenCalled();
+        }
+      });
     });
   });
 };
@@ -83,6 +118,23 @@ const testCases: TestCase[] = [
     calls: {
       actorId: 5,
       actor: [{ fn: "changeClass", args: [3, true] }],
+    },
+  },
+  {
+    name: "Change Actor Class",
+    command: makeCommandChangeClass({
+      actorId: 7,
+      classId: 0,
+      keepExp: true,
+    }),
+    commandLiteral: {
+      code: CHANGE_CLASS,
+      parameters: [7, 0, true],
+      indent: 0,
+    },
+    calls: {
+      actorId: 7,
+      actor: [],
     },
   },
 ];
