685356baf6690f9d767dcbeb6d6635102c4e5513
Sigureya <sigureya1208@gmail.com>
2026-02-18 19:33:00 +0900

    イベントコマンド生成関数を追加。Map名表示のON/OFFはbooleanで分けると面倒そうなので、このような形にした

---
+++ src/rmmz/eventCommand/commands/map/make.ts
@@ -0,0 +1,48 @@
+import type {
+  Command_ChangeParallax,
+  ParamObject_ChangeParallax,
+  Command_ScrollMap,
+  ParamObject_ScrollMap,
+  Command_HideMapNameDisplay,
+  Command_ShowMapNameDisplay,
+} from "./types";
+
+export const makeCommandChangeParallax = (
+  params: ParamObject_ChangeParallax,
+  indent: number = 0,
+): Command_ChangeParallax => ({
+  code: 284,
+  indent,
+  parameters: [
+    params.parallaxName,
+    params.loopX,
+    params.loopY,
+    params.sx,
+    params.sy,
+  ],
+});
+
+export const makeCommandScrollMap = (
+  params: ParamObject_ScrollMap,
+  indent: number = 0,
+): Command_ScrollMap => ({
+  code: 204,
+  indent,
+  parameters: [params.direction, params.distance, params.speed, params.waiting],
+});
+
+export const makeCommandShowMapNameDisplay = (
+  indent: number = 0,
+): Command_ShowMapNameDisplay => ({
+  code: 281,
+  indent,
+  parameters: [0],
+});
+
+export const makeCommandHideMapNameDisplay = (
+  indent: number = 0,
+): Command_HideMapNameDisplay => ({
+  code: 281,
+  indent,
+  parameters: [1],
+});
