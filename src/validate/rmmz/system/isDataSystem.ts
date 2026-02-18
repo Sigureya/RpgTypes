772d148d04f54b1a4e752647f95fc7e5087ce631
Sigureya <sigureya1208@gmail.com>
2026-02-18 14:33:03 +0900

    MV用のData_Systemのバリデーション。関数を近くに置いてもまあOKでしょう

--- src/validate/rmmz/system/isDataSystem.ts
+++ src/validate/rmmz/system/isDataSystem.ts
@@ -1,6 +1,11 @@
-import type { Data_System } from "@RpgTypes/rmmz/system";
+import type { Data_System, Data_SystemMV } from "@RpgTypes/rmmz/system";
+import validateMV from "./systemMVValidate";
 import validate from "./systemValidate";
 
 export const isDataSystem = (data: unknown): data is Data_System => {
   return validate(data);
+};
+
+export const isDataSystemMV = (data: unknown): data is Data_SystemMV => {
+  return validateMV(data);
 };
