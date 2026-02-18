014527fa4d534ec93564553358300696eb9a776f
Sigureya <sigureya1208@gmail.com>
2026-02-19 01:30:35 +0900

    イベントコマンドの実装漏れ。型チェック機能によって自動で発見できた

--- src/rmmz/eventCommand/commands/actor/make.ts
+++ src/rmmz/eventCommand/commands/actor/make.ts
@@ -1,4 +1,9 @@
-import type { Command_ChangeClaass, ParamObject_ChangeClass } from "./types";
+import type {
+  Command_ChangeClaass,
+  ParamObject_ChangeClass,
+  Command_ChangeEquip,
+  ParamObject_ChangeEquip,
+} from "./types";
 
 export const makeCommandChangeClass = (
   param: ParamObject_ChangeClass,
@@ -6,5 +11,14 @@ export const makeCommandChangeClass = (
 ): Command_ChangeClaass => ({
   code: 321,
   parameters: [param.actorId, param.classId, param.keepExp],
+  indent,
+});
+
+export const makeCommandChangeEquip = (
+  param: ParamObject_ChangeEquip,
+  indent: number = 0,
+): Command_ChangeEquip => ({
+  code: 319,
+  parameters: [param.actorId, param.equipType, param.equipId],
   indent,
 });
