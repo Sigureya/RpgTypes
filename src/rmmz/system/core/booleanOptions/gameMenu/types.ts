9e26f12fcfa278f5a652febc393beb15c3781618
Sigureya <sigureya1208@gmail.com>
2026-02-18 15:17:27 +0900

    MV版からの変換の都合により、型や美帰趨を微修正。一発で変換する関数も用意しておく

--- src/rmmz/system/core/booleanOptions/gameMenu/types.ts
+++ src/rmmz/system/core/booleanOptions/gameMenu/types.ts
@@ -1,6 +1,10 @@
 import type { ItemCategoriesArray, MenuCommandsEnabledArray } from ".";
 
-export interface System_BooleanGameMenuOptions {
+export interface System_BooleanGameMenuOptions extends System_BooleanMenuCommands {
   itemCategories: ItemCategoriesArray;
+  menuCommands: MenuCommandsEnabledArray;
+}
+
+export interface System_BooleanMenuCommands {
   menuCommands: MenuCommandsEnabledArray;
 }
