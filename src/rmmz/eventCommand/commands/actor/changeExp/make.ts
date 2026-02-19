1bbb61eb338f98a2419f01c0d0e41532959acb13
Sigureya <sigureya1208@gmail.com>
2026-02-20 00:06:39 +0900

    パラメータのパターンを全て関数で網羅して問題を隠蔽

---
+++ src/rmmz/eventCommand/commands/actor/changeExp/make.ts
@@ -0,0 +1,73 @@
+import type {
+  ParamObject_ChangeExp,
+  Command_ChangeExp,
+  ParamArray_GainExp_Direct,
+  ParamArray_LoseExp_Direct,
+  ParamObject_ChangeExpFromVariable,
+  ParamArray_GainExp_Variable,
+  ParamArray_LoseExp_Variable,
+} from "./types";
+
+export const makeCommandGainExp = (
+  param: ParamObject_ChangeExp,
+  indent: number = 0,
+): Command_ChangeExp => ({
+  code: 315,
+  indent,
+  parameters: [
+    0,
+    param.actorId,
+    0,
+    0,
+    param.value,
+    param.showMessaage,
+  ] satisfies ParamArray_GainExp_Direct,
+});
+
+export const makeCommandLoseExp = (
+  param: ParamObject_ChangeExp,
+  indent: number = 0,
+): Command_ChangeExp => ({
+  code: 315,
+  indent,
+  parameters: [
+    0,
+    param.actorId,
+    1,
+    0,
+    param.value,
+    param.showMessaage,
+  ] satisfies ParamArray_LoseExp_Direct,
+});
+
+export const makeCommandGainExpVariable = (
+  param: ParamObject_ChangeExpFromVariable,
+  indent: number = 0,
+): Command_ChangeExp => ({
+  code: 315,
+  indent,
+  parameters: [
+    0,
+    param.actorId,
+    0,
+    1,
+    param.variableId,
+    param.showMessaage,
+  ] satisfies ParamArray_GainExp_Variable,
+});
+
+export const makeCommandLoseExpVariable = (
+  param: ParamObject_ChangeExpFromVariable,
+  indent: number = 0,
+): Command_ChangeExp => ({
+  code: 315,
+  indent,
+  parameters: [
+    0,
+    param.actorId,
+    1,
+    1,
+    param.variableId,
+    param.showMessaage,
+  ] satisfies ParamArray_LoseExp_Variable,
+});
--- src/rmmz/eventCommand/commands/actor/changeExp/types.ts
+++ src/rmmz/eventCommand/commands/actor/changeExp/types.ts
@@ -1,18 +1,15 @@
 import type { EventCommandLike } from "@RpgTypes/rmmz/eventCommand/frame";
 import type { CHANGE_EXP } from "@RpgTypes/rmmz/rpg";
-import type { DIRECT, VARIABLE } from "./constants";
 
 export interface Command_ChangeExp extends EventCommandLike<typeof CHANGE_EXP> {
   parameters: ParamArray_ChangeExp;
 }
 
-export type ParamArray_ChangeExp = [
-  disignation: typeof DIRECT | typeof VARIABLE,
-  operation: number,
-  valueType: number,
-  value: number,
-  showMessaage: boolean,
-];
+export type ParamArray_ChangeExp =
+  | ParamArray_GainExp_Direct
+  | ParamArray_LoseExp_Direct
+  | ParamArray_GainExp_Variable
+  | ParamArray_LoseExp_Variable;
 
 export type ParamArray_GainExp_Direct = [
   targetDirect: 0,
@@ -31,3 +28,33 @@ export type ParamArray_LoseExp_Direct = [
   value: number,
   showMessaage: boolean,
 ];
+
+export type ParamArray_GainExp_Variable = [
+  targetDirect: 0,
+  actorId: number,
+  operationPlus: 0,
+  operandDirect: 1,
+  variableId: number,
+  showMessaage: boolean,
+];
+
+export type ParamArray_LoseExp_Variable = [
+  targetDirect: 0,
+  actorId: number,
+  operationMinus: 1,
+  operandDirect: 1,
+  variableId: number,
+  showMessaage: boolean,
+];
+
+export interface ParamObject_ChangeExp {
+  value: number;
+  showMessaage: boolean;
+  actorId: number;
+}
+
+export interface ParamObject_ChangeExpFromVariable {
+  variableId: number;
+  showMessaage: boolean;
+  actorId: number;
+}
