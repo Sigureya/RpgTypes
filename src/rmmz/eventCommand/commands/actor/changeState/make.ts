b22b42142267748c73b0a26d540b62f86fc20866
Sigureya <sigureya1208@gmail.com>
2026-02-19 19:24:58 +0900

    対象指定が変数のバージョンを追加

--- src/rmmz/eventCommand/commands/actor/changeState/make.ts
+++ src/rmmz/eventCommand/commands/actor/changeState/make.ts
@@ -2,10 +2,13 @@ import type {
   Command_ChangeActorState,
   ParamArray_AddActorStateEach,
   ParamArray_AddActorStateTarget,
+  ParamArray_AddActorStateVariable,
   ParamArray_RemoveActorStateEach,
   ParamArray_RemoveActorStateTarget,
+  ParamArray_RemoveActorStateVariable,
   ParamObject_ChangeActorState,
   ParamObject_ChangeActorStateEach,
+  ParamObject_ChangeActorStateVariable,
 } from "./types";
 
 export const makeCommandAddActorStateTarget = (
@@ -57,4 +60,32 @@ export const makeCommandRemoveActorStateEach = (
     1,
     param.stateId,
   ] satisfies ParamArray_RemoveActorStateEach,
+});
+
+export const makeCommandAddActorStateVariable = (
+  param: ParamObject_ChangeActorStateVariable,
+  indent: number = 0,
+): Command_ChangeActorState => ({
+  code: 313,
+  indent,
+  parameters: [
+    1,
+    param.actorIdVariable,
+    0,
+    param.stateId,
+  ] satisfies ParamArray_AddActorStateVariable,
+});
+
+export const makeCommandRemoveActorStateVariable = (
+  param: ParamObject_ChangeActorStateVariable,
+  indent: number = 0,
+): Command_ChangeActorState => ({
+  code: 313,
+  indent,
+  parameters: [
+    1,
+    param.actorIdVariable,
+    1,
+    param.stateId,
+  ] satisfies ParamArray_RemoveActorStateVariable,
 });
