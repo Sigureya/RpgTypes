27f8bdf8ef4c252d1ca141c016df224da31baae1
Sigureya <sigureya1208@gmail.com>
2026-02-20 09:21:54 +0900

    Level部分を切り離した

--- src/rmmzRuntime/objects/core/battle/battler/actor.ts
+++ src/rmmzRuntime/objects/core/battle/battler/actor.ts
@@ -1,6 +1,7 @@
+import type { Rmmz_Level } from "./base/level";
 import type { Rmmz_Battler } from "./battler";
 
-export interface Rmmz_Actor extends Rmmz_Battler {
+export interface Rmmz_Actor extends Rmmz_Battler, Rmmz_Level {
   actorId(): number;
   changeEquipById(etypeId: number, itemId: number): void;
   setName(name: string): void;
@@ -10,8 +11,6 @@ export interface Rmmz_Actor extends Rmmz_Battler {
   changeClass(classId: number, keepExp: boolean): void;
   setNickname(nickname: string): void;
   setProfile(profile: string): void;
-  changeExp(exp: number, show: boolean): void;
-  changeLevel(level: number, show: boolean): void;
   addParam(paramId: number, value: number): void;
   learnSkill(skillId: number): void;
   forgetSkill(skillId: number): void;
