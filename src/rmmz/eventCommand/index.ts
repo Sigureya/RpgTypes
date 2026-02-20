357961902c8150d4ec7395d62abe00387a2dcf59
Sigureya <sigureya1208@gmail.com>
2026-02-20 11:28:31 +0900

    利用する場合を想定して巻き込みでexport。実運用と設計に溝がある

--- src/rmmz/eventCommand/index.ts
+++ src/rmmz/eventCommand/index.ts
@@ -2,4 +2,5 @@ export * from "./commands";
 export * from "./commands/tableType";
 export * from "./unionTypes";
 export * from "./utils";
+export * from "@RpgTypes/libs/eventCommand";
 export type { EventCommand } from "./commands/union";
