3bc484eeb1253cc6ce99b353f49b22be79b2d9f5
Sigureya <sigureya1208@gmail.com>
2026-02-21 14:07:34 +0900

    エラー付きのバリデーション関数。名前は後で決める

--- src/validate/rmmz/events/map/isDataMap.ts
+++ src/validate/rmmz/events/map/isDataMap.ts
@@ -1,6 +1,13 @@
+import type { ValidateResult } from "@RpgTypes/libs/ajv/validateWithErrors";
+import { validateWithErros } from "@RpgTypes/libs/ajv/validateWithErrors";
 import type { Data_Map } from "@RpgTypes/rmmz/rpg";
+import type { ValidateFunction } from "ajv";
 import validate from "./mapValidate";
 
 export const isDataMap = (data: unknown): data is Data_Map => {
   return validate(data);
+};
+
+export const xxxMap = (data: unknown): ValidateResult<Data_Map> => {
+  return validateWithErros(data, validate as ValidateFunction<Data_Map>);
 };
