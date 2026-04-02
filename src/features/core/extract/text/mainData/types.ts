8fb4466bbe5ad2a43c3b3ee4ec9ae2b62320e5ca
Sigureya <sigureya1208@gmail.com>
2026-04-03 01:23:23 +0900

    データ抽出の関連でbundleが必要になった

--- src/features/core/extract/text/mainData/types.ts
+++ src/features/core/extract/text/mainData/types.ts
@@ -1,14 +1,28 @@
 import type { PickByType, PickByTypeKeys } from "@RpgTypes/libs/templates";
+import type {
+  Data_Actor,
+  Data_Enemy,
+  Data_Weapon,
+  Data_Armor,
+  Data_Skill,
+  Data_State,
+  Data_Item,
+  Data_Class,
+} from "@RpgTypes/rmmz";
 
 export type TextExtractable<T> = PickByType<T, string> & {
   id: number;
   note: string;
 };
 
-export type ExtractedText<T> = {
+export interface ExtractedNoteList {
+  note: ExtractedTextItem[];
+}
+
+export interface ExtractedText<T> extends ExtractedNoteList {
   main: ExtractedTextProperty<T>[];
   note: ExtractedTextItem[];
-};
+}
 
 export interface ExtractedTextItem {
   key: string;
@@ -20,4 +34,15 @@ export interface ExtractedTextProperty<T> {
   key: PickByTypeKeys<T, string>;
   text: string;
   id: number;
+}
+
+export interface ExtractedDataBundle {
+  actors: ExtractedText<Data_Actor>[];
+  enemies: ExtractedText<Data_Enemy>[];
+  weapons: ExtractedText<Data_Weapon>[];
+  armors: ExtractedText<Data_Armor>[];
+  skills: ExtractedText<Data_Skill>[];
+  states: ExtractedText<Data_State>[];
+  items: ExtractedText<Data_Item>[];
+  classes: ExtractedText<Data_Class>[];
 }
