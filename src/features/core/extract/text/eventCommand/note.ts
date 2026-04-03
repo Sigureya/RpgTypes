261b01e8baf913c095812d6592ec2518fcd946a3
Sigureya <sigureya1208@gmail.com>
2026-04-03 18:39:07 +0900

    mapのnote関連。本質がfilterなので、そこを引数にして回想を移動させた

---
+++ src/features/core/extract/text/eventCommand/note.ts
@@ -0,0 +1,46 @@
+import type { NoteReadResult } from "@RpgTypes/rmmz";
+import type {
+  ExtractedMapEventTexts,
+  ExtractedMapTexts,
+  PluginCommandMzParameter,
+} from "./types";
+
+export const extractAllMapNotes = (
+  map: readonly ExtractedMapTexts[],
+): NoteReadResult[] => {
+  return map.map(n2).flat(4);
+};
+
+const n2 = (
+  map: ExtractedMapTexts,
+): [body: NoteReadResult[], events: NoteReadResult[][]] => {
+  return [map.noteItems, map.events.map((e) => e.noteItems)];
+};
+
+export const filterNoteFromMapTexts = <
+  Command extends PluginCommandMzParameter,
+>(
+  map: ExtractedMapTexts<Command>,
+  fn: (noteItem: NoteReadResult) => boolean,
+): ExtractedMapTexts<Command> => {
+  return {
+    displayedName: map.displayedName,
+    note: map.note,
+    noteItems: map.noteItems.filter(fn),
+    events: map.events.map((e) => e3(e, fn)),
+  };
+};
+
+const e3 = <Command extends PluginCommandMzParameter>(
+  mapEvent: ExtractedMapEventTexts<Command>,
+  fn: (noteItem: NoteReadResult, index: number) => boolean,
+): ExtractedMapEventTexts<Command> => {
+  return {
+    eventId: mapEvent.eventId,
+    name: mapEvent.name,
+    pageIndex: mapEvent.pageIndex,
+    commands: mapEvent.commands,
+    note: mapEvent.note,
+    noteItems: mapEvent.noteItems.filter(fn),
+  };
+};
