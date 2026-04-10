3a690aab470353fb63f5d1bb0e5d6eaa1807efc7
Sigureya <sigureya1208@gmail.com>
2026-04-11 03:35:05 +0900

    mapNote抽出の修正。引数型の差異を吸収できるようにした

--- dist/types/features/core/extract/text/eventCommand/note.d.ts
+++ dist/types/features/core/extract/text/eventCommand/note.d.ts
@@ -1,4 +1,5 @@
 import { NoteReadResult } from '../../../../../rmmz';
 import { ExtractedMapTexts, PluginCommandMzParameter } from './types';
 export declare const extractAllMapNotes: (map: readonly ExtractedMapTexts[]) => NoteReadResult[];
+export declare const extractAllMapNotesEx: <T>(map: ReadonlyArray<T>, fn: (map: T) => ExtractedMapTexts) => NoteReadResult[];
 export declare const filterNoteFromMapTexts: <Command extends PluginCommandMzParameter>(map: ExtractedMapTexts<Command>, fn: (noteItem: NoteReadResult) => boolean) => ExtractedMapTexts<Command>;
