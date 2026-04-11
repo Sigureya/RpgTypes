5180b09a794d71bc349d8506880ff96696061bc7
Sigureya <sigureya1208@gmail.com>
2026-04-12 06:43:04 +0900

    音声以外をunionでまとめる

---
+++ src/libs/utils/moveRoute/types/union.ts
@@ -0,0 +1,28 @@
+import type {
+  MoveCommand_Direction,
+  MoveCommand_Move,
+} from "./nonParamCommnads";
+import type {
+  MoveRouteCommand_ChangeBlendMode,
+  MoveRouteCommand_ChangeImage,
+  MoveRouteCommand_ChangeOpacity,
+  MoveRouteCommand_ChangeSpeed,
+  MoveRouteCommand_ChangeFrequency,
+  MoveRouteCommand_Jump,
+  MoveRouteCommand_SwitchOff,
+  MoveRouteCommand_SwitchOn,
+  MoveRouteCommand_Wait,
+} from "./paramedCommands";
+
+export type MoveRouteCommandV2 =
+  | MoveCommand_Move
+  | MoveCommand_Direction
+  | MoveRouteCommand_ChangeBlendMode
+  | MoveRouteCommand_ChangeImage
+  | MoveRouteCommand_ChangeOpacity
+  | MoveRouteCommand_ChangeSpeed
+  | MoveRouteCommand_ChangeFrequency
+  | MoveRouteCommand_Jump
+  | MoveRouteCommand_SwitchOff
+  | MoveRouteCommand_SwitchOn
+  | MoveRouteCommand_Wait;
