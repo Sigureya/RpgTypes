25040a2f64a769c12b1fa41aa03bfa220a9aa5e1
Sigureya <sigureya1208@gmail.com>
2026-04-12 03:11:29 +0900

    イベントコードから使用する音声を判定する関数

---
+++ src/rmmz/eventCommand/commands/audio/play/anyAudio.ts
@@ -0,0 +1,18 @@
+import type { AudioFolders } from "@RpgTypes/rmmz/folderNames";
+import type { CommandUnion_AnyAudio } from ".";
+
+const TABLE = {
+  132: "bgm",
+  133: "me",
+  139: "me",
+  241: "bgm",
+  245: "bgs",
+  249: "me",
+  250: "se",
+} as const satisfies Record<CommandUnion_AnyAudio["code"], AudioFolders>;
+
+export const assetDirectoryName = (
+  code: CommandUnion_AnyAudio["code"],
+): AudioFolders => {
+  return TABLE[code];
+};
--- src/rmmz/eventCommand/commands/audio/play/index.ts
+++ src/rmmz/eventCommand/commands/audio/play/index.ts
@@ -1,2 +1,3 @@
+export * from "./anyAudio";
 export * from "./make";
 export * from "./types";
