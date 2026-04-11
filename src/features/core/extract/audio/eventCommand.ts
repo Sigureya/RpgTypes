111d700d7b355b47204b582c4b4d1e11e8da1051
Sigureya <sigureya1208@gmail.com>
2026-04-12 03:48:56 +0900

    イベントコマンドから音声だけを取り出す関数。テキスト抽出と微妙に変えることで効率化している

--- src/features/core/extract/audio/eventCommand.ts
+++ src/features/core/extract/audio/eventCommand.ts
@@ -1,18 +1,45 @@
-import { makeAudioFileParams } from "@RpgTypes/libs";
+import { makeAudioFileParams, PLUGIN_COMMAND_MZ } from "@RpgTypes/libs";
 import type {
+  Command_PluginCommandMZ,
   CommandUnion_AnyAudio,
   EventCommand,
 } from "@RpgTypes/rmmz/eventCommand";
 import { isCommandAnyAudio } from "@RpgTypes/rmmz/eventCommand";
+import type { PluginCommandMzParameter } from "../text/eventCommand";
 import type { AudioCommandParameter } from "./types/result";
 
-export const extractAudioCommands = (
+export const extractAudioCommands = <T extends PluginCommandMzParameter>(
   commands: ReadonlyArray<EventCommand>,
-): AudioCommandParameter[] => {
-  return commands.filter(isCommandAnyAudio).map(xxx);
+  pluginCommandEvaltor: (command: Command_PluginCommandMZ) => T[] = () => [],
+): (AudioCommandParameter | T)[] => {
+  return extractAudioCommandsCore(commands, pluginCommandEvaltor);
 };
 
-const xxx = (command: CommandUnion_AnyAudio): AudioCommandParameter => ({
+const extractAudioCommandsCore = <T extends PluginCommandMzParameter>(
+  commands: ReadonlyArray<EventCommand>,
+  pluginCommandEvaltor: (command: Command_PluginCommandMZ) => T[],
+): (AudioCommandParameter | T)[] => {
+  return commands
+    .filter(isPluginCommandOrAudioCommand)
+    .flatMap<
+      T | AudioCommandParameter
+    >((command): T[] | AudioCommandParameter => {
+      if (command.code === PLUGIN_COMMAND_MZ) {
+        return pluginCommandEvaltor(command);
+      }
+      return pickAudioParam(command);
+    });
+};
+
+const isPluginCommandOrAudioCommand = (
+  command: EventCommand,
+): command is CommandUnion_AnyAudio | Command_PluginCommandMZ => {
+  return command.code === PLUGIN_COMMAND_MZ || isCommandAnyAudio(command);
+};
+
+const pickAudioParam = (
+  command: CommandUnion_AnyAudio,
+): AudioCommandParameter => ({
   code: command.code,
   paramIndex: 0,
   value: makeAudioFileParams(command.parameters[0]),
