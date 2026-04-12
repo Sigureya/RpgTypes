52f11d92bba6999871c6b8a7a86de14749a1442c
Sigureya <sigureya1208@gmail.com>
2026-04-12 20:23:59 +0900

    初歩の関数差し替え。手動でやる

--- src/validate/rmmz/events/map/iaDataMap.test.ts
+++ src/validate/rmmz/events/map/iaDataMap.test.ts
@@ -5,8 +5,8 @@ import type { EventCommand } from "@RpgTypes/rmmz";
 import {
   makeCommandShowMessage,
   makeCommandShowMessageBody,
-  makeCommandVariableFromConstant,
 } from "@RpgTypes/rmmz";
+import { makeCommandVariableFromConstant2 } from "@RpgTypes/rmmz/eventCommand/commands/variable/literal/make";
 import type { Data_Map, MapEvent } from "@RpgTypes/rmmz/rpg";
 import {
   makeMapData,
@@ -23,7 +23,10 @@ const isDataMap = (data: unknown): data is Data_Map => {
 const mockEventCommands: EventCommand[] = [
   makeCommandShowMessage({ speakerName: "Hero" }),
   makeCommandShowMessageBody("abc"),
-  makeCommandVariableFromConstant({ startId: 1 }, { value: 123 }),
+  makeCommandVariableFromConstant2({
+    startId: 1,
+    value: 5,
+  }),
 ];
 
 const makeMapWithEvents = <T extends EventCommandUnknown>(
