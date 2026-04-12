6e9e7c90e9cdae307138f6159de6d5da8ebe41ee
Sigureya <sigureya1208@gmail.com>
2026-04-12 21:42:26 +0900

    更新した型を追加

--- src/rmmz/eventCommand/commands/variable/unionType.ts
+++ src/rmmz/eventCommand/commands/variable/unionType.ts
@@ -1,5 +1,10 @@
 import type { Command_ControlVariables_FromActor } from "./actor";
 import type { Command_ControlVariables_FromEnemy } from "./enemy/types";
+import type {
+  Command_ControlVariables_FromArmor,
+  Command_ControlVariables_FromItem,
+  Command_ControlVariables_FromWeapon,
+} from "./goods/types";
 import type { Command_ControlVariables_FromConstant } from "./literal";
 import type { Command_ControlVariables_FromSystem } from "./system";
 import type { Command_ControlVariables_FromLastData } from "./templLastData";
@@ -9,11 +14,10 @@ export type Command_ControlVariables2 =
   | Command_ControlVariables_FromEnemy
   | Command_ControlVariables_FromConstant
   | Command_ControlVariables_FromActor
-  | Command_ControlVariables_FromLastData;
+  | Command_ControlVariables_FromLastData
+  | Command_ControlVariables_FromArmor
+  | Command_ControlVariables_FromItem
+  | Command_ControlVariables_FromWeapon;
 
 export type ParamArray_ControlVariablesUnion =
-  | Command_ControlVariables_FromSystem["parameters"]
-  | Command_ControlVariables_FromEnemy["parameters"]
-  | Command_ControlVariables_FromConstant["parameters"]
-  | Command_ControlVariables_FromActor["parameters"]
-  | Command_ControlVariables_FromLastData["parameters"];
+  Command_ControlVariables2["parameters"];
