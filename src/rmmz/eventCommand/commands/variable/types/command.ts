d345f65364323475fca48eef9a2d724ca91e087f
Sigureya <sigureya1208@gmail.com>
2026-04-12 20:47:46 +0900

    古い方を非推奨にする。影響が大きいので、すぐには消さない

--- src/rmmz/eventCommand/commands/variable/types/command.ts
+++ src/rmmz/eventCommand/commands/variable/types/command.ts
@@ -4,6 +4,9 @@ import type {
 } from "@RpgTypes/libs/eventCommand";
 import type { ParamArray_ControlVariables } from "./paramArray";
 
+/**
+ * @deprecated Use Command_ControlVariables_FromConstant, Command_ControlVariables_FromActor, Command_ControlVariables_FromEnemy, Command_ControlVariables_FromSystem instead.
+ */
 export interface Command_ControlVariables<
   Param extends ParamArray_ControlVariables = ParamArray_ControlVariables,
 > extends EventCommandLike<typeof CONTROL_VARIABLES> {
