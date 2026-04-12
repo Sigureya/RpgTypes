1b14e77f5b7b466c9d8f3606af874fc7d00cc275
Sigureya <sigureya1208@gmail.com>
2026-04-12 22:27:18 +0900

    現状では使ってない部分を削除。段階的に消していき、フォルダ移動も進めていく

--- src/rmmz/eventCommand/commands/variable/types/actor/fromActorParams.ts
+++
@@ -1,6 +0,0 @@
-export interface ParamObject_Operand_ActorStatus {
-  startId: number;
-  endId?: number;
-  actorId: number;
-  operation: number;
-}
--- src/rmmz/eventCommand/commands/variable/types/last/paramObject.ts
+++
@@ -1,5 +0,0 @@
-import type { VARIABLE_SRC_LAST } from "./dataSource";
-
-export interface ParamObject_Operand_LastData {
-  param: keyof typeof VARIABLE_SRC_LAST;
-}
