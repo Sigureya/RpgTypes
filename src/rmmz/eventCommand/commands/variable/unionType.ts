72d9eeaa80a309944fc2309c2845d852a8239438
Sigureya <sigureya1208@gmail.com>
2026-04-13 00:23:13 +0900

    古いコードを片付けたので、こっちメインに切り替え

--- src/rmmz/eventCommand/commands/variable/unionType.ts
+++ src/rmmz/eventCommand/commands/variable/unionType.ts
@@ -1,4 +1,3 @@
-import type { CONTROL_VARIABLES, EventCommandLike } from "@RpgTypes/libs";
 import type { Command_ControlVariables_FromActor } from "./actor";
 import type { Command_ControlVariables_FromEnemy } from "./enemy/types";
 import type { Command_ControlVariables_FromVariable } from "./gameVariable2";
@@ -14,12 +13,7 @@ import type { Command_ControlVariables_FromRandom } from "./random";
 import type { Command_ControlVariables_FromSystem } from "./system";
 import type { Command_ControlVariables_FromLastData } from "./templLastData";
 
-export type Command_ControlVariables2 = EventCommandLike<
-  typeof CONTROL_VARIABLES,
-  ParamArray_ControlVariablesUnion
->;
-
-export type ParamArray_ControlVariablesUnion = (
+export type Command_ControlVariables =
   | Command_ControlVariables_FromSystem
   | Command_ControlVariables_FromEnemy
   | Command_ControlVariables_FromConstant
@@ -31,5 +25,7 @@ export type ParamArray_ControlVariablesUnion = (
   | Command_ControlVariables_FromParty
   | Command_ControlVariables_FromArmor
   | Command_ControlVariables_FromItem
-  | Command_ControlVariables_FromWeapon
-)["parameters"];
+  | Command_ControlVariables_FromWeapon;
+
+export type ParamArray_ControlVariables =
+  Command_ControlVariables["parameters"];
