67a9f72c0fcc5546c933759fe30605c7c2397409
Sigureya <sigureya1208@gmail.com>
2026-04-13 03:19:14 +0900

    座標指定直接版を追加。

--- src/rmmz/eventCommand/commands/mapEvent/getLocationInfo/make.ts
+++ src/rmmz/eventCommand/commands/mapEvent/getLocationInfo/make.ts
@@ -1,17 +1,30 @@
 import { GET_LOCATION_INFO } from "@RpgTypes/libs/eventCommand";
-import type { Command_GetEventIdXY } from "./types/eventXY";
-import type { ParamObject_GetLocationInfo } from "./types/getLocation";
-import type { Command_GetRegionId } from "./types/getRegionId";
 import type {
+  Command_GetEventIdXY,
+  Command_GetEventIdXyDirect,
+  Command_GetRegionId,
+  Command_GetRegionIdDirect,
+  Command_GetTerrainTag,
+  Command_GetTerrainTagDirect,
   Command_GetTileId,
+  Command_GetTileIdDirect,
+  ParamObject_GetLocationInfo,
   ParamObject_GetTileId,
-} from "./types/getTileId";
-import type { Command_GetTerrainTag } from "./types/getTrarainTag";
+} from "./types";
 
 export const makeCommandGetTerrainTag = (
   param: ParamObject_GetLocationInfo,
   indent: number = 0,
 ): Command_GetTerrainTag => ({
+  code: GET_LOCATION_INFO,
+  indent,
+  parameters: [param.variableId, 0, 1, param.x, param.y],
+});
+
+export const makeCommandGetTerrainTagDirect = (
+  param: ParamObject_GetLocationInfo,
+  indent: number = 0,
+): Command_GetTerrainTagDirect => ({
   code: GET_LOCATION_INFO,
   indent,
   parameters: [param.variableId, 0, 0, param.x, param.y],
@@ -23,6 +36,15 @@ export const makeCommandGetEventIdXY = (
 ): Command_GetEventIdXY => ({
   code: GET_LOCATION_INFO,
   indent,
+  parameters: [param.variableId, 1, 1, param.x, param.y],
+});
+
+export const makeCommandGetEventIdXYDirect = (
+  param: ParamObject_GetLocationInfo,
+  indent: number = 0,
+): Command_GetEventIdXyDirect => ({
+  code: GET_LOCATION_INFO,
+  indent,
   parameters: [param.variableId, 1, 0, param.x, param.y],
 });
 
@@ -32,6 +54,15 @@ export const makeCommandGetRegionId = (
 ): Command_GetRegionId => ({
   code: GET_LOCATION_INFO,
   indent,
+  parameters: [param.variableId, 6, 1, param.x, param.y],
+});
+
+export const makeCommandGetRegionIdDirect = (
+  param: ParamObject_GetLocationInfo,
+  indent: number = 0,
+): Command_GetRegionIdDirect => ({
+  code: GET_LOCATION_INFO,
+  indent,
   parameters: [param.variableId, 6, 0, param.x, param.y],
 });
 
@@ -39,6 +70,21 @@ export const makeCommandGetTileId = (
   param: ParamObject_GetTileId,
   indent: number = 0,
 ): Command_GetTileId => ({
+  code: GET_LOCATION_INFO,
+  indent,
+  parameters: [
+    param.variableId,
+    LAYER_TABLE[param.layer] ?? 2,
+    1,
+    param.x,
+    param.y,
+  ],
+});
+
+export const makeCommandGetTileIdDirect = (
+  param: ParamObject_GetTileId,
+  indent: number = 0,
+): Command_GetTileIdDirect => ({
   code: GET_LOCATION_INFO,
   indent,
   parameters: [
