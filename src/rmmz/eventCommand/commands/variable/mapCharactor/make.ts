a0b05015ef5d52cf62a86f01b47e35e54794d26c
Sigureya <sigureya1208@gmail.com>
2026-04-12 22:36:47 +0900

    関数を仮実装

---
+++ src/rmmz/eventCommand/commands/variable/mapCharactor/make.ts
@@ -0,0 +1,76 @@
+import { CONTROL_VARIABLES } from "@RpgTypes/libs";
+import type { ValueOf } from "src/libs/templates/valueOf";
+import type {
+  Command_ControlVariables_FromMapCharactor,
+  ParamArray_VariableFromMapCharactor,
+  ParamObject_VariableFromMapCharactor,
+} from "./types";
+import { MAP_CHARACTOR_PARAM } from "./types";
+
+export const makeCommandVariableFromMapCharactorX = (
+  params: ParamObject_VariableFromMapCharactor,
+  indent: number = 0,
+): Command_ControlVariables_FromMapCharactor => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent,
+    parameters: paramToArray(params, MAP_CHARACTOR_PARAM.X),
+  };
+};
+
+export const makeCommandVariableFromMapCharactorY = (
+  params: ParamObject_VariableFromMapCharactor,
+  indent: number = 0,
+): Command_ControlVariables_FromMapCharactor => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent,
+    parameters: paramToArray(params, MAP_CHARACTOR_PARAM.Y),
+  };
+};
+
+export const makeCommandVariableFromMapCharactorDirection = (
+  params: ParamObject_VariableFromMapCharactor,
+  indent: number = 0,
+): Command_ControlVariables_FromMapCharactor => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent,
+    parameters: paramToArray(params, MAP_CHARACTOR_PARAM.DIRECTION),
+  };
+};
+
+export const makeCommandVariableFromMapCharactorScreenX = (
+  params: ParamObject_VariableFromMapCharactor,
+  indent: number = 0,
+): Command_ControlVariables_FromMapCharactor => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent,
+    parameters: paramToArray(params, MAP_CHARACTOR_PARAM.SCREEN_X),
+  };
+};
+
+export const makeCommandVariableFromMapCharactorScreenY = (
+  params: ParamObject_VariableFromMapCharactor,
+  indent: number = 0,
+): Command_ControlVariables_FromMapCharactor => {
+  return {
+    code: CONTROL_VARIABLES,
+    indent,
+    parameters: paramToArray(params, MAP_CHARACTOR_PARAM.SCREEN_Y),
+  };
+};
+
+const paramToArray = (
+  params: ParamObject_VariableFromMapCharactor,
+  param: ValueOf<typeof MAP_CHARACTOR_PARAM>,
+): ParamArray_VariableFromMapCharactor => [
+  params.startId,
+  params.endId ?? params.startId,
+  params.operation ?? 0,
+  3,
+  5,
+  params.charactorId,
+  param,
+];
