91342e20647b7a7b1be2a9a0b0a0c484780d7eb8
Sigureya <sigureya1208@gmail.com>
2026-04-17 06:06:45 +0900

    ファイル読み込みの都合で分割。Recrodのキーに使うことを考えた

--- src/fileIO/data/types.ts
+++ src/fileIO/data/types.ts
@@ -1,8 +1,6 @@
 import type * as Name from "./arrayData/constants/fileNames";
-import type { MapFileNameWithExt } from "./map";
 import type { FILENAME_SYSTEM } from "./system";
 
 export type DataFileNames =
-  | MapFileNameWithExt
   | (typeof Name)[keyof typeof Name]
   | typeof FILENAME_SYSTEM;
