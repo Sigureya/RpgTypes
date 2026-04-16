6da56286c2bca43a984e1f381b3b5f8101bfdade
Sigureya <sigureya1208@gmail.com>
2026-04-17 02:39:05 +0900

    一度単純な状態で固定。AIが悲鳴を上げていたので

---
+++ src/fileIO/data/resultType.ts
@@ -0,0 +1,66 @@
+import type {
+  Data_Actor,
+  Data_Armor,
+  Data_Class,
+  Data_CommonEvent,
+  Data_Enemy,
+  Data_Item,
+  Data_MapInfo,
+  Data_Skill,
+  Data_State,
+  Data_Troop,
+  Data_Weapon,
+} from "@RpgTypes/rmmz";
+import type { ReadArrayResult } from "./arrayData";
+import type { ReadSystemResult, ReadSystemResultEx } from "./system";
+
+export interface ReadHandledResult<T> {
+  succcess: boolean;
+  fileName: string;
+  data: T | (T extends unknown[] ? [] : null);
+  error: string;
+}
+
+export type ReadHandledArrayResult<T> = ReadHandledResult<T[]>;
+
+export interface ReadAllGameDataResult<
+  Commmon,
+  System,
+  Actor,
+  Skill,
+  Item,
+  Weapon,
+  Armor,
+  Enemy,
+  Class,
+  State,
+  Troop,
+> {
+  actor: ReadHandledArrayResult<Actor>;
+  armor: ReadHandledArrayResult<Armor>;
+  classes: ReadHandledArrayResult<Class>;
+  commonEvent: ReadHandledArrayResult<Commmon>;
+  enemies: ReadHandledArrayResult<Enemy>;
+  item: ReadHandledArrayResult<Item>;
+  mapInfo: ReadArrayResult<Data_MapInfo>;
+  skill: ReadHandledArrayResult<Skill>;
+  state: ReadHandledArrayResult<State>;
+  system: ReadSystemResultEx<System>;
+  troop: ReadHandledArrayResult<Troop>;
+  weapon: ReadHandledArrayResult<Weapon>;
+}
+
+export interface ReadAllArrayDataResult {
+  actor: ReadArrayResult<Data_Actor>;
+  armor: ReadArrayResult<Data_Armor>;
+  classes: ReadArrayResult<Data_Class>;
+  commonEvent: ReadArrayResult<Data_CommonEvent>;
+  enemies: ReadArrayResult<Data_Enemy>;
+  item: ReadArrayResult<Data_Item>;
+  mapInfo: ReadArrayResult<Data_MapInfo>;
+  skill: ReadArrayResult<Data_Skill>;
+  state: ReadArrayResult<Data_State>;
+  system: ReadSystemResult;
+  troop: ReadArrayResult<Data_Troop>;
+  weapon: ReadArrayResult<Data_Weapon>;
+}
