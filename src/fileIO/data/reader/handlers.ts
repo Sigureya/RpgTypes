743d9fbf10d3e8a46b51f30c5222804a8682d524
Sigureya <sigureya1208@gmail.com>
2026-04-17 02:36:14 +0900

    配列限定と汎用版を分割

--- src/fileIO/data/reader/handlers.ts
+++ src/fileIO/data/reader/handlers.ts
@@ -44,6 +44,34 @@ export interface RpgDataReadHandlers<
   Class,
   State,
   Troop,
+> {
+  readSkill(skill: Data_Skill[], filePath: string): Skill;
+  readItem(item: Data_Item[], filePath: string): Item;
+  readWeapon(weapon: Data_Weapon[], filePath: string): Weapon;
+  readArmor(armor: Data_Armor[], filePath: string): Armor;
+  readEnemy(enemy: Data_Enemy[], filePath: string): Enemy;
+  readClass(cls: Data_Class[], filePath: string): Class;
+  readState(state: Data_State[], filePath: string): State;
+  readActor(actors: Data_Actor[], filePath: string): Actor;
+  readCommonEvent(events: Data_CommonEvent[], filePath: string): Commmon;
+  readSystem(system: Data_System, filePath: string): System;
+  readMap(map: MapFileInfo): Map;
+  readTroop(troops: Data_Troop[], filePath: string): Troop;
+}
+
+export interface RpgDataReadHandlersA<
+  Commmon,
+  Map,
+  System,
+  Actor,
+  Skill,
+  Item,
+  Weapon,
+  Armor,
+  Enemy,
+  Class,
+  State,
+  Troop,
 > extends RpgDataReadHandlersBase {
   readSkill(skill: Data_Skill[], filePath: string): Skill[];
   readItem(item: Data_Item[], filePath: string): Item[];
