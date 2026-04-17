021ccb451f2e98dca08e83acdd7cb1796294adac
Sigureya <sigureya1208@gmail.com>
2026-04-17 22:25:47 +0900

    外部公開を追加

--- src/validate/rmmz/events/index.ts
+++ src/validate/rmmz/events/index.ts
@@ -1,3 +1,4 @@
 export * from "./eventCommand";
 export * from "./map/isDataMap";
 export { isDataCommonEvent } from "./commonEvent/isDataCommonEvent";
+export { isDataTroop } from "./troop/isDataTroop";
