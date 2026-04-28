262e797a6a58c60b5b2398df897c3fb3548b488e
Sigureya <sigureya1208@gmail.com>
2026-04-28 18:59:49 +0900

    型定義を明示的に追加

--- src/rmmz/rpg/data/main/makeTestData.ts
+++ src/rmmz/rpg/data/main/makeTestData.ts
@@ -1,5 +1,12 @@
 import type { TestDataSource, TestDataSourceWithNote } from "@RpgTypes/libs";
-import type { Data_Actor } from "./traitContainers";
+import type {
+  Data_Actor,
+  Data_Armor,
+  Data_Class,
+  Data_Enemy,
+  Data_State,
+  Data_Weapon,
+} from "./traitContainers";
 import {
   makeActorData,
   makeArmorData,
@@ -8,6 +15,7 @@ import {
   makeStateData,
   makeWeaponData,
 } from "./traitContainers";
+import type { Data_Item, Data_Skill } from "./usableItems";
 import { makeItemData, makeSkillData } from "./usableItems";
 
 export const makeActorDataFromTestSoruce = (
@@ -24,14 +32,18 @@ export const makeActorDataFromTestSoruce = (
   });
 };
 
-export const makeClassDataFromTestSoruce = (soruce: TestDataSource) => {
+export const makeClassDataFromTestSoruce = (
+  soruce: TestDataSource,
+): Data_Class => {
   return makeClassData({
     name: soruce.text,
     note: soruce.text,
   });
 };
 
-export const makeWeaponDataFromTestSoruce = (soruce: TestDataSource) => {
+export const makeWeaponDataFromTestSoruce = (
+  soruce: TestDataSource,
+): Data_Weapon => {
   return makeWeaponData({
     name: soruce.text,
     note: soruce.text,
@@ -39,7 +51,9 @@ export const makeWeaponDataFromTestSoruce = (soruce: TestDataSource) => {
   });
 };
 
-export const makeArmorDataFromTestSoruce = (soruce: TestDataSource) => {
+export const makeArmorDataFromTestSoruce = (
+  soruce: TestDataSource,
+): Data_Armor => {
   return makeArmorData({
     name: soruce.text,
     note: soruce.text,
@@ -47,7 +61,9 @@ export const makeArmorDataFromTestSoruce = (soruce: TestDataSource) => {
   });
 };
 
-export const makeEnemyDataFromTestSoruce = (soruce: TestDataSource) => {
+export const makeEnemyDataFromTestSoruce = (
+  soruce: TestDataSource,
+): Data_Enemy => {
   return makeEnemyData({
     name: soruce.text,
     note: soruce.text,
@@ -55,7 +71,9 @@ export const makeEnemyDataFromTestSoruce = (soruce: TestDataSource) => {
   });
 };
 
-export const makeStateDataFromTestSoruce = (soruce: TestDataSource) => {
+export const makeStateDataFromTestSoruce = (
+  soruce: TestDataSource,
+): Data_State => {
   return makeStateData({
     name: soruce.text,
     note: soruce.text,
@@ -66,7 +84,9 @@ export const makeStateDataFromTestSoruce = (soruce: TestDataSource) => {
   });
 };
 
-export const makeSkillDataFromTestSoruce = (soruce: TestDataSource) => {
+export const makeSkillDataFromTestSoruce = (
+  soruce: TestDataSource,
+): Data_Skill => {
   return makeSkillData({
     name: soruce.text,
     note: soruce.text,
@@ -77,7 +97,9 @@ export const makeSkillDataFromTestSoruce = (soruce: TestDataSource) => {
   });
 };
 
-export const makeItemDataFromTestSoruce = (soruce: TestDataSource) => {
+export const makeItemDataFromTestSoruce = (
+  soruce: TestDataSource,
+): Data_Item => {
   return makeItemData({
     name: soruce.text,
     note: soruce.text,
