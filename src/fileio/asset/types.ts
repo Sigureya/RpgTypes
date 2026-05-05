ec0084ce5930fb91c4a134128910613fb67ec98e
Sigureya <sigureya1208@gmail.com>
2026-05-05 19:20:01 +0900

    アセットの塊色々。flatするためにフォルダ移動する

--- src/fileio/asset/types.ts
+++ src/fileio/asset/types.ts
@@ -1,3 +1,12 @@
+import type { AudioFilesSet } from "./audio";
+import type { ImageFilesSet } from "./image";
+
 export interface OtherFilesSet {
   movies: ReadonlySet<string>;
+}
+
+export interface AssetFilesBundle {
+  audioFiles: AudioFilesSet;
+  imageFiles: ImageFilesSet;
+  otherFiles: OtherFilesSet;
 }
