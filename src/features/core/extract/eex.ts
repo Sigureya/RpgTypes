f4f4e5ada1651c39b13b3b91209be53a093b7346
Sigureya <sigureya1208@gmail.com>
2026-05-05 18:10:38 +0900

    データ変換関数。色々と問題はあるが、最初のバージョンとして作製

---
+++ src/features/core/extract/eex.ts
@@ -0,0 +1,199 @@
+import type {
+  ReadAllDataErrorMessages,
+  AudioFilesSet,
+  ImageFilesSet,
+  OtherFilesSet,
+  RawGameData,
+  DataReadErrors,
+  DataReadErrorItem,
+  MapBatchReadResult,
+  ReadSystemResult,
+  FileReadFailed,
+  FileReadBundle,
+} from "@RpgTypes/fileio";
+import {
+  summarizeReadErrors,
+  FILENAME_ARMORS,
+  FILENAME_ACTORS,
+  FILENAME_CLASSES,
+  FILENAME_COMMON_EVENTS,
+  FILENAME_ENEMIES,
+  FILENAME_ITEMS,
+  FILENAME_SKILLS,
+  FILENAME_STATES,
+  FILENAME_WEAPONS,
+  FILENAME_TROOPS,
+  FILENAME_SYSTEM,
+} from "@RpgTypes/fileio";
+import type { Data_Actor, MapFileInfo } from "@RpgTypes/rmmz";
+import { createActorControlChars } from "@RpgTypes/rmmz";
+import type { ExtractedSystemTexts } from "dist/types";
+import { extractTextFromRawGameData } from "./bundle";
+import { ccedFromList, ctx } from "./commonEvent";
+import { convertDataList, convertStateData } from "./mainData";
+import { extractMapEventTexts } from "./map";
+import type { SummarizedNote, SummarizedNoteValue } from "./note";
+import { buildRawGameDataNoteNormalization } from "./noteNormarize";
+import type {
+  SystemKinds,
+  Terms,
+  ExtractedTextFinal,
+  SystemTexts,
+  ExtractedTextItemG,
+} from "./sss/types";
+import type {
+  TextCommandParameter,
+  EventContainerExtractor,
+  ExtractedMapTexts,
+} from "./text";
+import { convertSystemTypes } from "./text/system/conv";
+import type { RawGameDataNoteNormalization } from "./types";
+
+const READ_ERROR_MESSAGES: ReadAllDataErrorMessages = {
+  dataConvertError: "データの変換に失敗しました。",
+  fileNotFound: "ファイルが見つかりませんでした。",
+  jsonParseError: "JSONの解析に失敗しました。",
+  notArray: "データが配列ではありません。",
+  invalidStructure: "データの構造が正しくありません。",
+  mvAsMz: "RPGツクールMVのプロジェクトはサポートされていません。",
+  validateFunctionError: "データの検証中にエラーが発生しました。",
+};
+
+const readErrors2 = (data: RawGameData) => {
+  const readErrors: DataReadErrors = summarizeReadErrors(data);
+  return toFileReadFailedList(readErrors);
+};
+
+export const buildExtractResult2 = <UUID>(
+  kinds: SystemKinds,
+  bundle: FileReadBundle,
+  terms: Terms,
+  uuidGen: (text: string) => UUID,
+  commandNameFn: (command: TextCommandParameter) => string,
+  extractor: EventContainerExtractor,
+): ExtractedTextFinal<UUID> => {
+  const extractedRawData = extractTextFromRawGameData(bundle.data, extractor);
+  const normalizeNote = buildRawGameDataNoteNormalization(
+    extractedRawData,
+    bundle,
+  );
+
+  return buildSuccessData(
+    bundle.data.actors.data,
+    normalizeNote,
+    kinds,
+    terms,
+    uuidGen,
+    commandNameFn,
+  );
+};
+
+const toFileReadFailedList = (readErrors: DataReadErrors): FileReadFailed[] => {
+  return [
+    ...readErrors.main.map(toFileReadFailedItem),
+    ...readErrors.map.map(toFileReadFailedItem),
+  ].flat();
+};
+
+const toFileReadFailedItem = (item: DataReadErrorItem): FileReadFailed => ({
+  success: false,
+  message: item.error,
+  filename: item.fileName,
+});
+
+const ggn = (
+  normalizedData: RawGameDataNoteNormalization,
+): SummarizedNote<SummarizedNoteValue>[] => {
+  return [...normalizedData.dataNoteSummary, ...normalizedData.mapNoteSummary];
+};
+
+const buildSuccessData = <UUID>(
+  actors: ReadonlyArray<Data_Actor>,
+  normalizedData: RawGameDataNoteNormalization,
+  kinds: SystemKinds,
+  terms: Terms,
+  uuidGen: (text: string) => UUID,
+  commandNameFn: (command: TextCommandParameter) => string,
+): ExtractedTextFinal<UUID> => {
+  const { eventData, mainData, mapFiles, system } = normalizedData.data.value;
+  return {
+    noteX: ggn(normalizedData),
+    pluginParams: [],
+    armors: convertDataList(mainData.armors, FILENAME_ARMORS, terms, uuidGen),
+    system: buildSystemTexts(system, uuidGen, kinds),
+    actors: {
+      texts: convertDataList(mainData.actors, FILENAME_ACTORS, terms, uuidGen),
+      controlChars: createActorControlChars(actors),
+    },
+    classes: convertDataList(
+      mainData.classes,
+      FILENAME_CLASSES,
+      terms,
+      uuidGen,
+    ),
+    commonEvents: ccedFromList(
+      FILENAME_COMMON_EVENTS,
+      eventData.commonEvents,
+      uuidGen,
+      commandNameFn,
+    ),
+    enemies: convertDataList(
+      mainData.enemies,
+      FILENAME_ENEMIES,
+      terms,
+      uuidGen,
+    ),
+    items: convertDataList(mainData.items, FILENAME_ITEMS, terms, uuidGen),
+    skills: convertDataList(mainData.skills, FILENAME_SKILLS, terms, uuidGen),
+    states: convertStateData(mainData.states, FILENAME_STATES, terms, uuidGen),
+    weapons: convertDataList(
+      mainData.weapons,
+      FILENAME_WEAPONS,
+      terms,
+      uuidGen,
+    ),
+    troops: ctx(eventData.troops, FILENAME_TROOPS, commandNameFn, uuidGen),
+    map: flattenMapTexts(mapFiles, uuidGen, commandNameFn),
+  };
+};
+
+const flattenMapTexts = <UUID>(
+  mapFiles: MapBatchReadResult<ExtractedMapTexts>,
+  uuidGen: (text: string) => UUID,
+  commandNameFn: (command: TextCommandParameter) => string,
+): ExtractedTextItemG<UUID>[] => {
+  const mapEventTextGroups: ExtractedTextItemG<UUID>[][][][] =
+    mapFiles.validMaps.map((map) =>
+      extractMapTextGroups(map, uuidGen, commandNameFn),
+    );
+  return mapEventTextGroups.flat(3);
+};
+
+const extractMapTextGroups = <UUID>(
+  map: MapFileInfo<ExtractedMapTexts>,
+  uuidGen: (text: string) => UUID,
+  commandNameFn: (command: TextCommandParameter) => string,
+): ExtractedTextItemG<UUID>[][][] => {
+  return map.map.events.map((mapEvent): ExtractedTextItemG<UUID>[][] =>
+    extractMapEventTexts(mapEvent, map, uuidGen, commandNameFn),
+  );
+};
+
+const buildSystemTexts = <UUID>(
+  system: ReadSystemResult<ExtractedSystemTexts>,
+  uuidGen: (text: string) => UUID,
+  kinds: SystemKinds,
+): SystemTexts<UUID> => {
+  if (system.system === null) {
+    return {
+      gameTitle: "",
+      filename: FILENAME_SYSTEM,
+      texts: [],
+    };
+  }
+  return {
+    gameTitle: system.system.gameTitle,
+    filename: FILENAME_SYSTEM,
+    texts: convertSystemTypes(system.system, FILENAME_SYSTEM, kinds, uuidGen),
+  };
+};
