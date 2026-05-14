13bafd82078b163c20c02b26181c3f4c56403041
Sigureya <sigureya1208@gmail.com>
2026-05-14 16:41:18 +0900

    アクターテキスト抽出の改造。最初から自動で辞書を作った方が楽

--- dist/types/features/core/rpg/extractActorText.d.ts
+++ dist/types/features/core/rpg/extractActorText.d.ts
@@ -1,2 +1,4 @@
+import { KeyValuePairEx } from '../../../libs';
 import { Data_Actor, Data_Map, Data_CommonEvent, Data_Troop } from '../../../rmmz';
+export declare const createActorTextDictionary: <T>(actors: ReadonlyArray<Data_Actor>, maps: ReadonlyArray<Data_Map>, commons: ReadonlyArray<Data_CommonEvent>, troops: ReadonlyArray<Data_Troop>, hashFn: (text: string) => T) => KeyValuePairEx<T, string>[];
 export declare const extractActorTexts: (actors: ReadonlyArray<Data_Actor>, maps: ReadonlyArray<Data_Map>, commons: ReadonlyArray<Data_CommonEvent>, troops: ReadonlyArray<Data_Troop>) => Set<string>;
--- dist/types/libs/utils/types/keyValuePair.d.ts
+++ dist/types/libs/utils/types/keyValuePair.d.ts
@@ -2,3 +2,7 @@ export interface KeyValuePair {
     key: string;
     value: string;
 }
+export interface KeyValuePairEx<K, V> {
+    key: K;
+    value: V;
+}
