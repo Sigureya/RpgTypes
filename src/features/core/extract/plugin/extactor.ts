666da5f3f7251e10c81de76592773b63af915591
Sigureya <sigureya1208@gmail.com>
2026-05-29 21:27:21 +0900

    関数名微修正。仮の名前だったが、修正コメントのサジェストがうるさいので直した

--- src/features/core/extract/plugin/extactor.ts
+++ src/features/core/extract/plugin/extactor.ts
@@ -8,39 +8,22 @@ import {
 } from "@sigureya/rmmz-plugin-schema";
 import type {
   PluginCommandExtractErrorHandlers,
-  PluginCommandExtractionOutput,
-  PluginExtractedValue,
   PluginCommandExtractorSource,
 } from "@sigureya/rmmz-plugin-schema";
-import type { PluginCommandExtractorV2 } from "./types";
+import { buildPluginCommandArgInfo } from "./conv";
+import type { PluginCommandExtractor } from "./types/types";
 
 export const createPluginCommandExtractorV2 = (
   src: PluginCommandExtractorSource,
-): PluginCommandExtractorV2 => {
+): PluginCommandExtractor => {
   const map = createCommandExtractorMapFromPipeline(src);
   return {
     extractArgs(
       command: Command_PluginCommandMZ,
       handles: PluginCommandExtractErrorHandlers,
     ): PluginCommandParameter<string>[] {
-      const xxx = extractPluginCommandWithExtractor(command, map, handles);
-      return fff(xxx);
+      const data = extractPluginCommandWithExtractor(command, map, handles);
+      return data.args.map((arg) => buildPluginCommandArgInfo(data, arg));
     },
   };
-};
-
-const fff = (
-  ss: PluginCommandExtractionOutput,
-): PluginCommandParameter<string>[] => {
-  return ss.args.map(
-    (gg: PluginExtractedValue): PluginCommandParameter<string> => ({
-      code: 357,
-      pluginName: ss.pluginName,
-      commandName: ss.commandName,
-      argName: gg.param.name,
-      value: typeof gg.value === "string" ? gg.value : "",
-      argTitle: gg.param.attr.text || gg.param.name,
-      paramIndex: 3,
-    }),
-  );
 };
