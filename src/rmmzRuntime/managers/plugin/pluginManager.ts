f19b3699b323244447089f5ace7ace55396603c7
Sigureya <sigureya1208@gmail.com>
2026-06-07 22:58:53 +0900

    標準のプラグインマネージャー。一応定義しておく

--- src/rmmzRuntime/managers/plugin/pluginManager.ts
+++ src/rmmzRuntime/managers/plugin/pluginManager.ts
@@ -1,16 +1,20 @@
-export interface Rmmz_PluginManager<InterPreter> {
+import type { Rmmz_PluginManagerBase } from "@RpgTypes/libs/rmmz/pluginManager";
+
+export interface Rmmz_PluginManager<
+  InterPreter,
+> extends Rmmz_PluginManagerBase {
   _commands: Record<string, (args: Record<string, string>) => void>;
 
   parameters(name: string): Record<string, string>;
   registerCommand(
     pluginName: string,
     commandName: string,
-    func: () => void
+    func: (args: Record<string, string>) => void,
   ): void;
   callCommand(
     self: InterPreter,
     pluginName: string,
     commandName: string,
-    args: Record<string, string>
+    args: Record<string, string>,
   ): void;
 }
