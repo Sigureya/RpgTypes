52ce077cf495e964c0d2e0c72982107443c52808
Sigureya <sigureya1208@gmail.com>
2026-06-14 01:17:28 +0900

    型を微修正。専用の型を定義しておいた方が安全そう

--- dist/types/features/core/extract/bundle.d.ts
+++ dist/types/features/core/extract/bundle.d.ts
@@ -1,4 +1,3 @@
-import { RawGameData } from '../../../fileio';
-import { Data_System, EventCommand } from '../../../rmmz';
+import { RawGameDataNullableSystem } from '../../../fileio';
 import { EventContainerExtractor, ExtractedRawGameDataTexts } from './text';
-export declare const extractTextFromRawGameData: (data: RawGameData<EventCommand, Data_System | null>, extractor: EventContainerExtractor) => ExtractedRawGameDataTexts;
+export declare const extractTextFromRawGameData: (data: RawGameDataNullableSystem, extractor: EventContainerExtractor) => ExtractedRawGameDataTexts;
