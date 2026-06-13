033240100cb5a9e03f94a674c4d379812fe1e6cc
Sigureya <sigureya1208@gmail.com>
2026-06-14 04:39:43 +0900

    プラグインコマンドを増設。システム系は別にした方が安定しそう

--- src/features/core/extract/dictionary/manifest/constatants.ts
+++ src/features/core/extract/dictionary/manifest/constatants.ts
@@ -1,3 +1,5 @@
 export const PLUGIN_NAME_HONYAKU_EX = "HonyakuEx" as const;
-export const PLUGIN_COMMAND_HONYAKU_SETUP = "Setup" as const;
+export const PLUGIN_COMMAND_HONYAKU_SETUP_DICTIONARY = "Setup" as const;
+export const PLUGIN_COMMAND_HONYAKU_SETUP_SYSTEM_TEXTS =
+  "SetupSystemTexts" as const;
 export const PLUGIN_COMMAND_READ_PLUGINS = "ReadPlugins" as const;
