0324f9b5262c775ebbcc67a7fb6da9ebaca0711b
Sigureya <sigureya1208@gmail.com>
2026-06-14 15:38:28 +0900

    scehmaの微修正とテスト追加。今は動くことが最優先

--- src/validate/rmmz/system/terms/schema.ts
+++ src/validate/rmmz/system/terms/schema.ts
@@ -169,7 +169,6 @@ export const SCHEMA_SYSTEM_TEXTS = {
             "actorNoHit",
             "enemyNoDamage",
             "enemyNoHit",
-            "enemyRecovery",
             "evasion",
             "magicEvasion",
             "magicReflection",
