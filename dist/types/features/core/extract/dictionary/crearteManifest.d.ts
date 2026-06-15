2528d83919085f0bf7197ff0a026b1a52242c6a6
Sigureya <sigureya1208@gmail.com>
2026-06-15 17:39:13 +0900

    辞書データの出力改造を実装し、ビルド。翻訳済みデータと、翻訳前データのペアになった

--- dist/types/features/core/extract/dictionary/crearteManifest.d.ts
+++ dist/types/features/core/extract/dictionary/crearteManifest.d.ts
@@ -4,6 +4,9 @@ import { PluginManifestData, PluginParamsRecord, PluginManagerTemplate } from '@
 import { RuntimePluginBundleOptions, PLUGIN_NAME_HONYAKU_EX, PLUGIN_COMMAND_HONYAKU_SETUP_DICTIONARY, PLUGIN_COMMAND_HONYAKU_SETUP_SYSTEM_TEXTS } from './manifest';
 import { RuntimeDictionaryData, RuntimeDictionaryDataWithSystem } from './types';
 export type Rmmz_PluginManager_Translation = PluginManagerTemplate<typeof PLUGIN_NAME_HONYAKU_EX, typeof PLUGIN_COMMAND_HONYAKU_SETUP_DICTIONARY, RuntimeDictionaryData<string>> & PluginManagerTemplate<typeof PLUGIN_NAME_HONYAKU_EX, typeof PLUGIN_COMMAND_HONYAKU_SETUP_SYSTEM_TEXTS, SystemTexts>;
-export declare const pluginManifestFiles: (data: RuntimeDictionaryDataWithSystem<string>, options?: RuntimePluginBundleOptions) => TextFileEntry;
+export declare const pluginManifestFiles: (data: RuntimeDictionaryDataWithSystem<string>, options: RuntimePluginBundleOptions) => TextFileEntry;
 export declare const createPluginSnapshotSetupScript: (params: ReadonlyArray<PluginManifestData>) => string;
+/**
+ * @deprecated 設計ミスにより削除予定
+ */
 export declare const createPluginManifest2: (options: RuntimePluginBundleOptions) => PluginParamsRecord[];
