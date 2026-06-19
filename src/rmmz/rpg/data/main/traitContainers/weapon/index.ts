c70d21fd0fdf9d595cc37f9eccba63e83bdc95a6
take-cheeze <takechi101010@gmail.com>
2026-06-19 19:11:06 +0900

    Fix

--- src/rmmz/rpg/data/main/traitContainers/weapon/index.ts
+++ src/rmmz/rpg/data/main/traitContainers/weapon/index.ts
@@ -1,5 +1,5 @@
-export * from "./make";
 export * from "../../../../../../validate/rmmz/rpg/weapon/schema";
+export * from "./make";
 export * from "./weapon";
 export * from "./weaponDefault";
 export * from "./weaponLabels";
