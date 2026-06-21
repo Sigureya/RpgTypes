9bd8132bc6cf348737c750ead28f1237e2ef46b2
Sigureya <sigureya1208@gmail.com>
2026-06-22 01:27:20 +0900

    不足している関数が発覚したので修正

--- src/rmmzRuntime/objects/core/battle/battler/enemy.ts
+++ src/rmmzRuntime/objects/core/battle/battler/enemy.ts
@@ -1,6 +1,12 @@
-import type { Data_AnyGoodsUnion } from "@RpgTypes/rmmz/rpg";
-import type { Rmmz_BattlerBase } from "./base";
+import type { Data_AnyGoodsUnion, Data_Enemy } from "@RpgTypes/rmmz/rpg";
+import type { Rmmz_Battler } from "./battler";
+import type { Rmmz_EnemyBase } from "./enemyBase/enemy";
 
-export interface Rmmz_Enemy extends Rmmz_BattlerBase {
+export interface Rmmz_Enemy extends Rmmz_Battler, Rmmz_EnemyBase {
   makeDropItems(): Data_AnyGoodsUnion[];
+  name(): string;
+  battlerHue(): number;
+  battlerName(): string;
+  originalName(): string;
+  enemy(): Data_Enemy;
 }
