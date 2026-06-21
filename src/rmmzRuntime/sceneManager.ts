483dcf82d3f5b325152bbcaba27f7281bc80db95
Sigureya <sigureya1208@gmail.com>
2026-06-22 02:05:53 +0900

    Sceneの方が決まっているので修正

---
+++ src/rmmzRuntime/sceneManager.ts
@@ -0,0 +1,7 @@
+import type { Rmmz_Scene_Base } from "./scene/core";
+
+export interface Rmmz_SceneManager {
+  goto(sceneClass: new () => Rmmz_Scene_Base): void;
+  push(sceneClass: new () => Rmmz_Scene_Base): void;
+  pop(): void;
+}
