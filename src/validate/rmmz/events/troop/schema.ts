53e71c15362781351bdcd3d1e1bb5425814aedf7
Sigureya <sigureya1208@gmail.com>
2026-06-23 02:36:25 +0900

    イベント実行条件の抜け落ちに対応。イベントコマンドの都合でschema定義が遠回りになっている

--- src/validate/rmmz/events/troop/schema.ts
+++ src/validate/rmmz/events/troop/schema.ts
@@ -1,55 +1,99 @@
-import type { Data_Troop } from "@RpgTypes/rmmz/events";
+import type { EventCommandUnknown } from "@RpgTypes/libs";
+import type {
+  Data_TroopUnknonw,
+  Troop_Member,
+  Troop_EventConditions,
+  BattleEventPage,
+} from "@RpgTypes/rmmz";
+import type { JSONSchemaType } from "ajv";
+
+const SCHEMA_TROOP_MEMBER_ARRAY = {
+  type: "array",
+  items: {
+    type: "object",
+    properties: {
+      enemyId: { type: "integer" },
+      x: { type: "integer" },
+      y: { type: "integer" },
+      hidden: { type: "boolean" },
+    },
+    required: ["enemyId", "x", "y", "hidden"],
+  },
+} as const satisfies JSONSchemaType<Troop_Member[]>;
+
+const SCHEMA_TROOP_EVENT_CONDITIONS = {
+  type: "object",
+  required: [
+    "actorHp",
+    "actorId",
+    "actorValid",
+
+    "enemyValid",
+    "enemyIndex",
+    "enemyHp",
+
+    "switchValid",
+    "switchId",
+
+    "turnEnding",
+    "turnValid",
+  ],
+  properties: {
+    actorValid: { type: "boolean" },
+    actorHp: { type: "integer", minimum: 0 },
+    actorId: { type: "integer", minimum: 0 },
+    enemyValid: { type: "boolean" },
+    enemyIndex: { type: "integer", minimum: 0 },
+    enemyHp: { type: "integer", minimum: 0 },
+    switchValid: { type: "boolean", minimum: 0 },
+    switchId: { type: "integer", minimum: 0 },
+    turnEnding: { type: "boolean" },
+    turnValid: { type: "boolean" },
+  },
+} satisfies JSONSchemaType<Troop_EventConditions>;
 
 export const SCHEMA_DATA_TROOP = {
   type: "object",
-  required: ["id", "name", "members", "pages"] satisfies (keyof Data_Troop)[],
+  required: [
+    "id",
+    "name",
+    "members",
+    "pages",
+  ] satisfies (keyof Data_TroopUnknonw)[],
   properties: {
     id: { type: "integer", minimum: 0 },
     name: { type: "string" },
-    members: {
-      type: "array",
-      items: {
-        type: "object",
-        required: ["enemyId", "x", "y", "hidden"],
-        properties: {
-          enemyId: { type: "integer", minimum: 0 },
-          x: { type: "integer" },
-          y: { type: "integer" },
-          hidden: { type: "boolean" },
-        },
-      },
-    },
+    members: SCHEMA_TROOP_MEMBER_ARRAY,
     pages: {
       type: "array",
       items: {
         type: "object",
-        required: ["conditions", "list", "span"],
+        additionalProperties: true,
+        required: ["conditions", "span"],
         properties: {
-          conditions: {
-            type: "object",
-            required: ["actorHp", "actorId", "enemyValid", "switchValid"],
-            properties: {
-              actorHp: { type: "integer", minimum: 0 },
-              actorId: { type: "integer", minimum: 0 },
-              enemyValid: { type: "integer", minimum: 0 },
-              switchValid: { type: "integer", minimum: 0 },
-            },
-          },
+          conditions: SCHEMA_TROOP_EVENT_CONDITIONS,
+          span: { type: "number" },
           list: {
             type: "array",
             items: {
               type: "object",
-              required: ["code", "indent", "parameters"],
+              additionalProperties: true,
+              required: [
+                "code",
+                "indent",
+                "parameters",
+              ] satisfies (keyof EventCommandUnknown)[],
               properties: {
                 code: { type: "integer" },
                 indent: { type: "integer" },
-                parameters: { type: "array" },
+                parameters: {
+                  type: "array",
+                },
               },
             },
           },
-          span: { type: "integer", minimum: 0 },
         },
-      },
+      } satisfies JSONSchemaType<Omit<BattleEventPage, "list">>,
     },
   },
 } as const;
