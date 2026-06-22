5ec44fb5abc8f1fc852cc425923a51a96de68615
Sigureya <sigureya1208@gmail.com>
2026-06-23 03:15:18 +0900

    色々あって今は使ってない。削除してOK

--- src/validate/rmmz/events/troop/isDataTroop.ts
+++
@@ -1,6 +0,0 @@
-import type { Data_Troop } from "@RpgTypes/rmmz/events";
-import validate from "./troopValidate";
-
-export const isDataTroop = (data: unknown): data is Data_Troop => {
-  return validate(data);
-};
