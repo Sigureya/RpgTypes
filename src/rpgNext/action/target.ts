d420cb1b7e3ceb844fc4bcc6942b2907b8bb9c48
Sigureya <sigureya1208@gmail.com>
2026-06-23 19:54:58 +0900

    ターゲット指定の調整。主語が難になるか調整している

--- src/rpgNext/action/target.ts
+++ src/rpgNext/action/target.ts
@@ -1,14 +1,47 @@
 import type {
   Rmmz_Action,
   Rmmz_Battler,
-  Rmmz_Unit,
+  Rmmz_BattlerContainer,
 } from "@RpgTypes/rmmzRuntime";
 
-export const actionDecideRandomTarget = (
-  action: Rmmz_Action,
-  friendsUnit: Rmmz_Unit,
-  opponentsUnit: Rmmz_Unit,
+export const battlresRandomTarget = (
+  battlers: ReadonlyArray<Rmmz_Battler>,
+  randomValue: number,
 ): Rmmz_Battler | null => {
+  const tgrSum = battlers.reduce((r, b) => r + b.tgr, 0);
+  // eslint-disable-next-line @functional/no-let
+  let targetTgr = randomValue * tgrSum;
+  // eslint-disable-next-line @functional/no-loop-statements
+  for (const battler of battlers) {
+    targetTgr -= battler.tgr;
+    if (targetTgr <= 0) {
+      return battler;
+    }
+  }
+  return null;
+};
+
+export const battlersRandomDeadTarget = (
+  battlers: ReadonlyArray<Rmmz_Battler>,
+  randomValue: number,
+): Rmmz_Battler | null => {
+  const b2 = battlers.filter((b) => b.isDead());
+  return battlresRandomTarget(b2, randomValue);
+};
+
+export const battlersRandomAliveTarget = (
+  battlers: ReadonlyArray<Rmmz_Battler>,
+  randomValue: number,
+): Rmmz_Battler | null => {
+  const b2 = battlers.filter((b) => b.isAlive());
+  return battlresRandomTarget(b2, randomValue);
+};
+
+export const actionDecideRandomTarget = <Battler>(
+  action: Rmmz_Action,
+  friendsUnit: Rmmz_BattlerContainer<Battler>,
+  opponentsUnit: Rmmz_BattlerContainer<Battler>,
+): Battler | null => {
   if (action.isForDeadFriend()) {
     return friendsUnit.randomDeadTarget();
   }
