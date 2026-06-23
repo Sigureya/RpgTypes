faf18b5de8891b92c1c0e13c6f1f7e80ab3e2032
Sigureya <sigureya1208@gmail.com>
2026-06-23 11:14:55 +0900

    一部の関数定義を別の場所に移動。これもテストの簡略化用

--- src/rmmzRuntime/objects/core/battle/action/types.ts
+++ src/rmmzRuntime/objects/core/battle/action/types.ts
@@ -3,11 +3,17 @@ import type {
   Enemy_Action,
   ItemEffect,
 } from "@RpgTypes/rmmz/rpg";
+import type { Rmmz_Action_Damage } from "./damage";
 import type { Rmmz_Action_ItemEffect } from "./itemEffect";
+import type { Rmmz_Action_Target } from "./target";
 import type { Rmmz_Action_Trait } from "./trait";
 
 export interface Rmmz_ActionBase<Battler, Team>
-  extends Rmmz_Action_ItemEffect<Battler>, Rmmz_Action_Trait {
+  extends
+    Rmmz_Action_ItemEffect<Battler>,
+    Rmmz_Action_Trait,
+    Rmmz_Action_Damage<Battler>,
+    Rmmz_Action_Target<Battler, Team> {
   // initialize(subject: IBattler, forcing: boolean): void;
   clear(): void;
   setSubject(subject: Battler): void;
@@ -30,16 +36,6 @@ export interface Rmmz_ActionBase<Battler, Team>
   prepare(): void;
   isValid(): boolean;
   speed(): number;
-  makeTargets(): Battler[];
-  repeatTargets(targets: ReadonlyArray<Battler>): Battler[];
-  confusionTarget(): Battler;
-  targetsForEveryone(): Battler[];
-  targetsForOpponents(): Battler[];
-  targetsForFriends(): Battler[];
-  randomTargets(unit: Team): Battler[];
-  targetsForDead(unit: Team): Battler[];
-  targetsForAlive(unit: Team): Battler[];
-  targetsForDeadAndAlive(unit: Team): Battler[];
   evaluate(): number;
   itemTargetCandidates(): number;
   evaluateWithTarget(target: Battler): number;
@@ -53,17 +49,4 @@ export interface Rmmz_ActionBase<Battler, Team>
   itemEva(target: Battler): number;
   itemCri(target: Battler): number;
   apply(target: Battler): void;
-  makeDamageValue(target: Battler, critical: boolean): number;
-  evalDamageFormula(target: Battler): number;
-  calcElementRate(target: Battler): number;
-  elementsMaxRate(target: Battler, elements: ReadonlyArray<number>): number;
-  applyCritical(damage: number): number;
-  applyVariance(damage: number, variance: number): number;
-  applyGuard(damage: number, target: Battler): number;
-  executeDamage(target: Battler, value: number): void;
-  executeHpDamage(target: Battler, value: number): void;
-  executeMpDamage(target: Battler, value: number): void;
-  gainDrainedHp(value: number): void;
-  gainDrainedMp(value: number): void;
-  makeSuccess(target: Battler): void;
 }
