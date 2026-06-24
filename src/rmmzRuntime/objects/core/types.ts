435f82ae61b9e660d9597ad82256efc18915802d
Sigureya <sigureya1208@gmail.com>
2026-06-25 03:40:55 +0900

    定義方法を変更。コンストラクタで頑張っていく

--- src/rmmzRuntime/objects/core/types.ts
+++ src/rmmzRuntime/objects/core/types.ts
@@ -9,10 +9,13 @@ import type {
 import type { InterpreterMapper, Rmmz_EventCommandRunner } from "./interpreter";
 import type { Rmmz_InterpreterBattler } from "./interpreter/types/actor";
 
-export interface Rmmz_Action extends Rmmz_ActionBase<
+export interface Rmmz_Action<Subject = Rmmz_Battler> extends Rmmz_ActionBase<
   Rmmz_Battler,
   Rmmz_Unit<Rmmz_Battler>
-> {}
+> {
+  setSubject(subject: Subject): void;
+  subject(): Subject;
+}
 
 export interface Rmmz_Interpreter
   extends
