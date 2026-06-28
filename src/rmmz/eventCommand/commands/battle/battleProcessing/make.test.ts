d68c0ea197eb801e07fef1df7224c4f82a81323c
Sigureya <sigureya1208@gmail.com>
2026-06-29 06:41:23 +0900

    import修正

--- src/rmmz/eventCommand/commands/battle/battleProcessing/make.test.ts
+++ src/rmmz/eventCommand/commands/battle/battleProcessing/make.test.ts
@@ -6,12 +6,12 @@ import {
   makeCommandBattleProcessingVariable,
 } from "./make";
 import type {
+  ParamObject_BattleProcessing_Direct,
   Command_BattleProcessing,
+  ParamObject_BattleProcessing_Variable,
   Command_BattleProcessing_Variable,
   ParamObject_BattleBase,
-  ParamObject_BattleProcessing_Direct,
-  ParamObject_BattleProcessing_Variable,
-} from "./types2/startBattle";
+} from "./types";
 
 describe("makeCommandBattleProcessingDirect", () => {
   const params: ParamObject_BattleProcessing_Direct = {
