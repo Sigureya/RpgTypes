3a55e31bf1d089b39bf42a1ef891b78e8d4a79ff
Sigureya <sigureya1208@gmail.com>
2026-06-28 21:28:10 +0900

    選択肢ブロックの作成。戦闘も部分的に用意

--- src/rmmz/eventCommand/commands/indentBlock.ts
+++ src/rmmz/eventCommand/commands/indentBlock.ts
@@ -1,7 +1,31 @@
-import type { EventCommandUnknown } from "@RpgTypes/libs";
-import { CONDITIONAL_BRANCH_ELSE } from "@RpgTypes/libs";
+import type {
+  EventCommandUnknown,
+  EventCommanNoOperation,
+} from "@RpgTypes/libs";
+import { CONDITIONAL_BRANCH_ELSE, NO_OPERATION } from "@RpgTypes/libs";
+import type {
+  Command_BattleProcessing,
+  Command_BattleProcessing_Variable,
+} from "./battle";
 import type { CommnandUnion_Branch } from "./flow/branch/types/command";
 import type { Command_BranchElse } from "./flow/branch/types/else";
+import type {
+  Command_ShowChoices,
+  Command_ShowChoiceWhen,
+  ParamObject_SetupChoiceOptions,
+} from "./message";
+import { makeCommandSetupChoice, makeCommandShowChoiceItem } from "./message";
+
+export interface ChoiceThenBlock<Command extends EventCommandUnknown> {
+  thenBlock: Command[];
+  choiceText: string;
+}
+
+export interface BattleBlocks<Command extends EventCommandUnknown> {
+  escape: Command[];
+  lose: Command[];
+  win: Command[];
+}
 
 export const makeComamndBlockBranch = <Command extends EventCommandUnknown>(
   branch: CommnandUnion_Branch,
@@ -19,6 +43,55 @@ export const makeComamndBlockBranch = <Command extends EventCommandUnknown>(
 export const makeCommandBracnhElse = (indent: number): Command_BranchElse => {
   return {
     code: CONDITIONAL_BRANCH_ELSE,
+    indent,
+    parameters: [],
+  };
+};
+
+const makeCommandBlockByBattle = <Command extends EventCommandUnknown>(
+  battle: Command_BattleProcessing | Command_BattleProcessing_Variable,
+  blocks: BattleBlocks<Command>,
+) => {};
+
+export const makeCommandBlockChoice = <Command extends EventCommandUnknown>(
+  indent: number,
+  options: Partial<ParamObject_SetupChoiceOptions>,
+  choices: readonly ChoiceThenBlock<Command>[],
+): (
+  | Command
+  | Command_ShowChoices
+  | Command_ShowChoiceWhen
+  | Command_BranchElse
+  | EventCommanNoOperation
+)[] => {
+  const c: Command_ShowChoices = makeCommandSetupChoice(
+    {
+      background: options.background,
+      cancelType: options.cancelType,
+      defaultType: options.defaultType,
+      positionType: options.positionType,
+      choices: choices.map((c) => c.choiceText),
+    },
+    indent,
+  );
+  return [c, ...xxx(indent, choices), makeNope(indent)];
+};
+
+const xxx = <T extends EventCommandUnknown>(
+  indent: number,
+  cx: readonly ChoiceThenBlock<T>[],
+): (T | Command_ShowChoiceWhen)[] => {
+  return cx.flatMap((c, index) => {
+    return [
+      makeCommandShowChoiceItem({ index, name: c.choiceText }, indent),
+      ...c.thenBlock,
+    ];
+  });
+};
+
+const makeNope = (indent: number): EventCommanNoOperation => {
+  return {
+    code: NO_OPERATION,
     indent,
     parameters: [],
   };
