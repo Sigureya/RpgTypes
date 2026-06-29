889ad1cdaee71f1e402e4fc04af2b3852d5953ac
Sigureya <sigureya1208@gmail.com>
2026-06-29 10:28:26 +0900

    methodの移動。テストコードを書くうちに必要性が判明している

--- dist/types/rmmzRuntime/managers/battle/interface/process.d.ts
+++ dist/types/rmmzRuntime/managers/battle/interface/process.d.ts
@@ -1,3 +1,7 @@
+export declare const BATTLE_RESULT_WIN = 0;
+export declare const BATTLE_RESULT_ESCAPE = 1;
+export declare const BATTLE_RESULT_LOSE = 2;
+export type BattleResult = typeof BATTLE_RESULT_WIN | typeof BATTLE_RESULT_ESCAPE | typeof BATTLE_RESULT_LOSE;
 export interface Process {
     processAbort(): void;
     rocessPartyEscape(): void;
@@ -6,4 +10,6 @@ export interface Process {
     onEscapeSuccess(): void;
     processEscape(): void;
     processVictory(): void;
+    endBattle(result: BattleResult): void;
+    setEventCallback(callback: (result: BattleResult) => void): void;
 }
--- dist/types/rmmzRuntime/sceneManager.d.ts
+++ dist/types/rmmzRuntime/sceneManager.d.ts
@@ -3,4 +3,5 @@ export interface Rmmz_SceneManager {
     goto(sceneClass: new () => Rmmz_Scene_Base): void;
     push(sceneClass: new () => Rmmz_Scene_Base): void;
     pop(): void;
+    isSceneChanging(): boolean;
 }
