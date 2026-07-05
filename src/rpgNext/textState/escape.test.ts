9b21e6e6d26bbeac2640cd3a8afd9a9fcf83994d
Sigureya <sigureya1208@gmail.com>
2026-07-06 06:20:29 +0900

    呼び出し検証部分の修正。置き換え範囲を読みやすくしておく

--- src/rpgNext/textState/escape.test.ts
+++ src/rpgNext/textState/escape.test.ts
@@ -12,6 +12,7 @@ import {
 interface TestCase {
   input: string;
   expected: string;
+  expected2: string;
   calls: {
     ctrl: string;
     value: number;
@@ -43,31 +44,32 @@ const mockParty: Rmmz_Members<{ name: () => string }> = {
   members: () => [{ name: () => "Bob" }, { name: () => "Alice" }],
 };
 
+const newTextFn = (ctrl: string, value: number): string | undefined => {
+  if (ctrl === "V") {
+    return `${value * 2}`;
+  }
+  if (ctrl === "N") {
+    if (value === 1) {
+      return "Alice";
+    }
+    if (value === 2) {
+      return "Bob";
+    }
+  }
+};
 const runTestCase = (testCases: TestCase) => {
   describe(testCases.input, () => {
     test("calls", () => {
       const fn = vi.fn((ctrl, value) => `@${ctrl}:${value}`);
-      convertEscapeCharacters(testCases.input, fn);
+      const result = convertEscapeCharacters(testCases.input, fn);
       testCases.calls.forEach((call) => {
         const msg = `${call.ctrl}(${call.value}) should be called`;
         expect(fn, msg).toHaveBeenCalledWith(call.ctrl, call.value);
       });
+      expect(result).toBe(testCases.expected2);
     });
     test("should convert escape characters correctly", () => {
-      const result = convertEscapeCharacters(testCases.input, (ctrl, value) => {
-        if (ctrl === "V") {
-          return `${value * 2}`;
-        }
-        if (ctrl === "N") {
-          if (value === 1) {
-            return "Alice";
-          }
-          if (value === 2) {
-            return "Bob";
-          }
-        }
-        return `@${ctrl}:${value}`;
-      });
+      const result = convertEscapeCharacters(testCases.input, newTextFn);
       expect(result).toBe(testCases.expected);
     });
     test("mz style", () => {
@@ -86,17 +88,27 @@ const testCases: TestCase[] = [
   {
     input: "hello",
     expected: "hello",
+    expected2: "hello",
     calls: [],
   },
   {
     input: "My name is \\N[1]",
     expected: "My name is Alice",
+    expected2: "My name is @N:1",
+
     calls: [{ ctrl: "N", value: 1 }],
   },
   {
     input: "price is \\V[124] gold",
     expected: "price is 248 gold",
+    expected2: "price is @V:124 gold",
     calls: [{ ctrl: "V", value: 124 }],
+  },
+  {
+    input: "price is \\v[321] gold",
+    expected: "price is 642 gold",
+    expected2: "price is @V:321 gold",
+    calls: [{ ctrl: "V", value: 321 }],
   },
 ];
 
