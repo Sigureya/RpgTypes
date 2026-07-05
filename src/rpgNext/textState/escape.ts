388c2891e462bf6f086a031ca630f7f52c5900a6
Sigureya <sigureya1208@gmail.com>
2026-07-06 06:19:55 +0900

    エディタで読みやすくするための微修正

--- src/rpgNext/textState/escape.ts
+++ src/rpgNext/textState/escape.ts
@@ -10,8 +10,12 @@ interface HasName {
 
 export const convertEscapeCharactersMzStyle = (
   text: string,
-  actors: Rmmz_ActorsReadonly<HasName>,
-  party: Rmmz_Members<HasName>,
+  actors: Rmmz_ActorsReadonly<{
+    name(): string;
+  }>,
+  party: Rmmz_Members<{
+    name(): string;
+  }>,
   variables: Rmmz_Variables,
 ): string => {
   return convertEscapeCharacters(text, (ctrl, value) =>
@@ -48,21 +52,23 @@ export const convertEscapeCharacters = (
 ): string => {
   const ttx = text.replace(/\\/g, "\x1b").replace(/\x1b\x1b/g, "\\");
   const vex = replaceVariableText(ttx, fn);
-  return replaceXXX(vex, fn);
+  return replaceName(vex, fn);
 };
 
-const replaceXXX = (
+const replaceName = (
   text: string,
   fn: (key: string, value: number) => string | undefined,
 ): string => {
-  return text.matchAll(/\x1b([NP])\[(\d+)\]/gi).reduce((acc, match): string => {
-    const p1 = parseInt(match[2]);
-    const vText = fn(match[1], p1);
-    if (vText === undefined) {
-      return acc;
-    }
-    return acc.replaceAll(match[0], vText);
-  }, text);
+  return text
+    .matchAll(/\x1b([A-Z]{1,16})\[(\d+)\]/gi)
+    .reduce((acc, match): string => {
+      const p1 = parseInt(match[2]);
+      const vText = fn(match[1], p1);
+      if (vText === undefined) {
+        return acc;
+      }
+      return acc.replaceAll(match[0], vText);
+    }, text);
 };
 
 const replaceVariableText = (
