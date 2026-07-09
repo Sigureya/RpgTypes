5d6109089f1bc1a8568845dd29bca6593aa46dd5
Sigureya <sigureya1208@gmail.com>
2026-07-10 06:32:22 +0900

    初期状態の作成はflushの処理と関連性が高い。まとめる

--- src/rpgNext/window/textState/flush.ts
+++ src/rpgNext/window/textState/flush.ts
@@ -1,5 +1,45 @@
 import type { Rmmz_TextState, Rmmz_Bitmap } from "@RpgTypes/rmmzRuntime";
-import { createTextBuffer } from "./ctrl";
+import {
+  BUFFER_INITIAL_TEXT_RTL_TURE,
+  BUFFER_INITIAL_TEXT_RTL_FALSE,
+} from "./constants";
+import { convertEscapeCharacters } from "./escape";
+
+const containsArabic = function (str: string): boolean {
+  const regExp = /[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF]/;
+  return regExp.test(str);
+};
+
+export const createTextBuffer = (rtl: boolean) => {
+  return rtl ? BUFFER_INITIAL_TEXT_RTL_TURE : BUFFER_INITIAL_TEXT_RTL_FALSE;
+};
+
+export const createTextState = (
+  text: string,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  variableFn: (valiableId: number) => string | number,
+  textFn: (ctrl: string, value: number) => string | undefined,
+): Rmmz_TextState => {
+  const rtl = containsArabic(text);
+  return {
+    buffer: createTextBuffer(rtl),
+    rtl: rtl,
+    text: convertEscapeCharacters(text, variableFn, textFn),
+    index: 0,
+    x: x,
+    y: y,
+    width: width,
+    height: height,
+    startX: x,
+    startY: y,
+    drawing: true,
+    outputHeight: 0,
+    outputWidth: 0,
+  };
+};
 
 export const flashTextState = (
   textState: Rmmz_TextState,
