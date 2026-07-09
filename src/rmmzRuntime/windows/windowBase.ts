ba9252e3979da305dcee2023ee511fba3adb06e8
Sigureya <sigureya1208@gmail.com>
2026-07-09 14:19:41 +0900

    制御文字対応の関係でコールバック用ハンドラが必要

--- src/rmmzRuntime/windows/windowBase.ts
+++ src/rmmzRuntime/windows/windowBase.ts
@@ -1,9 +1,20 @@
-export interface Rmmz_Window_Base {
+import type { Rmmz_TextState } from "./textState";
+export interface Rmmz_Window_Base_ContrlolChar {
+  drawIcon(iconIndex: number, x: number, y: number): void;
+  makeFontBigger(): void;
+  makeFontSmaller(): void;
+}
+
+export interface Rmmz_Window_Base extends Rmmz_Window_Base_ContrlolChar {
   lineHeight(): number;
   get contents(): Rmmz_Contents;
   convertEscapeCharacters(text: string): string;
   actorName(n: number): string;
   partyMemberName(n: number): string;
+  processNewLine(textState: Rmmz_TextState): void;
+  obtainEscapeCode(textState: Rmmz_TextState): string;
+  obtainEscapeParam(textState: Rmmz_TextState): number | "";
+  processEscapeCharacter(code: string, textState: Rmmz_TextState): void;
 }
 
 export interface Rmmz_Contents {
