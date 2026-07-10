2d41de9c52786c571d6d2f7bf06104c69e136785
Sigureya <sigureya1208@gmail.com>
2026-07-10 23:26:24 +0900

    Trait関連を追加。テストで必要になったため

---
+++ src/rmmzRuntime/objects/core/battle/battler/base/trait.ts
@@ -0,0 +1,14 @@
+import type { Trait } from "@RpgTypes/rmmz/rpg";
+
+export interface Rmmz_TraitContainer {
+  traits(code: number): Trait[];
+  allTraits(): Trait[];
+  traitsWithId(code: number, id: number): Trait[];
+  traitsPi(code: number, id: number): number;
+  traitsSum(code: number, id: number): number;
+  traitsSumAll(code: number): number;
+  traitsSet(code: number): boolean;
+  elementRate(elementId: number): number;
+  debuffRate(paramId: number): number;
+  stateRate(stateId: number): number;
+}
