1d843702098d841b349b25262bb883a309123df0
Sigureya <sigureya1208@gmail.com>
2026-07-13 22:21:23 +0900

    パラメータの合計値を計算する関数。バトルで使う

--- src/rmmz/rpg/data/main/traitContainers/equipment.ts
+++ src/rmmz/rpg/data/main/traitContainers/equipment.ts
@@ -1,4 +1,8 @@
-import type { ParamArray } from "./members";
+import {
+  makeParamArray,
+  type ParamArray,
+  type StatusParamObject,
+} from "./members";
 import type { Data_TraitContainer, Trait } from "./trait";
 
 export interface Data_Equipment extends Data_TraitContainer {
@@ -12,3 +16,29 @@ export interface Data_Equipment extends Data_TraitContainer {
   note: string;
   etypeId: number;
 }
+
+export const calcEquipParamsArray = (
+  equips: ReadonlyArray<Data_Equipment>,
+): ParamArray => {
+  const obj = calcEquipParamsObject(equips);
+  return makeParamArray(obj);
+};
+
+export const calcEquipParamsObject = (
+  equips: ReadonlyArray<Data_Equipment>,
+): StatusParamObject => {
+  return equips.reduce<StatusParamObject>(
+    (acc: StatusParamObject, equip): StatusParamObject => {
+      acc.mhp += equip.params[0];
+      acc.mmp += equip.params[1];
+      acc.atk += equip.params[2];
+      acc.def += equip.params[3];
+      acc.mat += equip.params[4];
+      acc.mdf += equip.params[5];
+      acc.agi += equip.params[6];
+      acc.luk += equip.params[7];
+      return acc;
+    },
+    { mhp: 0, mmp: 0, atk: 0, def: 0, mat: 0, mdf: 0, agi: 0, luk: 0 },
+  );
+};
