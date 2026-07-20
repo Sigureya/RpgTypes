f27c227125a2f797dea5ac6b3b185cdede64608e
Sigureya <sigureya1208@gmail.com>
2026-07-21 04:10:32 +0900

    xparam対応。フルネームにしてある

---
+++ src/rmmz/rpg/data/main/traitContainers/trait/core/constants/xparam.ts
@@ -0,0 +1,11 @@
+export const XPARAM_HIT_RATE = 0 as const;
+export const XPARAM_EVA_RATE = 1 as const;
+export const XPARAM_CRI_CRITICAL_RATE = 2 as const;
+export const XPARAM_CEV_CRITICAL_EVASION_RATE = 3 as const;
+export const XPARAM_MCR_MAGIC_EVASION_RATE = 4 as const;
+export const XPARAM_MRF_MAGIC_REFLECTION_RATE = 5 as const;
+
+export const XPARAM_CNT_COUNTER_ATTACK_RATE = 6 as const;
+export const XPARAM_HRG_HP_REGEN_RATE = 7 as const;
+export const XPARAM_MRG_MP_REGEN_RATE = 8 as const;
+export const XPARAM_TRG_TP_REGEN_RATE = 9 as const;
