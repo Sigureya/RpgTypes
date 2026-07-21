989950841f738519f4e4ac6f08ceedf3feaf647b
Sigureya <sigureya1208@gmail.com>
2026-07-22 07:39:01 +0900

    パラメータの計算関数

---
+++ src/rmmz/rpg/data/main/traitContainers/battlerParam.ts
@@ -0,0 +1,69 @@
+import type { Data_Class } from "./class";
+import type { Data_Enemy } from "./enemy";
+import type { ParamArray } from "./members";
+import type { Trait } from "./trait";
+import { traitParamRate } from "./trait";
+
+export interface ParamCalcOptions {
+  buffStep: number;
+  buffXX: number;
+}
+
+export const calcParamFromClass = (
+  paramId: number,
+  class_: Data_Class,
+  level: number,
+  traits: ReadonlyArray<Trait>,
+  paramPlus: ParamArray,
+  buffs: ParamArray,
+  option: ParamCalcOptions,
+): number => {
+  const baseValue = classBaseValue(paramId, class_, level);
+  const paramPlusValue = paramPlus[paramId];
+  const paramRate = traitParamRate(traits, paramId);
+  const buffRate = paramBuffRate(paramId, buffs, option);
+  return (baseValue + paramPlusValue) * paramRate * buffRate;
+};
+
+export const classBaseValue = (
+  paramId: number,
+  class_: Data_Class,
+  level: number,
+): number => {
+  const baseParamarray = class_.params[paramId];
+  if (baseParamarray === undefined) {
+    return 0;
+  }
+  const clampedLevel = Math.max(0, Math.min(level, baseParamarray.length - 1));
+  return baseParamarray[clampedLevel];
+};
+
+export const calcParamFromEnemyData = (
+  paramId: number,
+  enemy: Data_Enemy,
+  traits: ReadonlyArray<Trait>,
+  paramPlus: ParamArray,
+  buffs: ParamArray,
+  option: ParamCalcOptions,
+): number => {
+  const basePlus = enemyParamBasePlus(paramId, enemy, paramPlus);
+  const paramRate = traitParamRate(traits, paramId);
+  const buff = paramBuffRate(paramId, buffs, option);
+  return basePlus * paramRate * buff;
+};
+
+const enemyParamBasePlus = (
+  paramId: number,
+  enemy: Data_Enemy,
+  paramPlus: ParamArray,
+): number => {
+  return Math.max(0, enemy.params[paramId] + paramPlus[paramId]);
+};
+
+const paramBuffRate = (
+  paramId: number,
+  buffs: ParamArray,
+  opt: ParamCalcOptions,
+): number => {
+  return buffs[paramId] * opt.buffStep + opt.buffXX;
+};
