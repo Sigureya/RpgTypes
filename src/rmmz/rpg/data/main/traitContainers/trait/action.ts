d8c2fb82689add88db9a8acc472d7e9b2d0c61db
Sigureya <sigureya1208@gmail.com>
2026-07-22 20:36:22 +0900

    trait関連を少し追加

--- src/rmmz/rpg/data/main/traitContainers/trait/action.ts
+++ src/rmmz/rpg/data/main/traitContainers/trait/action.ts
@@ -1,8 +1,27 @@
-import { TRAIT_ACTION_PLUS } from "./core";
+import {
+  FLAG_ID_AUTO_BATTLE,
+  FLAG_ID_GUARD,
+  TRAIT_ACTION_PLUS,
+  TRAIT_SPECIAL_FLAG,
+} from "./core";
 import type { Trait } from "./types";
 
 export const traitActionPlusSet = (traits: ReadonlyArray<Trait>): number[] => {
   return traits
     .filter((trait) => trait.code === TRAIT_ACTION_PLUS)
     .map((trait) => trait.value);
+};
+
+export const traitIsAutoBattle = (traits: ReadonlyArray<Trait>): boolean => {
+  return traits.some(
+    (trait) =>
+      trait.code === TRAIT_SPECIAL_FLAG && trait.value === FLAG_ID_AUTO_BATTLE,
+  );
+};
+
+export const traitIsGuardTrait = (traits: ReadonlyArray<Trait>): boolean => {
+  return traits.some(
+    (trait) =>
+      trait.code === TRAIT_SPECIAL_FLAG && trait.value === FLAG_ID_GUARD,
+  );
 };
