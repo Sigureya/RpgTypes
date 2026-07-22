6a7cf2ba95d7ab83bb34c1b1ae0e4da9d53bfd26
Sigureya <sigureya1208@gmail.com>
2026-07-22 18:30:31 +0900

    攻撃関連のtrait

---
+++ src/rmmz/rpg/data/main/traitContainers/trait/attack.ts
@@ -0,0 +1,39 @@
+import {
+  TRAIT_ATTACK_ELEMENT,
+  TRAIT_ATTACK_SKILL,
+  TRAIT_ATTACK_SPEED,
+  TRAIT_ATTACK_STATE,
+  TRAIT_ATTACK_TIMES,
+} from "./core";
+import { traitSet, traitSum, traitSumAll } from "./trait";
+import type { Trait } from "./types";
+
+export const traitAttackElements = (traits: ReadonlyArray<Trait>): number[] => {
+  return traitSet(traits, TRAIT_ATTACK_ELEMENT);
+};
+
+export const traitAttackStateRate = (
+  traits: ReadonlyArray<Trait>,
+  stateId: number,
+): number => {
+  return traitSum(traits, TRAIT_ATTACK_STATE, stateId);
+};
+
+export const traitAtttackSpeed = (traits: ReadonlyArray<Trait>): number => {
+  return traitSumAll(traits, TRAIT_ATTACK_SPEED);
+};
+
+export const traitAttackTimesAdd = (traits: ReadonlyArray<Trait>): number => {
+  return Math.max(0, traitSumAll(traits, TRAIT_ATTACK_TIMES));
+};
+
+export const traitAttackSkillId = (traits: ReadonlyArray<Trait>): number => {
+  return traits.reduce(skillIdAcc, 1);
+};
+
+const skillIdAcc = (skillId: number, trait: Trait): number => {
+  if (trait.code === TRAIT_ATTACK_SKILL) {
+    return Math.max(skillId, trait.dataId);
+  }
+  return skillId;
+};
