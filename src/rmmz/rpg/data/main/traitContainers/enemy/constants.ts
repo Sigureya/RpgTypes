6fd62ea838c8a1563c467db9bbb4f241b14969fa
Sigureya <sigureya1208@gmail.com>
2026-07-23 05:08:22 +0900

    敵の行動条件に使う定数

---
+++ src/rmmz/rpg/data/main/traitContainers/enemy/constants.ts
@@ -0,0 +1,7 @@
+export const ENEMY_ACTION_CONDITION_NONE = 0 as const;
+export const ENEMY_ACTION_CONDITION_TURN = 1 as const;
+export const ENEMY_ACTION_CONDITION_HP = 2 as const;
+export const ENEMY_ACTION_CONDITION_MP = 3 as const;
+export const ENEMY_ACTION_CONDITION_STATE = 4 as const;
+export const ENEMY_ACTION_CONDITION_PARTY_LEVEL = 5 as const;
+export const ENEMY_ACTION_CONDITION_SWITCH = 6 as const;
