98f3866db893f9f49e89ec11cd4d97f9c2bda9fc
Sigureya <sigureya1208@gmail.com>
2026-07-23 13:03:46 +0900

    EnemyActionの実動作。ほぼ同じ機能を純粋関数で表現している

--- src/rpgNext/action/enemyAction.ts
+++ src/rpgNext/action/enemyAction.ts
@@ -1,3 +1,4 @@
+import type { Data_Skill, Data_State, Enemy_Action } from "@RpgTypes/rmmz";
 import {
   ENEMY_ACTION_CONDITION_HP_RATE,
   ENEMY_ACTION_CONDITION_MP_RATE,
@@ -5,13 +6,47 @@ import {
   ENEMY_ACTION_CONDITION_STATE,
   ENEMY_ACTION_CONDITION_SWITCH,
   ENEMY_ACTION_CONDITION_TURN,
-  enemyActionMeetsConditionPointRate,
-  enemyActionMeetsConditionTurn,
-  enemyActionMeetsCondtionState,
-  type Enemy_Action,
+  filterUsableSkillsEx,
 } from "@RpgTypes/rmmz";
-import type { Rmmz_Enemy, Rmmz_Party } from "@RpgTypes/rmmzRuntime";
-import type { Rmmz_Switches } from "@RpgTypes/rmmzRuntime/objects/core/variables";
+import type {
+  Rmmz_Enemy,
+  Rmmz_Party,
+  Rmmz_Switches,
+} from "@RpgTypes/rmmzRuntime";
+
+export const filterUsableEnemyActions = (
+  actions: ReadonlyArray<Enemy_Action>,
+  battler: Rmmz_Enemy,
+  party: Rmmz_Party,
+  switches: Rmmz_Switches,
+  skillFn: (action: Enemy_Action) => Data_Skill | null | undefined,
+): Enemy_Action[] => {
+  return filterUsableSkillsEx(
+    actions,
+    battler.allTraits(),
+    { mp: battler.mp, tp: battler.tp }, // この2個しか使わないと明言する。オブジェクト1個の作成なのでコストも小さい
+    (act): Data_Skill | null | undefined => {
+      if (!enemyActionMeetsCondition(act, battler, party, switches)) {
+        // 事前に枝刈り
+        return null;
+      }
+      return skillFn(act);
+    },
+  );
+};
+
+export const filterEnemyActionByRating = (
+  actions: ReadonlyArray<Enemy_Action>,
+  ratingDistance: number = 3, // デフォルト値はRPGツクールMZの仕様に合わせる
+): Enemy_Action[] => {
+  const maxRating: number = findMaxRating(actions);
+  const ratingZero: number = maxRating - ratingDistance;
+  return actions.filter((action) => action.rating > ratingZero);
+};
+
+const findMaxRating = (actions: ReadonlyArray<Enemy_Action>): number => {
+  return actions.reduce((max, action) => Math.max(max, action.rating), 0);
+};
 
 export const enemyActionMeetsCondition = (
   action: Enemy_Action,
@@ -29,7 +64,7 @@ export const enemyActionMeetsCondition = (
     case ENEMY_ACTION_CONDITION_PARTY_LEVEL:
       return enemyActionMeetsConditionHighestLevel(action, party);
     case ENEMY_ACTION_CONDITION_STATE:
-      return enemyActionMeetsCondtionState(action, enemy.states());
+      return enemyActionMeetsConditionState(action, enemy.states());
     case ENEMY_ACTION_CONDITION_SWITCH:
       return enemyActionMeetsConditionSwitch(action, switches);
   }
@@ -48,4 +83,33 @@ const enemyActionMeetsConditionSwitch = (
   switches: Rmmz_Switches,
 ): boolean => {
   return switches.value(action.conditionParam1);
+};
+
+const enemyActionMeetsConditionTurn = (
+  { conditionParam1: param1, conditionParam2: param2 }: Enemy_Action,
+  turnCount: number,
+): boolean => {
+  if (param2 === 0) {
+    return turnCount === param1;
+  } else {
+    return (
+      turnCount > 0 &&
+      turnCount >= param1 &&
+      turnCount % param2 === param1 % param2
+    );
+  }
+};
+// 1つの関数の引数は可能な限り異なる型にする方針による実装
+const enemyActionMeetsConditionPointRate = (
+  action: Enemy_Action,
+  point: number,
+): boolean => {
+  return point >= action.conditionParam1 && point <= action.conditionParam2;
+};
+
+const enemyActionMeetsConditionState = (
+  action: Enemy_Action,
+  states: ReadonlyArray<Data_State>,
+): boolean => {
+  return states.some((state) => state.id === action.conditionParam1);
 };
