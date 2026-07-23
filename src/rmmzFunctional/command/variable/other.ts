55715bf77c52e3d9b3238f691a1f2bc43598d83c
Sigureya <sigureya1208@gmail.com>
2026-07-23 22:20:41 +0900

    その他データの取得。変更の影響が大きいため、FromSystemは一時的に維持する

---
+++ src/rmmzFunctional/command/variable/other.ts
@@ -0,0 +1,61 @@
+import type {
+  Command_ControlVariables_FromLastData,
+  Command_ControlVariables_FromSystem,
+} from "@RpgTypes/rmmz/eventCommand";
+import type {
+  Rmmz_MapId,
+  Rmmz_Party,
+  Rmmz_SystemCounter,
+  Rmmz_Temp,
+  Rmmz_Timer,
+} from "@RpgTypes/rmmzRuntime";
+
+export const variableFromLastData = (
+  command: Command_ControlVariables_FromLastData,
+  temp: Rmmz_Temp,
+): number => {
+  return temp.lastActionData(command.parameters[5]);
+};
+
+export const variableFromOther = (
+  command: Command_ControlVariables_FromSystem,
+  map: Rmmz_MapId,
+  party: Rmmz_Party,
+  system: Rmmz_SystemCounter,
+  timer: Rmmz_Timer,
+  fallback: number = 0,
+): number => {
+  switch (command.parameters[5]) {
+    case 0:
+      return map.mapId();
+
+    case 1:
+      return party.size();
+
+    case 2:
+      return party.gold();
+
+    case 3:
+      return party.steps();
+
+    case 4:
+      return system.playtime();
+
+    case 5:
+      return timer.seconds();
+
+    case 6:
+      return system.saveCount();
+
+    case 7:
+      return system.battleCount();
+
+    case 8:
+      return system.winCount();
+
+    case 9:
+      return system.escapeCount();
+  }
+
+  return fallback;
+};
