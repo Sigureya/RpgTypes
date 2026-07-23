dc4734ffad2bf2b9cb10631c6ed0c31252e3d6e5
Sigureya <sigureya1208@gmail.com>
2026-07-23 23:29:23 +0900

    直前でのコミット忘れ。

---
+++ src/rmmz/eventCommand/commands/variable/gamedata/system/types.ts
@@ -0,0 +1,28 @@
+import type {
+  CONTROL_VARIABLES,
+  EventCommandLike,
+  VariableOperation,
+} from "@RpgTypes/libs";
+
+export interface Command_ControlVariables_FromOthre extends EventCommandLike<
+  typeof CONTROL_VARIABLES
+> {
+  code: typeof CONTROL_VARIABLES;
+  indent: number;
+  parameters: ParamArray_VariableFromOther;
+}
+
+export interface ParamObject_VariableFromOther {
+  startId: number;
+  endId?: number;
+  operation?: VariableOperation;
+}
+
+export type ParamArray_VariableFromOther = [
+  rangeStartId: number,
+  rangeEndId: number,
+  operation: VariableOperation,
+  operandGameData: 3,
+  typeSystem: 7,
+  param: number,
+];
