ac2503bab1a2c2eb7750071ca5c13942cdabb634
Sigureya <sigureya1208@gmail.com>
2026-07-23 22:19:30 +0900

    MapIdだけ取得したい状況が多いので独立

--- src/rmmzRuntime/objects/core/map/index.ts
+++ src/rmmzRuntime/objects/core/map/index.ts
@@ -1,5 +1,5 @@
+export type * from "./map";
 export type { Rmmz_MapCharacter as Rmmz_Character } from "./character";
 export type { Rmmz_Event } from "./event";
-export type { Rmmz_Map } from "./map";
 export type { Rmmz_PlayerCharactor } from "./player";
 export type { Rmmz_Vehicle } from "./vehicle";
