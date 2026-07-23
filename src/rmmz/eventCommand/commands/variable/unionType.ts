060ba98b8b8828938e892a48b4e4856f3a456590
Sigureya <sigureya1208@gmail.com>
2026-07-24 00:25:01 +0900

    定義を簡略化

--- src/rmmz/eventCommand/commands/variable/unionType.ts
+++ src/rmmz/eventCommand/commands/variable/unionType.ts
@@ -1,33 +1,13 @@
-import type {
-  Command_ControlVariables_FromEnemy,
-  Command_ControlVariables_FromActor,
-  Command_ControlVariables_FromMapCharactor,
-  Command_ControlVariables_FromParty,
-  Command_ControlVariables_FromLastData,
-  Command_ControlVariables_FromOthre,
-} from "./gamedata";
+import type { Command_ControlVariables_FromGameData } from "./gamedata";
 import type { Command_ControlVariables_FromVariable } from "./gameVariable2";
-import type {
-  Command_ControlVariables_FromArmor,
-  Command_ControlVariables_FromItem,
-  Command_ControlVariables_FromWeapon,
-} from "./goods";
 import type { Command_ControlVariables_FromConstant } from "./literal";
 import type { Command_ControlVariables_FromRandom } from "./random";
 
 export type Command_ControlVariables =
-  | Command_ControlVariables_FromOthre
-  | Command_ControlVariables_FromEnemy
   | Command_ControlVariables_FromConstant
-  | Command_ControlVariables_FromActor
-  | Command_ControlVariables_FromLastData
   | Command_ControlVariables_FromVariable
   | Command_ControlVariables_FromRandom
-  | Command_ControlVariables_FromMapCharactor
-  | Command_ControlVariables_FromParty
-  | Command_ControlVariables_FromArmor
-  | Command_ControlVariables_FromItem
-  | Command_ControlVariables_FromWeapon;
+  | Command_ControlVariables_FromGameData;
 
 export type ParamArray_ControlVariables =
   Command_ControlVariables["parameters"];
