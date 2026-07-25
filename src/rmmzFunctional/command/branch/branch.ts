2ebe9440beed8d91bd05be11a4aeeac98c69bd6b
Sigureya <sigureya1208@gmail.com>
2026-07-25 22:03:44 +0900

    条件分岐。おそらこれで扱える

--- src/rmmzFunctional/command/branch/branch.ts
+++ src/rmmzFunctional/command/branch/branch.ts
@@ -1,4 +1,9 @@
-import type { Command_ConditionalBranch } from "@RpgTypes/rmmz/eventCommand";
+import type {
+  BranchParam_Character,
+  BranchParam_Enemy,
+  BranchParam_Timer,
+  Command_ConditionalBranch,
+} from "@RpgTypes/rmmz/eventCommand";
 import type {
   Rmmz_BranchSourceParty,
   Rmmz_SelfSwitches,
@@ -20,7 +25,7 @@ import type {
 } from "./types";
 import { evaluteBranchByVariable } from "./variables";
 
-const evaluteBranchCommand = (
+export const evaluteBranchCommand = (
   mapEventId: number,
   { parameters }: Command_ConditionalBranch,
   provider: Rmmz_BranchSourceProvider,
@@ -37,8 +42,14 @@ const evaluteBranchCommand = (
       return evaluteBranchByVariable(parameters, variables);
     case 2:
       return evaluteSelfSwitchBranch(parameters, selfFwitches, mapEventId);
+    case 3:
+      return evaluteBranchByTimer(parameters, timer);
     case 4:
       return evaluateActorBranch(parameters, pary, provider);
+    case 5:
+      return evaluteBranchByEnemy(parameters, provider);
+    case 6:
+      return evaluteBranchByCharacter(parameters, provider);
     case 7:
       return evaluteBranchByGold(parameters, pary);
     case 8:
@@ -54,5 +65,50 @@ const evaluteBranchCommand = (
         provider.armorData(armorId),
       );
   }
+  return false;
+};
+
+export const evaluteBranchByTimer = (
+  parameters: BranchParam_Timer,
+  timer: Rmmz_Timer,
+): boolean => {
+  if (!timer.isWorking()) {
+    return false;
+  }
+  const sec = timer.seconds() / 60;
+  if (parameters[2] === 0) {
+    return sec >= parameters[1];
+  }
+  return sec <= parameters[1];
+};
+
+export const evaluteBranchByEnemy = (
+  parameters: BranchParam_Enemy,
+  provider: Rmmz_BranchSourceProvider,
+): boolean => {
+  const enemy = provider.gameEnemy(parameters[1]);
+  if (!enemy) {
+    return false;
+  }
+
+  switch (parameters[2]) {
+    case 0:
+      return enemy.isAlive();
+    case 1:
+      return enemy.isStateAffected(parameters[3]);
+    default:
+      return false;
+  }
+};
+
+export const evaluteBranchByCharacter = (
+  parameters: BranchParam_Character,
+  provider: Rmmz_BranchSourceProvider,
+): boolean => {
+  const character = provider.mapCharacter(parameters[1]);
+  if (character) {
+    return character.direction() === parameters[2];
+  }
+
   return false;
 };
