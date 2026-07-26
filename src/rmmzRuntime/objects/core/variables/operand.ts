ae38d87641e7d657c1afe3eaa022a6dd7569dbe3
Sigureya <sigureya1208@gmail.com>
2026-07-26 10:47:25 +0900

    追加で抽象化。これによりEnemyにも対応できるし、actorを知らなくてOKになった

--- src/rmmzRuntime/objects/core/variables/operand.ts
+++ src/rmmzRuntime/objects/core/variables/operand.ts
@@ -1,21 +1,3 @@
-import type {
-  Command_ChangeActorHP,
-  Command_ChangeActorMP,
-  Command_ChangeActorTP,
-  ParamArray_ChangeActorHP,
-  ParamArray_ChangeActorMP,
-  ParamArray_ChangeActorTP,
-  ParamArray_ChangeActorValue,
-  ParamArray_ChangeArmors,
-  ParamArray_ChangeEnemyHP,
-  ParamArray_ChangeGold,
-  ParamArray_ChangeItems,
-  ParamArray_ChangeWeapons,
-} from "@RpgTypes/rmmz/eventCommand";
-import type {
-  ParamArray_ChangeEnemyMP,
-  ParamArray_ChangeEnemyTP,
-} from "@RpgTypes/rmmz/eventCommand/commands/enemy/change";
 import type { Rmmz_Variables } from "./types";
 
 export const operateValue = (
@@ -29,22 +11,31 @@ export const operateValue = (
   return operantion === 0 ? value : -value;
 };
 
-export const operateValue012 = (
+interface ProvideActo<T> {
+  allMembers: () => T[];
+  gameActor: (actorId: number) => T | undefined;
+}
+
+export const resolveTargets = <T>(
+  param1: 0 | 1,
+  param2: number,
+  provider: ProvideActo<T>,
   variables: Rmmz_Variables,
-  param: ParamArray_ChangeGold,
-): number => {
-  return operateValue(variables, param[0], param[1], param[2]);
+): T[] => {
+  if (param1 === 0) {
+    return provider.allMembers();
+  }
+  const actorId = variables.value(param2);
+  return actorsById(provider, actorId);
 };
 
-export const operateValue123 = (
-  variables: Rmmz_Variables,
-  param:
-    | ParamArray_ChangeItems
-    | ParamArray_ChangeWeapons
-    | ParamArray_ChangeArmors
-    | ParamArray_ChangeEnemyHP
-    | ParamArray_ChangeEnemyMP
-    | ParamArray_ChangeEnemyTP,
-): number => {
-  return operateValue(variables, param[1], param[2], param[3]);
+const actorsById = <T>(provider: ProvideActo<T>, actorId: number): T[] => {
+  if (actorId === 0) {
+    return provider.allMembers();
+  }
+  const actor = provider.gameActor(actorId);
+  if (actor) {
+    return [actor];
+  }
+  return [];
 };
