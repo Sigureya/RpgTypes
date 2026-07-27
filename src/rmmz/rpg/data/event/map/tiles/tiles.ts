d50e0646ab55adc12ba63c3d54144b9ca4fdd427
Sigureya <sigureya1208@gmail.com>
2026-07-27 14:30:31 +0900

    機能が複雑化する兆候が見えたので、単独ファイルに封じ込める

---
+++ src/rmmz/rpg/data/event/map/tiles/tiles.ts
@@ -0,0 +1,20 @@
+import type { Data_MapTiles } from "./types";
+
+export const isValidMapPosition = (
+  map: Data_MapTiles,
+  x: number,
+  y: number,
+): boolean => {
+  return x >= 0 && y >= 0 && x < map.width && y < map.height;
+};
+
+export const mapTileIdToIndex = (
+  map: Data_MapTiles,
+  x: number,
+  y: number,
+  z: number,
+): number => {
+  const width = map.width;
+  const height = map.height;
+  return map.data[(z * height + y) * width + x] || 0;
+};
