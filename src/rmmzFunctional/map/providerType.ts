32bcf81cdf38ecd66aa98c18b976e8b61d58e475
Sigureya <sigureya1208@gmail.com>
2026-07-31 03:33:54 +0900

    マップ関連の実装。テストが無いので未検証

---
+++ src/rmmzFunctional/map/providerType.ts
@@ -0,0 +1,27 @@
+import type {
+  EventCommandUnknown,
+  MoveRouteCommandUnknown,
+} from "@RpgTypes/libs";
+import type { Data_Map } from "@RpgTypes/rmmz/events";
+import type { MapEventPage } from "../../rmmz/rpg/data/event/map/event/page";
+import type { MapEvent } from "../../rmmz/rpg/data/event/map/event/page/mapEvent";
+
+export interface Provider_MapTilesetFlags {
+  tilesetFlags(map: Pick<Data_Map, "tilesetId">): ReadonlyArray<number>;
+}
+
+export interface Provider_MapTileEventTileIds {
+  tileEventTileIds(map: Data_Map, x: number, y: number): ReadonlyArray<number>;
+}
+
+export type Provider_MapPassage = Provider_MapTilesetFlags &
+  Provider_MapTileEventTileIds;
+
+export interface Provider_MapEventPageResolver<
+  CommandType extends EventCommandUnknown = EventCommandUnknown,
+  MoveRoute extends MoveRouteCommandUnknown = MoveRouteCommandUnknown,
+> {
+  activePage(
+    event: MapEvent<CommandType, MoveRoute>,
+  ): MapEventPage<CommandType, MoveRoute> | null | undefined;
+}
