b9ee6b2ce40e39db741b5db22e7ea4aeee4466e1
Sigureya <sigureya1208@gmail.com>
2026-08-02 04:39:31 +0900

    stateの解除タイミング

--- src/rmmz/rpg/data/main/traitContainers/state/constants.ts
+++ src/rmmz/rpg/data/main/traitContainers/state/constants.ts
@@ -3,3 +3,7 @@ export const STATE_RESTRICTION_CONFUSION_RANDOM_OPPONENT = 1 as const;
 export const STATE_RESTRICTION_CONFUSION_RANDOM_ALL = 2 as const;
 export const STATE_RESTRICTION_CONFUSION_RANDOM_FRIEND = 3 as const;
 export const STATE_RESTRICTION_NOT_MOVE = 4 as const;
+
+export const STATE_REMOVE_TIMING_NONE = 0 as const;
+export const STATE_REMOVE_TIMING_END_OF_TURN = 1 as const;
+export const STATE_REMOVE_TIMING_END_OF_ACTION = 2 as const;
