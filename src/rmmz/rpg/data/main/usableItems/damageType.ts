8dbf78f4b009e605d97aaee9383dc2782fc571c5
Sigureya <sigureya1208@gmail.com>
2026-08-05 03:05:13 +0900

    ダメージ種別の再設計。定数は上に置く

--- src/rmmz/rpg/data/main/usableItems/damageType.ts
+++ src/rmmz/rpg/data/main/usableItems/damageType.ts
@@ -7,28 +7,6 @@ export const DAMAGE_TYPE_MP_RECOVER = 4 as const;
 export const DAMAGE_TYPE_HP_DRAIN = 5 as const;
 export const DAMAGE_TYPE_MP_DRAIN = 6 as const;
 
-export const isHpRecoverItem = (item: Data_UsableItem): boolean => {
-  return item.damage.type === DAMAGE_TYPE_HP_RECOVER;
-};
-
-export const isMpRecoverItem = (item: Data_UsableItem): boolean => {
-  return item.damage.type === DAMAGE_TYPE_MP_RECOVER;
-};
-
-export const isHpDamage = (item: Data_UsableItem): boolean => {
-  return (
-    item.damage.type === DAMAGE_TYPE_HP_DAMAGE ||
-    item.damage.type === DAMAGE_TYPE_HP_DRAIN
-  );
-};
-
-export const isMpDamage = (item: Data_UsableItem): boolean => {
-  return (
-    item.damage.type === DAMAGE_TYPE_MP_DAMAGE ||
-    item.damage.type === DAMAGE_TYPE_MP_DRAIN
-  );
-};
-
 export const isHpEffect = (item: Data_UsableItem): boolean => {
   return (
     item.damage.type === DAMAGE_TYPE_HP_DAMAGE ||
@@ -43,4 +21,41 @@ export const isMpEffect = (item: Data_UsableItem): boolean => {
     item.damage.type === DAMAGE_TYPE_MP_DRAIN ||
     item.damage.type === DAMAGE_TYPE_MP_RECOVER
   );
+};
+
+export const isDamage = (item: Data_UsableItem): boolean => {
+  return (
+    item.damage.type === DAMAGE_TYPE_HP_DAMAGE ||
+    item.damage.type === DAMAGE_TYPE_MP_DAMAGE
+  );
+};
+
+export const isRecover = (item: Data_UsableItem): boolean => {
+  return (
+    item.damage.type === DAMAGE_TYPE_HP_RECOVER ||
+    item.damage.type === DAMAGE_TYPE_MP_RECOVER
+  );
+};
+
+export const isDrain = (item: Data_UsableItem): boolean => {
+  return (
+    item.damage.type === DAMAGE_TYPE_HP_DRAIN ||
+    item.damage.type === DAMAGE_TYPE_MP_DRAIN
+  );
+};
+
+export const isHpRecoverItem = (item: Data_UsableItem): boolean => {
+  return item.damage.type === DAMAGE_TYPE_HP_RECOVER;
+};
+
+export const isMpRecoverItem = (item: Data_UsableItem): boolean => {
+  return item.damage.type === DAMAGE_TYPE_MP_RECOVER;
+};
+
+export const isHpRecovery = (item: Data_UsableItem): boolean => {
+  return item.damage.type === DAMAGE_TYPE_HP_RECOVER;
+};
+
+export const isMpRecovery = (item: Data_UsableItem): boolean => {
+  return item.damage.type === DAMAGE_TYPE_MP_RECOVER;
 };
