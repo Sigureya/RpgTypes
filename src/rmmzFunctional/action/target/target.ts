68a37f087527d9f443a344acf009809dd3a89ccf
Sigureya <sigureya1208@gmail.com>
2026-08-05 00:22:31 +0900

    仮実装。テストが非常に面倒

--- src/rmmzFunctional/action/target/target.ts
+++ src/rmmzFunctional/action/target/target.ts
@@ -1,18 +1,42 @@
 import type { Data_UsableItem } from "@RpgTypes/rmmz/rpg";
-import {
-  scopeIsForAliveFriend,
-  scopeIsForDeadFriend,
-  scopeIsForRandom,
-} from "@RpgTypes/rmmz/rpg";
+import { scopeRandomNumTargets } from "@RpgTypes/rmmz/rpg";
 import type { Rmmz_Battler } from "@RpgTypes/rmmzRuntime";
+import { battlresRandomTarget } from "./randomSelect";
+import { smoothAliveTarget } from "./support";
 
-export const actionTargetsForOpponents = (
+export const actionTargetsForOpponents = <T extends Rmmz_Battler>(
   item: Data_UsableItem,
-  opponentsUnit: ReadonlyArray<Rmmz_Battler>,
-): Rmmz_Battler[] => {
-  // if(scopeIsForR
-  if (scopeIsForRandom(item)) {
+  opponentsUnit: ReadonlyArray<T>,
+  targetIndex: number,
+  randomFn: () => number,
+): T[] => {
+  if (opponentsUnit.length === 0) {
     return [];
   }
-  return [];
+  const randomRepeat = scopeRandomNumTargets(item);
+  if (randomRepeat > 0) {
+    return randomTargets(opponentsUnit, randomRepeat, randomFn);
+  }
+  const single = smoothAliveTarget(opponentsUnit, targetIndex);
+  return single ? [single] : [];
+};
+
+const randomTargets = <T extends Rmmz_Battler>(
+  list: ReadonlyArray<T>,
+  repeat: number,
+  randomFn: () => number,
+): T[] => {
+  if (list.length === 1) {
+    return Array(repeat).fill(list[0]);
+  }
+  const result: T[] = [];
+  // eslint-disable-next-line @functional/no-loop-statements, @functional/no-let
+  for (let i = 0; i < repeat; i++) {
+    const rnd = randomFn();
+    const target = battlresRandomTarget(list, rnd);
+    if (target) {
+      result.push(target);
+    }
+  }
+  return result;
 };
