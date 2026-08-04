88b6614e0e497aa6a2d1b144a5d0a8acd3c9f8b4
Sigureya <sigureya1208@gmail.com>
2026-08-05 01:55:43 +0900

    ターゲット列挙関数。重複があるなどリファクタリングが必要だが、暫定的にこれで行く

--- src/rmmzFunctional/action/target/target.ts
+++ src/rmmzFunctional/action/target/target.ts
@@ -1,8 +1,72 @@
 import type { Data_UsableItem } from "@RpgTypes/rmmz/rpg";
-import { scopeRandomNumTargets } from "@RpgTypes/rmmz/rpg";
+import { scopeIsForOne, scopeRandomNumTargets } from "@RpgTypes/rmmz/rpg";
 import type { Rmmz_Battler } from "@RpgTypes/rmmzRuntime";
 import { battlresRandomTarget } from "./randomSelect";
-import { smoothAliveTarget } from "./support";
+import { smoothAliveTarget, smoothDeadTarget } from "./support";
+
+export const repeatTargets = <T extends Rmmz_Battler>(
+  battlers: ReadonlyArray<T>,
+  repeat: number,
+): T[] => {
+  return battlers.flatMap((b) => Array(repeat).fill(b));
+};
+
+export const actionTargetForDead = <T extends Rmmz_Battler>(
+  item: Data_UsableItem,
+  unit: ReadonlyArray<T>,
+  targetIndex: number,
+): T[] => {
+  if (scopeIsForOne(item)) {
+    return singleDeadTarget(unit, targetIndex);
+  }
+  return unit.filter((b) => b.isDead());
+};
+
+export const actionTargetForDeadAndAlive = <T extends Rmmz_Battler>(
+  item: Data_UsableItem,
+  unit: Array<T>,
+  targetIndex: number,
+): T[] => {
+  if (scopeIsForOne(item)) {
+    return singleTarget(unit, targetIndex);
+  }
+  return unit;
+};
+
+export const actionTargetForAlive = <T extends Rmmz_Battler>(
+  item: Data_UsableItem,
+  unit: ReadonlyArray<T>,
+  targetIndex: number,
+): T[] => {
+  if (scopeIsForOne(item)) {
+    return singleTarget(unit, targetIndex);
+  }
+  return unit.filter((b) => b.isAlive());
+};
+
+const singleDeadTarget = <T extends Rmmz_Battler>(
+  unit: ReadonlyArray<T>,
+  targetIndex: number,
+): T[] => {
+  if (targetIndex < 0) {
+    const t = smoothDeadTarget(unit, 0);
+    return t ? [t] : [];
+  }
+  const target = smoothDeadTarget(unit, targetIndex);
+  return target ? [target] : [];
+};
+
+const singleTarget = <T extends Rmmz_Battler>(
+  unit: ReadonlyArray<T>,
+  targetIndex: number,
+): T[] => {
+  if (targetIndex < 0) {
+    const t = smoothAliveTarget(unit, 0);
+    return t ? [t] : [];
+  }
+  const target = smoothAliveTarget(unit, targetIndex);
+  return target ? [target] : [];
+};
 
 export const actionTargetsForOpponents = <T extends Rmmz_Battler>(
   item: Data_UsableItem,
