7b77f768aa0038cdbbaf2098738087fd2547ba44
Sigureya <sigureya1208@gmail.com>
2026-08-05 20:09:57 +0900

    インターフェースを狭くして再定義

--- src/rmmzFunctional/action/target/types.ts
+++ src/rmmzFunctional/action/target/types.ts
@@ -1,6 +1,8 @@
-import type { Rmmz_Battler } from "@RpgTypes/rmmzRuntime";
+import type { Rmmz_Battler_Targetable } from "@RpgTypes/rmmzRuntime/objects/core/battle/battler/base";
 
-export interface Provider_Battlers<T extends Rmmz_Battler = Rmmz_Battler> {
+export interface Provider_Battlers<
+  T extends Rmmz_Battler_Targetable = Rmmz_Battler_Targetable,
+> {
   opponentsUnit(): ReadonlyArray<T>;
   friendsUnit(): ReadonlyArray<T>;
 }
