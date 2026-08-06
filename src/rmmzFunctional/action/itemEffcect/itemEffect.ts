4f112c377f4c6dda5f14342efe2df2a0df150dbb
Sigureya <sigureya1208@gmail.com>
2026-08-06 10:05:31 +0900

    何らかの効果があるか判定する関数

---
+++ src/rmmzFunctional/action/itemEffcect/itemEffect.ts
@@ -0,0 +1,40 @@
+import type { ItemEffect } from "@RpgTypes/rmmz/rpg";
+import {
+  EFFECT_ADD_BUFF,
+  EFFECT_ADD_STATE,
+  EFFECT_RECOVER_HP,
+  EFFECT_RECOVER_MP,
+  EFFECT_REMOVE_BUFF,
+  EFFECT_REMOVE_STATE,
+} from "@RpgTypes/rmmz/rpg";
+import type { Rmmz_Battler } from "@RpgTypes/rmmzRuntime";
+
+export const actionTestItemEffect = (
+  target: Rmmz_Battler,
+  effect: ItemEffect,
+): boolean => {
+  switch (effect.code) {
+    case EFFECT_RECOVER_HP:
+      return hppxx(target, effect);
+    case EFFECT_RECOVER_MP:
+      return mppxx(target, effect);
+    case EFFECT_ADD_STATE:
+      return !target.isStateAffected(effect.dataId);
+    case EFFECT_REMOVE_STATE:
+      return target.isStateAffected(effect.dataId);
+    case EFFECT_ADD_BUFF:
+      return !target.isBuffAffected(effect.dataId);
+    case EFFECT_REMOVE_BUFF:
+      return target.isBuffAffected(effect.dataId);
+    default:
+      return true;
+  }
+};
+
+const hppxx = (target: Rmmz_Battler, effect: ItemEffect): boolean => {
+  return target.hp < target.mhp || effect.value1 < 0 || effect.value2 < 0;
+};
+
+const mppxx = (target: Rmmz_Battler, effect: ItemEffect): boolean => {
+  return target.mp < target.mmp || effect.value1 < 0 || effect.value2 < 0;
+};
