fc23015b702b96aa50be3fb6fc448be06c51e964
Sigureya <sigureya1208@gmail.com>
2026-08-09 00:15:46 +0900

    exportを追加

--- src/rmmzRuntime/objects/core/battle/battler/index.ts
+++ src/rmmzRuntime/objects/core/battle/battler/index.ts
@@ -7,5 +7,5 @@ export type {
   Rmmz_ActorsTemplate,
 } from "./actors";
 export type { Rmmz_Battler } from "./battler";
-export type { Rmmz_BattlerBase } from "./base";
+export type * from "./base";
 export type { Rmmz_Enemy } from "./enemy";
