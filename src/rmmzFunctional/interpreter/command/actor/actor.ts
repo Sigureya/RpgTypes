59de8e2897532852fcb9cd9484a3e3f59d55ca36
Sigureya <sigureya1208@gmail.com>
2026-08-09 16:54:06 +0900

    機能的にRmmz_Actrosだと確定したので修正

--- src/rmmzFunctional/interpreter/command/actor/actor.ts
+++ src/rmmzFunctional/interpreter/command/actor/actor.ts
@@ -3,16 +3,16 @@ import type {
   Command_ChangeClaass,
 } from "@RpgTypes/rmmz/eventCommand";
 import type {
-  Provider_Target,
   Rmmz_Actor,
+  Rmmz_ActorsReadonly,
   Rmmz_PlayerCharactor,
 } from "@RpgTypes/rmmzRuntime";
 
 export const commandChangeActorClass = (
   command: Command_ChangeClaass,
-  provider: Provider_Target<Rmmz_Actor>,
+  provider: Rmmz_ActorsReadonly<Rmmz_Actor>,
 ): boolean => {
-  const actor = provider.gameActor(command.parameters[0]);
+  const actor = provider.actor(command.parameters[0]);
   if (actor) {
     const classId = command.parameters[1];
     const keepExp = command.parameters[2];
@@ -23,10 +23,10 @@ export const commandChangeActorClass = (
 
 export const commandChangeActorImages = (
   { parameters }: Command_ChangeActorImages,
-  provider: Provider_Target<Rmmz_Actor>,
+  provider: Rmmz_ActorsReadonly<Rmmz_Actor>,
   gamePlayer: Rmmz_PlayerCharactor,
 ): boolean => {
-  const actor = provider.gameActor(parameters[0]);
+  const actor = provider.actor(parameters[0]);
   if (actor) {
     actor.setCharacterImage(parameters[1], parameters[2]);
     actor.setFaceImage(parameters[3], parameters[4]);
