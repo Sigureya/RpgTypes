fafc3836349ad06d3cd2fa2b132e4609704d9be3
Sigureya <sigureya1208@gmail.com>
2026-08-11 03:45:16 +0900

    コマンド追加。unionへの追加は後でやる

---
+++ src/rmmz/eventCommand/commands/message/setupChoice/types/cancel.ts
@@ -0,0 +1,7 @@
+import type { EventCommandLike, SHOW_CHOICES_CANCEL } from "@RpgTypes/libs";
+
+export interface Command_ShowChoicesCancel extends EventCommandLike<
+  typeof SHOW_CHOICES_CANCEL
+> {
+  parameters: [cancelType: number];
+}
