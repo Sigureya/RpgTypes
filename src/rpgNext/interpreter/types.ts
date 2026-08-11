7b543fc53065906b7a3408910bec42de405de5d6
Sigureya <sigureya1208@gmail.com>
2026-08-11 16:08:36 +0900

    sourceはインタプリタの起動時に固定されるため、stateではない

--- src/rpgNext/interpreter/types.ts
+++ src/rpgNext/interpreter/types.ts
@@ -1,7 +1,10 @@
 export interface InterpreterState2 {
   index: number;
   indent: number;
-  source: number;
   waitCode: number;
   ppResult: number;
+}
+
+export interface InterpreterXXX {
+  source: number;
 }
