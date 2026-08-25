939527affce06e149771d9e0e8151d89c56d42e5
Sigureya <sigureya1208@gmail.com>
2026-08-25 17:53:51 +0900

    HPやMPを扱う箇所だけ分離。これらはBattlerBaseには無い

--- src/rmmzRuntime/objects/core/battle/battler/battler.ts
+++ src/rmmzRuntime/objects/core/battle/battler/battler.ts
@@ -1,13 +1,16 @@
 import type { ParamID } from "@RpgTypes/rmmz/rpg";
 import type { Rmmz_BattlerBase } from "./base";
 
-export interface Rmmz_Battler extends Rmmz_BattlerBase {
+export interface Rmmz_Battler_Poitns {
   gainHp(value: number, allowDeath?: boolean): void;
   gainMp(value: number): void;
   gainTp(value: number): void;
   loseHp(value: number): void;
   loseMp(value: number): void;
   loseTp(value: number): void;
+}
+
+export interface Rmmz_Battler extends Rmmz_BattlerBase, Rmmz_Battler_Poitns {
   performCollapse(): void;
   clearResult(): void;
   recoverAll(): void;
