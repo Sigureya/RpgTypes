b0691d18b4aa8eb215ee92c5c2093cc67deecfb0
Sigureya <sigureya1208@gmail.com>
2026-08-27 11:59:19 +0900

    アクション色々の引数を追加

--- src/rmmz/rpg/data/main/traitContainers/enemy/enemy.ts
+++ src/rmmz/rpg/data/main/traitContainers/enemy/enemy.ts
@@ -1,9 +1,21 @@
 import { makeParamArray } from "../members";
-import { ENEMY_ACTION_CONDITION_TURN } from "./constants";
+import {
+  ENEMY_ACTION_CONDITION_HP_RATE,
+  ENEMY_ACTION_CONDITION_MP_RATE,
+  ENEMY_ACTION_CONDITION_PARTY_LEVEL,
+  ENEMY_ACTION_CONDITION_STATE,
+  ENEMY_ACTION_CONDITION_SWITCH,
+  ENEMY_ACTION_CONDITION_TURN,
+} from "./constants";
 import type {
   Data_Enemy,
   DropItem,
   Enemy_Action,
+  Enemy_Action_HpRate,
+  Enemy_Action_MpRate,
+  Enemy_Action_PartyLevel,
+  Enemy_Action_State,
+  Enemy_Action_Switch,
   Enemy_Action_Turn,
 } from "./types";
 
@@ -29,6 +41,58 @@ export const makeEnemyActionTurn = (e: Enemy_Action_Turn): Enemy_Action => {
     conditionParam1: e.turnA,
     conditionParam2: e.turnB,
     conditionType: ENEMY_ACTION_CONDITION_TURN,
+    rating: e.rating,
+    skillId: e.skillId,
+  };
+};
+
+export const makeEnemyActionHpRate = (e: Enemy_Action_HpRate): Enemy_Action => {
+  return {
+    conditionParam1: e.hpRateA,
+    conditionParam2: e.hpRateB,
+    conditionType: ENEMY_ACTION_CONDITION_HP_RATE,
+    rating: e.rating,
+    skillId: e.skillId,
+  };
+};
+
+export const makeEnemyActionMpRate = (e: Enemy_Action_MpRate): Enemy_Action => {
+  return {
+    conditionParam1: e.mpRateA,
+    conditionParam2: e.mpRateB,
+    conditionType: ENEMY_ACTION_CONDITION_MP_RATE,
+    rating: e.rating,
+    skillId: e.skillId,
+  };
+};
+
+export const makeEnemyActionState = (e: Enemy_Action_State): Enemy_Action => {
+  return {
+    conditionParam1: e.stateId,
+    conditionParam2: 0,
+    conditionType: ENEMY_ACTION_CONDITION_STATE,
+    rating: e.rating,
+    skillId: e.skillId,
+  };
+};
+
+export const makeEnemyActionPartyLevel = (
+  e: Enemy_Action_PartyLevel,
+): Enemy_Action => {
+  return {
+    conditionParam1: e.partyLevel,
+    conditionParam2: 0,
+    conditionType: ENEMY_ACTION_CONDITION_PARTY_LEVEL,
+    rating: e.rating,
+    skillId: e.skillId,
+  };
+};
+
+export const makeEnemyActionSwitch = (e: Enemy_Action_Switch): Enemy_Action => {
+  return {
+    conditionParam1: e.switchId,
+    conditionParam2: 0,
+    conditionType: ENEMY_ACTION_CONDITION_SWITCH,
     rating: e.rating,
     skillId: e.skillId,
   };
--- src/rmmz/rpg/data/main/traitContainers/enemy/types/action.ts
+++ src/rmmz/rpg/data/main/traitContainers/enemy/types/action.ts
@@ -12,8 +12,28 @@ export interface Enemy_Action_Base {
 }
 
 export interface Enemy_Action_Turn extends Enemy_Action_Base {
-  skillId: number;
-  rating: number;
   turnA: number;
   turnB: number;
+}
+
+export interface Enemy_Action_HpRate extends Enemy_Action_Base {
+  hpRateA: number;
+  hpRateB: number;
+}
+
+export interface Enemy_Action_MpRate extends Enemy_Action_Base {
+  mpRateA: number;
+  mpRateB: number;
+}
+
+export interface Enemy_Action_State extends Enemy_Action_Base {
+  stateId: number;
+}
+
+export interface Enemy_Action_PartyLevel extends Enemy_Action_Base {
+  partyLevel: number;
+}
+
+export interface Enemy_Action_Switch extends Enemy_Action_Base {
+  switchId: number;
 }
