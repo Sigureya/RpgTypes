5fc4623ad3509dab5a96b1ebee18fc9826830484
Sigureya <sigureya1208@gmail.com>
2026-08-27 12:09:17 +0900

    テストケースを追加

--- src/rmmzMock/core/objects/enemy.action.test.ts
+++ src/rmmzMock/core/objects/enemy.action.test.ts
@@ -1,17 +1,19 @@
+import type { MockedObject } from "vitest";
+import { vi, describe, test, expect } from "vitest";
 import type { Enemy_Action } from "@RpgTypes/rmmz/rpg";
 import {
-  enemyActionMeetsCondition,
-  type Rmmz_EnemyActionConditionType,
-} from "@RpgTypes/rmmzFunctional";
-import type { Rmmz_Party, Rmmz_Switches } from "@RpgTypes/rmmzRuntime";
-import type { MockedObject } from "vitest";
-import { vi } from "vitest";
-import { describe, test, expect } from "vitest";
-import { Game_Enemy } from "./rmmz_objects";
-import {
+  makeEnemyActionHpRate,
+  makeEnemyActionMpRate,
+  makeEnemyActionPartyLevel,
+  makeEnemyActionState,
+  makeEnemyActionSwitch,
   makeEnemyActionTurn,
   makeEnemyData,
-} from "@RpgTypes/rmmz/rpg/data/main/traitContainers/enemy/enemy";
+} from "@RpgTypes/rmmz/rpg";
+import type { Rmmz_EnemyActionConditionType } from "@RpgTypes/rmmzFunctional";
+import { enemyActionMeetsCondition } from "@RpgTypes/rmmzFunctional";
+import type { Rmmz_Party, Rmmz_Switches } from "@RpgTypes/rmmzRuntime";
+import { Game_Enemy } from "./rmmz_objects";
 
 type FakeParty = Pick<Rmmz_Party, "highestLevel">;
 const MOCK_HIGHEST_LEVEL = 44;
@@ -19,6 +21,8 @@ const MOCK_HIGHEST_LEVEL = 44;
 const MOCK_HP_RATE = 0.5;
 const MOCK_MP_RATE = 0.5;
 const MOCK_TURN_COUNT = 1;
+
+const MOCK_TRUELY_SWITCH_ID = 23;
 
 const MOCK_ENEMY_DATA = makeEnemyData({
   id: 1,
@@ -33,7 +37,7 @@ const createMockedParty = (): MockedObject<FakeParty> => {
 
 const createMockedSwitches = (): MockedObject<Rmmz_Switches> => {
   return {
-    value: vi.fn(() => true),
+    value: vi.fn((id) => id === MOCK_TRUELY_SWITCH_ID),
     setValue: vi
       .fn()
       .mockThrow(new Error("setValue should not be called in this test")),
@@ -47,7 +51,7 @@ const createMockedEnemy = (): MockedObject<Rmmz_EnemyActionConditionType> => {
     turnCount: vi.fn(() => MOCK_TURN_COUNT),
     hpRate: vi.fn(() => MOCK_HP_RATE),
     mpRate: vi.fn(() => MOCK_MP_RATE),
-    isStateAffected: vi.fn(() => false),
+    isStateAffected: vi.fn((stateId) => stateId === 5),
     allTraits: vi.fn(() => []),
   };
 };
@@ -136,6 +140,74 @@ const testCases: TestCase[] = [
     action: makeEnemyActionTurn({ skillId: 1, rating: 5, turnA: 1, turnB: 0 }),
     callXX: (context) => {
       expect(context.enemy.turnCount).toHaveBeenCalledOnce();
+    },
+  },
+  {
+    expected: true,
+    action: makeEnemyActionHpRate({
+      skillId: 2,
+      rating: 7,
+      hpRateA: 0.25,
+      hpRateB: 0.5,
+    }),
+    callXX: (context) => {
+      expect(context.enemy.hpRate).toHaveBeenCalledOnce();
+    },
+  },
+  {
+    expected: true,
+    action: makeEnemyActionMpRate({
+      skillId: 3,
+      rating: 8,
+      mpRateA: 0.3,
+      mpRateB: 0.5,
+    }),
+    callXX: (context) => {
+      expect(context.enemy.mpRate).toHaveBeenCalledOnce();
+    },
+  },
+  {
+    expected: true,
+    action: makeEnemyActionState({ skillId: 4, rating: 9, stateId: 5 }),
+    callXX: (context) => {
+      expect(context.enemy.isStateAffected).toHaveBeenCalledOnce();
+    },
+  },
+  {
+    expected: true,
+    action: makeEnemyActionPartyLevel({
+      skillId: 5,
+      rating: 6,
+      partyLevel: 11,
+    }),
+    callXX: (context) => {
+      expect(context.party.highestLevel).toHaveBeenCalledOnce();
+    },
+  },
+  {
+    expected: true,
+    action: makeEnemyActionSwitch({
+      skillId: 6,
+      rating: 8,
+      switchId: MOCK_TRUELY_SWITCH_ID,
+    }),
+    callXX: (context) => {
+      expect(context.switches.value).toHaveBeenCalledOnce();
+      expect(context.switches.value).toHaveBeenCalledWith(
+        MOCK_TRUELY_SWITCH_ID,
+      );
+    },
+  },
+  {
+    expected: false,
+    action: makeEnemyActionSwitch({
+      skillId: 6,
+      rating: 8,
+      switchId: 331,
+    }),
+    callXX: (context) => {
+      expect(context.switches.value).toHaveBeenCalledOnce();
+      expect(context.switches.value).toHaveBeenCalledWith(331);
     },
   },
 ];
